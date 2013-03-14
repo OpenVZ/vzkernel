a6ae21b0e25db8ac00d2d4b6c3143b25c00383c3
Konstantin Khlebnikov <khlebnikov@openvz.org>
2013-03-14 18:20:48 +0400

    VE/PROC: fix bug in proc_readdir_de()
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>

--- fs/proc/generic.c
+++ fs/proc/generic.c
@@ -350,11 +350,12 @@ int proc_readdir_de(struct proc_dir_entry *de, struct file *filp, void *dirent,
 					spin_unlock(&proc_subdir_lock);
 					goto out;
 				}
-				if (!i)
-					break;
-				de = pde_subdir_next(de);
-				if (!in_container || (de->mode & S_ISVTX))
+				if (!in_container || (de->mode & S_ISVTX)) {
+					if (!i)
+						break;
 					i--;
+				}
+				de = pde_subdir_next(de);
 			}
 
 			do {
