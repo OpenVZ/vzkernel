990d2e3882b1192dddc09850c3f499d4bdf5b27c
Maxim V. Patlasov <mpatlasov@parallels.com>
2013-04-08 20:33:18 +0400

    ext4: Don't show the active balloon to user
    This is a port of
    e123b6d ext4: Don't show the active balloon to user
    
    Fix the readdir and lookup. The former one pretends the inode doesn't
    exists, the latter one denies an access to on. Reporting negative dentry
    in lookup is pointless, as in that case smth will have to be don the
    ext4_create callback :\
    
    [VvS RH79 rebase vz7.170.x]: minor context changes
    
    (cherry picked from vz7 commit c231c40a93927f3080067e5d880ef11841de278c)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -1595,6 +1595,11 @@ static struct dentry *ext4_lookup(struct inode *dir, struct dentry *dentry, unsi
 			iput(inode);
 			return ERR_PTR(-EPERM);
 		}
+		if (!IS_ERR(inode) &&
+		    inode == EXT4_SB(inode->i_sb)->s_balloon_ino) {
+			iput(inode);
+			return ERR_PTR(-EPERM);
+		}
 	}
 	return d_splice_alias(inode, dentry);
 }
@@ -3061,6 +3066,10 @@ static int ext4_unlink(struct inode *dir, struct dentry *dentry)
 
 	if (IS_DIRSYNC(dir))
 		ext4_handle_sync(handle);
+
+	retval = -EPERM;
+	if (inode == EXT4_SB(dir->i_sb)->s_balloon_ino)
+		goto end_unlink;
 
 	retval = ext4_delete_entry(handle, dir, de, bh);
 	if (retval)
