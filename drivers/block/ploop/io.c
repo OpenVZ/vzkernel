f879f6b225936f08abf9fdbeb7d1b8258bcd7766
Maxim V. Patlasov <mpatlasov@parallels.com>
2013-04-11 20:07:03 +0400

    ploop: add ploop to the tree
    Only io_direct (images on local ext4) is currently
    supported. Some limited testing was passed:
    
    This is a port of
    23dadf0 Add ploop to the tree. Only io_direct (images on local ext4) is currently supported. Som
    
    $ modprobe ploop; modprobe pfmt_ploop1; modprobe pfmt_raw; modprobe pio_direct
    $ mkdir /space/d7
    $ ploop init -t ext4 -s 1g /space/d7/d.hdd
    $ ploop mount -m /mnt_ploop /space/d7/DiskDescriptor.xml
    $ fsx-linux /mnt_ploop/junkfile
    <Ctrl-C>
    $ ploop umount /space/d7/DiskDescriptor.xml
    $ rmmod pio_direct; rmmod pfmt_ploop1; rmmod pfmt_raw; rmmod ploop
    
    +++
    ploop: drop the obsolete drivers/block/ploop/compat.c
    
    It contained functions missing in 2.6.18-x kernels.
    
    https://jira.sw.ru/browse/PSBM-51358
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ploop: drop obsolete code related to old kernels
    
    https://jira.sw.ru/browse/PSBM-51358
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ploop: variable dereferenced before check 'fbd' in ploop_fb_get_reloc_block
    
    found by smatch:
    drivers/block/ploop/freeblks.c:469 ploop_fb_get_reloc_block() warn:
     variable dereferenced before check 'fbd' (see line 467)
    
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    +++
    fixup ploop: Cleanup in ploop_make_request()
    
    BUG_ON(bio->bi_idx) is already checked in start of this function,
    so there is no a reason to check that twise.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

---
+++ drivers/block/ploop/io.c
@@ -0,0 +1,143 @@
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/file.h>
+
+#include <linux/ploop/ploop.h>
+#include <linux/ploop/ploop_if.h>
+
+/* Generic IO routines. */
+
+static LIST_HEAD(ploop_ios);
+static DEFINE_MUTEX(ploop_ios_mutex);
+
+int ploop_register_io(struct ploop_io_ops * ops)
+{
+	mutex_lock(&ploop_ios_mutex);
+	list_add(&ops->list, &ploop_ios);
+	mutex_unlock(&ploop_ios_mutex);
+	return 0;
+}
+EXPORT_SYMBOL(ploop_register_io);
+
+void ploop_unregister_io(struct ploop_io_ops * ops)
+{
+	mutex_lock(&ploop_ios_mutex);
+	list_del(&ops->list);
+	mutex_unlock(&ploop_ios_mutex);
+}
+EXPORT_SYMBOL(ploop_unregister_io);
+
+static struct ploop_io_ops * ploop_io_get(struct ploop_io *io, unsigned int id)
+{
+	struct ploop_io_ops * ops;
+
+	mutex_lock(&ploop_ios_mutex);
+	list_for_each_entry(ops, &ploop_ios, list) {
+		if ((id == ops->id || id == PLOOP_IO_AUTO) &&
+		    !ops->autodetect(io) && try_module_get(ops->owner)) {
+			mutex_unlock(&ploop_ios_mutex);
+			return ops;
+		}
+	}
+	mutex_unlock(&ploop_ios_mutex);
+	return NULL;
+}
+
+void ploop_io_put(struct ploop_io_ops * ops)
+{
+	module_put(ops->owner);
+}
+
+
+int
+ploop_io_init(struct ploop_delta * delta, int nchunks, struct ploop_ctl_chunk * pc)
+{
+	int err;
+
+	if (nchunks != 1)
+		return -EINVAL;
+
+	if (pc[0].pctl_offset ||
+	    pc[0].pctl_start ||
+	    pc[0].pctl_len)
+		return -EINVAL;
+
+	memset(&delta->io, 0, sizeof(struct ploop_io));
+	delta->io.plo = delta->plo;
+	delta->io.n_chunks = 1;
+
+	err = -EBADF;
+	delta->io.files.file = fget(pc[0].pctl_fd);
+	if (!delta->io.files.file)
+		goto out_err;
+
+	err = -EOPNOTSUPP;
+	delta->io.ops = ploop_io_get(&delta->io, pc[0].pctl_type);
+	if (delta->io.ops == NULL)
+		goto out_err;
+
+	err = delta->io.ops->init(&delta->io);
+	if (err)
+		goto out_err;
+
+	return 0;
+
+out_err:
+	if (delta->io.files.file)
+		fput(delta->io.files.file);
+	delta->io.files.file = NULL;
+	if (delta->io.ops)
+		ploop_io_put(delta->io.ops);
+	delta->io.ops = NULL;
+	return err;
+}
+EXPORT_SYMBOL(ploop_io_init);
+
+int ploop_io_open(struct ploop_io * io)
+{
+	struct file * file;
+	struct ploop_delta * delta = container_of(io, struct ploop_delta, io);
+
+	if ((file = io->files.file) == NULL)
+		return -EBADF;
+
+	if ((delta->flags & PLOOP_FMT_RDONLY) &&
+	    (io->ops->f_mode(io) & FMODE_WRITE))
+		return -EINVAL;
+
+	if (!(delta->flags & PLOOP_FMT_RDONLY) &&
+	    !(io->ops->f_mode(io) & FMODE_WRITE))
+		return -EINVAL;
+
+	return io->ops->open(io);
+}
+EXPORT_SYMBOL(ploop_io_open);
+
+void ploop_io_destroy(struct ploop_io * io)
+{
+	if (io->ops) {
+		io->ops->destroy(io);
+		ploop_io_put(io->ops);
+		io->ops = NULL;
+	}
+}
+EXPORT_SYMBOL(ploop_io_destroy);
+
+void ploop_io_report_fn(struct file * file, char * msg)
+{
+	char *fn = "?";
+	char *path;
+
+	path = (char *)__get_free_page(GFP_KERNEL);
+	if (path) {
+		fn = d_path(&file->f_path, path, PAGE_SIZE);
+		if (IS_ERR(fn))
+			fn = "?";
+	}
+
+	printk("%s: %s\n", msg, fn);
+
+	if (path)
+		free_page((unsigned long)path);
+}
+EXPORT_SYMBOL(ploop_io_report_fn);
