404545330a12cb8a10a6b7b2711a5ff1646da3f1
Maxim V. Patlasov <mpatlasov@parallels.com>
2013-04-11 20:07:03 +0400

    ploop: add ploop to the tree
    Only io_direct (images on local ext4) is currently
    supported. Some limited testing was passed:
    
    This is a port of
    23dadf0 Add ploop to the tree. Only io_direct (images on local ext4) is currently supported. Som
    
    $ modprobe ploop; modprobe pfmt_ploop1; modprobe pfmt_raw; modprobe pio_direct
    $ mkdir /space/d7
    $ ploop init -t ext4 -s 1g /space/d7/d.hdd
    $ ploop mount -m /mnt_ploop /space/d7/DiskDescriptor.xml
    $ fsx-linux /mnt_ploop/junkfile
    <Ctrl-C>
    $ ploop umount /space/d7/DiskDescriptor.xml
    $ rmmod pio_direct; rmmod pfmt_ploop1; rmmod pfmt_raw; rmmod ploop
    
    +++
    ploop: drop the obsolete drivers/block/ploop/compat.c
    
    It contained functions missing in 2.6.18-x kernels.
    
    https://jira.sw.ru/browse/PSBM-51358
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ploop: drop obsolete code related to old kernels
    
    https://jira.sw.ru/browse/PSBM-51358
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

---
+++ drivers/block/ploop/io_direct_events.h
@@ -0,0 +1,42 @@
+#if !defined(_TRACE_IO_DIRECT_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_IO_DIRECT_H
+
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM ploop
+
+#include <linux/sched.h>
+#include <linux/tracepoint.h>
+
+#include <linux/ploop/ploop.h>
+#include "io_direct_map.h"
+
+TRACE_EVENT(add_extent_mapping,
+	TP_PROTO(struct extent_map *em),
+
+	TP_ARGS(em),
+
+	TP_STRUCT__entry(
+		__field(sector_t,  start)
+		__field(sector_t,  end)
+		__field(sector_t,  bstart)
+	),
+
+	TP_fast_assign(
+		__entry->start	= em->start;
+		__entry->end	= em->end;
+		__entry->bstart	= em->block_start;
+	),
+
+	TP_printk("start=0x%lx end=0x%lx block_start=0x%lx",
+			__entry->start, __entry->end, __entry->bstart)
+);
+
+#endif /* _TRACE_PLOOP_H */
+
+#undef TRACE_INCLUDE_PATH
+#undef TRACE_INCLUDE_FILE
+#define TRACE_INCLUDE_PATH .
+#define TRACE_INCLUDE_FILE io_direct_events
+
+/* This part must be outside protection */
+#include <trace/define_trace.h>
