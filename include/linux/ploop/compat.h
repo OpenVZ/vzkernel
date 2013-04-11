2b3bc4eb105857bd2d9a52de0569fff1377d25c7
Maxim V. Patlasov <mpatlasov@parallels.com>
2013-04-11 20:07:03 +0400

    ploop: add ploop to the tree
    Only io_direct (images on local ext4) is currently
    supported. Some limited testing was passed:
    
    This is a port of
    23dadf0 Add ploop to the tree. Only io_direct (images on local ext4) is currently supported. Som
    
    $ modprobe ploop; modprobe pfmt_ploop1; modprobe pfmt_raw; modprobe pio_direct
    $ mkdir /space/d7
    $ ploop init -t ext4 -s 1g /space/d7/d.hdd
    $ ploop mount -m /mnt_ploop /space/d7/DiskDescriptor.xml
    $ fsx-linux /mnt_ploop/junkfile
    <Ctrl-C>
    $ ploop umount /space/d7/DiskDescriptor.xml
    $ rmmod pio_direct; rmmod pfmt_ploop1; rmmod pfmt_raw; rmmod ploop
    
    +++
    ploop: drop the obsolete drivers/block/ploop/compat.c
    
    It contained functions missing in 2.6.18-x kernels.
    
    https://jira.sw.ru/browse/PSBM-51358
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ploop: drop obsolete code related to old kernels
    
    https://jira.sw.ru/browse/PSBM-51358
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ploop: variable dereferenced before check 'fbd' in ploop_fb_get_reloc_block
    
    found by smatch:
    drivers/block/ploop/freeblks.c:469 ploop_fb_get_reloc_block() warn:
     variable dereferenced before check 'fbd' (see line 467)
    
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    +++
    fixup ploop: Cleanup in ploop_make_request()
    
    BUG_ON(bio->bi_idx) is already checked in start of this function,
    so there is no a reason to check that twise.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

---
+++ include/linux/ploop/compat.h
@@ -0,0 +1,35 @@
+/*
+ *  include/linux/ploop/compat.h
+ *
+ *  This file contained macros to provide compatibility layer for 2.6.18,
+ *  where bio layer was different.
+ *
+ *  Copyright (c) 2010-2015 Parallels IP Holdings GmbH
+ *
+ */
+
+#ifndef _LINUX_PLOOP_COMPAT_H_
+#define _LINUX_PLOOP_COMPAT_H_
+
+#include <linux/version.h>
+
+#define DEFINE_BIO_CB(func) \
+static void func(struct bio *bio, int err) {
+
+#define END_BIO_CB(func)  }
+
+#define BIO_ENDIO(_bio, _err)	bio_endio(_bio, _err)
+
+#define F_DENTRY(file)	(file)->f_path.dentry
+#define F_MNT(file)	(file)->f_path.mnt
+
+#define KOBJECT_INIT(kobj, ktype) kobject_init(kobj, ktype)
+#define KOBJECT_ADD(kobj, parent, fmt, arg...) kobject_add(kobj, parent, fmt, arg)
+
+#endif
+
+#if LINUX_VERSION_CODE > KERNEL_VERSION(2,6,32)
+#define FOP_FSYNC(file, datasync) fsync(file, 0, LLONG_MAX, datasync)
+#else
+#define FOP_FSYNC(file, datasync) fsync(file, F_DENTRY(file), datasync)
+#endif
