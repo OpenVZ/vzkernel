565576edc95e3e88533ecb70c168f0f7da89ae16
Konstantin Khlebnikov <khlebnikov@openvz.org>
2013-06-13 21:28:21 +0400

    VE/NET: switch vz_security_*_check to netns
    Use net->owner_ve instead of get_exec_env
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -39,6 +39,7 @@
 #include <linux/if_vlan.h>
 #include <linux/pci.h>
 #include <linux/etherdevice.h>
+#include <linux/ve.h>
 
 #include <asm/uaccess.h>
 
@@ -2416,6 +2417,7 @@ static int rtnl_dump_all(struct sk_buff *skb, struct netlink_callback *cb)
 {
 	int idx;
 	int s_idx = cb->family;
+	struct net *net = sock_net(skb->sk);
 
 	if (s_idx == 0)
 		s_idx = 1;
@@ -2426,7 +2428,7 @@ static int rtnl_dump_all(struct sk_buff *skb, struct netlink_callback *cb)
 		if (rtnl_msg_handlers[idx] == NULL ||
 		    rtnl_msg_handlers[idx][type].dumpit == NULL)
 			continue;
-		if (vz_security_family_check(idx))
+		if (vz_security_family_check(net, idx))
 			continue;
 		if (idx > s_idx) {
 			memset(&cb->args[0], 0, sizeof(cb->args));
@@ -3231,7 +3233,7 @@ static int rtnetlink_rcv_msg(struct sk_buff *skb, struct nlmsghdr *nlh)
 		return 0;
 
 	family = ((struct rtgenmsg *)nlmsg_data(nlh))->rtgen_family;
-	if (vz_security_family_check(family))
+	if (vz_security_family_check(net, family))
 		return -EAFNOSUPPORT;
 
 	sz_idx = type>>2;
