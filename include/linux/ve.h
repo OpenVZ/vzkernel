93ac9c8d6b0d298842e923b5ef3b93dfa779756b
Konstantin Khlebnikov <khlebnikov@openvz.org>
2013-06-17 00:11:04 +0400

    VE/BINFTM: virtualization
    * keep deference from binfmt_misc sb to ve
    * store pointer to binfmt_misc data in ve->binfmt_misc
    
    Here bm_put_super() can race with load_misc_binary() caller, which is working
    with get_exec_env()->binfmt_misc.
    Will be fixed separately.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>

--- include/linux/ve.h
+++ include/linux/ve.h
@@ -153,6 +153,10 @@ struct ve_struct {
 
 	struct super_block	*devpts_sb;
 
+#if IS_ENABLED(CONFIG_BINFMT_MISC)
+	struct binfmt_misc	*binfmt_misc;
+#endif
+
 #define	MAX_NR_VTTY		12
 	struct tty_struct	*vtty[MAX_NR_VTTY];
 
