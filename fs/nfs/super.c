aeeac135020f8db62111ba254dba252d8182b6d0
Stanislav Kinsbursky <skinsbursky@parallels.com>
2013-06-17 00:20:42 +0400

    VE/NFS: check VE_FEATURE_NFS is set on mount
    This feature controls user ability to mount NFS in a container.
    
    Signed-off-by: Stanislav Kinsbursky <skinsbursky@parallels.com>

--- fs/nfs/super.c
+++ fs/nfs/super.c
@@ -56,6 +56,8 @@
 #include <linux/nsproxy.h>
 #include <linux/rcupdate.h>
 
+#include <linux/vzcalluser.h>
+
 #include <asm/uaccess.h>
 
 #include "nfs4_fs.h"
@@ -2621,6 +2623,9 @@ struct dentry *nfs_fs_mount(struct file_system_type *fs_type,
 	struct dentry *mntroot = ERR_PTR(-ENOMEM);
 	struct nfs_subversion *nfs_mod;
 	int error;
+
+	if (!(get_exec_env()->features & VE_FEATURE_NFS))
+		return ERR_PTR(-ENODEV);
 
 	mount_info.parsed = nfs_alloc_parsed_mount_data();
 	mount_info.mntfh = nfs_alloc_fhandle();
