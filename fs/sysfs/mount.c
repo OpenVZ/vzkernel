e34e2abb027121ea164e25e7ed94f59bd69d423a
Konstantin Khlebnikov <khlebnikov@openvz.org>
2013-06-17 02:59:19 +0400

    ve/fs: mark virtual filesystems as virtualized
    Allow to mount in container tmpfs, devpts, proc and sysfs.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>

--- fs/sysfs/mount.c
+++ fs/sysfs/mount.c
@@ -58,7 +58,7 @@ static struct file_system_type sysfs_fs_type = {
 	.name		= "sysfs",
 	.mount		= sysfs_mount,
 	.kill_sb	= sysfs_kill_sb,
-	.fs_flags	= FS_USERNS_MOUNT,
+	.fs_flags	= FS_USERNS_MOUNT | FS_VIRTUALIZED,
 };
 
 int __init sysfs_init(void)
