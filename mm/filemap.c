27060dd8661146438ae46e4b6e5bf66c098a13dc
Konstantin Khlebnikov <khlebnikov@openvz.org>
2013-09-09 18:28:50 +0400

    pfcache: add generic mapping peers
    ==========================================================
    This adds mapping peers and corresponding rmap management.
    
    https://jira.sw.ru/browse/PSBM-20098
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    
    +++
    ms/mm: drop support of non-linear mapping from unmap/zap codepath
    
    We have remap_file_pages(2) emulation in -mm tree for few release cycles
    and we plan to have it mainline in v3.20. This patchset removes rest of
    VM_NONLINEAR infrastructure.
    
    Patches 1-8 take care about generic code. They are pretty
    straight-forward and can be applied without other of patches.
    
    Rest patches removes pte_file()-related stuff from architecture-specific
    code. It usually frees up one bit in non-present pte. I've tried to reuse
    that bit for swap offset, where I was able to figure out how to do that.
    
    For obvious reason I cannot test all that arch-specific code and would
    like to see acks from maintainers.
    
    In total, remap_file_pages(2) required about 1.4K lines of not-so-trivial
    kernel code. That's too much for functionality nobody uses.
    
    Tested-by: Felipe Balbi <balbi@ti.com>
    
    This patch (of 38):
    
    We don't create non-linear mappings anymore. Let's drop code which
    handles them on unmap/zap.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    https://jira.sw.ru/browse/PSBM-52992
    
    (cherry picked from commit 8a5f14a23177061ec11daeaa3d09d0765d785c47)
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Reviewed-by: Cyrill Gorcunov <gorcunov@openvz.org>
    
    +++
    ms/rmap: drop support of non-linear mappings
    
    We don't create non-linear mappings anymore.  Let's drop code which
    handles them in rmap.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    https://jira.sw.ru/browse/PSBM-52992
    
    (cherry picked from commit 27ba0644ea9dfe6e7693abc85837b60e40583b96)
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Reviewed-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Rebase: ktkhai@:
    Partially merged "pfcache: fix path refcounting hazzard"
    Merged "pfcache: pick page from peer mapping after readahead"
    
    +++
    Rebase to RHEL7.6 beta 3.10.0-933.el7 kernel:
    dropped try_to_unmap_mapping() and try_to_unmap_file() functions,
    looks like not needed now.
    
    https://jira.sw.ru/browse/PSBM-89047
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    fs/splice: fix NULL-ptr deref in __generic_splice_read()
    
    splice_grow_spd() may reinitialize spd.pages pointer, so the spd.pages
    is not necessary points to 'pages' array. So the loop:
    
    	while (spd.nr_pages < nr_pages && mapping->i_peer_file) {
    		page = pick_peer_page(mapping, index, &in->f_ra,
    				      req_pages - spd.nr_pages);
    		if (!page)
    			break;
    		pages[spd.nr_pages++] = page;
    		index++;
    	}
    
    might increase spd.nr_pages but do not initialize spd.pages
    which might lead to crash later.
    
    Fix this by using spd.pages instead.
    
    https://jira.sw.ru/browse/PSBM-99561
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    khorenko@: this is a part of ms commit 35f3d14dbbc5 ("pipe: add support for
    shrinking and growing pipes")
    mFixes: 9d0a46d0bd06 ("pfcache: add generic mapping peers")

--- mm/filemap.c
+++ mm/filemap.c
@@ -1650,6 +1650,12 @@ find_page:
 		}
 
 		page = find_get_page(mapping, index);
+		if (!page && mapping->i_peer_file) {
+			page = pick_peer_page(mapping, index, ra,
+					      last_index - index);
+			if (page)
+				goto page_ok;
+		}
 		if (!page) {
 			page_cache_sync_readahead(mapping,
 					ra, filp,
@@ -2197,6 +2203,13 @@ int filemap_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
 	 * Do we have something in the page cache already?
 	 */
 	page = find_get_page(mapping, offset);
+	if (!page && mapping->i_peer_file) {
+		page = pick_peer_page(mapping, offset, ra, ra->ra_pages);
+		if (page) {
+			vmf->page = page;
+			return 0; /* unlocked page */
+		}
+	}
 	if (likely(page) && !(vmf->flags & FAULT_FLAG_TRIED)) {
 		/*
 		 * We found the page, so try async readahead before
