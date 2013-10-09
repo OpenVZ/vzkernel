11b765a4ecc8a30a2166dd4aa5b34f91851784cb
Vladimir Davydov <vdavydov@parallels.com>
2013-10-09 12:09:48 +0400

    bc/proc: fix compilation
    After the patchset applied the kernel compiles (if CONFIG_FUSE_FS is off),
    boots and allows starting containers. Fixing broken fuse bits will be done
    a bit later.
    
    Vladimir Davydov (13):
      Trivial compilation fixes
      sched: move declaration of cgroup related functions to global header
      khleper: fix compilation
      bc/proc: fix compilation
      cpuset: remove unused stuff and fix compilation
      ve/cgroup: fix compilation
      cgroup: cgroup->top_cgroup => &cgroup->root->top_cgroup
      cgroup/cpuset/fairsched: get rid of cgroup_lock
      Remove include/net/netlink_sock.h
      fairsched: use proc_mkdir for proc dir creation
      ve: use proc_mkdir for proc dir creation
      proc: mark only net dirs as virtualized by default
      proc-net: mark all files in proc-net directories as virtualized
    
    This patch description:
    
    move proc_dir_entry definition back to a global header, because bc/proc needs
    it, and substitute obsolete create_proc_entry() with proc_mkdir()
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- fs/proc/internal.h
+++ fs/proc/internal.h
@@ -29,27 +29,6 @@ struct mempolicy;
  * /proc file has a parent, but "subdir" is NULL for all
  * non-directory entries).
  */
-struct proc_dir_entry {
-	unsigned int low_ino;
-	umode_t mode;
-	nlink_t nlink;
-	kuid_t uid;
-	kgid_t gid;
-	loff_t size;
-	const struct inode_operations *proc_iops;
-	const struct file_operations *proc_fops;
-	struct proc_dir_entry *next, *parent, *subdir;
-	void *data;
-	atomic_t count;		/* use count */
-	atomic_t in_use;	/* number of callers into module in progress; */
-			/* negative -> it's going away RSN */
-	struct completion *pde_unload_completion;
-	struct list_head pde_openers;	/* who did ->open, but not ->release */
-	spinlock_t pde_unload_lock; /* proc_fops checks and pde_users bumps */
-	u8 namelen;
-	char name[];
-};
-
 union proc_op {
 	int (*proc_get_link)(struct dentry *, struct path *);
 	int (*proc_read)(struct task_struct *task, char *page);
