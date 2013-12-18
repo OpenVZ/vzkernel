28806609ac86d29d964a3c7cda5acf0e4bf8c243
Stanislav Kinsbursky <skinsbursky@parallels.com>
2013-12-18 13:11:04 +0400

    VE/sysrq-trigger: make it VE-namespace aware
    Logic inherited from rhel6: instead of doing something, echo passed command in
    container.
    
    https://jira.sw.ru/browse/PSBM-20664
    
    Signed-off-by: Stanislav Kinsbursky <skinsbursky@parallels.com>
    
    ==========================
    Original comment:
    
    It's backport of patch:
    diff-ve-fake-sysrq-trigger-in-ct-20091002
    
    Patch from Konstantin Khorenko <khorenko@openvz.org>:
    provides /proc/sysrq-trigger file inside a Container -
    Oracle 11g Release 1 RAC needs this.
    
    Writing to the file inside a CT leads to nothing, first 10 writes are logged.
    
    https://bugzilla.sw.ru/show_bug.cgi?id=448149
    ==========================

--- drivers/tty/sysrq.c
+++ drivers/tty/sysrq.c
@@ -45,6 +45,7 @@
 #include <linux/moduleparam.h>
 #include <linux/jiffies.h>
 #include <linux/rcupdate.h>
+#include <linux/ve.h>
 
 #include <asm/ptrace.h>
 #include <asm/irq_regs.h>
@@ -1039,10 +1040,16 @@ static ssize_t write_sysrq_trigger(struct file *file, const char __user *buf,
 {
 	if (count) {
 		char c;
+		struct ve_struct *cur = get_exec_env();
+		static int pnum = 10;
 
 		if (get_user(c, buf))
 			return -EFAULT;
-		__handle_sysrq(c, false);
+		if (ve_is_super(cur))
+			__handle_sysrq(c, false);
+		else if (pnum--)
+			printk("SysRq: CT#%s sent '%c' magic key.\n",
+				cur->ve_name, c);
 	}
 
 	return count;
@@ -1055,7 +1062,7 @@ static const struct file_operations proc_sysrq_trigger_operations = {
 
 static void sysrq_init_procfs(void)
 {
-	if (!proc_create("sysrq-trigger", S_IWUSR, NULL,
+	if (!proc_create("sysrq-trigger", S_ISVTX | S_IWUSR, NULL,
 			 &proc_sysrq_trigger_operations))
 		pr_err("Failed to register proc interface\n");
 }
