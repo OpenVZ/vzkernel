9cdddab3210d25970278c24c8141cba1e7255b6f
Stanislav Kinsbursky <skinsbursky@parallels.com>
2013-12-19 15:16:29 +0400

    ve/vzstat: move code to source file and cleanup headers
    VZ statistics requires "linux/sched.h" inclusion because of per-cpu accounting.
    And "linux/vzstat.h" must be included to "linux/ve.h", because ve_struct
    contains of object typeof "kstat_lat_pcpu_struct".
    This leads to redundant dependency on "linux/sched.h" for each source file,
    which includes "linux/ve.h".
    While "linux/sched.h" contains on many header files inclusions, wchih leads to
    excessive rebuild of sources files with "linux/ve.h" inclusion.
    This was annoying in rhel6 and remains here as well.
    Solution is to move this "linux/sched.h"-dependent code to separated source
    file and removing all this excessive inclusions of "linux/sched.h" from almost
    all files with "linux/ve.h" inclusion.
    
    Signed-off-by: Stanislav Kinsbursky <skinsbursky@parallels.com>

--- include/linux/vzstat.h
+++ include/linux/vzstat.h
@@ -123,104 +123,18 @@ KSTAT_PERF_ADD(struct kstat_perf_pcpu_struct *ptr, u64 real_time, u64 cpu_time)
 	sleep_time = current->se.statistics.sum_sleep_runtime - sleep_time; \
 	KSTAT_PERF_ADD(&kstat_glob.name, start, start - sleep_time);
 
+extern void KSTAT_LAT_ADD(struct kstat_lat_struct *p, u64 dur);
+extern void KSTAT_LAT_PCPU_ADD(struct kstat_lat_pcpu_struct *p, int cpu, u64 dur);
+extern void KSTAT_LAT_UPDATE(struct kstat_lat_struct *p);
+extern void KSTAT_LAT_PCPU_UPDATE(struct kstat_lat_pcpu_struct *p);
+
 #else
 #define KSTAT_PERF_ENTER(name)
 #define KSTAT_PERF_LEAVE(name)
+#define KSTAT_LAT_ADD(p, dur)
+#define KSTAT_LAT_PCPU_ADD(p, cpu, dur)
+#define KSTAT_LAT_UPDATE(p)
+#define KSTAT_LAT_PCPU_UPDATE(p)
 #endif
-
-/*
- * Add another statistics reading.
- * Serialization is the caller's due.
- */
-static inline void KSTAT_LAT_ADD(struct kstat_lat_struct *p,
-		u64 dur)
-{
-	p->cur.count++;
-	if (p->cur.maxlat < dur)
-		p->cur.maxlat = dur;
-	p->cur.totlat += dur;
-}
-
-/*
- * Must be called with disabled interrupts to remove any possible
- * locks and seqcounts under write-lock and avoid this 3-way deadlock:
- *
- * timer interrupt:
- *	write_seqlock(&xtime_lock);
- *	 spin_lock_irqsave(&kstat_glb_lock);
- *
- * update_schedule_latency():
- *	spin_lock_irq(&kstat_glb_lock);
- *	 read_seqcount_begin(&cur->lock)
- *
- * some-interrupt during KSTAT_LAT_PCPU_ADD()
- *   KSTAT_LAT_PCPU_ADD()
- *    write_seqcount_begin(&cur->lock);
- *     <interrupt>
- *      ktime_get()
- *       read_seqcount_begin(&xtime_lock);
- */
-static inline void KSTAT_LAT_PCPU_ADD(struct kstat_lat_pcpu_struct *p, int cpu,
-		u64 dur)
-{
-	struct kstat_lat_pcpu_snap_struct *cur;
-
-	cur = per_cpu_ptr(p->cur, cpu);
-	write_seqcount_begin(&cur->lock);
-	cur->count++;
-	if (cur->maxlat < dur)
-		cur->maxlat = dur;
-	cur->totlat += dur;
-	write_seqcount_end(&cur->lock);
-}
-
-/*
- * Move current statistics to last, clear last.
- * Serialization is the caller's due.
- */
-static inline void KSTAT_LAT_UPDATE(struct kstat_lat_struct *p)
-{
-	u64 m;
-	memcpy(&p->last, &p->cur, sizeof(p->last));
-	p->cur.maxlat = 0;
-	m = p->last.maxlat;
-	CALC_LOAD(p->avg[0], EXP_1, m)
-	CALC_LOAD(p->avg[1], EXP_5, m)
-	CALC_LOAD(p->avg[2], EXP_15, m)
-}
-
-static inline void KSTAT_LAT_PCPU_UPDATE(struct kstat_lat_pcpu_struct *p)
-{
-	unsigned i, cpu;
-	struct kstat_lat_pcpu_snap_struct snap, *cur;
-	u64 m;
-
-	memset(&p->last, 0, sizeof(p->last));
-	for_each_online_cpu(cpu) {
-		cur = per_cpu_ptr(p->cur, cpu);
-		do {
-			i = read_seqcount_begin(&cur->lock);
-			memcpy(&snap, cur, sizeof(snap));
-		} while (read_seqcount_retry(&cur->lock, i));
-		/* 
-		 * read above and this update of maxlat is not atomic,
-		 * but this is OK, since it happens rarely and losing
-		 * a couple of peaks is not essential. xemul
-		 */
-		cur->maxlat = 0;
-
-		p->last.count += snap.count;
-		p->last.totlat += snap.totlat;
-		if (p->last.maxlat < snap.maxlat)
-			p->last.maxlat = snap.maxlat;
-	}
-
-	m = (p->last.maxlat > p->max_snap ? p->last.maxlat : p->max_snap);
-	CALC_LOAD(p->avg[0], EXP_1, m);
-	CALC_LOAD(p->avg[1], EXP_5, m);
-	CALC_LOAD(p->avg[2], EXP_15, m);
-	/* reset max_snap to calculate it correctly next time */
-	p->max_snap = 0;
-}
 
 #endif /* __VZSTAT_H__ */
