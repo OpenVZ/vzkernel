9d613b9d3f58c7b662043ac1d3a3076fe2406eb8
Stanislav Kinsbursky <skinsbursky@parallels.com>
2013-12-20 15:43:28 +0400

    devtmpfs: containerize it with new obj ns operation
    This patch introduces new kobject namespace operation:
    
    const struct path *(*devtmpfs)(const struct kobject *);
    
    which returns devtmpfs mount point root, which is used by devtmpfs daemon to
    swap to and allows to create and delete device node in correct namespace.
    
    Note: devtmpfs mount is created per VE struct.
    
    Signed-off-by: Stanislav Kinsbursky <skinsbursky@parallels.com>
    Acked-by: Pavel Emelyanov <xemul@parallels.com>

--- include/linux/kobject_ns.h
+++ include/linux/kobject_ns.h
@@ -19,6 +19,7 @@
 
 struct sock;
 struct kobject;
+struct path;
 
 /*
  * Namespace types which are used to tag kobjects and sysfs entries.
@@ -44,6 +45,7 @@ struct kobj_ns_type_operations {
 	const void *(*netlink_ns)(struct sock *sk);
 	const void *(*initial_ns)(void);
 	void (*drop_ns)(void *);
+	const struct path *(*devtmpfs)(const struct kobject *);
 };
 
 int kobj_ns_type_register(const struct kobj_ns_type_operations *ops);
