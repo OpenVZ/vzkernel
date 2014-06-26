947f3e89a003d8f1ee58c438e3a8eb8125c38efd
Maxim V. Patlasov <mpatlasov@parallels.com>
2014-06-26 14:19:25 +0400

    fs: kernel direct aio
    Port of 95-diff-kernel-direct-aio-combined from
    https://jira.sw.ru/browse/PSBM-18169
    
    Signed-off-by: Maxim Patlasov <MPatlasov@parallels.com>

--- fs/cifs/file.c
+++ fs/cifs/file.c
@@ -2777,7 +2777,8 @@ cifs_readdata_to_iov(struct cifs_readdata *rdata, const struct iovec *iov,
 		/* go while there's data to be copied and no errors */
 		if (copy && !rc) {
 			pdata = kmap(page);
-			rc = memcpy_toiovecend(ii.iov, pdata, ii.iov_offset,
+			rc = memcpy_toiovecend((struct iovec *)ii.data,
+					       pdata, ii.iov_offset,
 						(int)copy);
 			kunmap(page);
 			if (!rc) {
