35639517375be9055390b1d25253285af3ef187f
Maxim Patlasov <MPatlasov@parallels.com>
2014-08-04 19:11:44 +0400

    fuse: direct_enable mount option
    Allow O_DIRECT operations only if fuse was mounted with "direct_enable" option.
    
    https://jira.sw.ru/browse/PSBM-18167
    
    Signed-off-by: Maxim Patlasov <MPatlasov@parallels.com>
    
    (cherry picked from commit 44ffdcc75a89c3a18bca2d6af77f2dddc38a5929)
    Signed-off-by: Liu Kui <Kui.Liu@acronis.com>
    Feature: vStorage

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -500,6 +500,7 @@ struct fuse_fs_context {
 	bool default_permissions:1;
 	bool allow_other:1;
 	bool writeback_cache:1;
+	bool direct_enable:1;
 	bool destroy:1;
 	bool no_control:1;
 	bool no_force_umount:1;
@@ -764,6 +765,9 @@ struct fuse_conn {
 
 	/* Propagate syncfs() to server */
 	unsigned int sync_fs:1;
+
+	/** Does the direct access enable? */
+	unsigned direct_enable:1;
 
 	/** The number of requests waiting for completion */
 	atomic_t num_waiting;
