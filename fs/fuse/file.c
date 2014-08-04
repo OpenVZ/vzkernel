e8fc11a338a5cee30b41424c7f19a79179f7415a
Maxim Patlasov <MPatlasov@parallels.com>
2014-08-04 19:11:47 +0400

    fuse: some fairness in handling writeback
    Port diff-fuse-some-fairness-in-handling-writeback from 2.6.32-*:
    
    fuse_mkdir() and other fuse file/dir ops may block in fuse_get_req() on
    fc->blocked. The value of fc->blocked completely depends on fc->num_background.
    
    The patch limits the number of fuse-requests generated in writeback. This is
    neccessary because otherwise writeback would keep fc->num_background high
    enough to have fuse_mkdir (or other opearation) blocked forever.
    
    Signed-off-by: Maxim V. Patlasov <MPatlasov@parallels.com>
    
    (cherry picked from commit 7c227301293f070c38b5af941bd8da267058c17e)
    Signed-off-by: Liu Kui <Kui.Liu@acronis.com>
    Feature: vStorage

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -2198,6 +2198,7 @@ static int fuse_writepages_fill(struct page *page,
 	struct fuse_conn *fc = get_fuse_conn(inode);
 	struct page *tmp_page;
 	int err;
+	int check_for_blocked = 0;
 
 	if (!data->ff) {
 		err = -EIO;
@@ -2248,6 +2249,8 @@ static int fuse_writepages_fill(struct page *page,
 		ap->args.end = fuse_writepage_end;
 		ap->num_pages = 0;
 		wpa->inode = inode;
+
+		check_for_blocked = 1;
 	}
 	set_page_writeback(page);
 
@@ -2277,6 +2280,9 @@ static int fuse_writepages_fill(struct page *page,
 out_unlock:
 	unlock_page(page);
 
+	if (wbc->sync_mode != WB_SYNC_NONE && check_for_blocked)
+		wait_event(fc->blocked_waitq, !fc->blocked);
+
 	return err;
 }
 
@@ -2291,6 +2297,8 @@ static int fuse_writepages(struct address_space *mapping,
 	err = -EIO;
 	if (fuse_is_bad(inode))
 		goto out;
+
+	wait_event(fc->blocked_waitq, !fc->blocked);
 
 	data.inode = inode;
 	data.wpa = NULL;
