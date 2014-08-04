8948428b07a5d543b24d10c384f206d3345867d0
Maxim Patlasov <MPatlasov@parallels.com>
2014-08-04 19:11:46 +0400

    fuse: Show open files in fusectl
    Port diff-fuse-show-open-files-in-fusectl from 2.6.32-*:
    
    Add the fusectl/open_files file which shows files, that are currently
    in use by fuse kernel module.
    
    The most interesting fields are FH, flags and path (to the fuse root),
    these will be used in fuse reconnect scenario.
    
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>
    
    [VvS RHEL77b rebase]

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -72,13 +72,23 @@ struct fuse_file *fuse_file_alloc(struct fuse_conn *fc)
 
 	spin_lock(&fc->lock);
 	ff->kh = ++fc->khctr;
+	ff->ff_dentry = NULL;
+	list_add_tail(&ff->fl, &fc->conn_files);
 	spin_unlock(&fc->lock);
 
 	return ff;
 }
 
+static void fuse_file_list_del(struct fuse_file *ff)
+{
+	spin_lock(&ff->fc->lock);
+	list_del_init(&ff->fl);
+	spin_unlock(&ff->fc->lock);
+}
+
 void fuse_file_free(struct fuse_file *ff)
 {
+	fuse_file_list_del(ff);
 	fuse_request_free(ff->reserved_req);
 	kfree(ff);
 }
@@ -138,9 +148,11 @@ static void fuse_file_put(struct fuse_file *ff, bool sync, bool isdir)
 		} else if (sync) {
 			__clear_bit(FR_BACKGROUND, &req->flags);
 			fuse_request_send(ff->fc, req);
+			fuse_file_list_del(ff);
 			path_put(&req->misc.release.path);
 			fuse_put_request(ff->fc, req);
 		} else {
+			fuse_file_list_del(ff);
 			req->end = fuse_release_end;
 			__set_bit(FR_BACKGROUND, &req->flags);
 			fuse_request_send_background(ff->fc, req);
@@ -207,6 +219,8 @@ void fuse_finish_open(struct inode *inode, struct file *file)
 {
 	struct fuse_file *ff = file->private_data;
 	struct fuse_conn *fc = get_fuse_conn(inode);
+
+	ff->ff_dentry = file->f_dentry;
 
 	if (ff->open_flags & FOPEN_DIRECT_IO)
 		file->f_op = &fuse_direct_io_file_operations;
@@ -345,6 +359,7 @@ static int fuse_release(struct inode *inode, struct file *file)
 void fuse_sync_release(struct fuse_inode *fi, struct fuse_file *ff, int flags)
 {
 	WARN_ON(atomic_read(&ff->count) > 1);
+	fuse_file_list_del(ff);
 	fuse_prepare_release(fi, ff, flags, FUSE_RELEASE);
 	__set_bit(FR_FORCE, &ff->reserved_req->flags);
 	__clear_bit(FR_BACKGROUND, &ff->reserved_req->flags);
