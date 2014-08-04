94b258c6006d214d6f4320913caa165facc4d7b0
Maxim Patlasov <MPatlasov@parallels.com>
2014-08-04 19:11:50 +0400

    fuse: i_size to be updated on first open
    Port diff-fuse-i_size-to-be-updated-on-first-open-2 from 2.6.32-*:
    
    fuse can't trust i_size which was set during fuse_lookup:
    
    1. In case of special pcs fuse files, it's zero till first open
    2. In case of regular files, the file can be changed (truncated) externally
    before we get lease in fuse_open.
    
    The patch fixes the problem only if WBCACHE enabled because otherwise, if
    it's disabled, in-kernel fuse must always use attr.size from userspace.
    
    https://jira.sw.ru/browse/PSBM-13636
    
    Signed-off-by: Maxim V. Patlasov <MPatlasov@parallels.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -935,7 +935,7 @@ static void fuse_fillattr(struct inode *inode, struct fuse_attr *attr,
 }
 
 static int fuse_do_getattr(struct inode *inode, struct kstat *stat,
-			   struct file *file)
+			   struct file *file, int get_size_form_attr)
 {
 	int err;
 	struct fuse_getattr_in inarg;
@@ -973,11 +973,26 @@ static int fuse_do_getattr(struct inode *inode, struct kstat *stat,
 			fuse_change_attributes(inode, &outarg.attr,
 					       attr_timeout(&outarg),
 					       attr_version);
-			if (stat)
+			if (get_size_form_attr)
+				stat->size = outarg.attr.size;
+			else if (stat)
 				fuse_fillattr(inode, &outarg.attr, stat);
 		}
 	}
 	return err;
+}
+
+int fuse_getattr_size(struct inode *inode, struct file *file, u64 *size)
+{
+	struct kstat stat;
+	int err;
+
+	err = fuse_do_getattr(inode, &stat, file, 1);
+	if (err)
+		return err;
+
+	*size = stat.size;
+	return 0;
 }
 
 static int fuse_update_get_attr(struct inode *inode, struct file *file,
@@ -999,7 +1014,7 @@ static int fuse_update_get_attr(struct inode *inode, struct file *file,
 
 	if (sync) {
 		forget_all_cached_acls(inode);
-		err = fuse_do_getattr(inode, stat, file);
+		err = fuse_do_getattr(inode, stat, file, 0);
 	} else if (stat) {
 		generic_fillattr(inode, stat);
 		stat->mode = fi->orig_i_mode;
@@ -1147,7 +1162,7 @@ static int fuse_perm_getattr(struct inode *inode, int mask)
 		return -ECHILD;
 
 	forget_all_cached_acls(inode);
-	return fuse_do_getattr(inode, NULL, NULL);
+	return fuse_do_getattr(inode, NULL, NULL, 0);
 }
 
 /*
@@ -1687,7 +1702,7 @@ static int fuse_setattr(struct dentry *entry, struct iattr *attr)
 			 * ia_mode calculation may have used stale i_mode.
 			 * Refresh and recalculate.
 			 */
-			ret = fuse_do_getattr(inode, NULL, file);
+			ret = fuse_do_getattr(inode, NULL, file, 0);
 			if (ret)
 				return ret;
 
