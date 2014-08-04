9a2910d7bdc07a818f3a215cca0f33ccd91848a5
Maxim Patlasov <MPatlasov@parallels.com>
2014-08-04 19:11:46 +0400

    fuse: Show active requests in fusectl files
    Port diff-fuse-show-active-requests-in-fusectl-files from 2.6.32-*:
    
    For debugging purposes it's good to know which requests are currently
    in action between kernel and userspace.
    
    Thus, the fusectl/*_req files are introduced, which print pending (those,
    waiting to be read), processing (those, waiting to be ack-ed back) and
    io (those, being copied between kernel and user spaces) requests, showing
    its opcode, IDs, state and flags. Arguments are omitted, but can be adder
    later.
    
    Those minimal amount of data was enough for my debugging.
    
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -38,7 +38,7 @@
 #define FUSE_NAME_MAX 1024
 
 /** Number of dentries for each connection in the control filesystem */
-#define FUSE_CTL_NUM_DENTRIES 5
+#define FUSE_CTL_NUM_DENTRIES 8
 
 /** Number of page pointers embedded in fuse_req */
 #define FUSE_REQ_INLINE_PAGES 1
