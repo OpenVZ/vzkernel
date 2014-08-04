f33edf0356eb01dcdc123ae1dfc311358a448344
Maxim Patlasov <MPatlasov@parallels.com>
2014-08-04 19:38:47 +0400

    fuse: add "killed" bit to fuse request
    Port diff-fuse-add-killed-bit-to-fuse-request-2 from 2.6.32-*:
    
    Introduce per-fuse-request "killed" bit. fuse_invalidate_files() will set it
    for requests which pages were already released. In this patch we add proper
    handling to .readpage and .readpages methods: skip releasing pages if the bit
    set.
    
    The patch deliberately moves "fuse_invalidate_attr(inode)" under "killed:"
    label. In case of killing a request by fuse_invalidate_files() we don't know
    whether fused already processed the request or not. So it's safer to
    invalidate fuse attributes than simply skip it.
    
    Also note that it's not legal to access to req->pages[0]->mapping in the
    beginning of fuse_readpages_end() anymore: if the request was killed, the
    page is already released.
    
    Signed-off-by: Maxim Patlasov <MPatlasov@parallels.com>
    [VvS RH77b] ported to new codebase

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -333,6 +333,9 @@ struct fuse_req {
 	/** Request contains pages from page-cache */
 	unsigned page_cache:1;
 
+	/** Request was killed -- pages were released */
+	unsigned killed:1;
+
 	/** The request input */
 	struct fuse_in in;
 
