f5df4e2fad513dfdb240ae6b8e6249ca45dbab07
Maxim Patlasov <MPatlasov@parallels.com>
2014-08-04 19:38:45 +0400

    fuse: add mount option to disable close_wait
    Port diff-fuse-add-mount-option-to-disable-close_wait from 2.6.32-*:
    
    close_wait is a feature making close (actually last fput()) synchronous. So
    far it was always enabled for pstorage mounts. The patch adds new mount
    option ('disable_close_wait') to optionally disable the close_wait. It's
    needed for testing.
    
    Signed-off-by: Maxim V. Patlasov <MPatlasov@parallels.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -496,6 +496,7 @@ struct fuse_fs_context {
 	bool writeback_cache:1;
 	bool direct_enable:1;
 	bool umount_wait:1;
+	bool disable_close_wait:1;
 	bool destroy:1;
 	bool no_control:1;
 	bool no_force_umount:1;
@@ -744,6 +745,9 @@ struct fuse_conn {
 
 	/** Does the unmount synchronous? */
 	unsigned umount_wait:1;
+
+	/** Does the disable synchronous close? */
+	unsigned disable_close_wait:1;
 
 	/** The number of requests waiting for completion */
 	atomic_t num_waiting;
