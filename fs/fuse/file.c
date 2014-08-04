7ff0230be25fa62a84be634859333c213c825a36
Maxim Patlasov <MPatlasov@parallels.com>
2014-08-04 19:11:51 +0400

    fuse kio: Read file attributes every first open
    Currently we do not reread file attributes in case of
    this is the existing inode:
    
    [root@s143 ~]# echo 1 >> /vzt/a.txt
    [root@s134 ~]# cat /vzt/a.txt
    1
    [root@s143 ~]# echo 1 >> /vzt/a.txt
    [root@s134 ~]# cat /vzt/a.txt
    1
    [root@s143 ~]# echo 1 >> /vzt/a.txt
    [root@s134 ~]# cat /vzt/a.txt
    1
    [root@s143 ~]# cat /vzt/a.txt
    1
    1
    1
    
    In this example the file was changed on s143, but s134
    used it's cached metadata.
    
    The patch reworks open sequence and makes dentry info
    to be populated every time we reopen the file again.
    Also, it fixed races during open of new file, since
    a new file may be obtained by another thread before
    the creator executes ->file_open(). So, we create
    every inode with num_users == 0, and then the first
    of them makes file_open().
    
    Note, that openers are synchronized by inode->mutex.
    Also, keep in mind that num_users are modified under
    fc->lock (except one unlikely path), and this exists
    long ago. So in further I'm going to make it like
    plain int, since this atomic does not do anything
    and just confuser a reader.
    
    https://jira.sw.ru/browse/PSBM-86234
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Acked-by: Alexey Kuznetsov <kuznet@virtuozzo.com>
    Feature: vStorage

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -310,12 +310,6 @@ out:
 		inode_unlock(inode);
 	}
 
-	if (!err && fc->kio.op && fc->kio.op->file_open &&
-	    fc->kio.op->file_open(file, inode)) {
-		fuse_release_common(file, false);
-		return -EINVAL;
-	}
-
 	if (!err && fc->writeback_cache && !isdir) {
 		struct fuse_inode *fi = get_fuse_inode(inode);
 		u64 size;
@@ -327,6 +321,10 @@ out:
 			fi->i_size_unstable = 1;
 			spin_unlock(&fi->lock);
 			err = fuse_getattr_size(inode, file, &size);
+
+			if (!err && fc->kio.op && fc->kio.op->file_open)
+				err = fc->kio.op->file_open(file, inode);
+
 			spin_lock(&fi->lock);
 			fi->i_size_unstable = 0;
 			if (err)
