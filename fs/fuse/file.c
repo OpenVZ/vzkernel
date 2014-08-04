a34bb0edcb2af0847c8b30f6f4bca0dae23fb9c8
Maxim Patlasov <MPatlasov@parallels.com>
2014-08-04 19:38:46 +0400

    fuse: do not flush files opened ro
    Port diff-fuse-do-not-flush-files-opened-ro from 2.6.32-*:
    
    From: Alexey Kuznetsov <kuznet@parallels.com>
    
    We have the following usability problem with pstorage:
    when something goes wrong and we lose some data chunk,
    we might want to try to repair images reading only readable parts.
    Currently, we cannot do this because each file close (even
    if file is open read-only), flushes all writeback and blocks
    there, if we have something dirty in broken area, and then
    we block in FLUSH, which also looks unnecessary.
    
    https://jira.sw.ru/browse/PSBM-20562
    
    [VvS RHEL77 rebase] fixed locking in fuse_release()
    
    Signed-off-by: Alexey Kuznetsov <kuznet@parallels.com>
    Signed-off-by: Maxim Patlasov <MPatlasov@parallels.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -400,14 +400,17 @@ static int fuse_release(struct inode *inode, struct file *file)
 
 	/* see fuse_vma_close() for !writeback_cache case */
 	if (ff->fc->writeback_cache) {
-		write_inode_now(inode, 1);
+		if (file->f_mode & FMODE_WRITE) {
+			write_inode_now(inode, 1);
 
-		/* Must remove file from write list. Otherwise it is possible this
-		 * file will get more writeback from another files rerouted via write_files
-		 */
-		spin_lock(&fi->lock);
-		list_del_init(&ff->write_entry);
-		spin_unlock(&fi->lock);
+			/* Must remove file from write list. Otherwise it is possible this
+			 * file will get more writeback from another files rerouted via write_files
+			 */
+			spin_lock(&fi->lock);
+			list_del_init(&ff->write_entry);
+			spin_unlock(&fi->lock);
+		} else
+			BUG_ON(!list_empty(&ff->write_entry));
 
 		/* This can livelock. Inode can be open via another file
 		 * and that file can generate continuous writeback.
@@ -559,7 +562,7 @@ static int fuse_flush(struct file *file, fl_owner_t id)
 	if (is_bad_inode(inode))
 		return -EIO;
 
-	if (fc->no_flush)
+	if (fc->no_flush || !(file->f_mode & FMODE_WRITE))
 		return 0;
 
 	err = write_inode_now(inode, 1);
