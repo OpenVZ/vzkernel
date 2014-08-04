5c2a7652eafd9906a046b45ad325c359ee6dafb3
Maxim Patlasov <MPatlasov@parallels.com>
2014-08-04 19:38:42 +0400

    fuse: cleanups in fuse writeback
    Port remaining part of diff-fuse-cleanups-in-writeback-3 from 2.6.32-*:
    
    This patch is not supposed to change anything real unless it is wrong.
    It affects only paths which never triggered in real life (otherwise
    we would see crashes dereferencing NULL, pages left in writeback state etc etc)
    and adds some debugging to catch situations like #PSBM-16442 earlier
    in case it was not due to the problem fixed by previous patch.
    
    Affects: #PSBM-16442
    
    Signed-off-by: Alexey Kuznetsov <kuznet@parallels.com>
    
    Signed-off-by: Maxim Patlasov <MPatlasov@parallels.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1874,7 +1874,8 @@ static int fuse_writepage_locked(struct page *page)
 	struct page *tmp_page;
 	int error = -ENOMEM;
 
-	set_page_writeback(page);
+	if (test_set_page_writeback(page))
+		BUG();
 
 	req = fuse_request_alloc_nofs(1);
 	if (!req)
