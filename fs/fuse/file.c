5c4739c3ab17cb1f04419811a9c7aa40b1c9e668
Maxim Patlasov <MPatlasov@parallels.com>
2014-08-04 19:11:55 +0400

    fuse: optimize fuse_vma_close()
    In case of fuse writeback is ON, it's not neccessary to wait for writeback
    in fuse_vma_close(), because we wait for it in fuse_release() anyway.
    
    That optimization was introduced to 2.6.32-* in March of 2012 and has existed
    there since then. No reason why don't have the same in 3.10-*.
    
    Signed-off-by: Maxim Patlasov <MPatlasov@parallels.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -2156,7 +2156,11 @@ static int fuse_launder_page(struct page *page)
  */
 static void fuse_vma_close(struct vm_area_struct *vma)
 {
-	filemap_write_and_wait(vma->vm_file->f_mapping);
+	struct file *file = vma->vm_file;
+	struct fuse_file *ff = file->private_data;
+
+	if (!ff->fc->writeback_cache)
+		filemap_write_and_wait(file->f_mapping);
 }
 
 /*
