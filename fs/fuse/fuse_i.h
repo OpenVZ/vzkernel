3f2f3c038b2878e4da525dd1416ba4752e1b0a54
Maxim Patlasov <MPatlasov@parallels.com>
2014-08-04 19:11:50 +0400

    fuse: i_size to be updated on first open
    Port diff-fuse-i_size-to-be-updated-on-first-open-2 from 2.6.32-*:
    
    fuse can't trust i_size which was set during fuse_lookup:
    
    1. In case of special pcs fuse files, it's zero till first open
    2. In case of regular files, the file can be changed (truncated) externally
    before we get lease in fuse_open.
    
    The patch fixes the problem only if WBCACHE enabled because otherwise, if
    it's disabled, in-kernel fuse must always use attr.size from userspace.
    
    https://jira.sw.ru/browse/PSBM-13636
    
    Signed-off-by: Maxim V. Patlasov <MPatlasov@parallels.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -148,6 +148,9 @@ struct fuse_inode {
 
 	/** Lock to protect write related fields */
 	spinlock_t lock;
+
+	/** Mostly to detect very first open */
+	atomic_t num_openers;
 };
 
 /** FUSE inode state bits */
@@ -1016,6 +1019,8 @@ u64 fuse_lock_owner_id(struct fuse_conn *fc, fl_owner_t id);
 void fuse_update_ctime(struct inode *inode);
 
 int fuse_update_attributes(struct inode *inode, struct file *file);
+
+int fuse_getattr_size(struct inode *inode, struct file *file, u64 *size);
 
 void fuse_flush_writepages(struct inode *inode);
 
