ab9b8591c0de803c7c51276b7b0668fe33b96103
Maxim Patlasov <MPatlasov@parallels.com>
2014-08-04 19:11:56 +0400

    fuse: improve bdi dirty memory limits for fuse
    Port diff-fuse-improve-bdi-dirty-memory-limits-for-fuse-2 from 2.6.32-*:
    
    So far default dirty ratio for fuse was 1% of global dirty limit. The latter
    is 20% of RAM by default. This is too small for PCS:
    
    BdiDirtyThresh WriteRate
    31664k         55,7 MB/sec
    63328k         75,0 MB/sec
    126436k        92,0 MB/sec
    158096k        107 MB/sec
    199816k        106 MB/sec
    
    The patch increase per-bdi dirty ratio for fuse from 1% to 20%. It can be
    changed by writing to /sys/class/bdi/<bdi>/max_ratio.
    
    The patch also introduces per-bdi min/max limits for dirty memory (measured
    in pages). They are 64MB/256MB for fuse by default. They can be changed by
    writing to /sys/class/bdi/<bdi>/{min|max}_dirty_pages.
    
    Rationale:
    1. For machines with plenty of RAM (>16GB) we give 256MB per fuse mount.
    This should be enough to avoid BdiDirtyThresh being bottleneck.
    2. For machines with scarcity of RAM (1GB or lesser) we give 64MB per fuse
    mount. We beleive it's not too much.
    3. For machines with moderate RAM size we get some value in 64MB..256MB range.
    E.g. in case of 4GB, BdiDirtyThresh should be (roughly) about 160MB (20% of
    20% of 4GB). Acording to measurements cited above this is enough to saturate
    PCS.
    
    https://jira.sw.ru/browse/PSBM-13700
    
    Signed-off-by: Maxim V. Patlasov <MPatlasov@parallels.com>
    
    Acked-by: Pavel Emelyanov <xemul@parallels.com>
    
    +++
    fs/mm: writeback: fix per bdi dirty background threshold calculation
    
    After patch [1] introduced upper and lower boundaries for per bdi dirty
    threshold (see bdi->min_dirty_pages and max_dirty_pages), it is
    incorrect to use bdi_dirty_limit() helper for calculating background
    threshold. E.g. on a 16 GB host, bdi_dirty_limit() would return the
    following values for a FUSE device if the upper boundary was unset:
    
      bdi_thresh = (16 GB * 20 / 100) * 20 / 100 = 655 MB
                    ^^^^^   ^^^^^^^^    ^^^^^^^^
                  RAM size           bdi->max_ratio
    
                        vm.dirty_ratio
    
      bdi_bg_thresh = (16 GB * 10 / 100) * 20 / 100 = 327 MB
                       ^^^^^   ^^^^^^^^    ^^^^^^^^
                     RAM size           bdi->max_ratio
    
                       vm.dirty_background_ratio
    
    which looks fine.
    
    However, with the default upper threshold of 256 MB for FUSE devices,
    both dirty and background thresholds will be equal to 256 MB. As a
    result the background flusher will only wake up once the writer is
    throttled. This obviously results in a huge write rate degradation.
    
    To fix this issue, let's use bdi_dirty_limit() helper only for
    calculating the throttle threshold, and compute the background threshold
    as follows:
    
      bdi_bg_thresh = bdi_thresh * global_background_thresh / global_thresh
    
    https://jira.sw.ru/browse/PSBM-45497
    
    mFixes: 2f5b9552e256d ("fuse: improve bdi dirty memory limits for fuse") [1]
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    Acked-by: Maxim Patlasov <mpatlasov@virtuozzo.com>

--- include/linux/backing-dev.h
+++ include/linux/backing-dev.h
@@ -97,6 +97,9 @@ struct backing_dev_info {
 	unsigned int min_ratio;
 	unsigned int max_ratio, max_prop_frac;
 
+	unsigned int min_dirty_pages;
+	unsigned int max_dirty_pages;
+
 	struct bdi_writeback wb;  /* default writeback info for this bdi */
 	spinlock_t wb_lock;	  /* protects work_list & wb.dwork scheduling */
 
@@ -221,6 +224,8 @@ static inline unsigned long bdi_stat_error(struct backing_dev_info *bdi)
 
 int bdi_set_min_ratio(struct backing_dev_info *bdi, unsigned int min_ratio);
 int bdi_set_max_ratio(struct backing_dev_info *bdi, unsigned int max_ratio);
+int bdi_set_min_dirty(struct backing_dev_info *bdi, unsigned int min_dirty);
+int bdi_set_max_dirty(struct backing_dev_info *bdi, unsigned int max_dirty);
 
 /*
  * Flags in backing_dev_info::capability
