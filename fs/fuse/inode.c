921a9bce126fbe3a5048e7549a79ce50fa54e496
Maxim Patlasov <MPatlasov@parallels.com>
2014-08-04 19:38:45 +0400

    fuse: make umount synchronous
    Problem statement: fuse umount doesn't wait for ACK from userspace. Hence,
    umount(2) completes, but userspace is still flushing its state for a while.
    No good way to find out when it finishes.
    
    Solution (suggested by Alexey Kuznetzov): decouple FUSE_DESTROY from fuseblk.
    I.e. FUSE_DESTROY already does what we need, but it was enabled only for
    fuseblk mounts. The patch allows to enable FUSE_DESTROY by mount option.
    
    Userspace (pstorage) changes will be required to utilize the feature:
     - pass 'umount_wait' as mount option
     - handle FUSE_DESTROY intelligently
    
    https://jira.sw.ru/browse/PSBM-19305
    
    Signed-off-by: Maxim V. Patlasov <MPatlasov@parallels.com>

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -491,6 +491,7 @@ enum {
 	OPT_BLKSIZE,
 	OPT_WBCACHE,
 	OPT_ODIRECT,
+	OPT_UMOUNT_WAIT,
 	OPT_ERR
 };
 
@@ -505,6 +506,7 @@ static const match_table_t tokens = {
 	{OPT_BLKSIZE,			"blksize=%u"},
 	{OPT_WBCACHE,			"writeback_enable"},
 	{OPT_ODIRECT,			"direct_enable"},
+	{OPT_UMOUNT_WAIT,		"umount_wait"},
 	{OPT_ERR,			NULL}
 };
 
@@ -591,6 +593,12 @@ static int parse_fuse_opt(char *opt, struct fuse_mount_data *d, int is_bdev,
 			d->flags |= FUSE_ODIRECT;
 			break;
 
+		case OPT_UMOUNT_WAIT:
+			if (!ve_is_super(get_exec_env()) && !fuse_ve_odirect)
+				return -EPERM;
+			d->flags |= FUSE_UMOUNT_WAIT;
+			break;
+
 		default:
 			return 0;
 		}
@@ -616,6 +624,8 @@ static int fuse_show_options(struct seq_file *m, struct dentry *root)
 		seq_puts(m, ",allow_other");
 	if (fc->flags & FUSE_ODIRECT)
 		seq_puts(m, ",direct_enable");
+	if (fc->flags & FUSE_UMOUNT_WAIT)
+		seq_puts(m, ",umount_wait");
 	if (fc->max_read != ~0)
 		seq_printf(m, ",max_read=%u", fc->max_read);
 	if (sb->s_bdev && sb->s_blocksize != FUSE_DEFAULT_BLKSIZE)
@@ -1205,7 +1215,7 @@ static int fuse_fill_super(struct super_block *sb, void *data, int silent)
 		goto err_put_root;
 	__set_bit(FR_BACKGROUND, &init_req->flags);
 
-	if (is_bdev) {
+	if (is_bdev || (fc->flags & FUSE_UMOUNT_WAIT)) {
 		fc->destroy_req = fuse_request_alloc(0);
 		if (!fc->destroy_req)
 			goto err_free_init_req;
