8527e0f7b18b9ee7b28bc7aba58a63f90b2c6b09
Maxim Patlasov <MPatlasov@parallels.com>
2014-08-04 19:11:50 +0400

    fuse: i_size to be updated on first open
    Port diff-fuse-i_size-to-be-updated-on-first-open-2 from 2.6.32-*:
    
    fuse can't trust i_size which was set during fuse_lookup:
    
    1. In case of special pcs fuse files, it's zero till first open
    2. In case of regular files, the file can be changed (truncated) externally
    before we get lease in fuse_open.
    
    The patch fixes the problem only if WBCACHE enabled because otherwise, if
    it's disabled, in-kernel fuse must always use attr.size from userspace.
    
    https://jira.sw.ru/browse/PSBM-13636
    
    Signed-off-by: Maxim V. Patlasov <MPatlasov@parallels.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -929,7 +929,7 @@ static void fuse_fillattr(struct inode *inode, struct fuse_attr *attr,
 }
 
 static int fuse_do_getattr(struct inode *inode, struct kstat *stat,
-			   struct file *file)
+			   struct file *file, int get_size_form_attr)
 {
 	int err;
 	struct fuse_getattr_in inarg;
@@ -975,11 +975,26 @@ static int fuse_do_getattr(struct inode *inode, struct kstat *stat,
 			fuse_change_attributes(inode, &outarg.attr,
 					       attr_timeout(&outarg),
 					       attr_version);
-			if (stat)
+			if (get_size_form_attr)
+				stat->size = outarg.attr.size;
+			else if (stat)
 				fuse_fillattr(inode, &outarg.attr, stat);
 		}
 	}
 	return err;
+}
+
+int fuse_getattr_size(struct inode *inode, struct file *file, u64 *size)
+{
+	struct kstat stat;
+	int err;
+
+	err = fuse_do_getattr(inode, &stat, file, 1);
+	if (err)
+		return err;
+
+	*size = stat.size;
+	return 0;
 }
 
 int fuse_update_attributes(struct inode *inode, struct kstat *stat,
@@ -991,7 +1006,7 @@ int fuse_update_attributes(struct inode *inode, struct kstat *stat,
 
 	if (time_before64(fi->i_time, get_jiffies_64())) {
 		r = true;
-		err = fuse_do_getattr(inode, stat, file);
+		err = fuse_do_getattr(inode, stat, file, 0);
 	} else {
 		r = false;
 		err = 0;
@@ -1141,7 +1156,7 @@ static int fuse_perm_getattr(struct inode *inode, int mask)
 	if (mask & MAY_NOT_BLOCK)
 		return -ECHILD;
 
-	return fuse_do_getattr(inode, NULL, NULL);
+	return fuse_do_getattr(inode, NULL, NULL, 0);
 }
 
 /*
