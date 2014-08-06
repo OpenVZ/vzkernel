b1413a4a9cc960d93dbafcf923efaa0be9e6f772
Kirill Tkhai <ktkhai@parallels.com>
2014-08-06 16:55:46 +0400

    ve/vfs: introduce "odirect_enable" sysctl and disable it by default
    khorenko@: we want to disable direct access from inside Container
    	because this is limited numbers of direct requests available
    	on the system (128), and in case they are busy next request
    	is provided only after some requst is completed.
    	There is no any scheduler at this level => DDoS is possible
    	from inside a CT: just run _many_ processes writing with O_DIRECT.
    
    diff-vfs-odirect-enable && diff-vfs-odirect-enable-location-fix
    
    Signed-off-by: Kirill Tkhai <ktkhai@parallels.com>

--- fs/open.c
+++ fs/open.c
@@ -34,6 +34,8 @@
 
 #include "internal.h"
 
+int odirect_enable = 0;
+
 int do_truncate(struct dentry *dentry, loff_t length, unsigned int time_attrs,
 	struct file *filp)
 {
@@ -850,6 +852,9 @@ struct file *dentry_open(const struct path *path, int flags,
 
 	/* We must always pass in a valid mount pointer. */
 	BUG_ON(!path->mnt);
+
+	if (!capable(CAP_SYS_RAWIO) && !odirect_enable)
+		flags &= ~O_DIRECT;
 
 	f = get_empty_filp();
 	if (!IS_ERR(f)) {
