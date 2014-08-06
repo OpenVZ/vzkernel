09ef4dd32a4c29c0cdf2d6be04cfb3ce95f1d776
Kirill Tkhai <ktkhai@parallels.com>
2014-08-06 17:01:08 +0400

    rh/fs: introduce "fs.relatime_interval" sysctl
    Add sysctl fs.relatime_interval, =86400 (one day) by default.
    Use =0 to disable relatime, and =-1 to make it infinite.
    
    http://bugzilla.openvz.org/show_bug.cgi?id=1834
    
    Ported from rhel5
    
    diff-ms-relatime-sysctl-interval
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Signed-off-by: Kirill Tkhai <ktkhai@parallels.com>

--- fs/inode.c
+++ fs/inode.c
@@ -1502,6 +1502,8 @@ sector_t bmap(struct inode *inode, sector_t block)
 }
 EXPORT_SYMBOL(bmap);
 
+unsigned __read_mostly relatime_interval = 24*60*60; /* one day */
+
 /*
  * Update times in overlayed inode from underlying real inode
  */
@@ -1542,10 +1544,10 @@ static int relatime_need_update(const struct path *path, struct inode *inode,
 		return 1;
 
 	/*
-	 * Is the previous atime value older than a day? If yes,
-	 * update atime:
+	 * Is the previous atime value older than a update interval?
+	 * If yes, update atime:
 	 */
-	if ((long)(now.tv_sec - inode->i_atime.tv_sec) >= 24*60*60)
+	if ((long)(now.tv_sec - inode->i_atime.tv_sec) >= relatime_interval)
 		return 1;
 	/*
 	 * Good, we can skip the atime update:
