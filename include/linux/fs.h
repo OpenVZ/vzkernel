13df6753b04185e22a82f00f2c5861030dc7a20b
Kirill Tkhai <ktkhai@parallels.com>
2014-08-06 17:59:04 +0400

    fs: add fadvise file_operation
    sys_fadvise result in direct f_mode modification, which  may be not
    suitable for some unusual filesytems where file mode invariant is more
    complex. In order to support such filesystems we have to delegate fadvise
    logic to filesystem layer.
    
    diff-vfs-fadvise-generic-fops
    
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>
    
    Signed-off-by: Kirill Tkhai <ktkhai@parallels.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1746,6 +1746,7 @@ struct file_operations {
 	int (*flock) (struct file *, int, struct file_lock *);
 	ssize_t (*splice_write)(struct pipe_inode_info *, struct file *, loff_t *, size_t, unsigned int);
 	ssize_t (*splice_read)(struct file *, loff_t *, struct pipe_inode_info *, size_t, unsigned int);
+	int (*fadvise)(struct file* file, loff_t offset, loff_t len, int advice);
 	int (*setlease)(struct file *, long, struct file_lock **);
 	long (*fallocate)(struct file *file, int mode, loff_t offset,
 			  loff_t len);
