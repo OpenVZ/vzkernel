cb52cbc6cb3ef651265f768843024acce6942d8c
Kirill Tkhai <ktkhai@parallels.com>
2014-08-06 15:37:15 +0400

    ve/futex/timeout: adjust futex timeout to absolule
    This converts ve-absolute-monotonic time to global-absolute-monotonic time.
    
    https://jira.sw.ru/browse/PSBM-14471
    
    diff-futex-reference-ct-monotonic-clock-from-ct-start
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Signed-off-by: Kirill Tkhai <ktkhai@parallels.com>
    
    (cherry picked from vz7 commit 14a4db52ee8c862eb7a9dec740b15c646e0b59aa)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- kernel/futex.c
+++ kernel/futex.c
@@ -69,6 +69,7 @@
 #include <linux/memblock.h>
 #include <linux/fault-inject.h>
 #include <linux/refcount.h>
+#include <linux/ve.h>
 
 #include <asm/futex.h>
 
@@ -3793,6 +3794,7 @@ long do_futex(u32 __user *uaddr, int op, u32 val, ktime_t *timeout,
 {
 	int cmd = op & FUTEX_CMD_MASK;
 	unsigned int flags = 0;
+	ktime_t abs_time;
 
 	if (!(op & FUTEX_PRIVATE_FLAG))
 		flags |= FLAGS_SHARED;
@@ -3802,6 +3804,12 @@ long do_futex(u32 __user *uaddr, int op, u32 val, ktime_t *timeout,
 		if (cmd != FUTEX_WAIT && cmd != FUTEX_WAIT_BITSET && \
 		    cmd != FUTEX_WAIT_REQUEUE_PI)
 			return -ENOSYS;
+	} else if (timeout) {
+		if (cmd == FUTEX_WAIT_BITSET || cmd == FUTEX_WAIT_REQUEUE_PI) {
+			abs_time = ktime_add(*timeout, ns_to_ktime(
+					     get_exec_env()->start_time));
+			timeout = &abs_time;
+		}
 	}
 
 	switch (cmd) {
