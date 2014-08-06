bab5a7eed2f2e87b21bb89e20878f977c5a697a8
Kirill Tkhai <ktkhai@parallels.com>
2014-08-06 17:14:14 +0400

    ext4: quarantee rename atomic behavior
    Ext4 already performs block allocation on rename, but
    it does it after rename completes, so race window is much tighter
    but still exits. In order to guarantee atomic behavior let's
    force block allocation before actual rename.
    
    http://wiki.parallels.com/index.php/Ext4_writeback
    
    diff-ms-ext4-force-atomic-rename
    
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>
    Signed-off-by: Kirill Tkhai <ktkhai@parallels.com>
    
    khorenko@: conflicts in fs/ext4/namei.c
    resolved during rebase to rh7 kernel-3.10.0-229.7.2.el7

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -3270,7 +3270,7 @@ static int ext4_rename(struct inode *old_dir, struct dentry *old_dentry,
 	 */
 	retval = -ENOENT;
 	if (!old.bh || le32_to_cpu(old.de->inode) != old.inode->i_ino)
-		goto end_rename;
+		goto out_release;
 
 	new.bh = ext4_find_entry(new.dir, &new.dentry->d_name,
 				 &new.de, &new.inlined);
@@ -3404,6 +3404,9 @@ static int ext4_rename(struct inode *old_dir, struct dentry *old_dentry,
 	retval = 0;
 
 end_rename:
+	if (handle)
+		ext4_journal_stop(handle);
+out_release:
 	brelse(old.dir_bh);
 	brelse(old.bh);
 	brelse(new.bh);
@@ -3413,8 +3416,6 @@ end_rename:
 		unlock_new_inode(whiteout);
 		iput(whiteout);
 	}
-	if (handle)
-		ext4_journal_stop(handle);
 	return retval;
 }
 
