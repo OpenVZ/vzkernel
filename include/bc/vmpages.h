dbd9c8dc0153732edcdf7a467e1dc468b7ee751a
Vladimir Davydov <vdavydov@parallels.com>
2014-10-08 15:18:42 +0400

    ve/bc/mm: port UB_PRIVVMPAGES, UB_SHMPAGES, and UB_LOCKEDPAGES accounting
    https://jira.sw.ru/browse/PSBM-20089
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Reviewed-by: Cyrill Gorcunov <gorcunov@openvz.org>
    
    Changes in v2:
     - drop vdso-32 pages accounting (gorcunov@)
       because in RH6 we account vdso pages on x86-32 but doesn't for pure x86-64
       (seems like due to historical reasons).
       At the same time it consumes a couple of pages =>
       decide to drop accounting for x86-32 to be in sync.
    
     fs/exec.c                       |   12 ++++-
     include/bc/vmpages.h            |   28 +++++++++++
     include/linux/shmem_fs.h        |    1 +
     include/uapi/asm-generic/mman.h |    1 -
     kernel/bc/vm_pages.c            |   95 +++++++++++++++++++++++++++++++++++++
     kernel/fork.c                   |    8 ++++
     mm/fremap.c                     |    2 +-
     mm/internal.h                   |    9 +++-
     mm/mlock.c                      |   24 ++++++++--
     mm/mmap.c                       |   64 +++++++++++++++++++++----
     mm/mprotect.c                   |   20 +++++++-
     mm/mremap.c                     |   30 ++++++++++--
     mm/shmem.c                      |   99 +++++++++++++++++++++++++++++++--------
     13 files changed, 350 insertions(+), 43 deletions(-)

--- include/bc/vmpages.h
+++ include/bc/vmpages.h
@@ -17,7 +17,35 @@
 #include <bc/beancounter.h>
 #include <bc/decl.h>
 
+/*
+ * Check whether vma has private or copy-on-write mapping.
+ */
+#define VM_UB_PRIVATE(__flags, __file)					\
+		( ((__flags) & VM_WRITE) ?				\
+			(__file) == NULL || !((__flags) & VM_SHARED) :	\
+			0						\
+		)
+
+UB_DECLARE_FUNC(int, ub_memory_charge(struct mm_struct *mm,
+			unsigned long size,
+			unsigned vm_flags,
+			struct file *vm_file,
+			int strict))
+UB_DECLARE_VOID_FUNC(ub_memory_uncharge(struct mm_struct *mm,
+			unsigned long size,
+			unsigned vm_flags,
+			struct file *vm_file))
+
 struct shmem_inode_info;
+
+UB_DECLARE_FUNC(int, ub_locked_charge(struct mm_struct *mm,
+			unsigned long size))
+UB_DECLARE_VOID_FUNC(ub_locked_uncharge(struct mm_struct *mm,
+			unsigned long size))
+UB_DECLARE_FUNC(int, ub_lockedshm_charge(struct shmem_inode_info *shi,
+			unsigned long size))
+UB_DECLARE_VOID_FUNC(ub_lockedshm_uncharge(struct shmem_inode_info *shi,
+			unsigned long size))
 
 extern void __ub_update_oomguarpages(struct user_beancounter *ub);
 
