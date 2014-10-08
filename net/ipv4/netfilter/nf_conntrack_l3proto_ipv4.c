c9f25ca8482ce7934a398228d241e27e78c98c46
Kirill Tkhai <ktkhai@parallels.com>
2014-10-08 13:43:21 +0400

    ve/netfilter: Implement pernet net->ct.max / virtualize "nf_conntrack_max" sysctl
    Rebasing and splitting netfilters sybsystem
    (port 66-diff-ve-net-netfilter-combined).
    Part 1.
    
    https://jira.sw.ru/browse/PSBM-18322
    
    Signed-off-by: Kirill Tkhai <ktkhai@parallels.com>

--- net/ipv4/netfilter/nf_conntrack_l3proto_ipv4.c
+++ net/ipv4/netfilter/nf_conntrack_l3proto_ipv4.c
@@ -372,7 +372,7 @@ static int ipv4_init_net(struct net *net)
 	if (!in->ctl_table)
 		return -ENOMEM;
 
-	in->ctl_table[0].data = &nf_conntrack_max;
+	in->ctl_table[0].data = &net->ct.max;
 	in->ctl_table[1].data = &net->ct.count;
 	in->ctl_table[2].data = &net->ct.htable_size;
 	in->ctl_table[3].data = &net->ct.sysctl_checksum;
