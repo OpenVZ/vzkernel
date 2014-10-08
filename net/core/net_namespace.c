5bff4e56508bcf67d685364a0bb1c211b970c715
Kirill Tkhai <ktkhai@parallels.com>
2014-10-08 13:42:22 +0400

    ve/netfilter: Implement net_ipt_permitted() etc
    Rebasing and splitting netfilters sybsystem
    (port 66-diff-ve-net-netfilter-combined).
    Part 1.
    
    https://jira.sw.ru/browse/PSBM-18322
    
    Signed-off-by: Kirill Tkhai <ktkhai@parallels.com>
    
    ve/net: Track netfilter modules per net-namespace-v3
    
    Porting patch diff-ve-net-track-netfilter-modules-per-net-namespace from
    2.6.32:
    
    cpt related hunks are separated in
    diff-cpt-track-netfilter-modules-per-net-namespace-v3
    
    Previously in rhel6 we've been not allowing to create nested net namespaces
    so all netfilter modules tracking has been done via VE structure
    
    	struct ve_struct {
    		...
    		__u64			ipt_mask;
    		__u64			_iptables_modules;
    		...
    	}
    
    Here @ipt_mask stands for features bits which are set up from VE
    configutarion and represent netfilter modules which are allowed
    to use inside VE (might be situation when modules are loaded on
    the node but forbidden inside VE).
    
    When some particular module is allowed inside VE and get loaded we're
    setting up an appropriate bit in @_iptables_modules and clear it back
    upon module unload: net_ipt_module_set() helper is doing that usually
    in module net-init() call. Same time this bit is signalling that
    module specific resource allocated for VE use should be freed once
    module is unloaded, iow net-exit() is called.
    
    For example
    
    	| static int __net_init iptable_mangle_net_init(struct net *net)
    	| {
    	| 	if (!net_ipt_permitted(net, VE_IP_MANGLE))
    	| 		return 0;
    	|
    	| 	/* Register table */
    	| 	net->ipv4.iptable_mangle =
    	| 		ipt_register_table(net, &packet_mangler, &initial_table.repl);
    	| 	if (IS_ERR(net->ipv4.iptable_mangle))
    	| 		return PTR_ERR(net->ipv4.iptable_mangle);
    	|
    	| 	net_ipt_module_set(net, VE_IP_MANGLE);
    	| 	return 0;
    	| }
    	|
    	| static void __net_exit iptable_mangle_net_exit(struct net *net)
    	| {
    	| 	if (!net_is_ipt_module_set(net, VE_IP_MANGLE))
    	| 		return;
    	| }
    	|
    	| static void __net_exit iptable_mangle_net_exit(struct net *net)
    	| {
    	| 	if (!net_is_ipt_module_set(net, VE_IP_MANGLE))
    	| 		return;
    	|
    	| 	ipt_unregister_table(net->ipv4.iptable_mangle);
    	|
    	| 	net_ipt_module_clear(net, VE_IP_MANGLE);
    	| }
    	|
    	| static struct pernet_operations iptable_mangle_net_ops = {
    	| 	.init = iptable_mangle_net_init,
    	| 	.exit = iptable_mangle_net_exit,
    	| };
    
    here we allocate @net->ipv4.iptable_mangle which should be cleaned up
    on exit, moreover the net-namespace engine is designed that way: if
    somehting if failed in init() call then exit() is called and must
    clear all data if been allocated.
    
    This worked well until we have allowed nested net-namespaces. Every
    nested net-namespace share same @owner_ve (ie the VE which is a keeper
    of it). Thus once nested namespace called net_ipt_module_clear()
    the shared @owner_ve get @_iptables_modules bit clear and finally
    the primary net namespace no longer see this bit set in @_iptables_modules.
    
    Lets move @_iptables_modules to struct net instead. Toplevel VE's
    net is referred via @ve_netns member which exist all the container's
    lifetime. Also we've to move it into @init_net thus node's net would
    be tracked same way automatically.
    
    https://jira.sw.ru/browse/PSBM-31451
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@parallels.com>
    
    Acked-by: Vladimir Davydov <vdavydov@parallels.com>
    CC: Andrey Vagin <avagin@parallels.com>
    CC: Kirill Tkhai <ktkhai@parallels.com>
    Signed-off-by: Kirill Tkhai <ktkhai@odin.com>

--- net/core/net_namespace.c
+++ net/core/net_namespace.c
@@ -24,7 +24,7 @@
 #include <net/netlink.h>
 #include <net/net_namespace.h>
 #include <net/netns/generic.h>
-#include <linux/ve.h>
+#include <linux/vziptable_defs.h>
 
 /*
  *	Our network namespace constructor/destructor lists
@@ -41,6 +41,9 @@ struct net init_net = {
 	.dev_base_head = LIST_HEAD_INIT(init_net.dev_base_head),
 #ifdef CONFIG_VE
 	.owner_ve = &ve0,
+#ifdef CONFIG_VE_IPTABLES
+	._iptables_modules = VE_IP_NONE,
+#endif
 #endif
 };
 EXPORT_SYMBOL(init_net);
