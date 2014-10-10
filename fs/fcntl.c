08655e9ffec01d39d001d8045215a8795df971d2
Kirill Tkhai <ktkhai@parallels.com>
2014-10-10 19:32:21 +0400

    ve/fs: Port fs.fsync-enable and fs.odirect_enable sysctls
    This is a part of 74-diff-ve-mix-combined.
    
    https://jira.sw.ru/browse/PSBM-17903
    
    Signed-off-by: Kirill Tkhai <ktkhai@parallels.com>

--- fs/fcntl.c
+++ fs/fcntl.c
@@ -22,6 +22,7 @@
 #include <linux/pid_namespace.h>
 #include <linux/user_namespace.h>
 #include <linux/shmem_fs.h>
+#include <linux/ve.h>
 
 #include <asm/poll.h>
 #include <asm/siginfo.h>
@@ -47,13 +48,32 @@ int generic_set_file_flags(struct file *filp, unsigned int arg)
 }
 EXPORT_SYMBOL(generic_set_file_flags);
 
+int may_use_odirect(void)
+{
+	int may;
+
+	if (ve_is_super(get_exec_env()))
+		return 1;
+
+	may = capable(CAP_SYS_RAWIO);
+	if (!may) {
+		may = get_exec_env()->odirect_enable;
+		if (may == 2)
+			may = get_ve0()->odirect_enable;
+	}
+
+	return may;
+}
+
 static int setfl(int fd, struct file * filp, unsigned long arg)
 {
 	struct inode * inode = file_inode(filp);
 	int error = 0;
 
-	if (!capable(CAP_SYS_RAWIO) && !odirect_enable)
+	if (!may_use_odirect())
 		arg &= ~O_DIRECT;
+	if (ve_fsync_behavior() == FSYNC_NEVER)
+		arg &= ~O_SYNC;
 	/*
 	 * O_APPEND cannot be cleared if the file is marked as append-only
 	 * and the file is open for write.
