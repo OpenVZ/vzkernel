0b49753c843af5be34f9504a3d19e0196ad255b8
Maxim Patlasov <MPatlasov@parallels.com>
2014-10-27 21:05:00 +0400

    fuse: release cleanup
    The patch deletes a few code duplications.
    
    v2: cosmetic change in fuse_readpages_end(): s/req->inode/inode
    
    Signed-off-by: Maxim Patlasov <MPatlasov@parallels.com>
    (cherry picked from commit eddd4541c88951eb33b2bdeb6df78c2d408cd174)
    Signed-off-by: Liu Kui <Kui.Liu@acronis.com>
    Feature: vStorage

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1042,6 +1042,21 @@ static int fuse_readpage(struct file *file, struct page *page)
 	return err;
 }
 
+void fuse_release_ff(struct inode *inode, struct fuse_file *ff)
+{
+	if (ff) {
+		if (ff->fm->fc->close_wait) {
+			struct fuse_inode *fi = get_fuse_inode(inode);
+			spin_lock(&fi->lock);
+			__fuse_file_put(ff);
+			wake_up(&get_fuse_inode(inode)->page_waitq);
+			spin_unlock(&fi->lock);
+		} else {
+			fuse_file_put(ff, false, false);
+		}
+	}
+}
+
 static void fuse_readpages_end(struct fuse_mount *fm, struct fuse_args *args,
 			       int err)
 {
@@ -1074,16 +1089,8 @@ static void fuse_readpages_end(struct fuse_mount *fm, struct fuse_args *args,
 killed:
 	fuse_invalidate_atime(inode);
 
-	if (ia->ff) {
-		if (fm->fc->close_wait) {
-			struct fuse_inode *fi = get_fuse_inode(inode);
-			spin_lock(&fi->lock);
-			__fuse_file_put(ia->ff);
-			wake_up(&get_fuse_inode(inode)->page_waitq);
-			spin_unlock(&fi->lock);
-		} else
-			fuse_file_put(ia->ff, false, false);
-	}
+	if (ia->ff)
+		fuse_release_ff(inode, ia->ff);
 
 	fuse_io_free(ia);
 }
@@ -2428,21 +2435,6 @@ static int fuse_dummy_writepage(struct page *page,
 {
 	unlock_page(page);
 	return 0;
-}
-
-void fuse_release_ff(struct inode *inode, struct fuse_file *ff)
-{
-	if (ff) {
-		if (ff->fm->fc->close_wait) {
-			struct fuse_inode *fi = get_fuse_inode(inode);
-			spin_lock(&fi->lock);
-			__fuse_file_put(ff);
-			wake_up(&get_fuse_inode(inode)->page_waitq);
-			spin_unlock(&fi->lock);
-		} else {
-			fuse_file_put(ff, false, false);
-		}
-	}
 }
 
 static int fuse_writepages(struct address_space *mapping,
