4b7b064430b4ba3dc16d8039c2e91e1b44515578
Kirill Tkhai <ktkhai@parallels.com>
2014-11-10 16:46:02 +0400

    ms/sched: Fix race between task_group and sched_task_group
    The race may happen when somebody is changing task_group of a forking task.
    Child's cgroup is the same as parent's after dup_task_struct() (there just
    memory copying). Also, cfs_rq and rt_rq are the same as parent's.
    
    But if parent changes its task_group before it's called cgroup_post_fork(),
    we do not reflect this situation on child. Child's cfs_rq and rt_rq remain
    the same, while child's task_group changes in cgroup_post_fork().
    
    To fix this we introduce fork() method, which calls sched_move_task() directly.
    This function changes sched_task_group on appropriate (also its logic has
    no problem with freshly created tasks, so we shouldn't introduce something
    special; we are able just to use it).
    
    Approved by Peter Zijlstra: https://lkml.org/lkml/2014/10/27/283, and will
    go to mainline.
    
    [There was a report about NULL pointer derefference, I'm suggesting it's
     connected with the problem we are solving at the moment:
                                        https://lkml.org/lkml/2014/10/24/456 ]
    
    Signed-off-by: Kirill Tkhai <ktkhai@parallels.com>
    Acked-by: Vladimir Davydov <vdavydov@parallels.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -9116,6 +9116,11 @@ static void cpu_cgroup_css_offline(struct cgroup *cgrp)
 	sched_offline_group(tg);
 }
 
+static void cpu_cgroup_fork(struct task_struct *task, void *private)
+{
+	sched_move_task(task);
+}
+
 static int cpu_cgroup_can_attach(struct cgroup *cgrp,
 				 struct cgroup_taskset *tset)
 {
@@ -9869,6 +9874,7 @@ struct cgroup_subsys cpu_cgroup_subsys = {
 	.css_free	= cpu_cgroup_css_free,
 	.css_online	= cpu_cgroup_css_online,
 	.css_offline	= cpu_cgroup_css_offline,
+	.fork		= cpu_cgroup_fork,
 	.can_attach	= cpu_cgroup_can_attach,
 	.attach		= cpu_cgroup_attach,
 	.exit		= cpu_cgroup_exit,
