0da256068817f6d3eb62379717d2cc3b8809a1c9
Li Zefan <lizefan@huawei.com>
2015-01-26 16:55:25 +0400

    ms/memcg: don't use mem_cgroup_get() when creating a kmemcg cache
    Use css_get()/css_put() instead of mem_cgroup_get()/mem_cgroup_put().
    
    There are two things being done in the current code:
    
    First, we acquired a css_ref to make sure that the underlying cgroup
    would not go away.  That is a short lived reference, and it is put as
    soon as the cache is created.
    
    At this point, we acquire a long-lived per-cache memcg reference count
    to guarantee that the memcg will still be alive.
    
    so it is:
    
      enqueue: css_get
      create : memcg_get, css_put
      destroy: memcg_put
    
    So we only need to get rid of the memcg_get, change the memcg_put to
    css_put, and get rid of the now extra css_put.
    
    (This changelog is mostly written by Glauber)
    
    Signed-off-by: Li Zefan <lizefan@huawei.com>
    Acked-by: Michal Hocko <mhocko@suse.cz>
    Acked-by: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Glauber Costa <glommer@openvz.org>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 20f05310ba62d5816fb339d08effe78683137197)
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -3366,7 +3366,7 @@ void memcg_unregister_cache(struct kmem_cache *s)
 	list_del(&s->memcg_params->list);
 	mutex_unlock(&memcg->slab_caches_mutex);
 
-	mem_cgroup_put(memcg);
+	css_put(&memcg->css);
 }
 
 /*
@@ -3524,10 +3524,10 @@ static struct kmem_cache *memcg_create_kmem_cache(struct mem_cgroup *memcg,
 	new_cachep = kmem_cache_dup(memcg, cachep);
 	if (new_cachep == NULL) {
 		new_cachep = cachep;
+		css_put(&memcg->css);
 		goto out;
 	}
 
-	mem_cgroup_get(memcg);
 out:
 	mutex_unlock(&memcg_cache_mutex);
 	return new_cachep;
@@ -3608,8 +3608,6 @@ static void memcg_create_cache_work_func(struct work_struct *w)
 
 	cw = container_of(w, struct create_work, work);
 	memcg_create_kmem_cache(cw->memcg, cw->cachep);
-	/* Drop the reference gotten when we enqueued. */
-	css_put(&cw->memcg->css);
 	kfree(cw);
 }
 
