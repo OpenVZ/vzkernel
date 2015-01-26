9ceeee4096214430f58fda0678bf9385cfcb3c11
Vladimir Davydov <vdavydov@parallels.com>
2015-01-26 17:08:36 +0400

    ms/memcg, slab: merge memcg_{bind,release}_pages to memcg_{un}charge_slab
    Currently we have two pairs of kmemcg-related functions that are called on
    slab alloc/free.  The first is memcg_{bind,release}_pages that count the
    total number of pages allocated on a kmem cache.  The second is
    memcg_{un}charge_slab that {un}charge slab pages to kmemcg resource
    counter.  Let's just merge them to keep the code clean.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@suse.cz>
    Cc: Glauber Costa <glommer@gmail.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit c67a8a685a6e9abbaf0235e084168f15a721ae39)
    
    Conflicts:
    	mm/slab.c
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- mm/slab.c
+++ mm/slab.c
@@ -1777,7 +1777,6 @@ static void *kmem_getpages(struct kmem_cache *cachep, gfp_t flags, int nodeid)
 		if (page_is_pfmemalloc(page))
 			SetPageSlabPfmemalloc(page + i);
 	}
-	memcg_bind_pages(cachep, cachep->gfporder);
 
 	if (kmemcheck_enabled && !(cachep->flags & SLAB_NOTRACK)) {
 		kmemcheck_alloc_shadow(page, cachep->gfporder, flags, nodeid);
@@ -1815,7 +1814,6 @@ static void kmem_freepages(struct kmem_cache *cachep, void *addr)
 		page++;
 	}
 
-	memcg_release_pages(cachep, cachep->gfporder);
 	if (current->reclaim_state)
 		current->reclaim_state->reclaimed_slab += nr_freed;
 	free_pages((unsigned long)addr, cachep->gfporder);
