0169b7e476127491485b1604ff0f0bd63756845a
Vladimir Davydov <vdavydov@parallels.com>
2015-01-26 17:08:38 +0400

    ms/memcg: memcg_kmem_create_cache: make memcg_name_buf statically allocated
    It isn't worth complicating the code by allocating it on the first access,
    because it only takes 256 bytes.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    Cc: Michal Hocko <mhocko@suse.cz>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 93f39eea9c229778361ae7ecf5f5e95d291757da)
    
    Conflicts:
    	mm/memcontrol.c
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -3270,7 +3270,8 @@ void memcg_free_cache_params(struct kmem_cache *s)
 static void memcg_kmem_create_cache(struct mem_cgroup *memcg,
 				    struct kmem_cache *root_cache)
 {
-	static char *memcg_name_buf;	/* protected by memcg_slab_mutex */
+	static char memcg_name_buf[NAME_MAX + 1]; /* protected by
+						     memcg_slab_mutex */
 	struct kmem_cache *cachep;
 	int id;
 
@@ -3285,12 +3286,6 @@ static void memcg_kmem_create_cache(struct mem_cgroup *memcg,
 	 */
 	if (cache_from_memcg(root_cache, id))
 		return;
-
-	if (!memcg_name_buf) {
-		memcg_name_buf = kmalloc(NAME_MAX + 1, GFP_KERNEL);
-		if (!memcg_name_buf)
-			return;
-	}
 
 	rcu_read_lock();
 	strlcpy(memcg_name_buf, cgroup_name(memcg->css.cgroup), NAME_MAX + 1);
