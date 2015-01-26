e3b7f706c60589a159e033f29c48f6f40f8c5765
Glauber Costa <glommer@openvz.org>
2015-01-26 14:51:43 +0400

    ms/list_lru: dynamically adjust node arrays
    We currently use a compile-time constant to size the node array for the
    list_lru structure.  Due to this, we don't need to allocate any memory at
    initialization time.  But as a consequence, the structures that contain
    embedded list_lru lists can become way too big (the superblock for
    instance contains two of them).
    
    This patch aims at ameliorating this situation by dynamically allocating
    the node arrays with the firmware provided nr_node_ids.
    
    Signed-off-by: Glauber Costa <glommer@openvz.org>
    Cc: Dave Chinner <dchinner@redhat.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: "Theodore Ts'o" <tytso@mit.edu>
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    Cc: Arve Hjønnevåg <arve@android.com>
    Cc: Carlos Maiolino <cmaiolino@redhat.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Chuck Lever <chuck.lever@oracle.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Gleb Natapov <gleb@redhat.com>
    Cc: Greg Thelen <gthelen@google.com>
    Cc: J. Bruce Fields <bfields@redhat.com>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Jerome Glisse <jglisse@redhat.com>
    Cc: John Stultz <john.stultz@linaro.org>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: Kent Overstreet <koverstreet@google.com>
    Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Marcelo Tosatti <mtosatti@redhat.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Steven Whitehouse <swhiteho@redhat.com>
    Cc: Thomas Hellstrom <thellstrom@vmware.com>
    Cc: Trond Myklebust <Trond.Myklebust@netapp.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    (cherry picked from commit 5ca302c8e502ca53b7d75f12127ec0289904003a)
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    +++
    ms/xfs_qm: lost hunk from "ms/list_lru: dynamically adjust node arrays"
    
    Patch series description:
    
    https://jira.sw.ru/browse/PSBM-42319
    
    Andrew Morton (1):
      xfs-convert-dquot-cache-lru-to-list_lru-fix
    
    Dave Chinner (2):
      xfs: convert dquot cache lru to list_lru
      xfs: fix dquot isolation hang
    
    Vladimir Davydov (3):
      xfs_qm: lost hunk from "ms/list_lru: dynamically adjust node arrays"
      xfs_qm: lost hunk from "ms/list_lru: introduce
        list_lru_shrink_{count,walk}"
      xfs_qm: lost hunk from "ms/list_lru: add helpers to isolate items"
    
    ================================
    This patch description:
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- fs/super.c
+++ fs/super.c
@@ -202,8 +202,12 @@ static struct super_block *alloc_super(struct file_system_type *type, int flags,
 	spin_lock_init(&s->s_inode_list_lock);
 	INIT_LIST_HEAD(&s->s_inodes_wb);
 	spin_lock_init(&s->s_inode_wblist_lock);
-	list_lru_init(&s->s_dentry_lru);
-	list_lru_init(&s->s_inode_lru);
+
+	if (list_lru_init(&s->s_dentry_lru))
+		goto fail;
+	if (list_lru_init(&s->s_inode_lru))
+		goto err_out_dentry_lru;
+
 	init_rwsem(&s->s_umount);
 	lockdep_set_class(&s->s_umount, &type->s_umount_key);
 	/*
@@ -240,6 +244,9 @@ static struct super_block *alloc_super(struct file_system_type *type, int flags,
 	s->s_shrink.batch = 1024;
 	s->s_shrink.flags = SHRINKER_NUMA_AWARE;
 	return s;
+
+err_out_dentry_lru:
+	list_lru_destroy(&s->s_dentry_lru);
 fail:
 	destroy_super(s);
 	return NULL;
