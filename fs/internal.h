230d7e9a943cbba672476c2827ecf83211178c39
Dave Chinner <dchinner@redhat.com>
2015-01-26 14:13:40 +0400

    ms/shrinker: convert superblock shrinkers to new API
    Convert superblock shrinker to use the new count/scan API, and propagate
    the API changes through to the filesystem callouts.  The filesystem
    callouts already use a count/scan API, so it's just changing counters to
    longs to match the VM API.
    
    This requires the dentry and inode shrinker callouts to be converted to
    the count/scan API.  This is mainly a mechanical change.
    
    [glommer@openvz.org: use mult_frac for fractional proportions, build fixes]
    Signed-off-by: Dave Chinner <dchinner@redhat.com>
    Signed-off-by: Glauber Costa <glommer@openvz.org>
    Acked-by: Mel Gorman <mgorman@suse.de>
    Cc: "Theodore Ts'o" <tytso@mit.edu>
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Artem Bityutskiy <artem.bityutskiy@linux.intel.com>
    Cc: Arve Hjønnevåg <arve@android.com>
    Cc: Carlos Maiolino <cmaiolino@redhat.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Chuck Lever <chuck.lever@oracle.com>
    Cc: Daniel Vetter <daniel.vetter@ffwll.ch>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Gleb Natapov <gleb@redhat.com>
    Cc: Greg Thelen <gthelen@google.com>
    Cc: J. Bruce Fields <bfields@redhat.com>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Jerome Glisse <jglisse@redhat.com>
    Cc: John Stultz <john.stultz@linaro.org>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Cc: Kent Overstreet <koverstreet@google.com>
    Cc: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Marcelo Tosatti <mtosatti@redhat.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Steven Whitehouse <swhiteho@redhat.com>
    Cc: Thomas Hellstrom <thellstrom@vmware.com>
    Cc: Trond Myklebust <Trond.Myklebust@netapp.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    (cherry picked from commit 0a234c6dcb79a270803f5c9773ed650b78730962)
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    +++
    ms/fs/superblock: correct locking in super_cache_scan() and super_cache_count()
    
    Initially prune_super() does have grab_super_passive()/drop_super().
    Then prune_super() was splitted into super_cache_scan() and
    super_cache_count() by 0a234c6dcb79a270803f5c9773ed650b78730962
    ("shrinker: convert superblock shrinkers to new API")
    
    and finally grab_super_passive()/drop_super() were dropped from
    super_cache_count() by d23da150a37c9fe3cc83dbaf71b3e37fd434ed52
    ("fs/superblock: avoid locking counting inodes and dentries before reclaiming
    them").
    
    RedHat (3.10.0-327.3.1) dropped grab_super_passive()/drop_super() from initial
    prune_super() without splitting it into 2 functions.
    
    So need to fix our port of 0a234c6dcb79a270803f5c9773ed650b78730962
    ("shrinker: convert superblock shrinkers to new API")
    to have grab_super_passive()/drop_super() balanced.
    
    https://bugs.openvz.org/browse/OVZ-6646
    https://jira.sw.ru/browse/PSBM-42760
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- fs/internal.h
+++ fs/internal.h
@@ -109,6 +109,7 @@ extern int open_check_o_direct(struct file *f);
  * inode.c
  */
 extern spinlock_t inode_sb_list_lock;
+extern long prune_icache_sb(struct super_block *sb, unsigned long nr_to_scan);
 extern void inode_add_lru(struct inode *inode);
 
 /*
@@ -125,6 +126,7 @@ extern int invalidate_inodes(struct super_block *, bool);
  */
 extern struct dentry *__d_alloc(struct super_block *, const struct qstr *);
 extern int d_set_mounted(struct dentry *dentry);
+extern long prune_dcache_sb(struct super_block *sb, unsigned long nr_to_scan);
 
 /*
  * read_write.c
