acdd576313535a0eb4921623e8e2833bdfe52b4e
Dmitry Guryanov <dguryanov@parallels.com>
2015-01-27 18:52:51 +0400

    vznetstat: apply vznetstat patches from 2.6.32 as is
    vzcommit: 1f5023a971dc1aa550ef2cbdc91462f705e6a765
    https://jira.sw.ru/browse/PSBM-20094
    
    Signed-off-by: Dmitry Guryanov <dguryanov@parallels.com>
    Acked-by: Stanislav Kinsbursky <skinsbursky@parallels.com>
    
    Changes:
      * license set to "GPL v2"
      * copyright message added
    
    Signed-off-by: Konstantin Khorenko <khorenko@parallels.com>
    
    +++
    vznetstat: fix compilation
    
    This is a port of following vzcommits:
        vzcommit: 5b7840880d1f8255e0f6a4706632bd5e91b4f75c
    
        vznetstat: fix compilation
    
        This patch enables vznetstat module in Makefile and
        fixes some compilation errors. But modules still will
        not work correctly.
    
        https://jira.sw.ru/browse/PSBM-20094
    
        Signed-off-by: Dmitry Guryanov <dguryanov@parallels.com>
        Acked-by: Stanislav Kinsbursky <skinsbursky@parallels.com>
    
    =====
        vzcommit: 2ee8e739e0a7ebb2b8dc9ec00d38eabb0777b492
        (vznetstat part of it)
    
        vznetstat/vzredir: compilation fix warning: initialization from incompatible pointer type
    
        Fixed compilation warning found in x64, gcc 4.8.3
    
        Build log:
        /mnt/sources/linux-3.9-vzmodules/vznetstat/ip6_vznetstat.c:62:2: warning: initialization from incompatible pointer type [enabled by default]
          .hook  = venet_acct_in_hook_v6,
          ^
        /mnt/sources/linux-3.9-vzmodules/vznetstat/ip6_vznetstat.c:62:2: warning: (near initialization for ‘venet_acct_in_ops.hook’) [enabled by default]
        /mnt/sources/linux-3.9-vzmodules/vznetstat/ip6_vznetstat.c:70:2: warning: initialization from incompatible pointer type [enabled by default]
          .hook  = venet_acct_out_hook_v6,
    
        Signed-off-by: Andrey Smetanin <asmetanin@parallels.com>
        Acked-by: Vladimir Davydov <vdavydov@parallels.com>
    
        khorenko@:
        RHEL7 3.10.0-123.1.2.el7 kernel has modified struct nf_hook_ops,
        so need to modify our hooks accordingly.
    
    Signed-off-by: Konstantin Khorenko <khorenko@parallels.com>
    
    +++
    vznetstat: remove redirection remains
    
    Some checks left after we decided to remove
    redirection code, so drop them as well.
    
    https://jira.sw.ru/browse/PSBM-32912
    
    Signed-off-by: Konstantin Khorenko <khorenko@openvz.org>
    
    +++
    ve/net: drop sk_buff->accounted
    
    It is not used anywhere.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

---
+++ kernel/ve/vznetstat/ip_vznetstat.c
@@ -0,0 +1,258 @@
+/*
+ * kernel/ve/vznetstat/ip_vznetstat.c
+ *
+ * Copyright (c) 2004-2015 Parallels IP Holdings GmbH
+ *
+ */
+
+/*
+ * Networking statistics for IPv4.
+ */
+
+#include <linux/sched.h>
+#include <linux/skbuff.h>
+#include <linux/proc_fs.h>
+#include <linux/seq_file.h>
+#include <linux/slab.h>
+#include <linux/spinlock.h>
+#include <asm/uaccess.h>
+#include <net/ip.h>
+#include <linux/module.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+
+#include <linux/ve.h>
+#include <linux/ve_proto.h>
+#include <linux/venet.h>
+#include <linux/vznetstat.h>
+
+#define VZNS_DEBUG 0
+
+static unsigned int venet_acct_in_hook(const struct nf_hook_ops *hook,
+				       struct sk_buff *skb,
+				       const struct net_device *in,
+				       const struct net_device *out,
+				       const struct nf_hook_state *state)
+{
+	int res;
+
+	res = NF_ACCEPT;
+
+	/* Skip loopback dev */
+	if (in == dev_net(in)->loopback_dev)
+		goto out;
+
+#if VZNS_DEBUG
+	printk("%s: in %s, out %s, size %d, in->owner_env=%d\n",
+		 __FUNCTION__, in ? in->name : NULL, out ? out->name : NULL,
+		 venet_acct_skb_size(skb),
+		 in ? in->nd_net->owner_ve->veid : -1);
+#endif
+
+	/*
+	 * Basically, pskb_may_pull() isn't necessary here, because it's done
+	 * in ip_rcv() before calling NF_IP_PRE_ROUTING NF_HOOK, but let's
+	 * have some insurance for the future.
+	 */
+	if (unlikely(!pskb_may_pull(skb, sizeof(struct iphdr))))
+		goto out_hdr_error;
+
+	venet_acct_classify_add_incoming(in->nd_net->owner_ve->stat, skb);
+
+out:
+	return res;
+
+out_hdr_error:
+	if (net_ratelimit())
+		printk("%s: IN accounting: IP header error\n", in->name);
+	res = NF_DROP;
+	goto out;
+}
+
+static unsigned int venet_acct_out_hook(const struct nf_hook_ops *hook,
+				        struct sk_buff *skb,
+				        const struct net_device *in,
+				        const struct net_device *out,
+				        const struct nf_hook_state *state)
+{
+	int res;
+
+	res = NF_ACCEPT;
+
+	/* Skip loopback dev */
+	if (out == dev_net(out)->loopback_dev)
+		goto out;
+
+	/* Paranoia */
+	if (unlikely(!pskb_may_pull(skb, sizeof(struct iphdr))))
+		goto out_hdr_error;
+
+#if VZNS_DEBUG
+	printk("%s: in %s, out %s, size %d, out->owner_env=%d\n",
+		 __FUNCTION__, in ? in->name : NULL, out ? out->name : NULL,
+		 venet_acct_skb_size(skb), out ? out->nd_net->owner_ve->veid : -1);
+#endif
+
+	/*
+	 * Basically, kproxy uses for accounting kp_account_check_in()
+	 * for incoming in it packets and kp_account_check_out() for
+	 * outgoing from it ones for both directions, from VE and to VE.
+	 *
+	 * So, for outgoing from VE packets on kproxy entrance
+	 * kp_account_check_in() substracts packet from accounting, then
+	 * kp_account_check_out() adds it back. Thus, we can don't worry
+	 * abount double accounting here.
+	 *
+	 * All kproxy's accounting can't be moved in this module,
+	 * since traffic amount between kproxy and outside world is a bit
+	 * different from traffic amount between VE and kproxy.
+	 */
+	skb->protocol = __constant_htons(ETH_P_IP);
+	venet_acct_classify_add_outgoing(out->nd_net->owner_ve->stat, skb);
+
+out:
+	return res;
+
+out_hdr_error:
+	if (net_ratelimit())
+		printk("%s: OUT accounting: IP header error\n", out->name);
+	res = NF_DROP;
+	goto out;
+}
+
+static struct nf_hook_ops venet_acct_in_ops = {
+	.hook		= venet_acct_in_hook,
+	.owner		= THIS_MODULE,
+	.pf		= PF_INET,
+	.hooknum	= NF_INET_LOCAL_IN,
+	.priority	= NF_IP_PRI_FIRST,
+};
+
+static struct nf_hook_ops venet_acct_out_ops = {
+	.hook		= venet_acct_out_hook,
+	.owner		= THIS_MODULE,
+	.pf		= PF_INET,
+	.hooknum	= NF_INET_LOCAL_OUT,
+	.priority	= NF_IP_PRI_LAST,
+};
+
+static int init_venet_acct_ip_stat(void *data)
+{
+	struct ve_struct *env;
+
+	env = (struct ve_struct *)data;
+
+	if (env->stat)
+		return -EEXIST;
+
+	env->stat = venet_acct_find_create_stat(env->veid);
+	if (env->stat == NULL)
+		return -ENOMEM;
+
+	__module_get(THIS_MODULE);
+
+	return 0;
+}
+
+static void fini_venet_acct_ip_stat(void *data)
+{
+	struct ve_struct *env;
+
+	env = (struct ve_struct *)data;
+	if (env->stat) {
+		venet_acct_put_stat(env->stat);
+		env->stat = NULL;
+		module_put(THIS_MODULE);
+	}
+}
+
+static int venet_acct_register_ip_hooks(void)
+{
+	int ret;
+
+	 /* Register hooks */
+	ret = nf_register_hook(&venet_acct_in_ops);
+	if (ret < 0)
+		goto out_free_stat;
+
+	ret = nf_register_hook(&venet_acct_out_ops);
+	if (ret < 0)
+		goto out_err_out_hook;
+
+	return 0;
+
+out_err_out_hook:
+	nf_unregister_hook(&venet_acct_in_ops);
+out_free_stat:
+	return ret;
+}
+
+static void venet_acct_unregister_ip_hooks(void)
+{
+	nf_unregister_hook(&venet_acct_out_ops);
+	nf_unregister_hook(&venet_acct_in_ops);
+}
+
+static struct ve_hook venet_acct_hook = {
+	.init		= init_venet_acct_ip_stat,
+	.priority	= HOOK_PRIO_NET_PRE,
+	.owner		= THIS_MODULE,
+};
+
+static struct ve_hook venet_acct_cleanup_hook = {
+	.fini		= fini_venet_acct_ip_stat,
+	.priority	= HOOK_PRIO_NET_PRE,
+	.owner		= THIS_MODULE,
+};
+
+/* For ip6_vznetstat dependency */
+void ip_vznetstat_touch(void)
+{
+}
+EXPORT_SYMBOL(ip_vznetstat_touch);
+
+/*
+ * ---------------------------------------------------------------------------
+ * Initialization
+ * ---------------------------------------------------------------------------
+ */
+
+int __init ip_venetstat_init(void)
+{
+	struct ve_struct *env = get_ve0();
+	int ret;
+
+	env->stat = venet_acct_find_create_stat(env->veid);
+	if (env->stat == NULL)
+		return -ENOMEM;
+
+	ret = venet_acct_register_ip_hooks();
+	if (ret < 0) {
+		venet_acct_put_stat(env->stat);
+		env->stat = NULL;
+		return ret;
+	}
+
+	ve_hook_register(VE_SS_CHAIN, &venet_acct_hook);
+// TODO	ve_hook_register(VE_CLEANUP_CHAIN, &venet_acct_cleanup_hook);
+	return 0;
+}
+
+void __exit ip_venetstat_exit(void)
+{
+	struct ve_struct *env = get_ve0();
+
+	venet_acct_unregister_ip_hooks();
+	venet_acct_put_stat(env->stat);
+	env->stat = NULL;
+	ve_hook_unregister(&venet_acct_cleanup_hook);
+	ve_hook_unregister(&venet_acct_hook);
+}
+
+#if defined(MODULE) && defined(VZ_AUDIT)
+VZ_AUDIT;
+#endif
+module_init(ip_venetstat_init);
+module_exit(ip_venetstat_exit);
+
+MODULE_LICENSE("GPL v2");
