dd218b54ae927dc8893a40baacee0cc88a620b21
Dmitry Guryanov <dguryanov@parallels.com>
2015-01-27 20:16:31 +0400

    vzlist: add vzlist from 2.6.32-x as is
    This is a port of
        vzcommit: 159f190ae0e6f0328f76c520053d135a2a2ac302
    
        vzlist: add vzlist as is
    
        https://jira.sw.ru/browse/PSBM-20095
    
        Signed-off-by: Dmitry Guryanov <dguryanov@parallels.com>
        Acked-by: Andrew Vagin <avagin@parallels.com>
    
    Changes:
        * license set to "GPL v2"
        * vzlist_core.c is renamed to vzlist.c
        * removed monitor_init() and monitor_fini() calls
        * copyright message added
    
    Signed-off-by: Konstantin Khorenko <khorenko@parallels.com>
    
    +++
    vzlist: fix get_vepids function
    
    This is a port of
    vzcommit: 74b85e1e28e52a9d4caa859de19ad68a796cfab1
    
        vzlist: fix get_vepids function
    
        get_vepids ioctl allows to get list of pids inside container.
        It's used only in one place: when use stop container with
        vzctl stop --fast vzctl executes reboot inside contaier and
        waits until vzctl enter will not be possible. If it reaches timout -
        it gets pids with help of this ioctl and kills each process with
        kill syscall.
    
        You can't geet number of proceeses in pid namespace, so we start with
        buffer for 512 proceeses and it it's not enough - set to tasks + 512.
    
        https://jira.sw.ru/browse/PSBM-20095
    
        Signed-off-by: Dmitry Guryanov <dguryanov@parallels.com>
        Acked-by: Andrew Vagin <avagin@parallels.com>
    
    +++
    vzlist: return second pid in container's namespace
    
    This is a port of
    vzcommit: 76b0bccd9cc5fe9bf16f7c4eafc670973c26dbb6
    
        vzlist: return second pid in container's namespace
    
        Second pid in pid array entry was pid of a process in a
        container's namespace in rhel5 kernel, but then we changed
        it for some unknown reason.
    
        Let's return pid in container's namespace instead of that
        process's namespace.
    
        https://jira.sw.ru/browse/PSBM-20095
    
        Signed-off-by: Dmitry Guryanov <dguryanov@parallels.com>
        Acked-by: Andrew Vagin <avagin@parallels.com>
    
    +++
    ve: add config options for vzlist and vznetstat modules
    
    Port diff-vz-hookin-generic-3
    
    + CONFIG_VZ_LIST
    + CONFIG_VE_NETDEV_ACCOUNTING
    
    https://jira.sw.ru/browse/PSBM-19217
    
    Signed-off-by: Kirill Tkhai <ktkhai@odin.com>

---
+++ include/uapi/linux/vzlist.h
@@ -0,0 +1,46 @@
+/*
+ * include/linux/vzlist.h
+ *
+ * Copyright (c) 2015 Parallels IP Holdings GmbH
+ *
+ */
+
+#ifndef _UAPI_LINUX_VZLIST_H
+#define _UAPI_LINUX_VZLIST_H
+
+#include <linux/types.h>
+#include <linux/ioctl.h>
+
+#ifndef __KERNEL__
+#define __user
+#endif
+
+#ifndef __ENVID_T_DEFINED__
+#define __ENVID_T_DEFINED__
+typedef unsigned int envid_t;
+#endif
+
+struct vzlist_veidctl {
+	unsigned int	num;
+	envid_t	__user	*id;
+};
+
+struct vzlist_vepidctl {
+	envid_t		veid;
+	unsigned int	num;
+	pid_t __user	*pid;
+};
+
+struct vzlist_veipctl {
+	envid_t		veid;
+	unsigned int	num;
+	void __user	*ip;
+};
+
+#define VZLISTTYPE		'x'
+#define VZCTL_GET_VEIDS		_IOR(VZLISTTYPE, 1, struct vzlist_veidctl)
+#define VZCTL_GET_VEPIDS	_IOR(VZLISTTYPE, 2, struct vzlist_vepidctl)
+#define VZCTL_GET_VEIPS		_IOR(VZLISTTYPE, 3, struct vzlist_veipctl)
+#define VZCTL_GET_VEIP6S	_IOR(VZLISTTYPE, 4, struct vzlist_veipctl)
+
+#endif /* _UAPI_LINUX_VZLIST_H */
