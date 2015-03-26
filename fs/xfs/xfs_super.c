fe223b07ffbf3b78f0ec47f401bc8b197f7b0baf
Vladimir Davydov <vdavydov@parallels.com>
2015-03-26 14:10:18 +0400

    ms/fs: consolidate {nr,free}_cached_objects args in shrink_control
    Patchset description:
    
    With this patch set memcg/kmem accounting becomes more-or-less usable.
    On hitting memory.limit not only user pages, but also kmem (inodes,
    dentries) will be scanned and reclaimed. As before, to enable kmem
    accounting for a memory cgroup one has to write something to
    memory.kmem.limit. Note that hitting memory.kmem.limit itself won't
    trigger kmem reclaim, therefore there is not much point in setting it to
    anything less than memory.limit.
    
    https://jira.sw.ru/browse/PSBM-25018
    
    Christoph Lameter (1):
      slub: use sysfs'es release mechanism for kmem_cache
    
    Johannes Weiner (1):
      mm: vmscan: invoke slab shrinkers from shrink_zone()
    
    Joonsoo Kim (2):
      mm/slab_common: move kmem_cache definition to internal header
      slub: fix off by one in number of slab tests
    
    Vladimir Davydov (26):
      memcg: zap memcg_can_account_kmem
      slab: print slabinfo header in seq show
      memcg: use generic slab iterators for showing slabinfo
      memcg: fix possible use-after-free in memcg_kmem_get_cache()
      memcg: zap __memcg_{charge,uncharge}_slab
      memcg: zap memcg_name argument of memcg_create_kmem_cache
      slub: do not drop slab_mutex for sysfs_slab_add
      slub: fix memcg_propagate_slab_attrs
      memcg: zap memcg_slab_caches and memcg_slab_mutex
      list_lru: introduce list_lru_shrink_{count,walk}
      fs: consolidate {nr,free}_cached_objects args in shrink_control
      vmscan: per memory cgroup slab shrinkers
      memcg: rename some cache id related variables
      memcg: add rwsem to synchronize against memcg_caches arrays
        relocation
      list_lru: get rid of ->active_nodes
      list_lru: organize all list_lrus to list
      list_lru: introduce per-memcg lists
      fs: make shrinker memcg aware
      slab: embed memcg_cache_params to kmem_cache
      slab: link memcg caches of the same kind into a list
      slab: use css id for naming per memcg caches
      memcg: free memcg_caches slot on css offline
      list_lru: add helpers to isolate items
      memcg: reparent list_lrus and free kmemcg_id on css offline
      slub: never fail to shrink cache
      slub: make dead caches discard free slabs immediately
    
    Wanpeng Li (2):
      mm/slab: Sharing s_next and s_stop between slab and slub
      mm/slab: Give s_next and s_stop slab-specific names
    
    =============================================================
    This patch description:
    
    We are going to make FS shrinkers memcg-aware.  To achieve that, we will
    have to pass the memcg to scan to the nr_cached_objects and
    free_cached_objects VFS methods, which currently take only the NUMA node
    to scan.  Since the shrink_control structure already holds the node, and
    the memcg to scan will be added to it when we introduce memcg-aware
    vmscan, let us consolidate the methods' arguments in this structure to
    keep things clean.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    Suggested-by: Dave Chinner <david@fromorbit.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@suse.cz>
    Cc: Greg Thelen <gthelen@google.com>
    Cc: Glauber Costa <glommer@gmail.com>
    Cc: Alexander Viro <viro@zeniv.linux.org.uk>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit dea8188f9d56eb596afea36e37c591116061488b)
    
    Conflicts:
    	fs/super.c
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- fs/xfs/xfs_super.c
+++ fs/xfs/xfs_super.c
@@ -1697,7 +1697,7 @@ xfs_fs_mount(
 static long
 xfs_fs_nr_cached_objects(
 	struct super_block	*sb,
-	int			nid)
+	struct shrink_control	*sc)
 {
 	return xfs_reclaim_inodes_count(XFS_M(sb));
 }
@@ -1705,10 +1705,9 @@ xfs_fs_nr_cached_objects(
 static long
 xfs_fs_free_cached_objects(
 	struct super_block	*sb,
-	long			nr_to_scan,
-	int			nid)
+	struct shrink_control	*sc)
 {
-	return xfs_reclaim_inodes_nr(XFS_M(sb), nr_to_scan);
+	return xfs_reclaim_inodes_nr(XFS_M(sb), sc->nr_to_scan);
 }
 
 /* RHEL7-specific super operations for the writeback list */
