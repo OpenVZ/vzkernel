2d2da42248a82c568d1a64c7ed2703a0556abe9c
Vladimir Davydov <vdavydov@parallels.com>
2015-03-26 14:12:20 +0400

    ms/slab: link memcg caches of the same kind into a list
    Patchset description:
    
    With this patch set memcg/kmem accounting becomes more-or-less usable.
    On hitting memory.limit not only user pages, but also kmem (inodes,
    dentries) will be scanned and reclaimed. As before, to enable kmem
    accounting for a memory cgroup one has to write something to
    memory.kmem.limit. Note that hitting memory.kmem.limit itself won't
    trigger kmem reclaim, therefore there is not much point in setting it to
    anything less than memory.limit.
    
    https://jira.sw.ru/browse/PSBM-25018
    
    Christoph Lameter (1):
      slub: use sysfs'es release mechanism for kmem_cache
    
    Johannes Weiner (1):
      mm: vmscan: invoke slab shrinkers from shrink_zone()
    
    Joonsoo Kim (2):
      mm/slab_common: move kmem_cache definition to internal header
      slub: fix off by one in number of slab tests
    
    Vladimir Davydov (26):
      memcg: zap memcg_can_account_kmem
      slab: print slabinfo header in seq show
      memcg: use generic slab iterators for showing slabinfo
      memcg: fix possible use-after-free in memcg_kmem_get_cache()
      memcg: zap __memcg_{charge,uncharge}_slab
      memcg: zap memcg_name argument of memcg_create_kmem_cache
      slub: do not drop slab_mutex for sysfs_slab_add
      slub: fix memcg_propagate_slab_attrs
      memcg: zap memcg_slab_caches and memcg_slab_mutex
      list_lru: introduce list_lru_shrink_{count,walk}
      fs: consolidate {nr,free}_cached_objects args in shrink_control
      vmscan: per memory cgroup slab shrinkers
      memcg: rename some cache id related variables
      memcg: add rwsem to synchronize against memcg_caches arrays
        relocation
      list_lru: get rid of ->active_nodes
      list_lru: organize all list_lrus to list
      list_lru: introduce per-memcg lists
      fs: make shrinker memcg aware
      slab: embed memcg_cache_params to kmem_cache
      slab: link memcg caches of the same kind into a list
      slab: use css id for naming per memcg caches
      memcg: free memcg_caches slot on css offline
      list_lru: add helpers to isolate items
      memcg: reparent list_lrus and free kmemcg_id on css offline
      slub: never fail to shrink cache
      slub: make dead caches discard free slabs immediately
    
    Wanpeng Li (2):
      mm/slab: Sharing s_next and s_stop between slab and slub
      mm/slab: Give s_next and s_stop slab-specific names
    
    =============================================================
    This patch description:
    
    [ this patch has not been merged yet, but it is already in the mm tree ]
    
    Sometimes, we need to iterate over all memcg copies of a particular root
    kmem cache.  Currently, we use memcg_cache_params->memcg_caches array for
    that, because it contains all existing memcg caches.
    
    However, it's a bad practice to keep all caches, including those that
    belong to offline cgroups, in this array, because it will be growing
    beyond any bounds then.  I'm going to wipe away dead caches from it to
    save space.  To still be able to perform iterations over all memcg caches
    of the same kind, let us link them into a list.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@suse.cz>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Dave Chinner <david@fromorbit.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit 4d816536e50affa7f76ab58569c017709dba5412)
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- mm/slab.h
+++ mm/slab.h
@@ -158,6 +158,18 @@ void __kmem_cache_free_bulk(struct kmem_cache *, size_t, void **);
 int __kmem_cache_alloc_bulk(struct kmem_cache *, gfp_t, size_t, void **);
 
 #ifdef CONFIG_MEMCG_KMEM
+/*
+ * Iterate over all memcg caches of the given root cache. The caller must hold
+ * slab_mutex.
+ */
+#define for_each_memcg_cache(iter, root) \
+	list_for_each_entry(iter, &(root)->memcg_params.list, \
+			    memcg_params.list)
+
+#define for_each_memcg_cache_safe(iter, tmp, root) \
+	list_for_each_entry_safe(iter, tmp, &(root)->memcg_params.list, \
+				 memcg_params.list)
+
 static inline bool is_root_cache(struct kmem_cache *s)
 {
 	return s->memcg_params.is_root_cache;
@@ -236,6 +248,11 @@ static __always_inline void memcg_uncharge_slab(struct kmem_cache *s, int order)
 extern void slab_init_memcg_params(struct kmem_cache *);
 
 #else /* !CONFIG_MEMCG_KMEM */
+
+#define for_each_memcg_cache(iter, root) \
+	for ((void)(iter), (void)(root); 0; )
+#define for_each_memcg_cache_safe(iter, tmp, root) \
+	for ((void)(iter), (void)(tmp), (void)(root); 0; )
 
 static inline bool is_root_cache(struct kmem_cache *s)
 {
