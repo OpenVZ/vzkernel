631d8c74870f39dcfec6312c7478a6699c9cf055
Vladimir Davydov <vdavydov@parallels.com>
2015-03-27 16:38:02 +0400

    ms/memcg: port low limit
    This patch backports low limit for memory cgroups, which was introduced
    upstream by commit 241994ed8649 ("mm: memcontrol: default hierarchy
    interface for memory"):
    
      - memory.low configures the lower end of the cgroup's expected
        memory consumption range.  The kernel considers memory below that
        boundary to be a reserve - the minimum that the workload needs in
        order to make forward progress - and generally avoids reclaiming
        it, unless there is an imminent risk of entering an OOM situation.
    
    It brings it to the legacy hierarchy as "memory.low_limit_in_bytes".
    
    https://jira.sw.ru/browse/PSBM-31752
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    +++
    mm/memcontrol: remove unused memcg_get_hierarchical_limit()
    
    This is probably should be merged into initial patch.
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -79,6 +79,9 @@ struct scan_control {
 	/* Can pages be swapped as part of reclaim? */
 	int may_swap;
 
+	/* Can cgroups be reclaimed below their normal consumption range? */
+	int may_thrash;
+
 	int order;
 
 	/* Scan (total_size >> priority) pages at once */
@@ -2336,6 +2339,9 @@ static void shrink_zone(struct zone *zone, struct scan_control *sc,
 			unsigned long lru_pages, scanned;
 			struct lruvec *lruvec;
 
+			if (!sc->may_thrash && mem_cgroup_low(root, memcg))
+				continue;
+
 			lruvec = mem_cgroup_zone_lruvec(zone, memcg);
 			scanned = sc->nr_scanned;
 
@@ -2362,8 +2368,7 @@ static void shrink_zone(struct zone *zone, struct scan_control *sc,
 				mem_cgroup_iter_break(root, memcg);
 				break;
 			}
-			memcg = mem_cgroup_iter(root, memcg, &reclaim);
-		} while (memcg);
+		} while ((memcg = mem_cgroup_iter(root, memcg, &reclaim)));
 
 		/*
 		 * Shrink the slab caches in the same proportion that
@@ -2562,10 +2567,13 @@ static bool all_unreclaimable(struct zonelist *zonelist,
 static unsigned long do_try_to_free_pages(struct zonelist *zonelist,
 					  struct scan_control *sc)
 {
+	int initial_priority = sc->priority;
 	unsigned long total_scanned = 0;
 	unsigned long writeback_threshold;
 	bool aborted_reclaim;
 
+retry:
+	{KSTAT_PERF_ENTER(ttfp);
 	delayacct_freepages_start();
 
 	if (global_reclaim(sc))
@@ -2608,6 +2616,7 @@ static unsigned long do_try_to_free_pages(struct zonelist *zonelist,
 
 out:
 	delayacct_freepages_end();
+	KSTAT_PERF_LEAVE(ttfp);}
 
 	if (sc->nr_reclaimed)
 		return sc->nr_reclaimed;
@@ -2623,6 +2632,13 @@ out:
 	/* Aborted reclaim to try compaction? don't OOM, then */
 	if (aborted_reclaim)
 		return 1;
+
+	/* Untapped cgroup reserves?  Don't OOM, retry. */
+	if (!sc->may_thrash) {
+		sc->priority = initial_priority;
+		sc->may_thrash = 1;
+		goto retry;
+	}
 
 	/* top priority shrink_zones still had more to do? don't OOM, then */
 	if (global_reclaim(sc) && !all_unreclaimable(zonelist, sc))
