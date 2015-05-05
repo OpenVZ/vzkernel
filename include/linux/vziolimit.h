1381039634654bf6f5a23ee3f93ecfa2059f8847
Dmitry Monakhov <dmonakhov@openvz.org>
2015-05-05 13:44:35 +0400

    vziolimit: port diff-iolimit-vzctl-api
    iolimit: vzctl ioctl interface
    
    Implement iolimit control block allocation, add get and set ioctl.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    
    ====================================
    https://jira.sw.ru/browse/PSBM-20104
    
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>

--- include/linux/vziolimit.h
+++ include/linux/vziolimit.h
@@ -14,4 +14,14 @@
 
 #define VZIOLIMITTYPE 'I'
 
+struct iolimit_state {
+	unsigned int id;
+	unsigned int speed;
+	unsigned int burst;
+	unsigned int latency;
+};
+
+#define VZCTL_SET_IOLIMIT	_IOW(VZIOLIMITTYPE, 0, struct iolimit_state)
+#define VZCTL_GET_IOLIMIT	_IOR(VZIOLIMITTYPE, 1, struct iolimit_state)
+
 #endif /* _LINUX_VZIOLIMIT_H */
