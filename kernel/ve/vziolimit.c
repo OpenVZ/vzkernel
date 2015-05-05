40ce0a352e7c31938a1fef54325e683afb5e1630
Dmitry Monakhov <dmonakhov@openvz.org>
2015-05-05 13:44:34 +0400

    vziolimit: port diff-iolimit-plain-throttler
    iolimit: implement plain throttler
    
    Implement throttler logic.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    
    ====================================
    https://jira.sw.ru/browse/PSBM-20104
    
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>

--- kernel/ve/vziolimit.c
+++ kernel/ve/vziolimit.c
@@ -13,6 +13,78 @@
 #include <linux/vziolimit.h>
 #include <bc/beancounter.h>
 
+struct throttle {
+       unsigned speed;		/* maximum speed, units per second */
+       unsigned burst;		/* maximum bust, units */
+       unsigned latency;	/* maximum wait delay, jiffies */
+       unsigned state;		/* current state */
+       unsigned long time;	/* wall time in jiffies */
+};
+
+/**
+ * set throttler initial state, externally serialized
+ * @speed	maximum speed (1/sec)
+ * @burst	maximum burst chunk
+ * @latency	maximum timeout (ms)
+ */
+static void throttle_setup(struct throttle *th, unsigned speed,
+		unsigned burst, unsigned latency)
+{
+	th->time = jiffies;
+	th->burst = burst;
+	th->latency = msecs_to_jiffies(latency);
+	th->state = 0;
+	wmb();
+	th->speed = speed;
+}
+
+/* externally serialized */
+static void throttle_charge(struct throttle *th, unsigned charge)
+{
+	unsigned long now = jiffies;
+	u64 step;
+
+	if (!th->speed)
+		return;
+
+	if (time_before(th->time, now)) {
+		step = (u64)th->speed * (now - th->time);
+		do_div(step, HZ);
+		th->state = min((unsigned)step + th->state, charge + th->burst);
+		th->time = now;
+	}
+
+	if (charge > th->state) {
+		charge -= th->state;
+		step = (u64)charge * HZ;
+		if (do_div(step, th->speed))
+			step++;
+		th->time += step;
+		step *= th->speed;
+		do_div(step, HZ);
+		th->state = max_t(int, (int)step - charge, 0);
+	} else
+		th->state -= charge;
+
+	if (time_after(th->time, now + th->latency))
+		th->time = now + th->latency;
+}
+
+/* lockless */
+static unsigned long throttle_timeout(struct throttle *th, unsigned long now)
+{
+	unsigned long time;
+
+	if (!th->speed)
+		return 0;
+	rmb();
+	time = th->time;
+	if (time_before(time, now))
+		return 0;
+	return min(time - now, (unsigned long)th->latency);
+}
+
+
 static int iolimit_virtinfo(struct vnotifier_block *nb,
 		unsigned long cmd, void *arg, int old_ret)
 {
