7ca5833391ba0418d9ec066408f2173983c74a55
Dmitry Monakhov <dmonakhov@openvz.org>
2015-05-05 13:44:34 +0400

    vziolimit: port diff-iolimit-initial-skeleton-opensource
    https://jira.sw.ru/browse/PSBM-20104
    
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>

---
+++ include/linux/vziolimit.h
@@ -0,0 +1,17 @@
+/*
+ *  include/linux/vziolimit.h
+ *
+ *  Copyright (C) 2010, Parallels inc.
+ *  All rights reserved.
+ *
+ */
+
+#ifndef _LINUX_VZIOLIMIT_H
+#define _LINUX_VZIOLIMIT_H
+
+#include <linux/types.h>
+#include <linux/ioctl.h>
+
+#define VZIOLIMITTYPE 'I'
+
+#endif /* _LINUX_VZIOLIMIT_H */
