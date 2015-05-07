d4bff22dcda740f4e030adb89af982fd6129e6f0
Kir Kolyshkin <kir@openvz.org>
2015-05-07 20:28:10 +0400

    ve/sched: put move_task_groups() under CONFIG_CFS_CPULIMIT
    This was found while tring to compile the kernel with a stock
    config (i.e. no CONFIG_BEANCOUNTERS, CONFIG_VE etc.) and
    boot it on IBM Power8.
    
    =============================================================
    
    Function move_task_groups() is not defined if CONFIG_CFS_CPULIMIT
    is not set, so compiler complains.
    
    This is a fix to commit 51fd1aec7.
    
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6960,6 +6960,7 @@ more_balance:
 		double_rq_unlock(env.dst_rq, busiest);
 		local_irq_restore(flags);
 
+#ifdef CONFIG_CFS_CPULIMIT
 		if (!ld_moved && (env.flags & LBF_ALL_PINNED)) {
 			env.loop = 0;
 			local_irq_save(flags);
@@ -6968,6 +6969,7 @@ more_balance:
 			double_rq_unlock(env.dst_rq, busiest);
 			local_irq_restore(flags);
 		}
+#endif
 
 		/*
 		 * some other cpu did the load balance for us.
