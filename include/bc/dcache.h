b21981ab6eeebe17406212e0134d2aee538cb7bd
Kir Kolyshkin <kir@openvz.org>
2015-05-07 20:28:09 +0400

    bc/dcache: add parameter names to ub_dcache_reclaim() declaration for !CONFIG_BEANCOUNTERS
    This was found while tring to compile the kernel with a stock
    config (i.e. no CONFIG_BEANCOUNTERS, CONFIG_VE etc.) and
    boot it on IBM Power8.
    
    =============================================================
    
    In case CONFIG_BEANCOUNTERS is not defined, UB_DECLARE_VOID_FUNC
    macro is expanded into static inline function (rather than a
    function declaration), which requires parameter names.
    
    	Fixes the following error:
    	  CC      kernel/ve/vecalls.o
    	In file included from include/bc/dcache.h:4:0,
    			 from kernel/ve/vecalls.c:78:
    	include/bc/dcache.h: In function ‘ub_dcache_reclaim’:
    	include/bc/dcache.h:15:47: error: parameter name omitted
    	 UB_DECLARE_VOID_FUNC(ub_dcache_reclaim(struct user_beancounter *ub, unsigned long, unsigned long))
    						       ^
    	include/bc/decl.h:34:21: note: in definition of macro ‘UB_DECLARE_VOID_FUNC’
    	  static inline void decl   \
    			     ^
    	include/bc/dcache.h:15:47: error: parameter name omitted
    	 UB_DECLARE_VOID_FUNC(ub_dcache_reclaim(struct user_beancounter *ub, unsigned long, unsigned long))
    						       ^
    	include/bc/decl.h:34:21: note: in definition of macro ‘UB_DECLARE_VOID_FUNC’
    	  static inline void decl   \
    			     ^
    
    Signed-off-by: Kir Kolyshkin <kir@openvz.org>

--- include/bc/dcache.h
+++ include/bc/dcache.h
@@ -11,7 +11,7 @@ UB_DECLARE_VOID_FUNC(ub_dcache_set_owner(struct dentry *d, struct user_beancount
 UB_DECLARE_VOID_FUNC(ub_dcache_change_owner(struct dentry *dentry, struct user_beancounter *ub))
 UB_DECLARE_VOID_FUNC(ub_dcache_clear_owner(struct dentry *dentry))
 UB_DECLARE_VOID_FUNC(ub_dcache_unuse(struct user_beancounter *ub))
-UB_DECLARE_VOID_FUNC(ub_dcache_reclaim(struct user_beancounter *ub, unsigned long, unsigned long))
+UB_DECLARE_VOID_FUNC(ub_dcache_reclaim(struct user_beancounter *ub, unsigned long numerator, unsigned long denominator))
 UB_DECLARE_FUNC(int, ub_dcache_shrink(struct user_beancounter *ub, unsigned long size, gfp_t gfp_mask))
 UB_DECLARE_FUNC(unsigned long, ub_dcache_get_size(struct dentry *dentry))
 
