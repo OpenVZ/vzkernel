f365fd69413efb39ae1362e59f8539c65c6f8457
Vasily Averin <vvs@odin.com>
2015-05-18 12:29:57 +0400

    ve/netfilter: ve_printk for "nf_conntrack: table full"
    port of diff-ve-printk-conntrack-tables-full from rh6-based kernels
    
    "nf_conntrack: table full, dropping packet" message
    should be visible both in CT and on HN and
    should contain CTID for reading simplicity.
    
    https://bugzilla.openvz.org/show_bug.cgi?id=2940
    
    Signed-off-by: Vasily Averin <vvs@openvz.org>

--- net/netfilter/nf_conntrack_expect.c
+++ net/netfilter/nf_conntrack_expect.c
@@ -35,6 +35,8 @@
 #include <net/netfilter/nf_conntrack_tuple.h>
 #include <net/netfilter/nf_conntrack_zones.h>
 
+#include <linux/ve.h>
+
 unsigned int nf_ct_expect_hsize __read_mostly;
 EXPORT_SYMBOL_GPL(nf_ct_expect_hsize);
 
@@ -450,7 +452,9 @@ static inline int __nf_ct_expect_check(struct nf_conntrack_expect *expect)
 	}
 
 	if (net->ct.expect_count >= nf_ct_expect_max) {
-		net_warn_ratelimited("nf_conntrack: expectation table full\n");
+		net_veboth_ratelimited(KERN_WARNING "VE%s "
+					"nf_conntrack: expectation table full\n",
+					net->owner_ve->ve_name);
 		ret = -EMFILE;
 	}
 out:
