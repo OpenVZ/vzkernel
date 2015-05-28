b2f03d3d2bd33f90f9bd2d1ff9ba25960b193eb2
Vladimir Davydov <vdavydov@parallels.com>
2015-05-28 20:03:41 +0400

    memcg: count oom kills
    Patchset description:
    
    This patch set adds memory.oom_guarantee file to memory cgroup which
    allows to protect a memory cgroup from OOM killer. It works as follows:
    OOM killer first selects from processes in cgroups that are above their
    OOM guarantee, and only if there is no such it switches to scanning
    processes from all cgroups. This behavior is similar to UB_OOMGUARPAGES.
    
    It also adds OOM kills counter to each memory cgroup and synchronizes
    beancounters' UB_OOMGUARPAGES resource with oom_guarantee/oom_kill_cnt
    obtained from mem_cgroup.
    
    Related to https://jira.sw.ru/browse/PSBM-20089
    
    Vladimir Davydov (3):
      memcg: add oom_guarantee
      memcg: count oom kills
      memcg: sync UB_OOMGUARPAGES
    
    This patch description:
    
    Whenever a task is OOM-killed, increment OOM kill counter in the memory
    cgroup it belongs to and all its ascendants.
    
    Required by UBC API (UB_OOMGUARPAGES.failcnt).
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -488,6 +488,7 @@ void oom_kill_process(struct task_struct *p, gfp_t gfp_mask, int order,
 
 	/* mm cannot safely be dereferenced after task_unlock(victim) */
 	mm = victim->mm;
+	mem_cgroup_note_oom_kill(memcg, mm);
 	pr_err("Killed process %d (%s) total-vm:%lukB, anon-rss:%lukB, file-rss:%lukB\n",
 		task_pid_nr(victim), victim->comm, K(victim->mm->total_vm),
 		K(get_mm_counter(victim->mm, MM_ANONPAGES)),
