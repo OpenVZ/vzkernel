63f1f8a096d8c378119cb1b8bc7f1e5bf178acea
Vladimir Davydov <vdavydov@parallels.com>
2015-05-29 11:55:35 +0400

    ms/memcg: rename memory.low_limit_in_bytes to memory.low
    This is how it called upstream (in the unified hierarchy). Another
    reason for rename is that memory.low is not actually a limit - it is a
    low wss estimate.
    
    Since memory.low was added not long before and it is only used by vcmmd
    and we are haven't released Beta yet, such a rename seems to be
    acceptable. In order not to break anything I'll make vcmmd work fine on
    both kernels with memory.low and ones with memory.low_limit_in_bytes.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Reviewed-by: Kirill Tkhai <ktkhai@odin.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -6070,7 +6070,7 @@ static struct cftype mem_cgroup_files[] = {
 		.read = mem_cgroup_read,
 	},
 	{
-		.name = "low_limit_in_bytes",
+		.name = "low",
 		.write_string = mem_cgroup_low_write,
 		.read = mem_cgroup_low_read,
 	},
