e71ef4c12ea0233237ee9e891ce7e3ff89267f0a
Vladimir Davydov <vdavydov@parallels.com>
2015-05-29 11:55:46 +0400

    ms/memcg: port memory.high
    This patch backports memory.high knob, which was introduced upstream by
    commit 241994ed8649 ("mm: memcontrol: default hierarchy interface for
    memory"):
    
      - memory.high configures the upper end of the cgroup's expected
        memory consumption range.  A cgroup whose consumption grows beyond
        this threshold is forced into direct reclaim, to work off the
        excess and to throttle new allocations heavily, but is generally
        allowed to continue and the OOM killer is not invoked.
    
    It may come in handy to avoid ENOMEM when hitting memcg limit with
    GFP_NOWAIT allocations - with memory.high < memory.limit_in_bytes such a
    situation becomes less likely.
    
    This knob is supposed to be tuned by vcmmd.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Reviewed-by: Kirill Tkhai <ktkhai@odin.com>
    
    [VvS RHEL77 rebase]

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2856,6 +2856,7 @@ unsigned long mem_cgroup_shrink_node_zone(struct mem_cgroup *memcg,
 }
 
 unsigned long try_to_free_mem_cgroup_pages(struct mem_cgroup *memcg,
+					   unsigned long nr_pages,
 					   gfp_t gfp_mask,
 					   bool noswap)
 {
@@ -2866,7 +2867,7 @@ unsigned long try_to_free_mem_cgroup_pages(struct mem_cgroup *memcg,
 		.may_writepage = !laptop_mode,
 		.may_unmap = 1,
 		.may_swap = !noswap,
-		.nr_to_reclaim = SWAP_CLUSTER_MAX,
+		.nr_to_reclaim = max(nr_pages, SWAP_CLUSTER_MAX),
 		.order = 0,
 		.priority = DEF_PRIORITY,
 		.target_mem_cgroup = memcg,
