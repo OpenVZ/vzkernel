b07d0b7cfcd6cc3c8cf5383fbbeecac333e3e673
Vladimir Davydov <vdavydov@parallels.com>
2015-06-04 16:58:45 +0400

    sched: use cpuacct->cpustat for showing cpu stats
    In contrast to RH6, where tg->cpustat was used, now cpu stats are
    accounted in the cpuacct cgroup. So zap tg->cpustat and use
    cpuacct->cpustat for showing cpu.proc.stat instead. Fortunately cpu and
    cpuacct cgroups are always mounted together (even by systemd by
    default), so this will work.
    
    Related to https://jira.sw.ru/browse/PSBM-33642
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- kernel/sched/cpuacct.h
+++ kernel/sched/cpuacct.h
@@ -2,6 +2,7 @@
 
 extern void cpuacct_charge(struct task_struct *tsk, u64 cputime);
 extern void cpuacct_account_field(struct task_struct *p, int index, u64 val);
+extern struct kernel_cpustat *cpuacct_cpustat(struct cgroup *cgrp, int cpu);
 
 #else
 
