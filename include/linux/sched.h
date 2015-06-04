e42aba2829783423b30197171dbe8abc064a62ae
Vladimir Davydov <vdavydov@parallels.com>
2015-06-04 15:53:00 +0400

    sched/vziolimit: Port diff-fairsched-do-not-account-iothrottled-tasks-in-loadavg-core
    Author: Vladimir Davydov
    Email: vdavydov@parallels.com
    Subject: sched: do not account iothrottled tasks in loadavg
    Date: Mon, 22 Oct 2012 14:27:25 +0400
    
    Changes from v1:
     * do not account tasks throttled while doing reclaim too
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    Acked-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    =============================================================================
    
    Related to https://jira.sw.ru/browse/PSBM-33642
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -225,6 +225,8 @@ print_cfs_rq(struct seq_file *m, int cpu, struct cfs_rq *cfs_rq);
 #define TASK_PARKED		512
 #define TASK_STATE_MAX		1024
 
+#define __TASK_IOTHROTTLED	1024
+
 #define TASK_STATE_TO_CHAR_STR "RSDTtZXxKWP"
 
 extern char ___assert_task_state[1 - 2*!!(
@@ -252,6 +254,7 @@ extern char ___assert_task_state[1 - 2*!!(
 #define task_contributes_to_load(task)	\
 				((task->state & TASK_UNINTERRUPTIBLE) != 0 && \
 				 (task->flags & PF_FROZEN) == 0)
+#define task_iothrottled(task)	((task->state & __TASK_IOTHROTTLED) != 0)
 
 #define __set_task_state(tsk, state_value)		\
 	do { (tsk)->state = (state_value); } while (0)
@@ -1476,6 +1479,7 @@ struct task_struct {
 	unsigned sched_contributes_to_load:1;
 	RH_KABI_FILL_HOLE(unsigned sched_remote_wakeup:1)
 	unsigned sched_interruptible_sleep:1;
+	unsigned sched_iothrottled_sleep:1;
 	unsigned :0; /* force alignment to the next boundary */
 
 	/* unserialized, strictly 'current' */
