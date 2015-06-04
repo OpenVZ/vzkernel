938ad16188905210d79799f664c7d446f06518c4
Vladimir Davydov <vdavydov@parallels.com>
2015-06-04 15:53:00 +0400

    sched/vziolimit: Port diff-fairsched-do-not-account-iothrottled-tasks-in-loadavg-core
    Author: Vladimir Davydov
    Email: vdavydov@parallels.com
    Subject: sched: do not account iothrottled tasks in loadavg
    Date: Mon, 22 Oct 2012 14:27:25 +0400
    
    Changes from v1:
     * do not account tasks throttled while doing reclaim too
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    Acked-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    =============================================================================
    
    Related to https://jira.sw.ru/browse/PSBM-33642
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -888,6 +888,8 @@ void activate_task(struct rq *rq, struct task_struct *p, int flags)
 {
 	if (task_contributes_to_load(p)) {
 		rq->nr_uninterruptible--;
+		if (task_iothrottled(p))
+			rq->nr_iothrottled--;
 		task_cfs_rq(p)->nr_unint--;
 	}
 
@@ -908,6 +910,8 @@ void deactivate_task(struct rq *rq, struct task_struct *p, int flags)
 
 	if (task_contributes_to_load(p)) {
 		rq->nr_uninterruptible++;
+		if (task_iothrottled(p))
+			rq->nr_iothrottled++;
 		task_cfs_rq(p)->nr_unint++;
 	}
 
@@ -1585,6 +1589,8 @@ ttwu_do_activate(struct rq *rq, struct task_struct *p, int wake_flags)
 #ifdef CONFIG_SMP
 	if (p->sched_contributes_to_load) {
 		rq->nr_uninterruptible--;
+		if (task_iothrottled(p))
+			rq->nr_iothrottled--;
 		task_cfs_rq(p)->nr_unint--;
 	}
 #endif
@@ -2462,6 +2468,7 @@ static long calc_load_fold_active(struct rq *this_rq)
 
 	nr_active = this_rq->nr_running;
 	nr_active += (long) this_rq->nr_uninterruptible;
+	nr_active -= (long) this_rq->nr_iothrottled;
 
 	if (nr_active != this_rq->calc_load_active) {
 		delta = nr_active - this_rq->calc_load_active;
