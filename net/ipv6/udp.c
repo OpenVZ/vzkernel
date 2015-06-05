4f8f783d6325ff727a71494eb041cdb066893223
Pavel Emelyanov <xemul@parallels.com>
2015-06-05 23:55:49 +0400

    udp: Charge ingress buffers into cg memory
    Right now UDP outgoing traffic is kmem-auto-charged into cg
    kmem. Incoming traffic is not, but it has tcp-like memory
    scheduler (but simpler, with just one limit). So here's the
    per-cgroup UDP read buffers limiting in the same was as TCP
    is done.
    
    https://jira.sw.ru/browse/PSBM-33584
    
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- net/ipv6/udp.c
+++ net/ipv6/udp.c
@@ -52,6 +52,7 @@
 #include <linux/seq_file.h>
 #include <trace/events/skb.h>
 #include "udp_impl.h"
+#include <net/udp_memcontrol.h>
 
 static unsigned int udp6_ehashfn(struct net *net,
 				  const struct in6_addr *laddr,
@@ -1516,6 +1517,7 @@ struct proto udpv6_prot = {
 	.connect	   = ip6_datagram_connect,
 	.disconnect	   = udp_disconnect,
 	.ioctl		   = udp_ioctl,
+	.init		   = udp_init_sock,
 	.destroy	   = udpv6_destroy_sock,
 	.setsockopt	   = udpv6_setsockopt,
 	.getsockopt	   = udpv6_getsockopt,
@@ -1538,6 +1540,9 @@ struct proto udpv6_prot = {
 	.compat_getsockopt = compat_udpv6_getsockopt,
 #endif
 	.clear_sk	   = udp_v6_clear_sk,
+#ifdef CONFIG_MEMCG_KMEM
+	.proto_cgroup		= udp_proto_cgroup,
+#endif
 };
 
 static struct inet_protosw udpv6_protosw = {
