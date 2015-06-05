1e3ab4509be714d5e820591c0561d6fc114d87e9
Pavel Emelyanov <xemul@parallels.com>
2015-06-05 23:55:35 +0400

    tcp: Charge socket buffers into cg memory (v3)
    TCP code already has internal memory management for both -- in
    and out traffic. The outgoing packets are also already auto
    accounted into kmem (and into cg memory), incoming traffic is
    not accounted into kmem. And this management is already per-cg
    thanks to Glauber work some time ago.
    
    So TCP mm fix is -- take existing TCP mem accounting code and
    add/sub those numbers into cg memory. To avoid double accounting
    (via TCP hooks and via slub/buddy) the sk_allocation is set to
    be __GFP_NOACCOUNT.
    
    changes:
    * v2: renamed memcg_(unr)charge_kmem routines
    * v3: charge socket mem into kmem too
    
    https://jira.sw.ru/browse/PSBM-33584
    
    khorenko@ changes:
    - make memcg_uncharge_kmem() global, it's used outside of memcontrol.c
    
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -521,6 +521,9 @@ void __memcg_kmem_put_cache(struct kmem_cache *cachep);
 
 struct mem_cgroup *__mem_cgroup_from_kmem(void *ptr);
 
+void memcg_charge_kmem_nofail(struct mem_cgroup *memcg, u64 size);
+void memcg_uncharge_kmem(struct mem_cgroup *memcg, u64 size);
+
 /**
  * memcg_kmem_newpage_charge: verify if a new kmem allocation is allowed.
  * @gfp: the gfp allocation flags.
