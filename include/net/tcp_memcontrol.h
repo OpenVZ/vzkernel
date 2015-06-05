52a272b1f74c27570e2a2539aa936c693c5f3b45
Pavel Emelyanov <xemul@parallels.com>
2015-06-05 23:55:43 +0400

    tcp: Limit orphan sockets per-cg
    Kernel limits the total number of orphan TCP sockets in the
    system. One container can eat all this limit and make others'
    container to suffer from TCP state machine breakage.
    
    Thus here's the per-CT limit on the number of prphans that
    doesn't affect the others. The limit is set to be 1/4-th
    of the system limit. Should be OK for hosters loads.
    
    https://jira.sw.ru/browse/PSBM-33584
    
    khorenko@ changes:
    - exported cg_orphan_count_inc(): used in dccp module
    
    Signed-off-by: Pavel Emelyanov <xemul@parallels.com>

--- include/net/tcp_memcontrol.h
+++ include/net/tcp_memcontrol.h
@@ -6,8 +6,10 @@ struct tcp_memcontrol {
 	/* per-cgroup tcp memory pressure knobs */
 	struct res_counter tcp_memory_allocated;
 	struct percpu_counter tcp_sockets_allocated;
+	struct percpu_counter tcp_orphan_count;
 	/* those two are read-mostly, leave them at the end */
 	long tcp_prot_mem[3];
+	int tcp_max_orphans;
 	int tcp_memory_pressure;
 };
 
@@ -16,4 +18,8 @@ int tcp_init_cgroup(struct mem_cgroup *memcg, struct cgroup_subsys *ss);
 void tcp_destroy_cgroup(struct mem_cgroup *memcg);
 unsigned long long tcp_max_memory(const struct mem_cgroup *memcg);
 void tcp_prot_mem(struct mem_cgroup *memcg, long val, int idx);
+
+void cg_orphan_count_inc(struct sock *sk);
+void cg_orphan_count_dec(struct sock *sk);
+bool cg_too_many_orphans(struct sock *sk, int shift);
 #endif /* _TCP_MEMCG_H */
