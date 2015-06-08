0a4c5de866bf412acb8a11f695180e3b26855460
Andrew Vagin <avagin@openvz.org>
2015-06-08 20:24:00 +0400

    vz/veth: remove vzethdev
    We are going to use the upstream veth
    
    Signed-off-by: Andrew Vagin <avagin@openvz.org>
    Reviewed-by: Kirill Tkhai <ktkhai@odin.com>

--- include/uapi/linux/veth.h
+++ include/uapi/linux/veth.h
@@ -18,29 +18,4 @@ enum {
 #define VETH_INFO_MAX	(__VETH_INFO_MAX - 1)
 };
 
-#ifdef __KERNEL__
-struct veth_struct
-{
-	struct net_device_stats stats;
-	struct net_device	*me;
-	struct net_device	*pair;
-	struct list_head	hwaddr_list;
-	struct net_device_stats	*real_stats;
-	int			allow_mac_change;
-};
-
-#define veth_from_netdev(dev) \
-	((struct veth_struct *)(netdev_priv(dev)))
-static inline struct net_device * veth_to_netdev(struct veth_struct *veth)
-{
-	return veth->me;
-}
-#endif
-
-static inline struct net_device_stats *
-veth_stats(struct net_device *dev, int cpuid)
-{
-	return per_cpu_ptr(veth_from_netdev(dev)->real_stats, cpuid);
-}
-
 #endif
