08c60cbbdb9c2c1457fcdcebac143a9413770e96
Dmitry Monakhov <dmonakhov@openvz.org>
2015-06-09 12:59:39 +0400

    pfcache: fix path refcounting hazzard
    peer's path management was broken during porting from rhel6 to rhel7
    
    - open_mapping_peer no longer responsible for path drop on error.
    - fix refcounting during race peer_open/peer_close
    
    #TESTCASE:
    VEID=101
    mkdir /tmp/pfcache
    mount -ttmpfs none /tmp/pfcache
    vzctl create $VEID
    vzctl mount $VEID
    dd if=/dev/zero of=/vz/root/$VEID/bin/f1 bs=4k count=1
    cp /vz/root/$VEID/bin/f1 /pfcache/1c/eaf73df40e531df3bfb26b4fb7cd95fb7bff1d
    vzctl umount $VEID
    mount /vz/root/$VEID -oremount,balloon_ino=12,pfcache_csum,pfcache=/tmp/pfcache
    stat /vz/root/$VEID/bin/f1 | echo FAIL
    vzctl umount $VEID
    umount /tmp/pfcache || echo FAIL
    
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>
    Acked-by: Andrew Vagin <avagin@odin.com>
    Reported-by: Oleksiy Shchukin <oshchukin@cloudlinux.com>

--- mm/memory.c
+++ mm/memory.c
@@ -4562,7 +4562,6 @@ restart:
 	if (!peer->i_peer_file) {
 		file = dentry_open(path, O_RDONLY | O_LARGEFILE, cred);
 		if (IS_ERR(file)) {
-			path_put(path);
 			return PTR_ERR(file);
 		}
 
@@ -4579,8 +4578,6 @@ restart:
 		}
 		if (peer->i_peer_file) {
 			spin_unlock(&inode->i_lock);
-			*path = file->f_path;
-			path_get(path);
 			fput(file);
 			file = NULL;
 			goto restart;
@@ -4606,9 +4603,7 @@ restart:
 	if (file) {
 		file_accessed(file);
 		fput(file);
-	} else
-		path_put(path);
-
+	}
 	return 0;
 }
 EXPORT_SYMBOL(open_mapping_peer);
