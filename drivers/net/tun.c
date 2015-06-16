6c783901d6a0a474c8885a6b5c5a2bca81095ff7
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2015-06-16 16:07:02 +0400

    ve/net/tun: don't loose NETIF_F_VIRTUAL device feature
    move setting NETIF_F_VIRTUAL from tun_setup to tun_set_iff
    where other features are set, as tun_setup is only called in
    tun_set_iff
    
    https://jira.sw.ru/browse/PSBM-34066
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Acked-by: Kirill Tkhai <ktkhai@odin.com>

--- drivers/net/tun.c
+++ drivers/net/tun.c
@@ -1554,7 +1554,6 @@ static void tun_setup(struct net_device *dev)
 
 	dev->ethtool_ops = &tun_ethtool_ops;
 	dev->destructor = tun_free_netdev;
-	dev->features |= NETIF_F_VIRTUAL;
 }
 
 /* Trivial set of netlink ops to allow deleting tun or tap
@@ -1794,7 +1793,7 @@ static int tun_set_iff(struct net *net, struct file *file, struct ifreq *ifr)
 		dev->hw_features = NETIF_F_SG | NETIF_F_FRAGLIST |
 				   TUN_USER_FEATURES | NETIF_F_HW_VLAN_CTAG_TX |
 				   NETIF_F_HW_VLAN_STAG_TX;
-		dev->features = dev->hw_features;
+		dev->features = dev->hw_features | NETIF_F_VIRTUAL;
 		dev->vlan_features = dev->features &
 				     ~(NETIF_F_HW_VLAN_CTAG_TX |
 				       NETIF_F_HW_VLAN_STAG_TX);
