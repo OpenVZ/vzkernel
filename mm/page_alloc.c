170e40613d2e35286d154af28bce8a1048fdb8d8
Konstantin Khorenko <khorenko@virtuozzo.com>
2015-06-16 20:32:51 +0400

    Revert "mm/fs: Port diff-mm-debug-memallocation-caused-fs-reentrance"
    This reverts commit 255427905323ac97a3c9b2d5acb2bf21ea2b31f6.
    
    The debug in question works fine and we know about the problem,
    and it will be fixed once we backport ms commit
    4fdb5543183d027a19805b72025b859af73d0863.
    
    Until then we revert the debug in order not to taint kernel and
    let tests run.
    
    We'll get the debug back once we fix the issue
    https://jira.sw.ru/browse/PSBM-34112
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2708,7 +2708,6 @@ __alloc_pages_nodemask(gfp_t gfp_mask, unsigned int order,
 	lockdep_trace_alloc(gfp_mask);
 
 	might_sleep_if(gfp_mask & __GFP_WAIT);
-	WARN_ON((gfp_mask & __GFP_FS) && current->journal_info);
 
 	if (should_fail_alloc_page(gfp_mask, order))
 		return NULL;
