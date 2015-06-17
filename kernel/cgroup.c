60a01f1df6f04133270406d36989224881be3593
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2015-06-17 14:40:14 +0400

    ve/cgroup: Temporarily allow to mount cgroups from VE
    When restore container criu prepares that named cgroups yard
    where it mount cgroups by its own. Strictly speaking we need
    "restoring" state for VE when we would allow to mount cgroups
    from inside of VE context until restore is complete. But while
    we have not yet implemented such state lets allow to mount
    cgroups to not block conatiners c/r testing.
    
    khorenko@:
    
     | The CRIU issue is to handled either via "restoring" state or
     | (which i like much more) to create venet directly via netlink.
    
    We'll disable this back in the scope of
    https://jira.sw.ru/browse/PSBM-34291
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    
    CC: Andrey Vagin <avagin@virtuozzo.com>
    CC: Vladimir Davydov <vdavydov@virtuozzo.com>
    CC: Konstantin Khorenko <khorenko@virtuozzo.com>
    CC: Pavel Emelyanov <xemul@virtuozzo.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -1587,8 +1587,17 @@ static struct dentry *cgroup_mount(struct file_system_type *fs_type,
 	struct inode *inode;
 
 #ifdef CONFIG_VE
-	if (!ve_is_super(get_exec_env()) && !(flags & MS_KERNMOUNT))
-		return ERR_PTR(-EACCES);
+	if (!ve_is_super(get_exec_env()) && !(flags & MS_KERNMOUNT)) {
+		/*
+		 * We should allow mounting cgroups from inside of
+		 * VE only when VE inside a special "restoring" state.
+		 * At moment we don't have yet this state implemented
+		 * but to not block the container from the restore
+		 * lets allow this temporarily.
+		 */
+		/* return ERR_PTR(-EACCES); */
+		pr_warn_once("FIXME: Mounting cgroups from inside of VE, restore?");
+	}
 #endif
 
 	/* First find the desired set of subsystems */
