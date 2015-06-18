e319563b71b9278a7b3da639020973724b2ec824
Vladimir Davydov <vdavydov@parallels.com>
2015-06-18 20:09:36 +0400

    Revert "ms/nmi: do not call unblank_screen() from nmi_show_regs()"
    This reverts commit db379bc41b62a568953ed544d2aa5d7f3d0f87f9.
    
    Not needed, because d0f4641de70c7 ("x86/NMI: introduce dumps via nmi"),
    which this patch fixes, will be dropped.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- kernel/printk.c
+++ kernel/printk.c
@@ -3163,7 +3163,7 @@ static void busted_show_regs(struct pt_regs *regs, int in_nmi)
 	bust_spinlocks(1);
 	printk("----------- IPI show regs -----------\n");
 	show_regs(regs);
-	bust_spinlocks(-1);
+	bust_spinlocks(0);
 }
 
 void nmi_show_regs(struct pt_regs *regs, int in_nmi)
@@ -3183,7 +3183,7 @@ void nmi_show_regs(struct pt_regs *regs, int in_nmi)
 		for_each_cpu_mask(cpu, nmi_show_regs_cpus)
 			printk("%d ", cpu);
 		printk("\n");
-		bust_spinlocks(-1);
+		bust_spinlocks(0);
 	}
 
 doit:
