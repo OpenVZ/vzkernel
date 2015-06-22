d7f53fb77a6fe22a6383a530e2f4bf12b1cc486b
Dmitry Monakhov <dmonakhov@openvz.org>
2015-06-22 14:38:38 +0400

    ext4: Add ioctl EXT4_IOC_SET_RSV_BLOCKS V2
    We have to update reservation block counts according to balloon size.
    One can do that directly via tune2fs which is not good for mounted fs,
    and should use this ioctl instead.
    
    changes from v1:
    - add missed ext4_handle_dirty_metadata
    - change error code
    
    https://jira.sw.ru/browse/PSBM-25929
    
    Orig-patch:diff-ext4-add-ioctl-EXT4_IOC_SET_RSV_BLOCKS-v2
    
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -621,6 +621,7 @@ enum {
 #define EXT4_IOC_SWAP_BOOT		_IO('f', 17)
 #define EXT4_IOC_OPEN_BALLOON		_IO('f', 42)
 #define EXT4_IOC_MFSYNC			_IO('f', 43)
+#define EXT4_IOC_SET_RSV_BLOCKS		_IOW('f', 44, __u64)
 
 #if defined(__KERNEL__) && defined(CONFIG_COMPAT)
 /*
