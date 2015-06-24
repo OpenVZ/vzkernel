54091045a3c66c43f98fec4a62ce308c235bd04b
Dmitry Monakhov <dmonakhov@openvz.org>
2015-06-24 14:03:00 +0400

    ext4: fallocate mode - convert and extend
    Original patch rh6/diff-ext4-fallocate-mode-convert-and-extend-v3
    
    The patch introduces new fallocate mode: FALLOC_FL_CONVERT_AND_EXTEND. It
    performs two actions:
     - convert all uninitialized extends in the range <offset, offset + length>
     - set i_size to "offset + length".
    
    The feature will be used by ploop io_direct module for optimizing submit_alloc
    path.
    
    Changed in v2 (thanks to Dima for findings):
     - moved journal start/stop into while(){...}
     - added update_fsync_trans call
    
    Changed in v3 (thanks again to Dima for findings):
     - protected operations on extnet tree by i_data_sem
    
    https://jira.sw.ru/browse/PSBM-22381
    
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>
    
    +++
    Rebase to rh7 kernel-3.10.0-229.7.2.el7:
    
    * ext4_ext_find_extent() call: zero flags added
    * ext4_ext_is_uninitialized() -> ext4_ext_is_unwritten() because rh
      backported ms 556615dcbf38b0a92a9e659f52c06686270dfc16:
        ext4: rename uninitialized extents to unwritten

--- include/uapi/linux/falloc.h
+++ include/uapi/linux/falloc.h
@@ -4,6 +4,9 @@
 #define FALLOC_FL_KEEP_SIZE	0x01 /* default is extend size */
 #define FALLOC_FL_PUNCH_HOLE	0x02 /* de-allocates range */
 #define FALLOC_FL_NO_HIDE_STALE	0x04 /* reserved codepoint */
+#define FALLOC_FL_CONVERT_AND_EXTEND 0x100 /* mark extents as initialized
+					    * and extend i_size */
+
 
 /*
  * FALLOC_FL_COLLAPSE_RANGE is used to remove a range of a file
