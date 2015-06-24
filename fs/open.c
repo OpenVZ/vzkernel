f515e73bdd4ecddea7d89e94647f1101c8796a33
Dmitry Monakhov <dmonakhov@openvz.org>
2015-06-24 14:03:00 +0400

    ext4: fallocate mode - convert and extend
    Original patch rh6/diff-ext4-fallocate-mode-convert-and-extend-v3
    
    The patch introduces new fallocate mode: FALLOC_FL_CONVERT_AND_EXTEND. It
    performs two actions:
     - convert all uninitialized extends in the range <offset, offset + length>
     - set i_size to "offset + length".
    
    The feature will be used by ploop io_direct module for optimizing submit_alloc
    path.
    
    Changed in v2 (thanks to Dima for findings):
     - moved journal start/stop into while(){...}
     - added update_fsync_trans call
    
    Changed in v3 (thanks again to Dima for findings):
     - protected operations on extnet tree by i_data_sem
    
    https://jira.sw.ru/browse/PSBM-22381
    
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>
    
    +++
    Merged rework:
    
    The patch reworks the feature and renames it to FALLOC_FL_CONVERT_UNWRITTEN.
    
    Intended use:
    
    0) pos := i_size_read(inode); len := 1MB; prealloc := 100MB
    
    1) fallocate(file, 0, pos, prealloc)
    2) fiemap(inode, &fieinfo, pos, 1)
    3) dio pwrite(block_device, data, len, fi_extent.fe_physical)
    4) fallocate(file, FALLOC_FL_CONVERT_UNWRITTEN, pos, len)
    
    ===
    This patch description:
    
    Since Oct 9 2014 (commit 671ddaaab74a8495a549fccae49eb4305c11e5d2), ploop has
    not used FALLOC_FL_KEEP_SIZE. This means that the part of
    fallocate(FALLOC_FL_CONVERT_AND_EXTEND) extending i_size is redundant now.
    Hence, we can use existing (and well tested) ext4_convert_unwritten_extents()
    instead of our home-made (buggy) algorithm.
    
    +++
    Rebase to rh7 kernel-3.10.0-229.7.2.el7:
    
    * ext4_ext_find_extent() call: zero flags added
    * ext4_ext_is_uninitialized() -> ext4_ext_is_unwritten() because rh
      backported ms 556615dcbf38b0a92a9e659f52c06686270dfc16:
        ext4: rename uninitialized extents to unwritten
    Rebase: ktkhai@: Merged "ext4: rework fallocate(FALLOC_FL_CONVERT_AND_EXTEND)"

--- fs/open.c
+++ fs/open.c
@@ -246,7 +246,8 @@ int vfs_fallocate(struct file *file, int mode, loff_t offset, loff_t len)
 
 	/* Return error if mode is not supported */
 	if (mode & ~(FALLOC_FL_KEEP_SIZE | FALLOC_FL_PUNCH_HOLE |
-		     FALLOC_FL_COLLAPSE_RANGE | FALLOC_FL_ZERO_RANGE))
+		     FALLOC_FL_COLLAPSE_RANGE | FALLOC_FL_ZERO_RANGE |
+		     FALLOC_FL_CONVERT_UNWRITTEN))
 		return -EOPNOTSUPP;
 
 	/* Punch hole and zero range are mutually exclusive */
@@ -262,6 +263,11 @@ int vfs_fallocate(struct file *file, int mode, loff_t offset, loff_t len)
 	/* Collapse range should only be used exclusively. */
 	if ((mode & FALLOC_FL_COLLAPSE_RANGE) &&
 	    (mode & ~FALLOC_FL_COLLAPSE_RANGE))
+		return -EINVAL;
+
+	/* Convert-and-extend should only be used exclusively. */
+	if ((mode & FALLOC_FL_CONVERT_UNWRITTEN) &&
+	    (mode & ~FALLOC_FL_CONVERT_UNWRITTEN))
 		return -EINVAL;
 
 	if (!(file->f_mode & FMODE_WRITE))
