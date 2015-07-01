c35b353984db73bc135b83be446602e97482e490
Miklos Szeredi <mszeredi@suse.cz>
2015-07-01 16:26:08 +0200

    ms/fuse: introduce per-instance fuse_dev structure
    ML commit cc080e9e9be1
    
    Allow fuse device clones to refer to be distinguished.  This patch just
    adds the infrastructure by associating a separate "struct fuse_dev" with
    each clone.
    
    Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    Reviewed-by: Ashish Samant <ashish.samant@oracle.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -407,6 +407,17 @@ struct fuse_pqueue {
 };
 
 /**
+ * Fuse device instance
+ */
+struct fuse_dev {
+	/** Fuse connection for this device */
+	struct fuse_conn *fc;
+
+	/** list entry on fc->devices */
+	struct list_head entry;
+};
+
+/**
  * A Fuse connection.
  *
  * This structure is created, when the filesystem is mounted, and is
@@ -433,6 +444,9 @@ struct fuse_conn {
 
 	/** The pid namespace for this mount */
 	struct pid_namespace *pid_ns;
+
+	/** The user namespace for this mount */
+	struct user_namespace *user_ns;
 
 	/** Maximum read size */
 	unsigned max_read;
@@ -625,8 +639,8 @@ struct fuse_conn {
 	/** Read/write semaphore to hold when accessing sb. */
 	struct rw_semaphore killsb;
 
-	/** The user namespace for this mount */
-	struct user_namespace *user_ns;
+	/** List of device instances belonging to this connection */
+	struct list_head devices;
 };
 
 static inline struct fuse_conn *get_fuse_conn_super(struct super_block *sb)
@@ -842,6 +856,9 @@ void fuse_conn_init(struct fuse_conn *fc, struct user_namespace *user_ns);
  * Release reference to fuse_conn
  */
 void fuse_conn_put(struct fuse_conn *fc);
+
+struct fuse_dev *fuse_dev_alloc(struct fuse_conn *fc);
+void fuse_dev_free(struct fuse_dev *fud);
 
 /**
  * Add connection to control filesystem
