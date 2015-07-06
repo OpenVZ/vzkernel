e1bbf5d1c3a6d58e31d5f941b987dbf0b1bf97d8
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2015-07-06 17:27:54 +0400

    ve/proc/ipc: allow mount proc and mqueue inside container
    Patchset description:
    
    capability fixes for docker
    
    allow what docker wants, need it to run integration-cli tests
    when we will prohibit CAP_SYS_ADMIN and CAP_NET_ADMIN in CT
    
    * after switching to user namespaces we won't need those patches
      which is planned in the scope of
      https://jira.sw.ru/browse/PSBM-33304
    
    to test without CAP_SYS_ADMIN and CAP_NET_ADMIN:
    vzctl set 206 --capability net_admin:off \
    	--capability sys_admin:off --save
    
    Pavel Tikhomirov (3):
      vfs: allow mount/umount, pivot_root with CAP_VE_SYS_ADMIN
      rtnl: allow move network devices into network namespace in CT
      vfs: allow mount proc and mqueue inside container
    
    ################################################################
    This patch description:
    
    need this for docker, as it tries to mount proc and mqueue
    and as we will not allow CAP_SYS_ADMIN in container.
    
    * after switching to user namespaces we won't need this patch
    https://jira.sw.ru/browse/PSBM-34523
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    It also allows to start container if CAP_SYS_ADMIN is unset with vzctl
    else we get error:
    	vzctl set 206 --capability sys_admin:off --save
    	vzctl start 206
    	Starting Container ...
    	Unmount image: /vz/private/206/root.hdd
    	Unmounting file system at /vz/root/206
    	Unmounting device /dev/ploop28512
    	Container is unmounted
    	Mount image: /vz/private/206/root.hdd
    	Opening delta /vz/private/206/root.hdd/root.hds
    	Adding delta dev=/dev/ploop28512 img=/vz/private/206/root.hdd/root.hds (rw)
    	Mounting /dev/ploop28512p1 at /vz/root/206 fstype=ext4
    	data='balloon_ino=12,pfcache_csum,,'
    	Container is mounted
    	Os release: 3.10.0-123.1.2.vz7.5.23
    	Failed to mount /proc: Operation not permitted
    	Unmount image: /vz/private/206/root.hdd
    	Unmounting file system at /vz/root/206
    	Unmounting device /dev/ploop28512
    	Container is unmounted
    	Failed to start the Container

--- fs/proc/root.c
+++ fs/proc/root.c
@@ -121,7 +121,8 @@ static struct dentry *proc_mount(struct file_system_type *fs_type,
 		options = data;
 
 		if (!current_user_ns()->may_mount_proc ||
-		    !ns_capable(ns->user_ns, CAP_SYS_ADMIN))
+		    (!ns_capable(ns->user_ns, CAP_SYS_ADMIN) &&
+		     !ns_capable(ns->user_ns, CAP_VE_SYS_ADMIN)))
 			return ERR_PTR(-EPERM);
 	}
 
