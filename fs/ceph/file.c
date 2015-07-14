56e06f0ec5adba904dca03eb881725b43fb14f1e
Konstantin Khorenko <khorenko@virtuozzo.com>
2015-07-14 19:09:20 +0400

    fs/ceph: honor kernel direct aio changes
    After commit 947f3e89a003d8f1ee58c438e3a8eb8125c38efd
    struct iov_iter has been changed,
    ceph fs should work with iov_iter accordingly.
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/ceph/file.c
+++ fs/ceph/file.c
@@ -423,8 +423,9 @@ static ssize_t ceph_sync_read(struct kiocb *iocb, struct iov_iter *i,
 
 	if (file->f_flags & O_DIRECT) {
 		while (iov_iter_count(i)) {
-			void __user *data = i->iov[0].iov_base + i->iov_offset;
-			size_t len = i->iov[0].iov_len - i->iov_offset;
+			struct iovec *iov = (struct iovec *)i->data;
+			void __user *data = iov->iov_base + i->iov_offset;
+			size_t len = iov->iov_len - i->iov_offset;
 
 			num_pages = calc_pages_for((unsigned long)data, len);
 			pages = ceph_get_direct_page_vector(data,
@@ -456,10 +457,10 @@ static ssize_t ceph_sync_read(struct kiocb *iocb, struct iov_iter *i,
 			size_t left = len = ret;
 
 			while (left) {
-				void __user *data = i->iov[0].iov_base
+				struct iovec *iov = (struct iovec *)i->data;
+				void __user *data = iov->iov_base
 							+ i->iov_offset;
-				l = min(i->iov[0].iov_len - i->iov_offset,
-					left);
+				l = min(iov->iov_len - i->iov_offset, left);
 
 				ret = ceph_copy_page_vector_to_user(&pages[k],
 								    data, off,
@@ -572,8 +573,9 @@ ceph_sync_direct_write(struct kiocb *iocb, const struct iovec *iov,
 	iov_iter_init(&i, iov, nr_segs, count, 0);
 
 	while (iov_iter_count(&i) > 0) {
-		void __user *data = i.iov->iov_base + i.iov_offset;
-		u64 len = i.iov->iov_len - i.iov_offset;
+		struct iovec *iov = (struct iovec *)i.data;
+		void __user *data = iov->iov_base + i.iov_offset;
+		u64 len = iov->iov_len - i.iov_offset;
 
 		page_align = (unsigned long)data & ~PAGE_MASK;
 
