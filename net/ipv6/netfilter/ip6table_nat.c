6d32a51e7847cf1684000beff7b0d6087b56f056
Kirill Tkhai <ktkhai@odin.com>
2015-07-16 17:30:33 +0400

    ve/net: Add ipt_mask checks into ip6table_nat
    Register pernet table only if it's allowed.
    
    https://jira.sw.ru/browse/PSBM-34748
    
    khorenko@: this is required in order nf_conntrack module
    "ip_conntrack_disable_ve0" option to handle both
    ipv4 and ipv6 nat cases.
    
    Signed-off-by: Kirill Tkhai <ktkhai@odin.com>
    Acked-by: Andrew Vagin <avagin@odin.com>

--- net/ipv6/netfilter/ip6table_nat.c
+++ net/ipv6/netfilter/ip6table_nat.c
@@ -115,6 +115,10 @@ static int __net_init ip6table_nat_net_init(struct net *net)
 {
 	struct ip6t_replace *repl;
 
+	if (!net_ipt_permitted(net, VE_IP_IPTABLE_NAT) ||
+	    !net_ipt_permitted(net, VE_IP_IPTABLES6))
+		return 0;
+
 	repl = ip6t_alloc_initial_table(&nf_nat_ipv6_table);
 	if (repl == NULL)
 		return -ENOMEM;
@@ -125,7 +129,11 @@ static int __net_init ip6table_nat_net_init(struct net *net)
 
 static void __net_exit ip6table_nat_net_exit(struct net *net)
 {
+	if (!net->ipv6.ip6table_nat)
+		return;
+
 	ip6t_unregister_table(net, net->ipv6.ip6table_nat);
+	net->ipv6.ip6table_nat = NULL;
 }
 
 static struct pernet_operations ip6table_nat_net_ops = {
