eff0bc3f18e2a8c1015b75f4e38c37fc9e8446a6
Cyrill Gorcunov <gorcunov@virtuozzo.com>
2015-07-21 19:22:33 +0400

    ve/vznetstat: Move VE networks statistics allocation into a common place
    We have three modules serving network statictics:
    
     - vznetstat.ko
     - ip_vznetstat.ko
     - ip6_vznetstat.ko
    
    where ip6_vznetstat depends on ip_vznetstat and both depends on general vznetstat
    module. While vznetstat does are real work on network packets counting the
    counters are allocated in somehow nontrivial way
    
     1) ip_vznetstat depends on ip6_vznetstat, and both require vznetstat module
     2) when ip_vznetstat loads it walks over all existing VE and allocates counters
        setting up a ve-startup hook to allocate counters for VE which will be
        started after
     3) in turn ip6_vznetstat module doesn't depend on ip_vznetstat (which does
        a real counters allocations, in commit 3ad7b9565dc63734 we fixed
        ip6_vznetstat so it allocates counters for newly created VEs if
        former ip_vznetstat get unloaded, but this doesn't take existing
        VEs into account)
    
    This makes counters allocation code really hard to read. Lets do their
    allocation in a different way
    
     - move counters allocation into general vznetstat module, upon its
       load it does:
    
       * walk over all existing running VEs and allocate/get counters
       * setup VE start/stop hooks to allocate/get counters for further
         VEs which be started after the module load
    
     - drop counters allocation from both ip6_vznetstat and ip_vznetstat,
       these modules simply register netfilter hooks for incoming/outgoing
       traffic and count packets
    
     - drop ip_vznetstat dependency on ip6_vznetstat, they are not bound
       anymore and each simply depends on general vznetstat module which
       allocates counters for them
    
    https://jira.sw.ru/browse/PSBM-35011
    
    v2:
     - don't forget to assign newly created counters into VEs
    
    v3:
     - switch to per-net operaions: instead of using start/stop
       hooks we simply register net init/exit hooks which either
       create/get or put/destroy the statistics. Note there is
       no longer module-get/put, we rely on net namespace
       synchronization, thus one can call rmmod over all
       modules mentioned above even with running VEs.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    Reviewed-by: Vladimir Davydov <vdavydov@parallels.com>
    
    CC: Andrey Vagin <avagin@virtuozzo.com>
    CC: Konstantin Khorenko <khorenko@virtuozzo.com>
    CC: Pavel Emelyanov <xemul@virtuozzo.com>
    CC: Igor Sukhih <igor@parallels.com>

--- kernel/ve/vznetstat/ip6_vznetstat.c
+++ kernel/ve/vznetstat/ip6_vznetstat.c
@@ -72,104 +72,27 @@ static struct nf_hook_ops venet_acct_out_ops = {
 	.priority	= NF_IP6_PRI_LAST,
 };
 
-static int init_venet_acct_ip6_stat(void *data)
-{
-	struct ve_struct *ve = (struct ve_struct *)data;
-
-	if (!ve->stat) {
-		ve->stat = venet_acct_find_create_stat(ve->veid);
-		if (!ve->stat)
-			return -ENODEV;
-	} else {
-		venet_acct_get_stat(ve->stat);
-	}
-
-	__module_get(THIS_MODULE);
-	set_bit(VE_NET_ACCT_V6, &ve->stat->flags);
-
-	return 0;
-}
-
-static void fini_venet_acct_ip6_stat(void *data)
-{
-	struct ve_struct *ve = (struct ve_struct *)data;
-
-	/* module was load after VE ? */
-	if (!ve->stat || !test_bit(VE_NET_ACCT_V6, &ve->stat->flags))
-		return;
-
-	clear_bit(VE_NET_ACCT_V6, &ve->stat->flags);
-	venet_acct_put_stat(ve->stat);
-	module_put(THIS_MODULE);
-}
-
-
-static int venet_acct_register_ip6_hooks(void *data)
-{
-	struct ve_struct *ve = (struct ve_struct *)data;
-	int ret;
-
-	if (!ve->stat)
-		return -ENODEV;
-
-	venet_acct_get_stat(ve->stat);
-
-	 /* Register hooks */
-	ret = nf_register_hook(&venet_acct_in_ops);
-	if (ret < 0)
-		goto out_free_stat;
-
-	ret = nf_register_hook(&venet_acct_out_ops);
-	if (ret < 0)
-		goto out_hook_in;
-
-	set_bit(VE_NET_ACCT_V6, &ve->stat->flags);
-
-	return 0;
-
-out_hook_in:
-	nf_unregister_hook(&venet_acct_in_ops);
-out_free_stat:
-	venet_acct_put_stat(ve->stat);
-	return ret;
-}
-
-static void venet_acct_unregister_ip6_hooks(void *data)
-{
-	struct ve_struct *ve = (struct ve_struct *)data;
-
-	clear_bit(VE_NET_ACCT_V6, &ve->stat->flags);
-
-	nf_unregister_hook(&venet_acct_out_ops);
-	nf_unregister_hook(&venet_acct_in_ops);
-
-	venet_acct_put_stat(ve->stat);
-}
-
-static struct ve_hook venet_acct_hook_v6 = {
-	.init		= init_venet_acct_ip6_stat,
-	.fini		= fini_venet_acct_ip6_stat,
-	.priority	= HOOK_PRIO_NET_ACCT_V6,
-	.owner		= THIS_MODULE,
-};
-
 int __init ip6_venetstat_init(void)
 {
 	int ret;
 
-	ret = venet_acct_register_ip6_hooks(get_ve0());
+	ret = nf_register_hook(&venet_acct_in_ops);
 	if (ret < 0)
 		return ret;
 
-	ve_hook_register(VE_SS_CHAIN, &venet_acct_hook_v6);
-	ip_vznetstat_touch();
+	ret = nf_register_hook(&venet_acct_out_ops);
+	if (ret < 0) {
+		nf_unregister_hook(&venet_acct_in_ops);
+		return ret;
+	}
+
 	return 0;
 }
 
 void __exit ip6_venetstat_exit(void)
 {
-	venet_acct_unregister_ip6_hooks(get_ve0());
-	ve_hook_unregister(&venet_acct_hook_v6);
+	nf_unregister_hook(&venet_acct_out_ops);
+	nf_unregister_hook(&venet_acct_in_ops);
 }
 
 module_init(ip6_venetstat_init);
