d347b13672aa26154153f0d2eeea13620dd32407
Kirill Tkhai <ktkhai@odin.com>
2015-08-26 17:50:37 +0400

    net/sched: restore "mpu xxx" handling
    commit 56b765b79 ("htb: improved accuracy at high rates")
    broke the "mpu xxx" handling.
    
    This patch restores the "mpu xxx" handling for htb:
    
    # tc class add dev eth0 classid 1:2 parent 1: htb rate 1mbit mpu 0
    # ping -f -c 1000 10.30.22.52
    rtt min/avg/max/mdev = 0.156/1.881/2.454/0.175 ms, ipg/ewma 1.908/1.925 ms
    
    # tc class change dev eth0 classid 1:2 parent 1: htb rate 1mbit mpu 1000
    # ping -f -c 1000 10.30.22.52
    rtt min/avg/max/mdev = 15.352/22.229/38.646/2.212 ms, pipe 3, ipg/ewma 15.999/22.609 ms
    
    https://jira.sw.ru/browse/PSBM-34874
    https://patchwork.ozlabs.org/patch/501666/
    
    Signed-off-by: Kirill Tkhai <ktkhai@odin.com>

--- net/sched/sch_htb.c
+++ net/sched/sch_htb.c
@@ -1109,6 +1109,8 @@ static int htb_dump_class(struct Qdisc *sch, unsigned long arg,
 	opt.buffer = PSCHED_NS2TICKS(cl->buffer);
 	psched_ratecfg_getrate(&opt.ceil, &cl->ceil);
 	opt.cbuffer = PSCHED_NS2TICKS(cl->cbuffer);
+	opt.rate.mpu = cl->rate.mpu;
+	opt.ceil.mpu = cl->ceil.mpu;
 	opt.quantum = cl->quantum;
 	opt.prio = cl->prio;
 	opt.level = cl->level;
@@ -1493,6 +1495,8 @@ static int htb_change_class(struct Qdisc *sch, u32 classid,
 
 	psched_ratecfg_precompute(&cl->rate, &hopt->rate);
 	psched_ratecfg_precompute(&cl->ceil, &hopt->ceil);
+	cl->rate.mpu = hopt->rate.mpu;
+	cl->ceil.mpu = hopt->ceil.mpu;
 
 	cl->buffer = PSCHED_TICKS2NS(hopt->buffer);
 	cl->cbuffer = PSCHED_TICKS2NS(hopt->cbuffer);
