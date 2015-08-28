c47a17a93b730ba88e33956df29c19ad02272f6f
Vladimir Davydov <vdavydov@parallels.com>
2015-08-28 16:42:41 +0400

    Revert "VE/VFS: use sb->s_ns member to store namespace for mount_ns() calls"
    Patchset description:
    
    zap sb->s_ns + fix memleak in binfmt_misc
    
    Vladimir Davydov (6):
      binfmt_misc: do not use sb->s_fs_info
      Revert "VE/VFS: use sb->s_ns member to store namespace for mount_ns()
        calls"
      Revert "ve/sunrpc: use correct pointer to net_namespace in auth_gss.c"
      Revert "nfsd/sunrpc/mqueue: use sb->s_ns instead of data in
        fill_super"
      binfmt_misc: do not use s_ns
      binfmt_misc: destroy all nodes on ve stop
    
    https://jira.sw.ru/browse/PSBM-39154
    
    Reviewed-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    
    ======================
    This patch description:
    
    This reverts commit 9e7411c5c3b53937171ef962ce7381337f125b28.
    
    This patch is not longer needed, because none of the mount_ns users
    needs sb->s_fs_info any more.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- fs/super.c
+++ fs/super.c
@@ -883,12 +883,12 @@ EXPORT_SYMBOL(kill_litter_super);
 
 static int ns_test_super(struct super_block *sb, void *data)
 {
-	return sb->s_ns == data;
+	return sb->s_fs_info == data;
 }
 
 static int ns_set_super(struct super_block *sb, void *data)
 {
-	sb->s_ns = data;
+	sb->s_fs_info = data;
 	return set_anon_super(sb, NULL);
 }
 
