afc4994f2e861941739ce8e0d699411d90fd5087
Vladimir Davydov <vdavydov@parallels.com>
2015-09-03 14:10:34 +0400

    ve/writeback: revert ub dirty limit related stuff
    This patch reverts ub dirty limit related hunks brought by the initial
    commit 2a8b5de95918. None of them actually works, so this patch
    introduces no functional changes. Dirty set control will be
    reimplemented in the scope of
    
    https://jira.sw.ru/browse/PSBM-33841
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- fs/fs-writeback.c
+++ fs/fs-writeback.c
@@ -40,7 +40,6 @@
 struct wb_writeback_work {
 	long nr_pages;
 	struct super_block *sb;
-	struct user_beancounter *ub;
 	unsigned long *older_than_this;
 	enum writeback_sync_modes sync_mode;
 	unsigned int tagged_writepages:1;
@@ -120,8 +119,8 @@ out_unlock:
 }
 
 static void
-__bdi_start_writeback(struct backing_dev_info *bdi,
-		      long nr_pages, bool range_cyclic, enum wb_reason reason)
+__bdi_start_writeback(struct backing_dev_info *bdi, long nr_pages,
+		      bool range_cyclic, enum wb_reason reason)
 {
 	struct wb_writeback_work *work;
 
@@ -140,7 +139,6 @@ __bdi_start_writeback(struct backing_dev_info *bdi,
 	work->nr_pages	= nr_pages;
 	work->range_cyclic = range_cyclic;
 	work->reason	= reason;
-	work->ub	= NULL;
 
 	bdi_queue_work(bdi, work);
 }
@@ -664,7 +662,6 @@ static long writeback_sb_inodes(struct super_block *sb,
 		.range_cyclic		= work->range_cyclic,
 		.range_start		= 0,
 		.range_end		= LLONG_MAX,
-		.wb_ub			= work->ub,
 	};
 	unsigned long start_time = jiffies;
 	long write_chunk;
@@ -698,14 +695,6 @@ static long writeback_sb_inodes(struct super_block *sb,
 		 * kind writeout is handled by the freer.
 		 */
 		spin_lock(&inode->i_lock);
-		if (wbc.wb_ub && !wb->bdi->dirty_exceeded &&
-		    (inode->i_mapping->dirtied_ub != wbc.wb_ub) &&
-		    (inode->i_state & I_DIRTY) == I_DIRTY_PAGES &&
-		    ub_should_skip_writeback(wbc.wb_ub, inode)) {
-			requeue_io(inode, wb);
-			continue;
-		}
-
 		if (inode->i_state & (I_NEW | I_FREEING | I_WILL_FREE)) {
 			spin_unlock(&inode->i_lock);
 			redirty_tail(inode, wb);
@@ -904,12 +893,9 @@ static long wb_writeback(struct bdi_writeback *wb,
 
 		/*
 		 * For background writeout, stop when we are below the
-		 * background dirty threshold. For filtered background
-		 * writeback we write all inodes dirtied before us,
-		 * because we cannot dereference this ub pointer.
+		 * background dirty threshold
 		 */
-		if (work->for_background && !work->ub &&
-		    !over_bground_thresh(wb->bdi))
+		if (work->for_background && !over_bground_thresh(wb->bdi))
 			break;
 
 		/*
@@ -1362,7 +1348,7 @@ out_unlock_inode:
 }
 EXPORT_SYMBOL(__mark_inode_dirty);
 
-static void wait_sb_inodes(struct super_block *sb, struct user_beancounter *ub)
+static void wait_sb_inodes(struct super_block *sb)
 {
 	struct inode *inode, *old_inode = NULL;
 
@@ -1387,11 +1373,6 @@ static void wait_sb_inodes(struct super_block *sb, struct user_beancounter *ub)
 		spin_lock(&inode->i_lock);
 		if ((inode->i_state & (I_FREEING|I_WILL_FREE|I_NEW)) ||
 		    (mapping->nrpages == 0)) {
-			spin_unlock(&inode->i_lock);
-			continue;
-		}
-		if (ub && (mapping->dirtied_ub != ub) &&
-		    (inode->i_state & I_DIRTY) == I_DIRTY_PAGES) {
 			spin_unlock(&inode->i_lock);
 			continue;
 		}
@@ -1518,12 +1499,11 @@ EXPORT_SYMBOL(try_to_writeback_inodes_sb);
  * This function writes and waits on any dirty inode belonging to this
  * super_block.
  */
-void sync_inodes_sb_ub(struct super_block *sb, struct user_beancounter *ub)
+void sync_inodes_sb(struct super_block *sb)
 {
 	DECLARE_COMPLETION_ONSTACK(done);
 	struct wb_writeback_work work = {
 		.sb		= sb,
-		.ub		= ub,
 		.sync_mode	= WB_SYNC_ALL,
 		.nr_pages	= LONG_MAX,
 		.range_cyclic	= 0,
@@ -1540,12 +1520,7 @@ void sync_inodes_sb_ub(struct super_block *sb, struct user_beancounter *ub)
 	bdi_queue_work(sb->s_bdi, &work);
 	wait_for_completion(&done);
 
-	wait_sb_inodes(sb, ub);
-}
-
-void sync_inodes_sb(struct super_block *sb)
-{
-	sync_inodes_sb_ub(sb, NULL);
+	wait_sb_inodes(sb);
 }
 EXPORT_SYMBOL(sync_inodes_sb);
 
