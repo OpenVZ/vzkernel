8a18298c392ee94e8ae418f9e4cc6eba063b2bf9
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2015-09-04 15:45:51 -0700

    ms/mm/slub: fix slab double-free in case of duplicate sysfs filename
    sysfs_slab_add() shouldn't call kobject_put at error path: this puts last
    reference of kmem-cache kobject and frees it.  Kmem cache will be freed
    second time at error path in kmem_cache_create().
    
    For example this happens when slub debug was enabled in runtime and
    somebody creates new kmem cache:
    
    "configfs_dir_cache" cannot be merged because existing slab have debug and
    cannot create new slab because unique name ":t-0000096" already taken.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
    Acked-by: Christoph Lameter <cl@linux.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Acked-by: David Rientjes <rientjes@google.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    https://jira.sw.ru/browse/PSBM-89047
    
    (cherry picked from commit 80da026a8e5da83b6ab029807844587960490e2b)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- mm/slub.c
+++ mm/slub.c
@@ -5680,7 +5680,7 @@ static int sysfs_slab_add(struct kmem_cache *s)
 	s->kobj.kset = cache_kset(s);
 	err = kobject_init_and_add(&s->kobj, &slab_ktype, NULL, "%s", name);
 	if (err)
-		goto out_put_kobj;
+		goto out;
 
 	err = sysfs_create_group(&s->kobj, &slab_attr_group);
 	if (err)
@@ -5707,8 +5707,6 @@ out:
 	return err;
 out_del_kobj:
 	kobject_del(&s->kobj);
-out_put_kobj:
-	kobject_put(&s->kobj);
 	goto out;
 }
 
