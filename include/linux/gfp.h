7ed07578716735d854af72d4e5839ed9d2e60eb2
Vladimir Davydov <vdavydov@parallels.com>
2015-09-07 14:15:47 +0400

    gfp: add __get_free_kmem_pages helper
    Works exactly as __get_free_pages except it also tries to charge newly
    allocated pages to memcg/kmem. It will be used by the next patch.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Reviewed-by: Kirill Tkhai <ktkhai@odin.com>

--- include/linux/gfp.h
+++ include/linux/gfp.h
@@ -358,6 +358,7 @@ extern struct page *alloc_kmem_pages_node(int nid, gfp_t gfp_mask,
 					  unsigned int order);
 
 extern unsigned long __get_free_pages(gfp_t gfp_mask, unsigned int order);
+extern unsigned long __get_free_kmem_pages(gfp_t gfp_mask, unsigned int order);
 extern unsigned long get_zeroed_page(gfp_t gfp_mask);
 
 void *alloc_pages_exact(size_t size, gfp_t gfp_mask);
