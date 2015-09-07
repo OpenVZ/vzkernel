6689559dde8d7fff427981154cfc7e576b75f7a0
Vladimir Davydov <vdavydov@parallels.com>
2015-09-07 14:15:47 +0400

    Revert "mm: account all page tables for mm"
    This reverts commit 86c4127b5cbdcf9a3d182c53d48159153af40c40.
    
    This commit added mm_struct->nr_ptds, which counts the number of page
    table directories used by a process. It was ported from PCS6.
    
    In PCS6 it is used for precharging page tables to UB_KMEMSIZE on fork.
    Plus, the counter is reported in /proc/pid/status as VmPTD.
    
    I'm not going to use this counter in the upcoming implementation of page
    tables accounting, so I'm reverting the patch that brought it. Regarding
    VmPTD, there is a mainstream commit dc6c9a35b66b5 ("mm: account pmd page
    tables to the process"), which adds a similar statistic - VmPMD. If we
    need it, we can always backport the commit.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -578,7 +578,6 @@ static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p)
 	INIT_LIST_HEAD(&mm->mmlist);
 	mm->core_state = NULL;
 	atomic_long_set(&mm->nr_ptes, 0);
-	mm->nr_ptds = 0;
 	memset(&mm->rss_stat, 0, sizeof(mm->rss_stat));
 	spin_lock_init(&mm->page_table_lock);
 	mm->free_area_cache = TASK_UNMAPPED_BASE;
