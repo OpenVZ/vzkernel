3f74bd44430295eed216bcb87fb51aeb13829fef
Michal Hocko <mhocko@suse.cz>
2015-09-08 19:31:00 +0400

    ms/memcg, oom: lock mem_cgroup_print_oom_info
    mem_cgroup_print_oom_info uses a static buffer (memcg_name) to store the
    name of the cgroup.  This is not safe as pointed out by David Rientjes
    because memcg oom is locked only for its hierarchy and nothing prevents
    another parallel hierarchy to trigger oom as well and overwrite the
    already in-use buffer.
    
    This patch introduces oom_info_lock hidden inside
    mem_cgroup_print_oom_info which is held throughout the function.  It
    makes access to memcg_name safe and as a bonus it also prevents parallel
    memcg ooms to interleave their statistics which would make the printed
    data hard to analyze otherwise.
    
    Signed-off-by: Michal Hocko <mhocko@suse.cz>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    Cc: KAMEZAWA Hiroyuki <kamezawa.hiroyu@jp.fujitsu.com>
    Acked-by: David Rientjes <rientjes@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 947b3dd1a84ba3fcb7163688fdc36671941786f4)
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1946,13 +1946,13 @@ static void move_unlock_mem_cgroup(struct mem_cgroup *memcg,
  */
 void mem_cgroup_print_oom_info(struct mem_cgroup *memcg, struct task_struct *p)
 {
+	/*
+	 * protects memcg_name and makes sure that parallel ooms do not
+	 * interleave
+	 */
+	static DEFINE_SPINLOCK(oom_info_lock);
 	struct cgroup *task_cgrp;
 	struct cgroup *mem_cgrp;
-	/*
-	 * Need a buffer in BSS, can't rely on allocations. The code relies
-	 * on the assumption that OOM is serialized for memory controller.
-	 * If this assumption is broken, revisit this code.
-	 */
 	static char memcg_name[PATH_MAX];
 	int ret;
 	struct mem_cgroup *iter;
@@ -1961,6 +1961,7 @@ void mem_cgroup_print_oom_info(struct mem_cgroup *memcg, struct task_struct *p)
 	if (!p)
 		return;
 
+	spin_lock(&oom_info_lock);
 	rcu_read_lock();
 
 	mem_cgrp = memcg->css.cgroup;
@@ -2026,6 +2027,7 @@ done:
 
 		pr_cont("\n");
 	}
+	spin_unlock(&oom_info_lock);
 }
 
 /*
