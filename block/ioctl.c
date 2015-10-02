91df1c061cb247544e861a50e9d3a81bd5e78ac8
Dmitry Monakhov <dmonakhov@openvz.org>
2015-10-02 11:54:26 +0400

    cbt: introduce changed block tracking
    Combined patch list:
    diff-cbt-add-changed-block-trace-infrastructure
    diff-cbt-fixup-use-after-free-inside-__blk_cbt_set
    diff-cbt-use-propper-mem-allocation-context
    diff-cbt-support-blockdevice-size-update-v2
    diff-cbt-blk_cbt_update_size-add-block_dev-sanity-check
    diff-cbt-ignore-device-shrink
    diff-block-cbt-fix-mistype-statement
    diff-cbt-add-get_once-feature
    diff-cbt-fix-bytes-to-block-conversion-bug
    diff-cbt-add-missed-mutex_unlock
    
    Only minor context fixes from original patches.
    
    https://jira.sw.ru/browse/PSBM-34156
    
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>
    
    +++
    cbt: drop "ret" var in blkdev_ioctl()
    
    To be merged into commit 719a99a0846ebcc4b04f6bdc7a738a2ff5852b07
    ("cbt: introduce changed block tracking")
    
    https://jira.sw.ru/browse/PSBM-54928
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    [VvS RHEL7.8 rebase] context changes

--- block/ioctl.c
+++ block/ioctl.c
@@ -140,7 +140,7 @@ static int blkpg_ioctl(struct block_device *bdev, struct blkpg_ioctl_arg __user 
 			}
 			disk_part_iter_exit(&piter);
 			part_nr_sects_write(part, (sector_t)length);
-			i_size_write(bdevp->bd_inode, p.length);
+			bd_write_size(bdevp, p.length);
 			mutex_unlock(&bdevp->bd_mutex);
 			mutex_unlock(&bdev->bd_mutex);
 			bdput(bdevp);
@@ -601,6 +601,12 @@ int blkdev_ioctl(struct block_device *bdev, fmode_t mode, unsigned cmd,
 		return blkdev_pr_preempt(bdev, argp, true);
 	case IOC_PR_CLEAR:
 		return blkdev_pr_clear(bdev, argp);
+	case BLKCBTSTART:
+	case BLKCBTSTOP:
+	case BLKCBTGET:
+	case BLKCBTSET:
+	case BLKCBTCLR:
+		return blk_cbt_ioctl(bdev, cmd, (char __user *)arg);
 	default:
 		return __blkdev_driver_ioctl(bdev, mode, cmd, arg);
 	}
--- fs/block_dev.c
+++ fs/block_dev.c
@@ -1146,10 +1146,17 @@ int check_disk_change(struct block_device *bdev)
 
 EXPORT_SYMBOL(check_disk_change);
 
+void bd_write_size(struct block_device *bdev, loff_t size)
+{
+	i_size_write(bdev->bd_inode, size);
+	blk_cbt_update_size(bdev);
+}
+EXPORT_SYMBOL(bd_write_size);
+
 void bd_set_size(struct block_device *bdev, loff_t size)
 {
 	mutex_lock(&bdev->bd_inode->i_mutex);
-	i_size_write(bdev->bd_inode, size);
+	bd_write_size(bdev, size);
 	mutex_unlock(&bdev->bd_inode->i_mutex);
 }
 EXPORT_SYMBOL(bd_set_size);
