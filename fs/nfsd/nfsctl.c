3159b8661b3345db15e5aac1bffa2695ec25ff83
Kirill Tkhai <ktkhai@odin.com>
2015-10-08 14:30:14 +0400

    ve/nfs,nfsd: Allow to mount fs from initial VE's user_ns
    Since VE's init starts in VE's own user_ns now, we should
    allow CT programs to mount nfs and nfsd.
    
    https://jira.sw.ru/browse/PSBM-40047
    
    Signed-off-by: Kirill Tkhai <ktkhai@odin.com>

--- fs/nfsd/nfsctl.c
+++ fs/nfsd/nfsctl.c
@@ -1167,6 +1167,8 @@ static int nfsd_fill_super(struct super_block * sb, void * data, int silent)
 static struct dentry *nfsd_mount(struct file_system_type *fs_type,
 	int flags, const char *dev_name, void *data)
 {
+	if (!current_user_ns_initial())
+		return ERR_PTR(-EPERM);
 	return mount_ns(fs_type, flags, current->nsproxy->net_ns, nfsd_fill_super);
 }
 
@@ -1183,7 +1185,7 @@ static struct file_system_type nfsd_fs_type = {
 	.name		= "nfsd",
 	.mount		= nfsd_mount,
 	.kill_sb	= nfsd_umount,
-	.fs_flags	= FS_VIRTUALIZED,
+	.fs_flags	= FS_VIRTUALIZED|FS_USERNS_MOUNT,
 };
 MODULE_ALIAS_FS("nfsd");
 
