2d5498e13d31871f5555d92c14655610c4b8cd2f
Konstantin Khorenko <khorenko@virtuozzo.com>
2015-10-09 18:03:10 +0400

    ve: mark SYSFS and IPGRE features deprecated
    Initially features were used in order to prevent CT owners
    from some functionality which might be dangerous or
    lead to some restrictions on the CT management like
    a CT with NFSd inside cannot be migrated online.
    
    In PCS6 we already had VE_FEATURE_IPGRE feature deprecated
    and now we don't see any reasons to have sysfs-related limits
    
    => mark VE_FEATURE_SYSFS and VE_FEATURE_IPGRE features deprecated.
    
    https://jira.sw.ru/browse/PSBM-33687
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Reviewed-by: Kirill Tkhai <ktkhai@odin.com>

--- include/uapi/linux/vzcalluser.h
+++ include/uapi/linux/vzcalluser.h
@@ -115,13 +115,13 @@ struct env_create_param3 {
 	__u64				known_features;
 };
 
-#define VE_FEATURE_SYSFS	(1ULL << 0)
+#define VE_FEATURE_SYSFS	(1ULL << 0)	/* deprecated */
 #define VE_FEATURE_NFS		(1ULL << 1)
 #define VE_FEATURE_DEF_PERMS	(1ULL << 2)
 #define VE_FEATURE_SIT          (1ULL << 3)
 #define VE_FEATURE_IPIP         (1ULL << 4)
 #define VE_FEATURE_PPP		(1ULL << 5)
-#define VE_FEATURE_IPGRE	(1ULL << 6)
+#define VE_FEATURE_IPGRE	(1ULL << 6)	/* deprecated */
 #define VE_FEATURE_BRIDGE	(1ULL << 7)
 #define VE_FEATURE_NFSD		(1ULL << 8)
 
