5b5fee11d9df3c7178a32388e54215e9bfc6769e
Vladimir Davydov <vdavydov@parallels.com>
2015-10-15 17:47:36 +0400

    ms/mm: oom_kill: clean up victim marking and exiting interfaces
    Patchset description: oom enhancements - part 1
    
    Pull mainstream patches that clean up TIF_MEMDIE handling. They will
    come in handy for the upcoming oom rework.
    
    https://jira.sw.ru/browse/PSBM-26973
    
    David Rientjes (1):
      mm, oom: remove unnecessary exit_state check
    
    Johannes Weiner (1):
      mm: oom_kill: clean up victim marking and exiting interfaces
    
    Michal Hocko (3):
      oom: make sure that TIF_MEMDIE is set under task_lock
      oom: add helpers for setting and clearing TIF_MEMDIE
      oom: thaw the OOM victim if it is frozen
    
    Tetsuo Handa (1):
      oom: don't count on mm-less current process
    
    ===============================================
    This patch desciption:
    
    From: Johannes Weiner <hannes@cmpxchg.org>
    
    Rename unmark_oom_victim() to exit_oom_victim().  Marking and unmarking
    are related in functionality, but the interface is not symmetrical at
    all: one is an internal OOM killer function used during the killing, the
    other is for an OOM victim to signal its own death on exit later on.
    This has locking implications, see follow-up changes.
    
    While at it, rename mark_tsk_oom_victim() to mark_oom_victim(), which
    is easier on the eye.
    
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: David Rientjes <rientjes@google.com>
    Acked-by: Michal Hocko <mhocko@suse.cz>
    Cc: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Dave Chinner <david@fromorbit.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    (cherry picked from commit 16e951966f05da5ccd650104176f6ba289f7fa20)
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    Reviewed-by: Kirill Tkhai <ktkhai@odin.com>
    
    Conflicts:
    	include/linux/oom.h
    	mm/memcontrol.c
    	mm/oom_kill.c
    
    [VvS RHEL77b rebase]
    [VvS RH79 rebase vz7.170.x]: minor context changes

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -420,10 +420,10 @@ void note_oom_kill(void)
 }
 
 /**
- * mark_tsk_oom_victim - marks the given taks as OOM victim.
+ * mark_oom_victim - mark the given task as OOM victim
  * @tsk: task to mark
  */
-void mark_tsk_oom_victim(struct task_struct *tsk)
+void mark_oom_victim(struct task_struct *tsk)
 {
 	set_tsk_thread_flag(tsk, TIF_MEMDIE);
 
@@ -437,9 +437,9 @@ void mark_tsk_oom_victim(struct task_struct *tsk)
 }
 
 /**
- * unmark_oom_victim - unmarks the current task as OOM victim.
+ * exit_oom_victim - note the exit of an OOM victim
  */
-void unmark_oom_victim(void)
+void exit_oom_victim(void)
 {
 	clear_thread_flag(TIF_MEMDIE);
 }
@@ -486,7 +486,7 @@ void oom_kill_process(struct task_struct *p, gfp_t gfp_mask, int order,
 	 */
 	task_lock(p);
 	if (task_will_free_mem(p)) {
-		mark_tsk_oom_victim(p);
+		mark_oom_victim(p);
 		task_unlock(p);
 		put_task_struct(p);
 		return;
@@ -556,7 +556,7 @@ void oom_kill_process(struct task_struct *p, gfp_t gfp_mask, int order,
 	 * space under its control.
 	 */
 	do_send_sig_info(SIGKILL, SEND_SIG_FORCED, victim, true);
-	mark_tsk_oom_victim(victim);
+	mark_oom_victim(victim);
 	rcu_read_lock();
 	pr_err("Killed process %d (%s) in VE \"%s\", UID %d, total-vm:%lukB, anon-rss:%lukB, file-rss:%lukB, shmem-rss:%lukB\n",
 		task_pid_nr(victim), victim->comm, task_ve_name(victim),
@@ -725,7 +725,7 @@ void out_of_memory(struct zonelist *zonelist, gfp_t gfp_mask,
 	 */
 	if (current->mm &&
 	    (fatal_signal_pending(current) || task_will_free_mem(current))) {
-		mark_tsk_oom_victim(current);
+		mark_oom_victim(current);
 		return;
 	}
 
