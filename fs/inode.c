ffb17e0110f362bf92a71295f68c91cc8dbb3148
Andrew Vagin <avagin@openvz.org>
2015-10-15 15:04:20 +0400

    ms/make default ->i_fop have ->open() fail with ENXIO
    The patch is required for CRIU.
    
    https://jira.sw.ru/browse/PSBM-39834
    
    ML: bd9b51e79cb0b8bc00a7e0076a4a8963ca4a797c
    
    As it is, default ->i_fop has NULL ->open() (along with all other methods).
    The only case where it matters is reopening (via procfs symlink) a file that
    didn't get its ->f_op from ->i_fop - anything else will have ->i_fop assigned
    to something sane (default would fail on read/write/ioctl/etc.).
    
    	Unfortunately, such case exists - alloc_file() users, especially
    anon_get_file() ones.  There we have tons of opened files of very different
    kinds sharing the same inode.  As the result, attempt to reopen those via
    procfs succeeds and you get a descriptor you can't do anything with.
    
    	Moreover, in case of sockets we set ->i_fop that will only be used
    on such reopen attempts - and put a failing ->open() into it to make sure
    those do not succeed.
    
    	It would be simpler to put such ->open() into default ->i_fop and leave
    it unchanged both for anon inode (as we do anyway) and for socket ones.  Result:
    	* everything going through do_dentry_open() works as it used to
    	* sock_no_open() kludge is gone
    	* attempts to reopen anon-inode files fail as they really ought to
    	* ditto for aio_private_file()
    	* ditto for perfmon - this one actually tried to imitate sock_no_open()
    trick, but failed to set ->i_fop, so in the current tree reopens succeed and
    yield completely useless descriptor.  Intent clearly had been to fail with
    -ENXIO on such reopens; now it actually does.
    	* everything else that used alloc_file() keeps working - it has ->i_fop
    set for its inodes anyway
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Andrew Vagin <avagin@openvz.org>

--- fs/inode.c
+++ fs/inode.c
@@ -136,6 +136,7 @@ static int no_open(struct inode *inode, struct file *file)
  */
 int inode_init_always(struct super_block *sb, struct inode *inode)
 {
+	static const struct inode_operations empty_iops;
 	static const struct file_operations no_open_fops = {.open = no_open};
 	struct address_space *const mapping = &inode->i_data;
 
