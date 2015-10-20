16154bb0f5658d8c4e7a69048964ff101c773c4f
Vladimir Davydov <vdavydov@virtuozzo.com>
2015-10-20 20:00:21 +0400

    arch/x86: only enable cpuid faulting for containers
    Port diff-arch-x86-only-enable-cpuid-faulting-for-containers
    
    Hypervisors aren't ready for the fact that CPUID may fault, so let's
    only enable the feature for processes inside containers.
    
    The feature is flipped on context switch. To avoid overhead, we cache
    the current state (enabled/disabled) in a per cpu var. We also do not
    flip it when switching to a kernel thread (e.g. idle task). As a result,
    the overhead will be negligible unless VMs and containers are not mixed
    on the same cpu.
    
    Note, the behaviour of /proc/cpuinfo already conforms to this change -
    we respect masking only inside containers.
    
    https://jira.sw.ru/browse/PSBM-30516
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    Reviewed-by: Denis V. Lunev <den@openvz.org>
    
    Q from den@:
    If mm created (inherited through fork()) with another CPU ID (is VE0) resides
    in container and this feature is on, migration should be prohibited.
    
    Input from xemul@:
    in case a process entered CT and did not exec, it has glibc files pointed to
    outside => no online migration.
    =============================================================================
    
    Changes in port:
     - merge diff-ve-arch-x86-only-enable-cpuid-faulting-for-containers-a
     - use on_each_cpu for toggling running tasks' cpuid faulting feature
    
    https://jira.sw.ru/browse/PSBM-33638
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -1112,4 +1112,7 @@ enum mds_mitigations {
 
 extern enum mds_mitigations mds_mitigation;
 
+extern void (*set_cpuid_faulting_cb)(bool enable);
+extern void set_cpuid_faulting(bool enable);
+
 #endif /* _ASM_X86_PROCESSOR_H */
