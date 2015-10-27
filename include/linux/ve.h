7dd6aa8d9b3e3fa6dde83ccdc81bf0e48c352959
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2015-10-27 19:47:39 +0400

    ve/sysctl/nf_conntrack: allow expose of priviledged sysctl inside containers
    Series:
    This series brings to vz7 all the nf_conntrack sysctl's,
    which are available in vz6.
    
    https://jira.sw.ru/browse/PSBM-40044
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    =============================================================
    
    ve/nf_conntrack: export nf_conntrack_hide_sysctl() helper
    
    From: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    
    Series:
    This series brings to vz7 all the nf_conntrack sysctl's,
    which are available in vz6.
    
    https://jira.sw.ru/browse/PSBM-40044
    
    Will be used for other tables
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    =============================================================
    
    ve/sysctl/net: move and rename *_hide_sysctl helper to ve.c
    
    Make it general for all net sysctls, will be use in next patch.
    
    https://jira.sw.ru/browse/PSBM-54530
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Reviewed-by: Cyrill Gorcunov <gorcunov@openvz.org>
    
    (cherry picked from commit c174b55d665b0d3edcf6445fe6279e7b081beb31)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ve/sysctl/netfilter: Include ve.h header into net/netfilter/nf_conntrack_standalone.c
    
    It's required for ve_net_hide_sysctl() declaration.
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- include/linux/ve.h
+++ include/linux/ve.h
@@ -95,6 +95,8 @@ extern struct cgroup *cgroup_get_ve_root1(struct cgroup *cgrp);
 extern int vz_security_family_check(struct net *net, int family, int type);
 extern int vz_security_protocol_check(struct net *net, int protocol);
 
+int ve_net_hide_sysctl(struct net *net);
+
 #else	/* CONFIG_VE */
 #define get_ve(ve)	(NULL)
 #define put_ve(ve)	do { } while (0)
