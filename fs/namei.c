99e81012f2ea4db87146cdb64aced3151ab493e7
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2015-10-28 18:19:58 +0400

    ve/fs/namei: check containers capabilities in may_linkat()
    This fixes -EPERM result for link creation by root in container.
    
    https://jira.sw.ru/browse/PSBM-40567
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- fs/namei.c
+++ fs/namei.c
@@ -860,7 +860,7 @@ static int may_linkat(struct path *link)
 	 * otherwise, it must be a safe source.
 	 */
 	if (uid_eq(cred->fsuid, inode->i_uid) || safe_hardlink_source(inode) ||
-	    capable(CAP_FOWNER))
+	    ve_capable(CAP_FOWNER))
 		return 0;
 
 	audit_log_link_denied("linkat", link);
@@ -4139,7 +4139,7 @@ SYSCALL_DEFINE5(linkat, int, olddfd, const char __user *, oldname,
 	 * handlink using the passed filedescriptor.
 	 */
 	if (flags & AT_EMPTY_PATH) {
-		if (!capable(CAP_DAC_READ_SEARCH))
+		if (!ve_capable(CAP_DAC_READ_SEARCH))
 			return -ENOENT;
 		how = LOOKUP_EMPTY;
 	}
