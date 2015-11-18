c448fa4a84af0a2a4c279b94c1106e1577ed0ae2
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2015-11-18 15:59:10 +0400

    fence-watchdog: Add fence-watchdog driver
    We need to forbid system to work without a special userspace
    daemon for purposes of HA cluster. So add this watchdog module,
    which will fence the node, if that daemon won't update timer
    value in the file /sys/kernel/watchdog_timer.
    The module is needed for pstorage, so we need to protect network
    from the broken node, so we can put check to net_rx_action.
    
    Signed-off-by: Dmitry Guryanov <dguryanov@parallels.com>
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Rebase: ktkhai@

--- net/core/dev.c
+++ net/core/dev.c
@@ -141,6 +141,7 @@
 #include <linux/hrtimer.h>
 #include <linux/crash_dump.h>
 #include <linux/sctp.h>
+#include <linux/fence-watchdog.h>
 #ifndef __GENKSYMS__
 #include <net/udp_tunnel.h>
 #endif
@@ -2955,6 +2956,14 @@ struct sk_buff *dev_hard_start_xmit(struct sk_buff *first, struct net_device *de
 	struct sk_buff *skb = first;
 	int rc = NETDEV_TX_OK;
 
+#ifdef CONFIG_FENCE_WATCHDOG
+	if (unlikely(fence_wdog_check_timer())) {
+		kfree_skb(skb);
+		*ret = rc;
+		return NULL;
+	}
+#endif
+
 	while (skb) {
 		struct sk_buff *next = skb->next;
 
@@ -5318,6 +5327,10 @@ static void net_rx_action(struct softirq_action *h)
 	local_irq_disable();
 	list_splice_init(&sd->poll_list, &list);
 	local_irq_enable();
+
+#ifdef CONFIG_FENCE_WATCHDOG
+	fence_wdog_check_timer();
+#endif
 
 	for (;;) {
 		struct napi_struct *n;
