32cb69d25fd47d9e46a25437e44fbac608f1698b
Vladimir Davydov <vdavydov@virtuozzo.com>
2015-12-25 13:48:41 +0400

    ms/xfs: convert dquot cache lru to list_lru fix
    Patch series description:
    
    https://jira.sw.ru/browse/PSBM-42319
    
    Andrew Morton (1):
      xfs-convert-dquot-cache-lru-to-list_lru-fix
    
    Dave Chinner (2):
      xfs: convert dquot cache lru to list_lru
      xfs: fix dquot isolation hang
    
    Vladimir Davydov (3):
      xfs_qm: lost hunk from "ms/list_lru: dynamically adjust node arrays"
      xfs_qm: lost hunk from "ms/list_lru: introduce
        list_lru_shrink_{count,walk}"
      xfs_qm: lost hunk from "ms/list_lru: add helpers to isolate items"
    
    ================================
    This patch description:
    
    From: Andrew Morton <akpm@linux-foundation.org>
    
    fix warnings
    
    Cc: Dave Chinner <dchinner@redhat.com>
    Cc: Glauber Costa <glommer@openvz.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    (cherry picked from commit 2f5b56f85674d75f35a10e2e9a4310e7539280da)
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- fs/xfs/xfs_qm.c
+++ fs/xfs/xfs_qm.c
@@ -541,7 +541,7 @@ out_unlock_dirty:
 	return 3;
 }
 
-static long
+static unsigned long
 xfs_qm_shrink_scan(
 	struct shrinker		*shrink,
 	struct shrink_control	*sc)
@@ -549,7 +549,7 @@ xfs_qm_shrink_scan(
 	struct xfs_quotainfo	*qi = container_of(shrink,
 					struct xfs_quotainfo, qi_shrinker);
 	struct xfs_qm_isolate	isol;
-	long			freed;
+	unsigned long		freed;
 	int			error;
 	unsigned long		nr_to_scan = sc->nr_to_scan;
 
@@ -577,7 +577,7 @@ xfs_qm_shrink_scan(
 	return freed;
 }
 
-static long
+static unsigned long
 xfs_qm_shrink_count(
 	struct shrinker		*shrink,
 	struct shrink_control	*sc)
