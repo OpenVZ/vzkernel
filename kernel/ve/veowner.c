d4632cc802d08aff413b195097b1d67f31cf164f
Evgenii Shatokhin <eshatokhin@odin.com>
2015-12-28 14:37:10 +0400

    ve/fs: add per-VE limit of mount points
    https://jira.sw.ru/browse/PSBM-34438
    
    (This fix was adapted from PCS6.)
    
    It is possible for a container to create lots of mount points, which may
    make operations with them slower. As some of these operations take
    global locks (namespace_sem, vfsmount_lock), it might affect other
    containers as well.
    
    Let us limit the maximum number of mount points a VE may create. The
    limit can be customized via /proc/sys/fs/ve-mount-nr knob.
    
    Changes in v.3:
    
    * Revisited VE-specific parts of the patch to reduce the impact on the
      generic code.
    
    Changes in v.2:
    
    * The situations where VE0 mounts something and another VE unmounts it
      seem to be of no concern. If so, it is OK not to alter struct  mount:
      the mount counter for a VE may become unbalanced but this is
      acceptable here.
    
    * The sysctl knob is now defined alongside other VE sysctls.
    
    Signed-off-by: Evgenii Shatokhin <eshatokhin@virtuozzo.com>
    Acked-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    
    Testing results:
    This "mount bomb" ends with "cannot allocate memory" and makes no harm:
       https://github.com/avagin/ctb/blob/master/mounts/run.sh

--- kernel/ve/veowner.c
+++ kernel/ve/veowner.c
@@ -55,6 +55,14 @@ static void prepare_proc(void)
 int ve_xattr_policy = VE_XATTR_POLICY_ACCEPT;
 static int ve_area_access_check;
 
+/*
+ * Operations with a big amount of mount points can require a lot of time.
+ * These operations take the global lock namespace_sem, so they can affect
+ * other containers. Let us allow no more than sysctl_ve_mount_nr mount
+ * points for a VE.
+ */
+unsigned int sysctl_ve_mount_nr = 4096;
+
 static struct ctl_table vz_fs_table[] = {
 	{
 		.procname	= "ve-area-access-check",
@@ -76,6 +84,13 @@ static struct ctl_table vz_fs_table[] = {
 		.maxlen		= sizeof(int),
 		.mode		= 0644 | S_ISVTX,
 		.proc_handler	= &proc_dointvec_virtual,
+	},
+	{
+		.procname       = "ve-mount-nr",
+		.data           = &sysctl_ve_mount_nr,
+		.maxlen         = sizeof(sysctl_ve_mount_nr),
+		.mode           = 0644,
+		.proc_handler   = proc_dointvec,
 	},
 	{ 0 }
 };
