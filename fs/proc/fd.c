6d60fccbb1b544f18e91e1d4838e4ca96b987c2b
Andrey Ryabinin <aryabinin@virtuozzo.com>
2015-12-28 17:52:15 +0400

    ve/fs: drop useless d_root_check()
    d_root_check() takes the following locks in wrong order:
    
    	write_seqlock(&rename_lock);
    	br_read_lock(&vfsmount_lock);
    
    thus we may endup with deadlock.
    While we could fix it by taking these locks in reverse
    order, it would be better to just delete this function.
    
    d_root_check() is a leftover from pre-mount namespace days,
    we don't need it today.
    
    https://jira.sw.ru/browse/PSBM-41919
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Acked-by: Andrew Vagin <avagin@virtuozzo.com>

--- fs/proc/fd.c
+++ fs/proc/fd.c
@@ -163,7 +163,7 @@ static int proc_fd_link(struct dentry *dentry, struct path *path)
 		spin_lock(&files->file_lock);
 		fd_file = fcheck_files(files, fd);
 		ret = -EACCES;
-		if (fd_file && !d_root_check(&fd_file->f_path)) {
+		if (fd_file) {
 			*path = fd_file->f_path;
 			path_get(&fd_file->f_path);
 			ret = 0;
