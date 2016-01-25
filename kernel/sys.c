000abe9f92d18bd790217e827e0cb325f89d6eec
Vladimir Davydov <vdavydov@virtuozzo.com>
2016-01-25 19:33:23 +0400

    ve/kernel: allow to increase rlimit from inside container
    This works on PCS6, so we should allow it on Vz7 either.
    
    https://jira.sw.ru/browse/PSBM-43410
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1783,7 +1783,7 @@ int do_prlimit(struct task_struct *tsk, unsigned int resource,
 		/* Keep the capable check against init_user_ns until
 		   cgroups can contain all limits */
 		if (new_rlim->rlim_max > rlim->rlim_max &&
-				!capable(CAP_SYS_RESOURCE))
+				!ve_capable(CAP_SYS_RESOURCE))
 			retval = -EPERM;
 		if (!retval)
 			retval = security_task_setrlimit(tsk->group_leader,
