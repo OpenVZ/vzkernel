6e7ff58653005963271a7a1f2510660dd52cb1ad
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2016-02-03 19:05:24 +0400

    ve/coredump: virtualize kernel.core_pattern sysctl
    1) put core_pattern on ve_struct and add virtualized proc_handler
    using macro sysctl_virtual()
    
    2) use in-container khelper kthread for collecting cores
    
    We need support of pipe'ing coredump in CT. If process crashes
    in CT and kernel.core_pattern begins with "|" we get error:
    
    CT:
      CT-101-bash-4.2# sysctl kernel.core_pattern
       kernel.core_pattern = |/root/core_test %p UID=%u GID=%g sig=%s
      CT-101-bash-4.2# sleep 100 &
       [1] 445
      CT-101-bash-4.2# kill -11 445
      CT-101-bash-4.2#
       [1]+  Segmentation fault      sleep 100
      CT-101-bash-4.2# ll /root
       total 8
       rwxr-xr-x 1 root root 7424 Jan 27 17:28 core_test
    Host:
      [root@s143 ~]# less /var/log/messages
       Jan 28 10:40:42 s143 kernel: [87698.969582] Core dump to
       |/root/core_test 445 UID=0 GID=0 sig=11 pipe failed
    
    Also docker is going to use pipe'ing in coredumps functionality:
    https://github.com/docker/docker/issues/19289
    
    https://jira.sw.ru/browse/PSBM-43596
    
    v2: split sysctl_virtual in separate patch
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Reviewed-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -103,7 +103,6 @@ extern int max_threads;
 extern int suid_dumpable;
 #ifdef CONFIG_COREDUMP
 extern int core_uses_pid;
-extern char core_pattern[];
 extern unsigned int core_pipe_limit;
 #endif
 extern int pid_max;
@@ -312,6 +311,10 @@ static int max_sched_tunable_scaling = SCHED_TUNABLESCALING_END-1;
 #ifdef CONFIG_COMPACTION
 static int min_extfrag_threshold;
 static int max_extfrag_threshold = 1000;
+#endif
+
+#ifdef CONFIG_COREDUMP
+sysctl_virtual(proc_dostring_coredump);
 #endif
 
 static struct ctl_table kern_table[] = {
@@ -534,10 +537,10 @@ static struct ctl_table kern_table[] = {
 	},
 	{
 		.procname	= "core_pattern",
-		.data		= core_pattern,
+		.data		= ve0.core_pattern,
 		.maxlen		= CORENAME_MAX_SIZE,
-		.mode		= 0644,
-		.proc_handler	= proc_dostring_coredump,
+		.mode		= 0644 | S_ISVTX,
+		.proc_handler	= proc_dostring_coredump_virtual,
 	},
 	{
 		.procname	= "core_pipe_limit",
@@ -2644,9 +2647,10 @@ int proc_dopipe_max_size(struct ctl_table *table, int write,
 
 static void validate_coredump_safety(void)
 {
+	struct ve_struct *ve = get_exec_env();
 #ifdef CONFIG_COREDUMP
 	if (suid_dumpable == SUID_DUMP_ROOT &&
-	    core_pattern[0] != '/' && core_pattern[0] != '|') {
+	    ve->core_pattern[0] != '/' && ve->core_pattern[0] != '|') {
 		printk(KERN_WARNING "Unsafe core_pattern used with "\
 			"suid_dumpable=2. Pipe handler or fully qualified "\
 			"core dump path required.\n");
