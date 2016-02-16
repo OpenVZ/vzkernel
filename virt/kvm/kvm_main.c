e61ec2f75781d24b2fe3bffbc364089e7cc80a42
Maxim Patlasov <mpatlasov@virtuozzo.com>
2016-02-16 14:22:19 +0400

    kvm: do not SetPageDirty for file mappings
    When guest modifies the content of page with file mappings, kernel kvm
    makes the page dirty by the following call-path:
    
    vmx_handle_exit ->
     handle_ept_violation ->
      __get_user_pages ->
       page_mkwrite ->
        SetPageDirty
    
    Since then the page is dirty from both guest and host point of view. Then
    the host makes writeback and mark the page as write-protected. So any
    further write from guest triggers call-path above again.
    
    So, for file mappings, it's not possible to have new data written to a page
    inside the guest w/o corresponding SetPageDirty on the host.
    
    This makes explicit SetPageDirty from kvm_set_pfn_dirty redundant.
    
    https://lkml.org/lkml/2016/2/12/472
    https://jira.sw.ru/browse/PSBM-43243
    
    Signed-off-by: Maxim Patlasov <mpatlasov@virtuozzo.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -1695,7 +1695,8 @@ void kvm_set_pfn_dirty(kvm_pfn_t pfn)
 	if (!kvm_is_reserved_pfn(pfn)) {
 		struct page *page = pfn_to_page(pfn);
 
-		if (!PageReserved(page))
+		if (!PageReserved(page) &&
+		    (!page->mapping || PageAnon(page)))
 			SetPageDirty(page);
 	}
 }
