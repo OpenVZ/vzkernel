5c71a4f0d6bfad76ad1358f23df1f5b447af0d67
Andrey Smetanin <asmetanin@virtuozzo.com>
2016-02-16 13:50:22 +0400

    ms/kvm/x86: Hyper-V SynIC message slot pending clearing at SINT ack
    The SynIC message protocol mandates that the message slot is claimed
    by atomically setting message type to something other than HVMSG_NONE.
    If another message is to be delivered while the slot is still busy,
    message pending flag is asserted to indicate to the guest that the
    hypervisor wants to be notified when the slot is released.
    
    To make sure the protocol works regardless of where the message
    sources are (kernel or userspace), clear the pending flag on SINT ACK
    notification, and let the message sources compete for the slot again.
    
    ms commit - 765eaa0f70eaa274ec8b815d8c210c20cf7b6dbc.
    
    Signed-off-by: Andrey Smetanin <asmetanin@virtuozzo.com>
    
    Reviewed-by: Roman Kagan <rkagan@virtuozzo.com>
    CC: Gleb Natapov <gleb@kernel.org>
    CC: Paolo Bonzini <pbonzini@redhat.com>
    CC: "K. Y. Srinivasan" <kys@microsoft.com>
    CC: Haiyang Zhang <haiyangz@microsoft.com>
    CC: Vitaly Kuznetsov <vkuznets@redhat.com>
    CC: Roman Kagan <rkagan@virtuozzo.com>
    CC: Denis V. Lunev <den@openvz.org>
    CC: qemu-devel@nongnu.org
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -459,6 +459,8 @@ struct kvm {
 #define vcpu_debug_ratelimited(vcpu, fmt, ...)				\
 	kvm_debug_ratelimited("vcpu%i " fmt, (vcpu)->vcpu_id,           \
 			      ## __VA_ARGS__)
+#define vcpu_err(vcpu, fmt, ...)					\
+	kvm_err("vcpu%i " fmt, (vcpu)->vcpu_id, ## __VA_ARGS__)
 
 static inline struct kvm_vcpu *kvm_get_vcpu(struct kvm *kvm, int i)
 {
