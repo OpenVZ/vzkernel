c98b69f392e0b3c518b2d2d4a7b8dd1e69b9acde
Andrey Smetanin <asmetanin@virtuozzo.com>
2016-02-20 20:11:49 +0400

    ms/kvm/x86: Pass return code of kvm_emulate_hypercall
    Series description:
    
    KVM: Hyper-V VMBus hypercalls
    
    The patch implements userspace exit 'KVM_EXIT_HYPERV'
    for Hyper-V VMBus hypercalls(postmsg, signalevent)
    to handle these hypercalls by QEMU.
    
    Changes v3:
    * use vcpu->arch.complete_userspace_io to setup hypercall
    result
    * rebase for 'next-20160211'
    
    Changes v2:
    * use KVM_EXIT_HYPERV for hypercalls
    
    https://jira.sw.ru/browse/PSBM-44419
    
    Andrey Smetanin (5):
    ms/kvm/x86: Rename Hyper-V long spin wait hypercall
    ms/drivers/hv: Move VMBus hypercall codes into Hyper-V UAPI header
    ms/kvm/x86: Pass return code of kvm_emulate_hypercall
    ms/kvm/x86: Reject Hyper-V hypercall continuation
    ms/kvm/x86: Hyper-V VMBus hypercall userspace exit
    
    Reviewed-by: Roman Kagan <rkagan@virtuozzo.com>
    ===============================================
    This patch description:
    
    Pass the return code from kvm_emulate_hypercall on to the caller,
    in order to allow it to indicate to the userspace that
    the hypercall has to be handled there.
    
    Also adjust all the existing code paths to return 1 to make sure the
    hypercall isn't passed to the userspace without setting kvm_run
    appropriately.
    
    Signed-off-by: Andrey Smetanin <asmetanin@virtuozzo.com>
    
    Reviewed-by: Roman Kagan <rkagan@virtuozzo.com>
    CC: Gleb Natapov <gleb@kernel.org>
    CC: Paolo Bonzini <pbonzini@redhat.com>
    CC: Joerg Roedel <joro@8bytes.org>
    CC: "K. Y. Srinivasan" <kys@microsoft.com>
    CC: Haiyang Zhang <haiyangz@microsoft.com>
    CC: Roman Kagan <rkagan@virtuozzo.com>
    CC: Denis V. Lunev <den@openvz.org>
    CC: qemu-devel@nongnu.org
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    (cherry picked from commit 0d9c055eaaf41bebb0e6b095fff447523121fad3)

--- arch/x86/kvm/svm.c
+++ arch/x86/kvm/svm.c
@@ -1942,8 +1942,7 @@ static int vmmcall_interception(struct vcpu_svm *svm)
 {
 	svm->next_rip = kvm_rip_read(&svm->vcpu) + 3;
 	skip_emulated_instruction(&svm->vcpu);
-	kvm_emulate_hypercall(&svm->vcpu);
-	return 1;
+	return kvm_emulate_hypercall(&svm->vcpu);
 }
 
 static unsigned long nested_svm_get_tdp_cr3(struct kvm_vcpu *vcpu)
