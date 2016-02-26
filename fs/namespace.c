24a88c0ab037c05780e389836502482303f38830
Andrey Ryabinin <aryabinin@virtuozzo.com>
2016-02-26 17:02:40 +0400

    ve/fs/sync: fix CT's mountpoints traversal
    Currently sync reads 've->root_path.mnt' mount and iterate over it childs.
    This doesn't work, as not all in-container mounts could be reached that way.
    
    This patch slightly rework's mounts traversal. Now sync just iterates over all
    mounts of ve's mount namespace (actually we iterate over all mounts that
    seen in container's /proc/mounts). This approach seems much more correct and
    also simpler.
    
    https://jira.sw.ru/browse/PSBM-44125
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    Note: this patch does not cover traversing sub mount namespaces
    and we are ok with that for now.
    https://jira.sw.ru/browse/PSBM-44684
    
    Reviewed-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -928,7 +928,7 @@ static void commit_tree(struct mount *mnt, struct mount *shadows)
 	touch_mnt_namespace(n);
 }
 
-struct mount *next_mnt(struct mount *p, struct mount *root)
+static struct mount *next_mnt(struct mount *p, struct mount *root)
 {
 	struct list_head *next = p->mnt_mounts.next;
 	if (next == &p->mnt_mounts) {
