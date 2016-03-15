c4d4588d8f459f061b5b2016450b5932dbc8bcba
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2016-03-15 20:04:11 +0400

    ms/fs/super.c: fix WARN on alloc_super() fail path
    ms commit b5bd856a0c2a ("fs/super.c: fix WARN on alloc_super() fail path")
    
    On fail path alloc_super() calls destroy_super(), which issues a warning
    if the sb's s_mounts list is not empty, in particular if it has not been
    initialized.  That said s_mounts must be initialized in alloc_super()
    before any possible failure, but currently it is initialized close to
    the end of the function leading to a useless warning dumped to log if
    either percpu_counter_init() or list_lru_init() fails.  Let's fix this.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Found during investigation of
    https://jira.sw.ru/browse/PSBM-43294

--- fs/super.c
+++ fs/super.c
@@ -172,6 +172,8 @@ static struct super_block *alloc_super(struct file_system_type *type, int flags)
 	if (!s)
 		return NULL;
 
+	INIT_LIST_HEAD(&s->s_mounts);
+
 	if (security_sb_alloc(s))
 		goto fail;
 	for (i = 0; i < SB_FREEZE_LEVELS; i++) {
@@ -194,7 +196,6 @@ static struct super_block *alloc_super(struct file_system_type *type, int flags)
 	if (list_lru_init_memcg(&s->s_inode_lru))
 		goto err_out_dentry_lru;
 
-	INIT_LIST_HEAD(&s->s_mounts);
 	init_rwsem(&s->s_umount);
 	lockdep_set_class(&s->s_umount, &type->s_umount_key);
 	/*
