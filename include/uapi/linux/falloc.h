3682625db741fd8308ce1b1cb82f78b895a3a443
Maxim Patlasov <mpatlasov@virtuozzo.com>
2016-03-16 13:23:59 +0400

    ext4: rename fallocate(FALLOC_FL_CONVERT_AND_EXTEND)
    Patchset description:
    
    The patch reworks the feature and renames it to FALLOC_FL_CONVERT_UNWRITTEN.
    
    Intended use:
    
    0) pos := i_size_read(inode); len := 1MB; prealloc := 100MB
    
    1) fallocate(file, 0, pos, prealloc)
    2) fiemap(inode, &fieinfo, pos, 1)
    3) dio pwrite(block_device, data, len, fi_extent.fe_physical)
    4) fallocate(file, FALLOC_FL_CONVERT_UNWRITTEN, pos, len)
    
    ===
    This patch description:
    
    The feature doesn't extend i_size anymore. Let's rename it properly:
    
    s/convert_and_extend/convert_unwritten
    s/CONVERT_AND_EXTEND/CONVERT_UNWRITTEN
    
    https://jira.sw.ru/browse/PSBM-22381
    
    Signed-off-by: Maxim Patlasov <mpatlasov@virtuozzo.com>
    Acked-by: Dmitry Monakhov <dmonakhov@virtuozzo.com>

--- include/uapi/linux/falloc.h
+++ include/uapi/linux/falloc.h
@@ -4,8 +4,7 @@
 #define FALLOC_FL_KEEP_SIZE	0x01 /* default is extend size */
 #define FALLOC_FL_PUNCH_HOLE	0x02 /* de-allocates range */
 #define FALLOC_FL_NO_HIDE_STALE	0x04 /* reserved codepoint */
-#define FALLOC_FL_CONVERT_AND_EXTEND 0x100 /* mark extents as initialized
-					    * and extend i_size */
+#define FALLOC_FL_CONVERT_UNWRITTEN 0x100 /* mark extents as initialized */
 
 
 /*
