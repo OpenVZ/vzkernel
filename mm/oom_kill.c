42e77f687adf99d129e4ef138ba3d7f451cf0315
Vladimir Davydov <vdavydov@virtuozzo.com>
2016-04-29 19:28:57 +0400

    oom: do not select child that has already been killed
    Patchset description:
    A few fixes for oom killer
    
    This is for https://jira.sw.ru/browse/PSBM-40842
    
    Vladimir Davydov (5):
      oom: do not select child that has already been killed
      mm: memcontrol: check more carefully if current is oom killed
      oom: do not ignore score of exiting tasks
      oom: zap unused oom_scan_process_thread arguments
      oom: make berserker more aggressive
    
    ======================
    This patch description:
    
    Otherwise, we might end up selecting the same process over and over
    again in case it got stuck somewhere in exit path for some reason.
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -884,6 +884,9 @@ void oom_kill_process(struct task_struct *p, gfp_t gfp_mask, int order,
 
 			if (process_shares_mm(child, p->mm))
 				continue;
+			if (!child->mm ||
+			    test_tsk_thread_flag(child, TIF_MEMDIE))
+				continue;
 			/*
 			 * oom_badness() returns 0 if the thread is unkillable
 			 */
