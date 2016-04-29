390ab63de6dfb44bf1c05bb7f853ad0aae788423
Vladimir Davydov <vdavydov@virtuozzo.com>
2016-04-29 19:28:59 +0400

    oom: zap unused oom_scan_process_thread arguments
    totalpages hasn't been used for ages. force_kill doesn't make sense in
    our case, because we skip TIF_MEMDIE tasks anyway.
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2691,7 +2691,7 @@ __alloc_pages_may_oom(gfp_t gfp_mask, unsigned int order,
 			goto out;
 	}
 	/* Exhausted what can be done so it's blamo time */
-	out_of_memory(zonelist, gfp_mask, order, nodemask, false);
+	out_of_memory(zonelist, gfp_mask, order, nodemask);
 
 out:
 	oom_unlock(NULL);
