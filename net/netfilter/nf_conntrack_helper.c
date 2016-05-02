06d9a9ff1c5a8aa787a673ec8b1aa8ecf8ee30fa
Florian Westphal <fw@strlen.de>
2016-05-02 18:39:55 +0200

    ms/netfilter: conntrack: use a single hashtable for all namespaces
    We already include netns address in the hash and compare the netns pointers
    during lookup, so even if namespaces have overlapping addresses entries
    will be spread across the table.
    
    Assuming 64k bucket size, this change saves 0.5 mbyte per namespace on a
    64bit system.
    
    NAT bysrc and expectation hash is still per namespace, those will
    changed too soon.
    
    Future patch will also make conntrack object slab cache global again.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    https://jira.sw.ru/browse/PSBM-103515
    
    (cherry picked from commit 56d52d4892d0e478a005b99ed10d0a7f488ea8c1)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- net/netfilter/nf_conntrack_helper.c
+++ net/netfilter/nf_conntrack_helper.c
@@ -489,11 +489,11 @@ static void __nf_conntrack_helper_unregister(struct nf_conntrack_helper *me,
 		spin_unlock_bh(&pcpu->lock);
 	}
 	local_bh_disable();
-	for (i = 0; i < net->ct.htable_size; i++) {
+	for (i = 0; i < nf_conntrack_htable_size; i++) {
 		spin_lock(&nf_conntrack_locks[i % CONNTRACK_LOCKS]);
-		if (i < net->ct.htable_size) {
+		if (i < nf_conntrack_htable_size) {
 			gmb();
-			hlist_nulls_for_each_entry(h, nn, &net->ct.hash[i], hnnode)
+			hlist_nulls_for_each_entry(h, nn, &nf_conntrack_hash[i], hnnode)
 				unhelp(h, me);
 		}
 		spin_unlock(&nf_conntrack_locks[i % CONNTRACK_LOCKS]);
