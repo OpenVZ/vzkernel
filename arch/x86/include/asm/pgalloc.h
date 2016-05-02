7019a88e03907331f4c4045e6a001b4c75e9af71
Vladimir Davydov <vdavydov@virtuozzo.com>
2016-05-02 18:08:29 +0400

    arch: x86: fix accounting page tables to kmemcg
    After we switched to the whitelist kmem accounting design, using
    alloc_kmem_pages is not enough for the new page to be accounted -
    one must also pass __GFP_ACCOUNT in gfp flags.
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- arch/x86/include/asm/pgalloc.h
+++ arch/x86/include/asm/pgalloc.h
@@ -92,7 +92,7 @@ static inline void pmd_populate(struct mm_struct *mm, pmd_t *pmd,
 static inline pmd_t *pmd_alloc_one(struct mm_struct *mm, unsigned long addr)
 {
 	struct page *page;
-	page = alloc_pages(GFP_KERNEL | __GFP_REPEAT | __GFP_ZERO, 0);
+	page = alloc_pages(GFP_KERNEL_ACCOUNT | __GFP_REPEAT | __GFP_ZERO, 0);
 	if (!page)
 		return NULL;
 	if (!pgtable_pmd_page_ctor(page)) {
@@ -136,7 +136,7 @@ static inline void pgd_populate(struct mm_struct *mm, pgd_t *pgd, pud_t *pud)
 
 static inline pud_t *pud_alloc_one(struct mm_struct *mm, unsigned long addr)
 {
-	return (pud_t *)__get_free_page(GFP_KERNEL|__GFP_REPEAT|
+	return (pud_t *)__get_free_page(GFP_KERNEL_ACCOUNT|__GFP_REPEAT|
 					__GFP_ZERO);
 }
 
