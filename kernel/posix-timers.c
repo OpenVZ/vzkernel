ac120d7c587d3924683c79e15f6e1cf4b566b1c9
Vladimir Davydov <vdavydov@virtuozzo.com>
2016-05-02 18:06:04 +0400

    memcg: charge kmem allocations accounted to UBC in PCS6 to memcg
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- kernel/posix-timers.c
+++ kernel/posix-timers.c
@@ -370,7 +370,7 @@ static __init int init_posix_timers(void)
 
 	posix_timers_cache = kmem_cache_create("posix_timers_cache",
 					sizeof (struct k_itimer), 0,
-					SLAB_PANIC, NULL);
+					SLAB_PANIC | SLAB_ACCOUNT, NULL);
 	return 0;
 }
 
