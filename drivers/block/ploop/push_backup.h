0e7bacda60b4a58156a0b8afd640ce9b2219773f
Maxim Patlasov <mpatlasov@virtuozzo.com>
2016-05-02 18:40:40 +0400

    ploop: implement PLOOP_IOC_PUSH_BACKUP_IO
    The ioctl(PLOOP_IOC_PUSH_BACKUP_IO) has two mode of operation:
    
    1) ctl.direction=PLOOP_READ tells userspace which cluster-blocks to
    push out-of-band; moves processed preq-s from pending_tree to reported_tree
    
    2) ctl.direction=PLOOP_WRITE tells kernel which cluster-blocks were pushed --
    they are either ordinarily processed preq-s or out-of-band ones; the kernel
    match the blocks to preq-s in reported_tree and re-schedules them.
    
    Signed-off-by: Maxim Patlasov <mpatlasov@virtuozzo.com>
    Acked-by: Dmitry Monakhov <dmonakhov@openvz.org>
    
    +++
    ploop: push_backup: fix reentrance in ploop_pb_get_pending()
    
    The patch implements what Dima Monakhov suggested:
    
    >  AFAIU you have a re-entrance issue if several tasks want performs ioctls
    >   task1:ioctl->wait
    > 		    task2:ioctl->wait
    >
    >   Just change wait sequence like this and you are safe:
    > 		   /* blocking case */
    > 		  if (unlikely(pbd->ppb_waiting))
    > 		       /* Other task is already waitng for event */
    > 		       err = -EBUSY;
    > 		       goto get_pending_unlock;
    > 		  }
    > 		  pbd->ppb_waiting = true;
    > 		  spin_unlock(&pbd->ppb_lock);
    > 		  mutex_unlock(&plo->ctl_mutex);
    
    mFixes commit adcff732cabc43be32649055afd8a1aed41c63d9
    ("ploop: implement PLOOP_IOC_PUSH_BACKUP_IO").
    
    https://jira.sw.ru/browse/PSBM-45000
    
    Signed-off-by: Maxim Patlasov <mpatlasov@virtuozzo.com>

--- drivers/block/ploop/push_backup.h
+++ drivers/block/ploop/push_backup.h
@@ -6,3 +6,8 @@ void ploop_pb_fini(struct ploop_pushbackup_desc *pbd);
 int ploop_pb_copy_cbt_to_user(struct ploop_pushbackup_desc *pbd, char *user_addr);
 unsigned long ploop_pb_stop(struct ploop_pushbackup_desc *pbd);
 int ploop_pb_check_uuid(struct ploop_pushbackup_desc *pbd, __u8 *uuid);
+
+int ploop_pb_get_pending(struct ploop_pushbackup_desc *pbd,
+			 cluster_t *clu_p, cluster_t *len_p, unsigned n_done);
+void ploop_pb_put_reported(struct ploop_pushbackup_desc *pbd,
+			   cluster_t clu, cluster_t len);
