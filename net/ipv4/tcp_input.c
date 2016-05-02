c772f516df42314520cc38df98e45e6ecc623a1a
Vladimir Davydov <vdavydov@virtuozzo.com>
2016-05-02 18:05:59 +0400

    Partially revert "tcp: Charge socket buffers into cg memory"
    This partially reverts commit 4be12b4b184cd000b76883950267ab560ef4935d.
    
    As we are going to switch to the whitelist kmem accounting policy, there
    will be no need in passing __GFP_NOACCOUNT to avoid double accounting of
    tcp socket buffers.
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -4652,7 +4652,7 @@ restart:
 		int copy = min_t(int, SKB_MAX_ORDER(0, 0), end - start);
 		struct sk_buff *nskb;
 
-		nskb = alloc_skb(copy, GFP_ATOMIC|__GFP_NOACCOUNT);
+		nskb = alloc_skb(copy, GFP_ATOMIC);
 		if (!nskb)
 			return;
 
