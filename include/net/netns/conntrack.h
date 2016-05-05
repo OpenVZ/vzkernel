1c17f632584b8b688fce841845bc27c7c69bc91a
Florian Westphal <fw@strlen.de>
2016-05-06 00:51:49 +0200

    ms/netfilter: conntrack: use a single expectation table for all namespaces
    We already include netns address in the hash and compare the netns pointers
    during lookup, so even if namespaces have overlapping addresses entries
    will be spread across the expectation table.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    https://jira.sw.ru/browse/PSBM-103515
    
    (cherry picked from commit 0a93aaedc46af2c5feecfb1066d98bfb491ec0b8)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- include/net/netns/conntrack.h
+++ include/net/netns/conntrack.h
@@ -132,7 +132,6 @@ struct netns_ct {
 
 	RH_KABI_DEPRECATE(seqcount_t, generation)
 	struct kmem_cache	*nf_conntrack_cachep;
-	struct hlist_head	*expect_hash;
 
 	/* next three hlist heads are unused in RHEL,
 	 * only kept for ABI compatibility. */
