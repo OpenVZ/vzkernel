4f7068be11e923a684ed7eaa5909b0b010582083
Vladimir Davydov <vdavydov@virtuozzo.com>
2016-05-13 19:13:56 +0400

    ub: add heuristic check for memory overcommit
    Currently, we only have the hard limit for virtual address space size
    inside containers. This is inflexible, because setting it to a small
    value will cause many normal allocations to fail. BTW that's why it is
    left unlimited by default in Vz7. OTOH allowing an application to
    allocate as much virtual address space as it wants may be bad for some
    application expect to be stopped gracefully by mmap returning ENOMEM
    instead of being killed by OOM.
    
    So this patch introduces the "heuristic" mode of overcommit accounting
    inside containers similar to the one used on most hosts by default
    (vm.overcommit_memory sysctl set to 0). It can be toggled system-wide by
    changing the value of ubc.overcommit_memory sysctl. Per-beancounter
    configuration is not supported yet, but it may be added later if needed.
    
    If enabled (ubc.overcommit_memory = 0, this is the default), an
    application inside a container will fail to allocate a virtual address
    range iff its length is greater than the amount of reclaimable memory
    accounted to the container. Note, the UBC_PRIVVMPAGES limit is still
    taken into account. If disabled (ubc.overcommit_memory = 1), only the
    UBC_PRIVVMPAGES limit will be checked.
    
    https://jira.sw.ru/browse/PSBM-45695
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    Reviewed-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- include/bc/vmpages.h
+++ include/bc/vmpages.h
@@ -17,6 +17,8 @@
 #include <bc/beancounter.h>
 #include <bc/decl.h>
 
+extern int ub_overcommit_memory;
+
 /*
  * Check whether vma has private or copy-on-write mapping.
  */
@@ -46,5 +48,7 @@ UB_DECLARE_FUNC(int, ub_lockedshm_charge(struct shmem_inode_info *shi,
 			unsigned long size))
 UB_DECLARE_VOID_FUNC(ub_lockedshm_uncharge(struct shmem_inode_info *shi,
 			unsigned long size))
+
+UB_DECLARE_FUNC(int, ub_enough_memory(struct mm_struct *mm, long pages))
 
 #endif /* __UB_PAGES_H_ */
