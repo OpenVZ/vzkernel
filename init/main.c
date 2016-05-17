bcb878961d71fb347ecb26913f58dadf160795e2
Vladimir Davydov <vdavydov@virtuozzo.com>
2016-05-17 18:50:45 +0400

    ub: init ub0 bound css early
    It may occur that a usermodehelper is scheduled before ub0 bound css is
    initialized during late init. If this happens, the system will crash in
    ub_enough_memory, which needs to dereference bound css.
    
    To fix that let's initialize bound css right after cgroup_init.
    
    https://jira.sw.ru/browse/PSBM-47147
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- init/main.c
+++ init/main.c
@@ -636,7 +636,6 @@ asmlinkage void __init start_kernel(void)
 	cred_init();
 	fork_init(totalram_pages);
 	proc_caches_init();
-	ub_init_late();
 	buffer_init();
 	key_init();
 	security_init();
@@ -649,6 +648,7 @@ asmlinkage void __init start_kernel(void)
 	proc_root_init();
 #endif
 	cgroup_init();
+	ub_init_late();
 	cpuset_init();
 	taskstats_init_early();
 	delayacct_init();
