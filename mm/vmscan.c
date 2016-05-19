22154d1245de0ebed14635568bfeaf2810bd5720
Vladimir Davydov <vdavydov@virtuozzo.com>
2016-05-19 15:23:42 +0400

    ve/mm: introduce min threshold for dcache
    This patch adds new sysctl vm.vfs_cache_min_ratio. If the ratio of
    reclaimable slabs (i.e. dcache and icache) to total memory usage of a
    cgroup is less than the value of this sysctl (2% by default), slabs
    won't be reclaimed from this cgroup on memory pressure.
    
    https://jira.sw.ru/browse/PSBM-34161
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -498,7 +498,7 @@ static unsigned long do_shrink_slab(struct shrink_control *shrinkctl,
  */
 static unsigned long shrink_slab(gfp_t gfp_mask, int nid,
 				 struct mem_cgroup *memcg,
-				 int priority)
+				 int priority, bool for_drop_caches)
 {
 	struct shrinker *shrinker;
 	unsigned long freed = 0;
@@ -517,6 +517,7 @@ static unsigned long shrink_slab(gfp_t gfp_mask, int nid,
 			.gfp_mask = gfp_mask,
 			.nid = nid,
 			.memcg = memcg,
+			.for_drop_caches = for_drop_caches,
 		};
 
 		/*
@@ -558,7 +559,7 @@ void drop_slab_node(int nid)
 
 		freed = 0;
 		do {
-			freed += shrink_slab(GFP_KERNEL, nid, memcg, 0);
+			freed += shrink_slab(GFP_KERNEL, nid, memcg, 0, true);
 		} while ((memcg = mem_cgroup_iter(NULL, memcg, NULL)) != NULL);
 	} while (freed > 10);
 }
@@ -2572,7 +2573,7 @@ static bool shrink_node(pg_data_t *pgdat, struct scan_control *sc)
 
 			if (memcg)
 				shrink_slab(sc->gfp_mask, pgdat->node_id,
-					    memcg, sc->priority);
+					    memcg, sc->priority, false);
 
 			/* Record the group's reclaim efficiency */
 			vmpressure(sc->gfp_mask, memcg, false,
@@ -2598,7 +2599,7 @@ static bool shrink_node(pg_data_t *pgdat, struct scan_control *sc)
 
 		if (global_reclaim(sc))
 			shrink_slab(sc->gfp_mask, pgdat->node_id, NULL,
-				    sc->priority);
+				    sc->priority, false);
 
 		if (reclaim_state) {
 			sc->nr_reclaimed += reclaim_state->reclaimed_slab;
