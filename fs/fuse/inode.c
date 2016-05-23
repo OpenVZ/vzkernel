85ff9d6040dd563cbc1b14d94e7460bf0490f5a5
Andrey Zaitsev <azaitsev@virtuozzo.com>
2016-05-23 16:47:14 +0400

    fuse: enable synchronous close for both "pstorage://" and "vstorage://" fs types
    At the moment FUSE enables the synchronous close features for
    pstorage:// mount points.
    
    After rename pStorage -> vStorage FUSE must do it for
    vstorage:// mount points as well.
    
    https://jira.sw.ru/browse/PSBM-47206
    
    To_merge: ("fuse: reimplement synchronous close")
    
    Signed-off-by: Zaitsev Andrey <azaitsev@virtuozzo.com>
    
    +++
    fuse: fix fs context
    
    "source" option is parsed before all others.
    So, during OPT_SOURCE parse disable_close_wait is never set.
    
    int do_new_mount()
    {
           vfs_parse_fs_string(fc, "source", name, strlen(name));
           parse_monolithic_mount_data(fc, data);
    }
    
    https://jira.sw.ru/browse/PSBM-131961
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    (cherry picked from commit d52f7c8c1bd6e6b57fe891fea1cc50b9520492ad)
    Signed-off-by: Liu Kui <Kui.Liu@acronis.com>
    Feature: vStorage

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -700,10 +700,10 @@ static int fuse_parse_param(struct fs_context *fc, struct fs_parameter *param)
 		/* Hack to distinguish pcs fuse service and to force
 		 * synchronous close for it.
 		 */
-		if (fc->source && strncmp(fc->source, "pstorage://", 11) == 0) {
-			if (!ctx->disable_close_wait)
-				ctx->close_wait = 1;
-
+		if (fc->source &&
+		    (strncmp(fc->source, "pstorage://", 11) == 0 ||
+		     strncmp(fc->source, "vstorage://", 11) == 0)) {
+			ctx->close_wait = 1;
 			ctx->compat_inval_files = 1;
 		}
 		break;
@@ -779,6 +779,7 @@ static int fuse_parse_param(struct fs_context *fc, struct fs_parameter *param)
 
 	case OPT_DISABLE_CLOSE_WAIT:
 		ctx->disable_close_wait = 1;
+		ctx->close_wait = 0;
 		break;
 
 	default:
