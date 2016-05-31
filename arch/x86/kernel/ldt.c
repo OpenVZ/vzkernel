0dd239ab6e6afbaa3caa51673a6074370a85ad26
Vladimir Davydov <vdavydov@virtuozzo.com>
2016-05-31 13:14:07 +0400

    mm: Drop alloc_kmem_pages and friends
    Patchset description:
    Some kmemcg related fixes
    
    This patch set backports some changes from the following patch set
    submitted upstream:
    
      lkml.kernel.org/r/cover.1464079537.git.vdavydov@virtuozzo.com
    
    [hasn't been merged yet]
    
    namely:
     - move kmemcg charge/uncharge to generic allocator paths
     - fix pipe buffer stealing
     - avoid charging kernel page tables
     - account unix socket buffers to kmemcg (PSBM-34562)
    
    Vladimir Davydov (7):
      Drop alloc_kmem_pages and friends
      mm: memcontrol: drop memcg_kmem_commit_charge
      Move PageBalloon and PageBuddy helpers to page-flags.h
      mm: charge/uncharge kmemcg from generic page allocator paths
      af_unix: charge buffers to kmemcg
      pipe: uncharge page on ->steal
      arch: x86: don't charge kernel page tables to kmemcg
    
    =======================
    This patch description:
    
    These functions work exactly like alloc_pages and others except they
    will charge allocated page to current memcg if __GFP_ACCOUNT is passed.
    In the next patch I'm going to move charge/uncharge to generic
    allocation paths, so that these special helpers won't be necessary.
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- arch/x86/kernel/ldt.c
+++ arch/x86/kernel/ldt.c
@@ -83,7 +83,7 @@ static int alloc_ldt(mm_context_t *pc, int mincount, int reload)
 		if (oldsize * LDT_ENTRY_SIZE > PAGE_SIZE)
 			vfree(oldldt);
 		else
-			put_page(virt_to_page(oldldt));
+			__free_page(virt_to_page(oldldt));
 	}
 	return 0;
 }
@@ -138,7 +138,7 @@ void destroy_context(struct mm_struct *mm)
 		if (mm->context.size * LDT_ENTRY_SIZE > PAGE_SIZE)
 			vfree(mm->context.ldt);
 		else
-			put_page(virt_to_page(mm->context.ldt));
+			__free_page(virt_to_page(mm->context.ldt));
 		mm->context.size = 0;
 	}
 }
