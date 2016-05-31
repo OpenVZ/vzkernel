32b9f6735fcebcb6f9e23578c1300b56bff7fd57
Vladimir Davydov <vdavydov@virtuozzo.com>
2016-05-31 13:14:09 +0400

    mm: charge/uncharge kmemcg from generic page allocator paths
    Currently, to charge a non-slab allocation to kmemcg one has to use
    alloc_kmem_pages helper with __GFP_ACCOUNT flag. A page allocated with
    this helper should finally be freed using free_kmem_pages, otherwise it
    won't be uncharged.
    
    This API suits its current users fine, but it turns out to be impossible
    to use along with page reference counting, i.e. when an allocation is
    supposed to be freed with put_page, as it is the case with pipe or unix
    socket buffers.
    
    To overcome this limitation, this patch moves charging/uncharging to
    generic page allocator paths, i.e. to __alloc_pages_nodemask and
    free_pages_prepare, and zaps alloc/free_kmem_pages helpers. This way,
    one can use any of the available page allocation functions to get the
    allocated page charged to kmemcg - it's enough to pass __GFP_ACCOUNT,
    just like in case of kmalloc and friends. A charged page will be
    automatically uncharged on free.
    
    To make it possible, we need to mark pages charged to kmemcg somehow. To
    avoid introducing a new page flag, we make use of page->_mapcount for
    marking such pages. Since pages charged to kmemcg are not supposed to be
    mapped to userspace, it should work just fine. There are other (ab)users
    of page->_mapcount - buddy and balloon pages - but we don't conflict
    with them.
    
    In case kmemcg is compiled out or not used at runtime, this patch
    introduces no overhead to generic page allocator paths. If kmemcg is
    used, it will be plus one gfp flags check on alloc and plus one
    page->_mapcount check on free, which shouldn't hurt performance, because
    the data accessed are hot.
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    
    +++
    ve/mm: fix PAGE_KMEMCG_MAPCOUNT_VALUE
    
    It should be -512, -256 is used for balloon pages.
    
    mFixes: 1265d3474391 ("mm: charge/uncharge kmemcg from generic page allocator paths")
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -586,6 +586,25 @@ PAGE_MAPCOUNT_OPS(Balloon, BALLOON)
 #define PAGE_OFFLINE_MAPCOUNT_VALUE		(-512)
 PAGE_MAPCOUNT_OPS(Offline, OFFLINE)
 
+#define PAGE_KMEMCG_MAPCOUNT_VALUE (-512)
+
+static inline int PageKmemcg(struct page *page)
+{
+	return atomic_read(&page->_mapcount) == PAGE_KMEMCG_MAPCOUNT_VALUE;
+}
+
+static inline void __SetPageKmemcg(struct page *page)
+{
+	VM_BUG_ON_PAGE(atomic_read(&page->_mapcount) != -1, page);
+	atomic_set(&page->_mapcount, PAGE_KMEMCG_MAPCOUNT_VALUE);
+}
+
+static inline void __ClearPageKmemcg(struct page *page)
+{
+	VM_BUG_ON_PAGE(!PageKmemcg(page), page);
+	atomic_set(&page->_mapcount, -1);
+}
+
 /*
  * If network-based swap is enabled, sl*b must keep track of whether pages
  * were allocated from pfmemalloc reserves.
