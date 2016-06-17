fef4b728a427682e8a3c856a193416746cb5bf0a
Andrey Ryabinin <aryabinin@virtuozzo.com>
2016-06-17 13:26:15 +0400

    ve/device_cgroup: kill ACC_QUOTA permission
    This is a leftover from PCS6. Currently this code does absolutely
    nothing, so let's remove it.
    
    https://jira.sw.ru/browse/PSBM-48482
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    khorenko@: keep MAY_QUOTACTL and ACC_QUOTA defines with
    comment about deprecation.

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -79,7 +79,7 @@ typedef void (dio_iodone_t)(struct kiocb *iocb, loff_t offset,
 /* called from RCU mode, don't block */
 #define MAY_NOT_BLOCK		0x00000080
 /* for devgroup-vs-openvz only */
-#define MAY_QUOTACTL		0x00010000
+#define MAY_QUOTACTL		0x00010000	/* deprecated */
 #define MAY_MOUNT		0x00020000
 
 /*
