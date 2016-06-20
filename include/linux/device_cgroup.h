051af32431a5e661a6d8166330e866e3104cb3c9
Vladimir Davydov <vdavydov@virtuozzo.com>
2016-06-20 19:40:13 +0300

    Use ve init task's css instead of opening cgroup via vfs
    Currently, whenever we need to get cpu or devices cgroup corresponding
    to a ve, we open it using cgroup_kernel_open(). This is inflexible,
    because it relies on the fact that all container cgroups are located at
    a specific location which can never change (at the top level). Since we
    want to move container cgroups to machine.slice, we need to rework this.
    
    This patch does the trick. It makes each ve remember its init task at
    container start, and use css corresponding to init task whenever we need
    to get a corresponding cgroup. Note, that after this patch is applied,
    we don't need to mount cpu and devices cgroup in kernel.
    
    https://jira.sw.ru/browse/PSBM-48629
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    
    +++
    ve/sched: Make functions parameters differ from global variable name
    
    We already have avenrun global varibale, and the function
    parameters and local variable with the same names confuse
    reader. Rename them in sane way.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    +++
    ve/sched: fix Containers loadavg calculation provided via KSTAT ioctl
    
    After cleanup ioctl(VZCTL_GET_CPU_STAT) returns loadavg of host
    for any Container due to lost variable rename.
    This that.
    
    mFixes 36839b0 ("ve/sched: Make functions parameters differ from global variable
    name")
    
    https://jira.sw.ru/browse/PSBM-74490
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ve/cpustat: don't try to update vcpustats for root_task_group
    
    root_task_group doesn't have vcpu stats. Attempt to update them leads
    to NULL-ptr deref:
    
    	BUG: unable to handle kernel NULL pointer dereference at           (null)
    	IP: [<ffffffff810b440c>] cpu_cgroup_update_vcpustat+0x13c/0x620
    	...
    	Call Trace:
    	 [<ffffffff810bee3b>] cpu_cgroup_get_stat+0x7b/0x180
    	 [<ffffffff810f1ef7>] ve_get_cpu_stat+0x27/0x70
    	 [<ffffffffa01836a1>] fill_cpu_stat+0x91/0x1e0 [vzmon]
    	 [<ffffffffa0183c6b>] vzcalls_ioctl+0x2bb/0x430 [vzmon]
    	 [<ffffffffa018d0d5>] vzctl_ioctl+0x45/0x60 [vzdev]
    	 [<ffffffff8120cfb5>] do_vfs_ioctl+0x255/0x4f0
    	 [<ffffffff8120d2a4>] SyS_ioctl+0x54/0xa0
    	 [<ffffffff81642ac9>] system_call_fastpath+0x16/0x1b
    
    So, return -ENOENT if we asked for vcpu stats of root_task_group.
    
    https://jira.sw.ru/browse/PSBM-48721
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Reviewed-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- include/linux/device_cgroup.h
+++ include/linux/device_cgroup.h
@@ -16,10 +16,9 @@ extern int devcgroup_device_permission(umode_t mode, dev_t dev, int mask);
 extern int devcgroup_device_visible(umode_t mode, int major,
 		int start_minor, int nr_minors);
 
-struct cgroup;
-int devcgroup_set_perms_ve(struct cgroup *cgroup, unsigned, dev_t, unsigned);
 struct ve_struct;
-int devcgroup_seq_show_ve(struct cgroup *devices_root, struct ve_struct *ve, struct seq_file *m);
+int devcgroup_set_perms_ve(struct ve_struct *, unsigned, dev_t, unsigned);
+int devcgroup_seq_show_ve(struct ve_struct *, struct seq_file *);
 
 #else
 static inline int devcgroup_inode_permission(struct inode *inode, int mask)
