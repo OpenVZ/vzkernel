94fa3ac65bd63108258ff138fc9427fba294d9f6
Maxim Patlasov <mpatlasov@virtuozzo.com>
2016-06-27 11:10:24 +0400

    ploop: remove preflush from dio_submit
    After commit c2247f3745 fixing barriers for ordinary
    requests and previous patch fixing delay_fua,
    that legacy code in dio_submit processing
    (preq->req_rw & REQ_FLUSH) by setting REQ_FLUSH in
    the first outgoing bio must die: it is incorrect
    anyway (we don't wait for completion of the first
    bio before sending others).
    
    Signed-off-by: Maxim Patlasov <mpatlasov@virtuozzo.com>
    Acked-by: Dmitry Monakhov <dmonakhov@virtuozzo.com>

--- drivers/block/ploop/io_direct.c
+++ drivers/block/ploop/io_direct.c
@@ -82,14 +82,11 @@ dio_submit(struct ploop_io *io, struct ploop_request * preq,
 	sector_t sec, nsec;
 	int err;
 	struct bio_list_walk bw;
-	int preflush;
 	int postfua = 0;
 	int write = !!(rw & REQ_WRITE);
 	int delayed_fua = 0;
 
 	trace_submit(preq);
-
-	preflush = !!(rw & REQ_FLUSH);
 
 	if (test_and_clear_bit(PLOOP_REQ_FORCE_FUA, &preq->state))
 		postfua = 1;
@@ -230,10 +227,6 @@ flush_bio:
 		b->bi_private = preq;
 		b->bi_end_io = dio_endio_async;
 
-		if (unlikely(preflush)) {
-			rw2 |= REQ_FLUSH;
-			preflush = 0;
-		}
 		if (unlikely(postfua && !bl.head))
 			rw2 |= REQ_FUA;
 
