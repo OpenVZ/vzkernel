66489b239b619366ac984e5f5553d0c456f14182
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2016-08-09 18:00:48 +0400

    ve/pid: Export kernel.pid_max via ve cgroup
    This member represents kernel.pid_max sysctl it is vz-specific but
    lays on pid namespace. To be able to c/r from libvzctl script it is
    better put pid_max in ve cgroup, these way we do not need to enter
    container root pid namespace to get/set these sysctl.
    
    Add ve_write_running_u64 helper to be able to set pid_max on running
    container, as we can't set pid_max before we have ve's pidns.
    
    https://jira.sw.ru/browse/PSBM-48397
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Acked-by: Cyrill Gorcunov <gorcunov@openvz.org>

--- kernel/ve/ve.c
+++ kernel/ve/ve.c
@@ -1199,6 +1199,7 @@ enum {
 	VE_CF_CLOCK_MONOTONIC,
 	VE_CF_CLOCK_BOOTBASED,
 	VE_CF_AIO_MAX_NR,
+	VE_CF_PID_MAX,
 	VE_CF_NETNS_MAX_NR,
 	VE_CF_NETNS_NR,
 	VE_CF_NETIF_MAX_NR,
@@ -1264,7 +1265,11 @@ static u64 ve_read_u64(struct cgroup *cg, struct cftype *cft)
 		return cgroup_ve(cg)->is_pseudosuper;
 	else if (cft->private == VE_CF_AIO_MAX_NR)
 		return cgroup_ve(cg)->aio_max_nr;
-	else if (cft->private == VE_CF_NETNS_MAX_NR)
+	else if (cft->private == VE_CF_PID_MAX) {
+		struct ve_struct *ve = cgroup_ve(cg);
+		if (ve->ve_ns && ve->ve_ns->pid_ns)
+			return ve->ve_ns->pid_ns->pid_max;
+	} else if (cft->private == VE_CF_NETNS_MAX_NR)
 		return cgroup_ve(cg)->netns_max_nr;
 	else if (cft->private == VE_CF_NETNS_NR)
 		return atomic_read(&cgroup_ve(cg)->netns_avail_nr);
@@ -1307,7 +1312,26 @@ static int ve_write_pseudosuper(struct cgroup *cg,
 	return 0;
 }
 
-static int ve_write_u64(struct cgroup *cg, struct cftype *cft, u64 value)
+extern int pid_max_min, pid_max_max;
+
+static int ve_write_pid_max(struct cgroup *cg,
+			    struct cftype *cft,
+			    u64 value)
+{
+	struct ve_struct *ve = cgroup_ve(cg);
+	if (!ve->ve_ns || !ve->ve_ns->pid_ns)
+		return -EBUSY;
+
+	if (pid_max_min > value ||
+	     pid_max_max < value)
+		return -EINVAL;
+
+	ve->ve_ns->pid_ns->pid_max = value;
+	return 0;
+}
+
+static int _ve_write_u64(struct cgroup *cg, struct cftype *cft,
+                         u64 value, int running)
 {
 	struct ve_struct *ve = cgroup_ve(cg);
 
@@ -1316,7 +1340,7 @@ static int ve_write_u64(struct cgroup *cg, struct cftype *cft, u64 value)
 		return -EPERM;
 
 	down_write(&ve->op_sem);
-	if (ve->is_running || ve->ve_ns) {
+	if (!running && (ve->is_running || ve->ve_ns)) {
 		up_write(&ve->op_sem);
 		return -EBUSY;
 	}
@@ -1329,7 +1353,12 @@ static int ve_write_u64(struct cgroup *cg, struct cftype *cft, u64 value)
 #endif
 	else if (cft->private == VE_CF_AIO_MAX_NR)
 		ve->aio_max_nr = value;
-	else if (cft->private == VE_CF_NETNS_MAX_NR) {
+	else if (cft->private == VE_CF_PID_MAX) {
+		int ret;
+		ret = ve_write_pid_max(cg, cft, value);
+		up_write(&ve->op_sem);
+		return ret;
+	} else if (cft->private == VE_CF_NETNS_MAX_NR) {
 		int delta = value - ve->netns_max_nr;
 
 		ve->netns_max_nr = value;
@@ -1342,6 +1371,16 @@ static int ve_write_u64(struct cgroup *cg, struct cftype *cft, u64 value)
 	}
 	up_write(&ve->op_sem);
 	return 0;
+}
+
+static int ve_write_u64(struct cgroup *cg, struct cftype *cft, u64 value)
+{
+	return _ve_write_u64(cg, cft, value, 0);
+}
+
+static int ve_write_running_u64(struct cgroup *cg, struct cftype *cft, u64 value)
+{
+	return _ve_write_u64(cg, cft, value, 1);
 }
 
 static struct cftype ve_cftypes[] = {
@@ -1415,6 +1454,13 @@ static struct cftype ve_cftypes[] = {
 		.read_u64		= ve_read_u64,
 		.write_u64		= ve_write_u64,
 		.private		= VE_CF_AIO_MAX_NR,
+	},
+	{
+		.name			= "pid_max",
+		.flags			= CFTYPE_NOT_ON_ROOT,
+		.read_u64		= ve_read_u64,
+		.write_u64		= ve_write_running_u64,
+		.private		= VE_CF_PID_MAX,
 	},
 	{
 		.name			= "netns_max_nr",
