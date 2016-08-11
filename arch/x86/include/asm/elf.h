4613ba3a35666b5e07e3a33242474a08aee3d2bd
Dmitry Safonov <dsafonov@virtuozzo.com>
2016-08-11 13:28:04 +0400

    x86/arch_prctl/vdso: add ARCH_MAP_VDSO_*
    Add API to change vdso blob type with arch_prctl.
    As this is usefull only by needs of CRIU, expose
    this interface under CONFIG_CHECKPOINT_RESTORE.
    
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    
    [Differences to vanilla patches:
     o API only for 32-bit vDSO mapping
     o unmap previous vdso just by mm->context.vdso pointer]
    
    https://jira.sw.ru/browse/PSBM-28517
    
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>

--- arch/x86/include/asm/elf.h
+++ arch/x86/include/asm/elf.h
@@ -336,6 +336,10 @@ extern int x32_setup_additional_pages(struct linux_binprm *bprm,
 extern int syscall32_setup_pages(struct linux_binprm *, int exstack);
 #define compat_arch_setup_additional_pages	syscall32_setup_pages
 
+#ifdef CONFIG_X86_64
+extern int do_map_compat_vdso(unsigned long addr);
+#endif
+
 extern unsigned long arch_randomize_brk(struct mm_struct *mm);
 #define arch_randomize_brk arch_randomize_brk
 
