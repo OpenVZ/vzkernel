05f328375183dc65abe0b6a7018da531dd6fcaba
Dmitry Safonov <dsafonov@virtuozzo.com>
2016-08-11 13:28:04 +0400

    ms/x86/signal: add SA_{X32,IA32}_ABI sa_flags
    Introduce new flags that defines which ABI to use on creating sigframe.
    Those flags kernel will set according to sigaction syscall ABI,
    which set handler for the signal being delivered.
    
    So that will drop the dependency on TIF_IA32/TIF_X32 flags on signal deliver.
    Those flags will be used only under CONFIG_COMPAT.
    
    Similar way ARM uses sa_flags to differ in which mode deliver signal
    for 26-bit applications (look at SA_THIRYTWO).
    
    Cc: Oleg Nesterov <oleg@redhat.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Cyrill Gorcunov <gorcunov@openvz.org>
    Cc: Pavel Emelyanov <xemul@virtuozzo.com>
    Cc: x86@kernel.org
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    
    Reviewed-by: Andy Lutomirski <luto@kernel.org>
    
    [backport notes: left non-const argument for copy_siginfo_to_user32(),
    x86-specific code in v4.6 differs with v3.10 by file-location mostly,
    didn't port my patch about renaming is_{ia32,x32}_task to
    in_{ia32,x32}_syscall.]
    
    https://jira.sw.ru/browse/PSBM-28517
    
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -3147,6 +3147,11 @@ COMPAT_SYSCALL_DEFINE4(rt_tgsigqueueinfo,
 }
 #endif
 
+void __weak sigaction_compat_abi(struct k_sigaction *act,
+		struct k_sigaction *oact)
+{
+}
+
 int do_sigaction(int sig, struct k_sigaction *act, struct k_sigaction *oact)
 {
 	struct task_struct *t = current;
@@ -3161,6 +3166,8 @@ int do_sigaction(int sig, struct k_sigaction *act, struct k_sigaction *oact)
 	spin_lock_irq(&current->sighand->siglock);
 	if (oact)
 		*oact = *k;
+
+	sigaction_compat_abi(act, oact);
 
 	if (act) {
 		sigdelsetmask(&act->sa.sa_mask,
