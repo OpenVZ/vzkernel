896f1c67b5e15bc72ba48c6004c7916ad3423710
Brian Gerst <brgerst@gmail.com>
2016-08-13 12:38:18 -0400

    ms/sched/x86: Add 'struct inactive_task_frame' to better document the sleeping task stack frame
    Add 'struct inactive_task_frame', which defines the layout of the stack for
    a sleeping process.  For now, the only defined field is the BP register
    (frame pointer).
    
    Signed-off-by: Brian Gerst <brgerst@gmail.com>
    Reviewed-by: Josh Poimboeuf <jpoimboe@redhat.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Denys Vlasenko <dvlasenk@redhat.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1471106302-10159-4-git-send-email-brgerst@gmail.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    (cherry picked from commit 7b32aeadbc95d4a41402c1c0da6aa3ab51af4c10)
    In the scope of https://jira.sw.ru/browse/HCI-171
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- arch/x86/kernel/process.c
+++ arch/x86/kernel/process.c
@@ -32,6 +32,7 @@
 #include <asm/mce.h>
 #include <asm/prctl.h>
 #include <asm/spec_ctrl.h>
+#include <asm/switch_to.h>
 
 /*
  * per-CPU TSS segments. Threads are completely 'soft' on Linux,
@@ -752,7 +753,7 @@ unsigned long get_wchan(struct task_struct *p)
 	if (sp < bottom || sp > top)
 		return 0;
 
-	fp = READ_ONCE_NOCHECK(*(unsigned long *)sp);
+	fp = READ_ONCE_NOCHECK(((struct inactive_task_frame *)sp)->bp);
 	do {
 		if (fp < bottom || fp > top)
 			return 0;
