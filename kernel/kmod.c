967e70147432f28983c0b728364f19cdecdb5a36
Dmitry Safonov <dsafonov@virtuozzo.com>
2016-09-01 16:15:55 +0400

    ve/kmod/whitelist: allow overlay fs module autoloading
    Note: overlayfs will not be allowed to be mounted inside a CT until admin
    enables experimental fs. To do it, write to the process knob:
    # echo 1 > /proc/sys/fs/experimental_fs_enable
    
    https://jira.sw.ru/browse/PSBM-51043
    
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    Acked-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- kernel/kmod.c
+++ kernel/kmod.c
@@ -378,6 +378,7 @@ static inline int module_payload_iptable_allowed(const char *module)
 /* ve0 allowed modules */
 static const char * const ve0_allowed_mod[] = {
 	"fs-binfmt_misc",
+	"fs-overlay",
 
 	/* inet_diag, inet6_diag  */
 	"net-pf-16-proto-4-type-2",	/* PF_NETLINK, NETLINK_SOCK_DIAG, AF_INET */
