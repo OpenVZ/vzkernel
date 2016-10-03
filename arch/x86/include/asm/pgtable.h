43ec3acd044cf075e113104e6dad19e2502322fe
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2016-10-03 14:52:38 +0000

    ms/x86: drop _PAGE_FILE and pte_file()-related helpers
    We've replaced remap_file_pages(2) implementation with emulation.  Nobody
    creates non-linear mapping anymore.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    https://jira.sw.ru/browse/PSBM-52992
    
    (cherry picked from commit 0a191362058391878cc2a4d4ccddcd8223eb4f79)
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Reviewed-by: Cyrill Gorcunov <gorcunov@openvz.org>
    
    +++
    x86: drop _PAGE_BIT_FILE leftover
    
    Added into commit c5e17859674db4936f2b11e3cc875adc806c0844
    ("ms/x86: drop _PAGE_FILE and pte_file()-related helpers")
    
    https://jira.sw.ru/browse/PSBM-54928
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    x86: drop _PAGE_BIT_FILE leftover (part2)
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- arch/x86/include/asm/pgtable.h
+++ arch/x86/include/asm/pgtable.h
@@ -147,11 +147,6 @@ static inline int pte_write(pte_t pte)
 	return pte_flags(pte) & _PAGE_RW;
 }
 
-static inline int pte_file(pte_t pte)
-{
-	return pte_flags(pte) & _PAGE_FILE;
-}
-
 static inline int pte_huge(pte_t pte)
 {
 	return pte_flags(pte) & _PAGE_PSE;
@@ -483,21 +478,6 @@ static inline pmd_t pmd_mksoft_dirty(pmd_t pmd)
 static inline pud_t pud_mksoft_dirty(pud_t pud)
 {
 	return pud_set_flags(pud, _PAGE_SOFT_DIRTY);
-}
-
-static inline pte_t pte_file_clear_soft_dirty(pte_t pte)
-{
-	return pte_clear_flags(pte, _PAGE_SOFT_DIRTY);
-}
-
-static inline pte_t pte_file_mksoft_dirty(pte_t pte)
-{
-	return pte_set_flags(pte, _PAGE_SOFT_DIRTY);
-}
-
-static inline int pte_file_soft_dirty(pte_t pte)
-{
-	return pte_flags(pte) & _PAGE_SOFT_DIRTY;
 }
 
 static inline pte_t pte_swp_mksoft_dirty(pte_t pte)
