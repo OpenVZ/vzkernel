cdfd8f539753a1e87c048c5819f64378805e9ae5
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2016-10-03 14:34:14 +0000

    ms/mm: drop vm_ops->remap_pages and generic_file_remap_pages() stub
    Nobody uses it anymore.
    
    [akpm@linux-foundation.org: fix filemap_xip.c]
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Wu Fengguang <fengguang.wu@intel.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    https://jira.sw.ru/browse/PSBM-52992
    
    (cherry picked from commit d83a08db5ba6072caa658745881f4baa9bad6a08)
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Reviewed-by: Cyrill Gorcunov <gorcunov@openvz.org>
    
    +++
    fs/ext4: remove generic_file_remap_pages
    
    This helper was removed by ms commit d83a08db5ba6 ("mm: drop
    vm_ops->remap_pages and generic_file_remap_pages() stub").
    
    The helper was used for remap_file_pages syscall, which from ~v4.0
    (and in .493 kernel) uses emulation by inserting multiple VMAs,
    see ms commit c8d78c1823f4 ("mm: replace remap_file_pages() syscall
    with emulation").
    
    To be squashed into commit 53dba9e212cc3f440acc60a8d333df3967931937
    ("Initial patch commit + compilation fixes").
    
    https://jira.sw.ru/browse/PSBM-54928
    
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -385,10 +385,6 @@ struct vm_operations_struct {
 	int (*migrate)(struct vm_area_struct *vma, const nodemask_t *from,
 		const nodemask_t *to, unsigned long flags);
 #endif
-	/* called by sys_remap_file_pages() to populate non-linear mapping */
-	int (*remap_pages)(struct vm_area_struct *vma, unsigned long addr,
-			   unsigned long size, pgoff_t pgoff);
-
 	/* same as page_mkwrite when using VM_PFNMAP|VM_MIXEDMAP */
 	RH_KABI_EXTEND(int (*pfn_mkwrite)(struct vm_area_struct *vma, struct vm_fault *vmf))
 	RH_KABI_EXTEND(int (*huge_fault)(struct vm_fault *vmf,
