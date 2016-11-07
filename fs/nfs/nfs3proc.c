6b5c45149fb1dc593c690245c55d790da7cf3549
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-11-07 11:38:51 +0400

    ms/NFS/CIFS/SUNRPC: don't allow to freeze execution
    Here is a deadlock scheme for 2 processes in one freezer cgroup, which is
    freezing:
    
    CPU 0                                   CPU 1
    --------                                --------
    do_last
    inode_lock(dir->d_inode)
    vfs_create
    nfs_create
    ...
    __rpc_execute
    rpc_wait_bit_killable
    __refrigerator
    				    do_last
    				    inode_lock(dir->d_inode)
    
    So, the problem is that one process takes directory inode mutex, executes
    creation request and goes to refrigerator.
    Another one waits till directory lock is released, remains "thawed" and
    thus
    freezer cgroup state never becomes "FROZEN".
    
    Notes:
    1) Interesting, that this is not a pure deadlock: one can thaw cgroup and
    then
    freeze it again.
    2) The issue was introduced by commit
    d310310cbff18ec385c6ab4d58f33b100192a96a.
    3) This patch is not aimed to fix the issue, but to show the problem root.
    Look like this problem might be applicable to other hunks from the commit,
    mentioned above.
    
    Reverts upstream commit: d310310cbff18ec385c6ab4d58f33b100192a96a
    Upstream discussion: https://lkml.org/lkml/2016/8/3/317
    
    https://jira.sw.ru/browse/PSBM-50671
    https://jira.sw.ru/browse/PSBM-54822
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- fs/nfs/nfs3proc.c
+++ fs/nfs/nfs3proc.c
@@ -17,7 +17,6 @@
 #include <linux/nfs_page.h>
 #include <linux/lockd/bind.h>
 #include <linux/nfs_mount.h>
-#include <linux/freezer.h>
 
 #include "iostat.h"
 #include "internal.h"
@@ -34,7 +33,7 @@ nfs3_rpc_wrapper(struct rpc_clnt *clnt, struct rpc_message *msg, int flags)
 		res = rpc_call_sync(clnt, msg, flags);
 		if (res != -EJUKEBOX)
 			break;
-		freezable_schedule_timeout_killable_unsafe(NFS_JUKEBOX_RETRY_TIME);
+		schedule_timeout_killable(NFS_JUKEBOX_RETRY_TIME);
 		res = -ERESTARTSYS;
 	} while (!fatal_signal_pending(current));
 	return res;
