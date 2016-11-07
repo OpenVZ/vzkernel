90446f11647d3f54821b84bed41cc6f8a61711b5
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2016-11-07 11:38:51 +0400

    ms/NFS/CIFS/SUNRPC: don't allow to freeze execution
    Here is a deadlock scheme for 2 processes in one freezer cgroup, which is
    freezing:
    
    CPU 0                                   CPU 1
    --------                                --------
    do_last
    inode_lock(dir->d_inode)
    vfs_create
    nfs_create
    ...
    __rpc_execute
    rpc_wait_bit_killable
    __refrigerator
    				    do_last
    				    inode_lock(dir->d_inode)
    
    So, the problem is that one process takes directory inode mutex, executes
    creation request and goes to refrigerator.
    Another one waits till directory lock is released, remains "thawed" and
    thus
    freezer cgroup state never becomes "FROZEN".
    
    Notes:
    1) Interesting, that this is not a pure deadlock: one can thaw cgroup and
    then
    freeze it again.
    2) The issue was introduced by commit
    d310310cbff18ec385c6ab4d58f33b100192a96a.
    3) This patch is not aimed to fix the issue, but to show the problem root.
    Look like this problem might be applicable to other hunks from the commit,
    mentioned above.
    
    Reverts upstream commit: d310310cbff18ec385c6ab4d58f33b100192a96a
    Upstream discussion: https://lkml.org/lkml/2016/8/3/317
    
    https://jira.sw.ru/browse/PSBM-50671
    https://jira.sw.ru/browse/PSBM-54822
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- include/linux/freezer.h
+++ include/linux/freezer.h
@@ -172,14 +172,6 @@ static inline void freezable_schedule(void)
 	freezer_count();
 }
 
-/* DO NOT ADD ANY NEW CALLERS OF THIS FUNCTION */
-static inline void freezable_schedule_unsafe(void)
-{
-	freezer_do_not_count();
-	schedule();
-	freezer_count_unsafe();
-}
-
 /*
  * Like freezable_schedule_timeout(), but should not block the freezer.  Do not
  * call this with locks held.
@@ -213,16 +205,6 @@ static inline long freezable_schedule_timeout_killable(long timeout)
 	freezer_do_not_count();
 	__retval = schedule_timeout_killable(timeout);
 	freezer_count();
-	return __retval;
-}
-
-/* DO NOT ADD ANY NEW CALLERS OF THIS FUNCTION */
-static inline long freezable_schedule_timeout_killable_unsafe(long timeout)
-{
-	long __retval;
-	freezer_do_not_count();
-	__retval = schedule_timeout_killable(timeout);
-	freezer_count_unsafe();
 	return __retval;
 }
 
@@ -315,17 +297,12 @@ static inline void set_freezable(void) {}
 
 #define freezable_schedule()  schedule()
 
-#define freezable_schedule_unsafe()  schedule()
-
 #define freezable_schedule_timeout(timeout)  schedule_timeout(timeout)
 
 #define freezable_schedule_timeout_interruptible(timeout)		\
 	schedule_timeout_interruptible(timeout)
 
 #define freezable_schedule_timeout_killable(timeout)			\
-	schedule_timeout_killable(timeout)
-
-#define freezable_schedule_timeout_killable_unsafe(timeout)		\
 	schedule_timeout_killable(timeout)
 
 #define freezable_schedule_hrtimeout_range(expires, delta, mode)	\
