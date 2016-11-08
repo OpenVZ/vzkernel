7dd3f608e4294d9817a68db1f1123a75e18d0a88
Konstantin Khorenko <khorenko@virtuozzo.com>
2016-11-08 14:04:18 +0400

    mm: get mm from vma->vm_mm in do_set_pte()
    To be merged somewhere.
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- mm/memory.c
+++ mm/memory.c
@@ -2844,7 +2844,7 @@ static void do_set_pte(struct vm_area_struct *vma, unsigned long address,
 		page_add_new_anon_rmap(page, vma, address);
 	} else {
 		inc_mm_counter_fast(vma->vm_mm, mm_counter_file(page));
-		page_add_file_rmap(page, mm);
+		page_add_file_rmap(page, vma->vm_mm);
 	}
 	set_pte_at(vma->vm_mm, address, pte, entry);
 
