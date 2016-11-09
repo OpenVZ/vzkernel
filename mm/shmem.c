a8acbfd634d9c3823f76bd7b6ce5decde355d54c
Hugh Dickins <hughd@google.com>
2016-11-09 17:54:46 +0400

    ms/tmpfs: fix SEEK_DATA/SEEK_HOLE regression
    Fixes a problem in shmem criu test and friends on VZ7 after criu
    commit 51554fa59eed ("shmem: use lseek(SEEK_DATA) instead of mincore"):
    
    https://ci.openvz.org/job/CRIU/job/CRIU-virtuozzo/branch/criu-dev/861/
    ============ Run zdtm/transition/shmem in h ===============
    Start test
    ./shmem --pidfile=shmem.pid --outfile=shmem.out
    Run criu dump
    =[log]=> dump/zdtm/transition/shmem/24/1/dump.log
      Commit 46a1c2c7ae53 ("vfs: export lseek_execute() to modules") broke the
      tmpfs SEEK_DATA/SEEK_HOLE implementation, because vfs_setpos() converts
      the carefully prepared -ENXIO to -EINVAL.  Other filesystems avoid it in
      error cases: do the same in tmpfs.
    
      Signed-off-by: Hugh Dickins <hughd@google.com>
      Cc: Jie Liu <jeff.liu@oracle.com>
      Cc: Al Viro <viro@zeniv.linux.org.uk>
      Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    https://jira.sw.ru/browse/PSBM-54804
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Acked-by: Andrew Vagin <avagin@virtuozzo.com>

--- mm/shmem.c
+++ mm/shmem.c
@@ -1978,7 +1978,8 @@ static loff_t shmem_file_llseek(struct file *file, loff_t offset, int whence)
 		}
 	}
 
-	offset = vfs_setpos(file, offset, MAX_LFS_FILESIZE);
+	if (offset >= 0)
+		offset = vfs_setpos(file, offset, MAX_LFS_FILESIZE);
 	mutex_unlock(&inode->i_mutex);
 	return offset;
 }
