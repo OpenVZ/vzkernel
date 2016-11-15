25242558222225f97e44a4b76e5899332270329a
Andrey Ryabinin <aryabinin@virtuozzo.com>
2016-11-15 20:20:39 +0400

    kernel/pid: restore standard PID_MAX_DEFAULT value
    Use standard value of the PID_MAX_DEFAULT. If admin needs more pids
    he could bump sysctl kernel.pid_max.
    
    On RHEL7.3 we fail to allocate struct saved_cmdlines_buffer because of
    this. saved_cmdlines_buffer contains array of PID_MAX_DEFAULT unsigned ints
    so it's too big to allocate and this causes boot failure.
    Note that there is no reason to keep that array in struct and allocate dynamically.
    Instead, we could just make it static and this will fix our problem.
    
    However, in presence of sysctl kernel.pid_max, I don't see the reason in having
    non standard PID_MAX_DEFAULT.
    
    khorenko@: previous Virtuozzo 7 kernels have kernel.pid_max hardcoded to
    1048576. Now we resurrect kernel default kernel.pid_max=32768,
    and "kernel.pid_max = 1048576" is gained via entry in /etc/sysctl.conf.
    
    https://jira.sw.ru/browse/PSBM-55365
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- include/linux/threads.h
+++ include/linux/threads.h
@@ -24,8 +24,8 @@
 /*
  * This controls the default maximum pid allocated to a process
  */
-#define PID_MAX_DEFAULT 	(sizeof(long) > 4 ? 1024 * 1024 : 32 * 1024)
-#define PID_MAX_NS_DEFAULT	(32 * 1024)
+#define PID_MAX_DEFAULT (CONFIG_BASE_SMALL ? 0x1000 : 0x8000)
+#define PID_MAX_NS_DEFAULT	(PID_MAX_DEFAULT)
 
 /*
  * A maximum of 4 million PIDs should be enough for a while.
