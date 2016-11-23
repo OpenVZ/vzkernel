779998458e79cb539cd9b39eb4722bed3d0ceac4
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2016-11-23 18:56:27 +0400

    ve/net/vxlan: enable support in a container
    vxlan is safe in CT as:
    
    1) Udp multicast socket to connect to outer word sits in creation net-
    namespace, and these socket can get packets only forwarded/routed
    in creation ns.
    
    2) Vxlan device is owned by second netns(could be same as first) as
    any other network device, so same all packets come to it are from
    the same ns.
    
    3) Vxlans logic works through vxlan_net placed on creation netns,
    vxlan_fdb and vxlan_rdst are per vxlan device. Thus entries can
    not intersec with entries from host and other CTs.
    
    * One problem I can see now is adding fdb with ifindex(index of
    device to route packets from UDP socket through) after vxlan is
    moved to second namespace in vxlan_fdb_parse we use second
    namespace to check ifindex by device lookup, but in
    vxlan_xmit_one->ip_route_output_key->...->__ip_route_output_key
    we use first(creation) namespace to lookup device and probably
    will fail. So all fdb configuration should go before moving to
    ns. Same is in mainstream AFAICS.
    
    https://jira.sw.ru/browse/PSBM-53629
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Acked-by: Andrei Vagin <avagin@virtuozzo.com>
    
    khorenko@: Docker Swarm requires vxlans.

--- drivers/net/vxlan.c
+++ drivers/net/vxlan.c
@@ -2637,6 +2637,7 @@ static void vxlan_setup(struct net_device *dev)
 	dev->features	|= NETIF_F_SG | NETIF_F_HW_CSUM;
 	dev->features   |= NETIF_F_RXCSUM;
 	dev->features   |= NETIF_F_GSO_SOFTWARE;
+	dev->features |= NETIF_F_VIRTUAL;
 
 	dev->vlan_features = dev->features;
 	dev->hw_features |= NETIF_F_SG | NETIF_F_HW_CSUM | NETIF_F_RXCSUM;
