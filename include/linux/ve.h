910a8b7e2b2fec398fe6b8f3f474434b99bb382b
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2016-11-23 18:56:29 +0400

    ve/netlink: allow messages with family PF_BRIDGE type RTM_xxxNEIGH in CT
    While reproducing the problem mentioned in patch 1 I found that
    we need it to be able to configure vxlan fdb (Forwarding Database entry).
    
    https://jira.sw.ru/browse/PSBM-53629
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Acked-by: Andrei Vagin <avagin@virtuozzo.com>

--- include/linux/ve.h
+++ include/linux/ve.h
@@ -143,7 +143,7 @@ extern unsigned int sysctl_ve_mount_nr;
 #ifdef CONFIG_VE
 #define ve_uevent_seqnum       (get_exec_env()->_uevent_seqnum)
 
-extern int vz_security_family_check(struct net *net, int family);
+extern int vz_security_family_check(struct net *net, int family, int type);
 extern int vz_security_protocol_check(struct net *net, int protocol);
 
 extern struct task_struct *kthread_create_on_node_ve(struct ve_struct *ve,
@@ -213,7 +213,7 @@ extern struct cgroup *cgroup_get_ve_root(struct cgroup *cgrp);
 
 #define ve_uevent_seqnum uevent_seqnum
 
-static inline int vz_security_family_check(struct net *net, int family) { return 0; }
+static inline int vz_security_family_check(struct net *net, int family, int type) { return 0; }
 static inline int vz_security_protocol_check(struct net *net, int protocol) { return 0; }
 
 #define ve_utsname	system_utsname
