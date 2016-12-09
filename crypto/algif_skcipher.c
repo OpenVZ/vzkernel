6a318f32862e2baa0b6cd518a6fe878fd2c7b4c9
Herbert Xu <herbert@gondor.apana.org.au>
2016-12-09 16:17:17 +0400

    ms/crypto: algif_skcipher - Add key check exception for cipher_null
    commit 6e8d8ecf438792ecf7a3207488fb4eebc4edb040 upstream.
    
    This patch adds an exception to the key check so that cipher_null
    users may continue to use algif_skcipher without setting a key.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    
    https://jira.sw.ru/browse/PSBM-53860
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- crypto/algif_skcipher.c
+++ crypto/algif_skcipher.c
@@ -765,7 +765,8 @@ static int skcipher_accept_parent(void *private, struct sock *sk)
 	struct crypto_tfm *ctfm = crypto_ablkcipher_tfm(tfm->ablkcipher);
 	struct crypto_alg *calg = ctfm->__crt_alg;
 
-	if (!tfm->has_key && calg->cra_u.ablkcipher.max_keysize)
+	if (!tfm->has_key && calg->cra_u.ablkcipher.max_keysize &&
+			crypto_ablkcipher_has_setkey(tfm->ablkcipher))
 		return -ENOKEY;
 
 	return skcipher_accept_parent_common(private, sk);
