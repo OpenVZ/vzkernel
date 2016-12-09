e961cbb5a288c30e64fee5199c81e21b5f01b006
Herbert Xu <herbert@gondor.apana.org.au>
2016-12-09 16:17:17 +0400

    ms/crypto: skcipher - Add crypto_skcipher_has_setkey
    commit a1383cd86a062fc798899ab20f0ec2116cce39cb upstream.
    
    This patch adds a way for skcipher users to determine whether a key
    is required by a transform.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>
    
    https://jira.sw.ru/browse/PSBM-53860
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- include/linux/crypto.h
+++ include/linux/crypto.h
@@ -362,6 +362,7 @@ struct ablkcipher_tfm {
 
 	unsigned int ivsize;
 	unsigned int reqsize;
+	bool has_setkey;
 };
 
 struct aead_tfm {
@@ -670,6 +671,13 @@ static inline int crypto_ablkcipher_setkey(struct crypto_ablkcipher *tfm,
 	struct ablkcipher_tfm *crt = crypto_ablkcipher_crt(tfm);
 
 	return crt->setkey(crt->base, key, keylen);
+}
+
+static inline bool crypto_ablkcipher_has_setkey(struct crypto_ablkcipher *tfm)
+{
+	struct ablkcipher_tfm *crt = crypto_ablkcipher_crt(tfm);
+
+	return crt->has_setkey;
 }
 
 static inline struct crypto_ablkcipher *crypto_ablkcipher_reqtfm(
