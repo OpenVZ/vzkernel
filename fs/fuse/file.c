044054adddbc031591992468bf8f8a72f21ec7ba
Maxim Patlasov <mpatlasov@virtuozzo.com>
2016-12-15 17:48:29 +0400

    fuse: trust server file size unless opened
    Before the patch, the only way to pick up updated file size from server (in a
    scenario when local inode was created earlier, then the file was updated
    from another node) was in fuse_open_common():
    
    > 		atomic_inc(&fi->num_openers);
    >
    > 		if (atomic_read(&fi->num_openers) == 1) {
    > 			err = fuse_getattr_size(inode, file, &size);
    > 			...
    > 			spin_lock(&fc->lock);
    > 			i_size_write(inode, size);
    > 			spin_unlock(&fc->lock);
    > 		}
    
    This is correct, but someone may ask about i_size w/o open, e.g.: ls -l foo.
    The patch ensures that every time the server reports us some file size, if no
    open-s happened yet (num_openers=0), fuse stores that server size in local
    inode->i_size. This resolves the following problem:
    
    -rwx------ 1 root root 29 Dec 14 16:31 /pcs1/foo
    -rwx------ 1 root root 29 Dec 14 16:31 /pcs2/foo
    
    -rwx------ 1 root root 58 Dec 14 16:31 /pcs1/foo
    -rwx------ 1 root root 29 Dec 14 16:31 /pcs2/foo
    
    https://jira.sw.ru/browse/PSBM-57047
    
    Signed-off-by: Maxim Patlasov <mpatlasov@virtuozzo.com>
    Acked-by: Dmitry Monakhov <dmonakhov@openvz.org>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -290,12 +290,20 @@ int fuse_open_common(struct inode *inode, struct file *file, bool isdir)
 		u64 size;
 
 		inode_lock(inode);
+
+		spin_lock(&fi->lock);
 		atomic_inc(&fi->num_openers);
 
 		if (atomic_read(&fi->num_openers) == 1) {
+			fi->i_size_unstable = 1;
+			spin_unlock(&fi->lock);
 			err = fuse_getattr_size(inode, file, &size);
 			if (err) {
+				spin_lock(&fi->lock);
 				atomic_dec(&fi->num_openers);
+				fi->i_size_unstable = 0;
+				spin_unlock(&fi->lock);
+
 				inode_unlock(inode);
 				fuse_release_common(file, FUSE_RELEASE);
 				return err;
@@ -303,8 +311,10 @@ int fuse_open_common(struct inode *inode, struct file *file, bool isdir)
 
 			spin_lock(&fi->lock);
 			i_size_write(inode, size);
+			fi->i_size_unstable = 0;
 			spin_unlock(&fi->lock);
-		}
+		} else
+			spin_unlock(&fi->lock);
 
 		inode_unlock(inode);
 	}
