4d3381e19f5ac2a726e291e46e14f7fa4d6466c7
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-01-10 13:22:00 +0400

    btrfs: compilation fix in btrfs_wq_run_delayed_node()
    RHEL7.3 has "fs_info" instead of "root" argument in
    btrfs_wq_run_delayed_node() => need to correct the patch accordingly:
      902694e ("ms/btrfs: limit async_work allocation and worker func duration")
    
    Fixes: 902694e ("ms/btrfs: limit async_work allocation and worker func
    duration")
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/btrfs/delayed-inode.c
+++ fs/btrfs/delayed-inode.c
@@ -1393,7 +1393,7 @@ static int btrfs_wq_run_delayed_node(struct btrfs_delayed_root *delayed_root,
 	struct btrfs_async_delayed_work *async_work;
 
 	if (atomic_read(&delayed_root->items) < BTRFS_DELAYED_BACKGROUND ||
-	    btrfs_workqueue_normal_congested(root->fs_info->delayed_workers))
+	    btrfs_workqueue_normal_congested(fs_info->delayed_workers))
 		return 0;
 
 	async_work = kmalloc(sizeof(*async_work), GFP_NOFS);
