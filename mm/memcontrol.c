80f42b949bb3306735e6b47bd69a0ca38e7130d0
Johannes Weiner <hannes@cmpxchg.org>
2017-01-16 20:27:05 +0400

    ms/memcg: get_mem_cgroup_from_mm()
    Instead of returning NULL from try_get_mem_cgroup_from_mm() when the mm
    owner is exiting, just return root_mem_cgroup.  This makes sense for all
    callsites and gets rid of some of them having to fallback manually.
    
    [fengguang.wu@intel.com: fix warnings]
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Signed-off-by: Fengguang Wu <fengguang.wu@intel.com>
    Acked-by: Michal Hocko <mhocko@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    https://jira.sw.ru/browse/PSBM-51558
    (cherry picked from commit df381975463996178d685f6ef7d3555c5f887201)
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1155,15 +1155,24 @@ struct mem_cgroup *mem_cgroup_from_task(struct task_struct *p)
 	return mem_cgroup_from_css(task_subsys_state(p, mem_cgroup_subsys_id));
 }
 
-struct mem_cgroup *try_get_mem_cgroup_from_mm(struct mm_struct *mm)
+struct mem_cgroup *get_mem_cgroup_from_mm(struct mm_struct *mm)
 {
 	struct mem_cgroup *memcg = NULL;
 
 	rcu_read_lock();
 	do {
-		memcg = mem_cgroup_from_task(rcu_dereference(mm->owner));
-		if (unlikely(!memcg))
-			break;
+		/*
+		 * Page cache insertions can happen withou an
+		 * actual mm context, e.g. during disk probing
+		 * on boot, loopback IO, acct() writes etc.
+		 */
+		if (unlikely(!mm))
+			memcg = root_mem_cgroup;
+		else {
+			memcg = mem_cgroup_from_task(rcu_dereference(mm->owner));
+			if (unlikely(!memcg))
+				memcg = root_mem_cgroup;
+		}
 	} while (!css_tryget(&memcg->css));
 	rcu_read_unlock();
 	return memcg;
@@ -1535,7 +1544,7 @@ int task_in_mem_cgroup(struct task_struct *task, const struct mem_cgroup *memcg)
 
 	p = find_lock_task_mm(task);
 	if (p) {
-		curr = try_get_mem_cgroup_from_mm(p->mm);
+		curr = get_mem_cgroup_from_mm(p->mm);
 		task_unlock(p);
 	} else {
 		/*
@@ -1549,8 +1558,6 @@ int task_in_mem_cgroup(struct task_struct *task, const struct mem_cgroup *memcg)
 			css_get(&curr->css);
 		task_unlock(task);
 	}
-	if (!curr)
-		return 0;
 	/*
 	 * We should check use_hierarchy of "memcg" not "curr". Because checking
 	 * use_hierarchy of "curr" here make this function true if hierarchy is
@@ -1682,7 +1689,7 @@ void mem_cgroup_note_oom_kill(struct mem_cgroup *root_memcg,
 
 	p = find_lock_task_mm(task);
 	if (p) {
-		memcg = try_get_mem_cgroup_from_mm(p->mm);
+		memcg = get_mem_cgroup_from_mm(p->mm);
 		task_unlock(p);
 	} else {
 		rcu_read_lock();
@@ -3428,9 +3435,7 @@ struct kmem_cache *__memcg_kmem_get_cache(struct kmem_cache *cachep,
 	if (!current->mm || current->memcg_kmem_skip_account)
 		return cachep;
 
-	memcg = try_get_mem_cgroup_from_mm(current->mm);
-	if (unlikely(!memcg))
-		return cachep;
+	memcg = get_mem_cgroup_from_mm(current->mm);
 
 	if (!memcg_kmem_is_active(memcg))
 		goto out;
@@ -3513,15 +3518,7 @@ __memcg_kmem_newpage_charge(struct page *page, gfp_t gfp, int order)
 	if (!current->mm || current->memcg_kmem_skip_account)
 		return true;
 
-	memcg = try_get_mem_cgroup_from_mm(current->mm);
-
-	/*
-	 * very rare case described in mem_cgroup_from_task. Unfortunately there
-	 * isn't much we can do without complicating this too much, and it would
-	 * be gfp-dependent anyway. Just let it go
-	 */
-	if (unlikely(!memcg))
-		return true;
+	memcg = get_mem_cgroup_from_mm(current->mm);
 
 	if (!memcg_kmem_is_active(memcg)) {
 		css_put(&memcg->css);
