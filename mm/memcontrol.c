db82eb1d7aecd69f67548bcd52ad89c23f07c575
Johannes Weiner <hannes@cmpxchg.org>
2017-01-16 20:27:08 +0400

    ms/mm: memcontrol: rearrange charging fast path
    The charging path currently starts out with OOM condition checks when
    OOM is the rarest possible case.
    
    Rearrange this code to run OOM/task dying checks only after trying the
    percpu charge and the res_counter charge and bail out before entering
    reclaim.  Attempting a charge does not hurt an (oom-)killed task as much
    as every charge attempt having to check OOM conditions.  Also, only
    check __GFP_NOFAIL when the charge would actually fail.
    
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Michal Hocko <mhocko@suse.cz>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Vladimir Davydov <vdavydov@parallels.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    https://jira.sw.ru/browse/PSBM-51558
    (cherry picked from commit 06b078fc065fe1fe7097675c8ee416aa2ef94fb3)
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -2767,21 +2767,6 @@ static int mem_cgroup_try_charge(struct mem_cgroup *memcg,
 
 	if (mem_cgroup_is_root(memcg))
 		goto done;
-	/*
-	 * Unlike in global OOM situations, memcg is not in a physical
-	 * memory shortage.  Allow dying and OOM-killed tasks to
-	 * bypass the last charges so that they can exit quickly and
-	 * free their memory.
-	 */
-	if (unlikely(test_thread_flag(TIF_MEMDIE) ||
-		     fatal_signal_pending(current)))
-		goto bypass;
-
-	if (unlikely(task_in_memcg_oom(current)))
-		goto nomem;
-
-	if (gfp_mask & __GFP_NOFAIL)
-		oom = false;
 retry:
 	if (consume_stock(memcg, nr_pages))
 		goto done;
@@ -2801,6 +2786,20 @@ retry:
 		batch = nr_pages;
 		goto retry;
 	}
+
+	/*
+	 * Unlike in global OOM situations, memcg is not in a physical
+	 * memory shortage.  Allow dying and OOM-killed tasks to
+	 * bypass the last charges so that they can exit quickly and
+	 * free their memory.
+	 */
+	if (unlikely(test_thread_flag(TIF_MEMDIE) ||
+		     fatal_signal_pending(current) ||
+		     current->flags & PF_EXITING))
+		goto bypass;
+
+	if (unlikely(task_in_memcg_oom(current)))
+		goto nomem;
 
 	if (!(gfp_mask & __GFP_WAIT))
 		goto nomem;
@@ -2829,6 +2828,9 @@ retry:
 	 */
 	if (mem_cgroup_wait_acct_move(mem_over_limit))
 		goto retry;
+
+	if (gfp_mask & __GFP_NOFAIL)
+		goto bypass;
 
 	if (fatal_signal_pending(current))
 		goto bypass;
