e1cb3a9aef95824cc762f484ed0e51b5c0548864
Andrey Ryabinin <aryabinin@virtuozzo.com>
2017-01-17 16:28:38 +0400

    ipv6/sit: account and don't WARN on ip_tunnel_prl structs allocation
    The size of the ip_tunnel_prl structs allocation is controllable from user-space,
    thus it's better to avoid spam in dmesg if allocation failed.
    Also add __GFP_ACCOUNT as this is a good candidate for per-memcg accounting.
    
    khorenko@: allocation is temporary and limited to 4MB.
    
    https://jira.sw.ru/browse/PSBM-58330
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- net/ipv6/sit.c
+++ net/ipv6/sit.c
@@ -311,7 +311,7 @@ static int ipip6_tunnel_get_prl(struct ip_tunnel *t,
 	 * we try harder to allocate.
 	 */
 	kp = (cmax <= 1 || ve_capable(CAP_NET_ADMIN)) ?
-		kcalloc(cmax, sizeof(*kp), GFP_KERNEL | __GFP_NOWARN) :
+		kcalloc(cmax, sizeof(*kp), GFP_KERNEL_ACCOUNT | __GFP_NOWARN) :
 		NULL;
 
 	rcu_read_lock();
@@ -324,7 +324,8 @@ static int ipip6_tunnel_get_prl(struct ip_tunnel *t,
 		 * For root users, retry allocating enough memory for
 		 * the answer.
 		 */
-		kp = kcalloc(ca, sizeof(*kp), GFP_ATOMIC);
+		kp = kcalloc(ca, sizeof(*kp), GFP_ATOMIC | __GFP_ACCOUNT |
+				__GFP_NOWARN);
 		if (!kp) {
 			ret = -ENOMEM;
 			goto out;
