d3b7c90f8218bae3092b99fb58138fdc8b8621ca
Josef Bacik <jbacik@fb.com>
2017-02-15 16:49:48 -0500

    nbd: cleanup workqueue on error properly
    If we fail to register the blockdev we need to make sure to destroy the
    recv workqueue.
    
    Signed-off-by: Josef Bacik <jbacik@fb.com>
    Signed-off-by: Jens Axboe <axboe@fb.com>
    (cherry picked from commit 6330a2d0b465527d621a9d95cad6b2fc0a959f13)
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- drivers/block/nbd.c
+++ drivers/block/nbd.c
@@ -1116,8 +1116,10 @@ static int __init nbd_init(void)
 	if (!recv_workqueue)
 		return -ENOMEM;
 
-	if (register_blkdev(NBD_MAJOR, "nbd"))
+	if (register_blkdev(NBD_MAJOR, "nbd")) {
+		destroy_workqueue(recv_workqueue);
 		return -EIO;
+	}
 
 	nbd_dbg_init();
 
