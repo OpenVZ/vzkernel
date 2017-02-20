36f1453da4572818407f3d7cd5ac4309944de40c
Maxim Patlasov <mpatlasov@virtuozzo.com>
2017-02-20 17:03:58 +0400

    fuse: fuse_prepare_write() cannot handle page from killed request
    After fuse_prepare_write() called __fuse_readpage(file, page, ...),
    the page might be already unlocked by fuse_kill_requests():
    
    >		for (i = 0; i < req->num_pages; i++) {
    >			struct page *page = req->pages[i];
    >			SetPageError(page);
    >			unlock_page(page);
    
    so it is incorrect to touch it at all. The problem can be easily
    fixed the same way is it was done in fuse_readpage() checking "killed"
    flag.
    
    Another minor complication is that there are three different use-cases
    for that snippet from fuse_kill_requests() above: fuse_readpages(),
    fuse_readpage() and fuse_prepare_write(). Among them only the latter
    needs explicit page_cache_release() call. That's why the patch introduces
    ad-hoc request flag "page_needs_release".
    
    https://jira.sw.ru/browse/PSBM-54547
    
    Signed-off-by: Maxim Patlasov <mpatlasov@virtuozzo.com>
    Acked-by: Dmitry Monakhov <dmonakhov@virtuozzo.com>
    (cherry picked from commit 87cf0d4d095458c6e35ed1b906c4c008cb883b2d)
    Signed-off-by: Liu Kui <Kui.Liu@acronis.com>
    Feature: vStorage

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -986,7 +986,7 @@ static void fuse_short_read(struct inode *inode, u64 attr_ver, size_t num_read,
 }
 
 static int fuse_do_readpage(struct file *file, struct page *page,
-		bool *killed_p)
+		bool page_needs_release, bool *killed_p)
 {
 	struct inode *inode = page->mapping->host;
 	struct fuse_mount *fm = get_fuse_mount(inode);
@@ -999,6 +999,7 @@ static int fuse_do_readpage(struct file *file, struct page *page,
 		.ap.pages = &page,
 		.ap.descs = &desc,
 		.ap.args.page_cache = 1,
+		.ap.args.page_needs_release = page_needs_release,
 	};
 	ssize_t res;
 	u64 attr_ver;
@@ -1047,7 +1048,7 @@ static int fuse_readpage(struct file *file, struct page *page)
 	if (fuse_is_bad(inode))
 		goto out;
 
-	err = fuse_do_readpage(file, page, &killed);
+	err = fuse_do_readpage(file, page, false, &killed);
 	fuse_invalidate_atime(inode);
  out:
 	if (!killed)
@@ -1122,6 +1123,7 @@ static void fuse_send_readpages(struct fuse_io_args *ia, struct file *file)
 	ap->args.page_zeroing = true;
 	ap->args.page_replace = true;
 	ap->args.page_cache = 1;
+	ap->args.page_needs_release = false;
 
 	/* Don't overflow end offset */
 	if (pos + (count - 1) == LLONG_MAX) {
@@ -2539,6 +2541,7 @@ static int fuse_write_begin(struct file *file, struct address_space *mapping,
 	struct page *page;
 	loff_t fsize;
 	int err = -ENOMEM;
+	bool killed = false;
 
 	WARN_ON(!fc->writeback_cache);
 
@@ -2566,7 +2569,7 @@ static int fuse_write_begin(struct file *file, struct address_space *mapping,
 			zero_user_segment(page, 0, off);
 		goto success;
 	}
-	err = fuse_do_readpage(file, page, NULL);
+	err = fuse_do_readpage(file, page, true, &killed);
 	if (err)
 		goto cleanup;
 success:
@@ -2574,8 +2577,10 @@ success:
 	return 0;
 
 cleanup:
-	unlock_page(page);
-	put_page(page);
+	if (!killed) {
+		unlock_page(page);
+		put_page(page);
+	}
 error:
 	return err;
 }
