0ebac1a555e3708268e688813bc91b1a882d0b74
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-03-09 13:42:41 +0400

    Revert: rh/kmod: use system_unbound_wq instead of khelper
    This reverts a mainstream commit 90f023030e26ce8f981b3e688cb79329d8d07cc3
    ("kmod: use system_unbound_wq instead of khelper"), manually backported by
    RedHat.
    
    https://access.redhat.com/labs/psb/versions/kernel-3.10.0-514.10.2.el7/patches/kernel-kmod-use-system_unbound_wq-instead-of-khelper?raw=true
    
    The revert is done due to a conflict with our commit
    aa03b01 ("VE: convert khelper_wq to kthread_worker").
    
    The reverted RedHat's patch is a subject to reapply later.
    
    ms commit message:
        kmod: use system_unbound_wq instead of khelper
    
        We need to launch the usermodehelper kernel threads with the widest
        affinity and this is partly why we use khelper.  This workqueue has
        unbound properties and thus a wide affinity inherited by all its children.
    
        Now khelper also has special properties that we aren't much interested in:
        ordered and singlethread.  There is really no need about ordering as all
        we do is creating kernel threads.  This can be done concurrently.  And
        singlethread is a useless limitation as well.
    
        The workqueue engine already proposes generic unbound workqueues that
        don't share these useless properties and handle well parallel jobs.
    
        The only worrysome specific is their affinity to the node of the current
        CPU.  It's fine for creating the usermodehelper kernel threads but those
        inherit this affinity for longer jobs such as requesting modules.
    
        This patch proposes to use these node affine unbound workqueues assuming
        that a node is sufficient to handle several parallel usermodehelper
        requests.
    
        Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
        Cc: Rik van Riel <riel@redhat.com>
        Reviewed-by: Oleg Nesterov <oleg@redhat.com>
        Cc: Christoph Lameter <cl@linux.com>
        Cc: Tejun Heo <tj@kernel.org>
        Cc: Rusty Russell <rusty@rustcorp.com.au>
        Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    https://jira.sw.ru/browse/PSBM-61660
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- kernel/kmod.c
+++ kernel/kmod.c
@@ -45,6 +45,8 @@
 
 extern int max_threads;
 
+static struct workqueue_struct *khelper_wq;
+
 /*
  * kmod_thread_locker is used for deadlock avoidance.  There is no explicit
  * locking to protect this global - it is private to the singleton khelper
@@ -572,7 +574,7 @@ int call_usermodehelper_exec(struct subprocess_info *sub_info, int wait)
 	if (sub_info->path[0] == '\0')
 		goto out;
 
-	if (usermodehelper_disabled) {
+	if (!khelper_wq || usermodehelper_disabled) {
 		retval = -EBUSY;
 		goto out;
 	}
@@ -590,7 +592,7 @@ int call_usermodehelper_exec(struct subprocess_info *sub_info, int wait)
 	sub_info->complete = &done;
 	sub_info->wait = wait;
 
-	queue_work(system_unbound_wq, &sub_info->work);
+	queue_work(khelper_wq, &sub_info->work);
 	if (wait == UMH_NO_WAIT)	/* task has freed sub_info */
 		goto unlock;
 
@@ -720,3 +722,9 @@ struct ctl_table usermodehelper_table[] = {
 	},
 	{ }
 };
+
+void __init usermodehelper_init(void)
+{
+	khelper_wq = create_singlethread_workqueue("khelper");
+	BUG_ON(!khelper_wq);
+}
