50aaffe01badd1d4f50de1b0971bfe4b41bfe328
Maxim Patlasov <mpatlasov@virtuozzo.com>
2017-03-24 19:29:30 -0700

    ms/fuse: separate pqueue for clones
    Backport from ml:
    
    commit c3696046beb3a4479715b48f67f6a8a3aef4b3bb
    Author: Miklos Szeredi <mszeredi@suse.cz>
    Date:   Wed Jul 1 16:26:09 2015 +0200
    
        fuse: separate pqueue for clones
    
        Make each fuse device clone refer to a separate processing queue.  The only
        constraint on userspace code is that the request answer must be written to
        the same device clone as it was read off.
    
        Signed-off-by: Miklos Szeredi <mszeredi@suse.cz>
    
    Signed-off-by: Maxim Patlasov <mpatlasov@virtuozzo.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -458,6 +458,9 @@ struct fuse_dev {
 	/** Fuse connection for this device */
 	struct fuse_conn *fc;
 
+	/** Processing queue */
+	struct fuse_pqueue pq;
+
 	/** list entry on fc->devices */
 	struct list_head entry;
 };
@@ -481,6 +484,9 @@ struct fuse_conn {
 
 	struct rcu_head rcu;
 
+	/** Number of fuse_dev's */
+	atomic_t dev_count;
+
 	/** The user id for this mount */
 	kuid_t user_id;
 
@@ -501,9 +507,6 @@ struct fuse_conn {
 
 	/** Input queue */
 	struct fuse_iqueue iq;
-
-	/** Processing queue */
-	struct fuse_pqueue pq;
 
 	/** The next unique kernel file handle */
 	u64 khctr;
