3c53cceec6c44258f2b3cf8d37f69f01ef02dc72
Andrey Ryabinin <aryabinin@virtuozzo.com>
2017-04-13 14:41:04 +0400

    fs/cleancache: fix data invalidation in the cleancache during direct_io
    Currently some direct_io fs hooks call invalidate_inode_pages2_range()
    conditionally iff mapping->nrpages is not zero. So if nrpages is zero,
    data in cleancache wouldn't be invalidated. So the next buffered read
    may get stale data from the cleancache.
    
    Fix this by calling invalidate_inode_pages2_range() regardless of nrpages
    value. And if nrpages is zero, bail out from invalidate_inode_pages2_range()
    only after cleancache_invalidate_inode(), so that we invalidate cleancache
    but still avoid pointless page cache lookups.
    
    https://jira.sw.ru/browse/PSBM-63908
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- fs/nfs/direct.c
+++ fs/nfs/direct.c
@@ -1132,12 +1132,10 @@ ssize_t nfs_file_direct_write(struct kiocb *iocb, const struct iovec *iov,
 	if (result)
 		goto out_unlock;
 
-	if (mapping->nrpages) {
-		result = invalidate_inode_pages2_range(mapping,
-					pos >> PAGE_CACHE_SHIFT, end);
-		if (result)
-			goto out_unlock;
-	}
+	result = invalidate_inode_pages2_range(mapping,
+				pos >> PAGE_CACHE_SHIFT, end);
+	if (result)
+		goto out_unlock;
 
 	task_io_account_write(count);
 
@@ -1161,10 +1159,8 @@ ssize_t nfs_file_direct_write(struct kiocb *iocb, const struct iovec *iov,
 
 	result = nfs_direct_write_schedule_iovec(dreq, iov, nr_segs, pos, uio);
 
-	if (mapping->nrpages) {
-		invalidate_inode_pages2_range(mapping,
-					      pos >> PAGE_CACHE_SHIFT, end);
-	}
+	invalidate_inode_pages2_range(mapping,
+				pos >> PAGE_CACHE_SHIFT, end);
 
 	mutex_unlock(&inode->i_mutex);
 
