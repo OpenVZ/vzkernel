e1cb51f511cad002b8754c8b0c3753f8677259db
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-04-14 18:40:27 +0400

    Revert "ms/KVM: x86: reset MMU on KVM_SET_VCPU_EVENTS"
    This reverts commit b36e7eda7523c63eac6cc524191c38b9ab9e7177.
    
    As it's suspected to have caused more problems than it fixes.
    
    Fixes: https://jira.sw.ru/browse/PSBM-62341
    
    Suspected to cause:
    https://jira.sw.ru/browse/PSBM-63993
    https://jira.sw.ru/browse/PSBM-64259
    https://jira.sw.ru/browse/PSBM-63253
    https://jira.sw.ru/browse/PSBM-63962
    
    Cc: Denis Plotnikov <dplotnikov@virtuozzo.com>
    Signed-off-by: Roman Kagan <rkagan@virtuozzo.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -3184,8 +3184,6 @@ static void kvm_vcpu_ioctl_x86_get_vcpu_events(struct kvm_vcpu *vcpu,
 	memset(&events->reserved, 0, sizeof(events->reserved));
 }
 
-static void kvm_set_hflags(struct kvm_vcpu *vcpu, unsigned emul_flags);
-
 static int kvm_vcpu_ioctl_x86_set_vcpu_events(struct kvm_vcpu *vcpu,
 					      struct kvm_vcpu_events *events)
 {
@@ -3222,13 +3220,10 @@ static int kvm_vcpu_ioctl_x86_set_vcpu_events(struct kvm_vcpu *vcpu,
 		vcpu->arch.apic->sipi_vector = events->sipi_vector;
 
 	if (events->flags & KVM_VCPUEVENT_VALID_SMM) {
-		u32 hflags = vcpu->arch.hflags;
 		if (events->smi.smm)
-			hflags |= HF_SMM_MASK;
+			vcpu->arch.hflags |= HF_SMM_MASK;
 		else
-			hflags &= ~HF_SMM_MASK;
-		kvm_set_hflags(vcpu, hflags);
-
+			vcpu->arch.hflags &= ~HF_SMM_MASK;
 		vcpu->arch.smi_pending = events->smi.pending;
 		if (events->smi.smm_inside_nmi)
 			vcpu->arch.hflags |= HF_SMM_INSIDE_NMI_MASK;
