96783f47e5c385dcf6412ef5769a52e7af68d176
Vasily Averin <vvs@virtuozzo.com>
2017-04-17 20:09:59 +0400

    KEYS: fix keyctl_set_reqkey_keyring() to not leak thread keyrings
    It was found that keyctl_set_reqkey_keyring() function
    leaks thread keyring which allows unprivileged local user
    to exhaust kernel memory.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1442086
    
    based on patch from  Eric Biggers <ebiggers@google.com>
    https://lkml.org/lkml/2017/4/1/235
    
    Original comment:
    ===
    Running the following program as an unprivileged user exhausts kernel
    memory by leaking thread keyrings:
    
    	#include <keyutils.h>
    
    	int main()
    	{
    		for (;;)
    			keyctl_set_reqkey_keyring(KEY_REQKEY_DEFL_THREAD_KEYRING);
    	}
    
    Fix it by only creating a new thread keyring if there wasn't one before,
    just as we do for process keyrings.
    ===
    
    https://jira.sw.ru/browse/PSBM-64435
    
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>

--- security/keys/process_keys.c
+++ security/keys/process_keys.c
@@ -132,6 +132,9 @@ int install_thread_keyring_to_cred(struct cred *new)
 {
 	struct key *keyring;
 
+	if (new->thread_keyring)
+		return 0;
+
 	keyring = keyring_alloc("_tid", new->uid, new->gid, new,
 				KEY_POS_ALL | KEY_USR_VIEW,
 				KEY_ALLOC_QUOTA_OVERRUN, NULL);
