542bde218da8e7cdb524d89716dc2640feb0463a
Dmitry Safonov <dsafonov@virtuozzo.com>
2017-05-31 15:59:51 +0400

    x86/mm: Support mremap()'ing vdso vma
    CRIU needs remapping vdso blob on restore. It moves vdso vma to the same
    position as it was on checkpoint with mremap() call.
    During mremap() the position of vma is changed with the result that
    current->mm->context.vdso pointer is no longer valid.
    By chance 64-bit signal processing code doesn't need context.vdso
    pointer for landing. But it's needed for ia32 signals.
    
    This is quite ugly backport of ms commit commit b059a453b1cf
    ("x86/vdso: Add mremap hook to vm_special_mapping").
    As vdso code was rewritten in ~3.16 kernel, it can't be backported
    as-is and here is the simplest result for the kernel which has
    no struct vm_special_mapping yet.
    
    Signed-off-by: Dmitry Safonov <dsafonov@virtuozzo.com>

---
+++ arch/x86/include/asm/mm-arch-hooks.h
@@ -0,0 +1,20 @@
+/*
+ * Architecture specific mm hooks
+ *
+ * Copyright (C) 2015, IBM Corporation
+ * Author: Laurent Dufour <ldufour@linux.vnet.ibm.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#ifndef _ASM_POWERPC_MM_ARCH_HOOKS_H
+#define _ASM_POWERPC_MM_ARCH_HOOKS_H
+
+extern void arch_remap(struct mm_struct *mm,
+		unsigned long old_start, unsigned long old_end,
+		unsigned long new_start, unsigned long new_end);
+#define arch_remap arch_remap
+
+#endif /* _ASM_POWERPC_MM_ARCH_HOOKS_H */
