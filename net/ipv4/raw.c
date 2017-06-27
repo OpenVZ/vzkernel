2ba08cd6a39bc3f423c09f3751b2cc2dd840a8aa
WANG Cong <xiyou.wangcong@gmail.com>
2017-06-27 20:30:21 +0400

    ms/ipv4: fix a deadlock in ip_ra_control
    ML:1215e51edad1272e669172b26aa12aac94810c7f
    
    Similar to commit 87e9f0315952
    ("ipv4: fix a potential deadlock in mcast getsockopt() path"),
    there is a deadlock scenario for IP_ROUTER_ALERT too:
    
           CPU0                    CPU1
           ----                    ----
      lock(rtnl_mutex);
                                   lock(sk_lock-AF_INET);
                                   lock(rtnl_mutex);
      lock(sk_lock-AF_INET);
    
    Fix this by always locking RTNL first on all setsockopt() paths.
    
    Note, after this patch ip_ra_lock is no longer needed either.
    
    Reported-by: Dmitry Vyukov <dvyukov@google.com>
    Tested-by: Andrey Konovalov <andreyknvl@google.com>
    Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    https://jira.sw.ru/browse/PSBM-64752
    
    Signed-off-by: Igor Redko <redkoi@virtuozzo.com>

--- net/ipv4/raw.c
+++ net/ipv4/raw.c
@@ -648,7 +648,9 @@ static void raw_close(struct sock *sk, long timeout)
 	/*
 	 * Raw sockets may have direct kernel references. Kill them.
 	 */
-	ip_ra_control(sk, 0, NULL);
+	rtnl_lock();
+    ip_ra_control(sk, 0, NULL);
+    rtnl_unlock();
 
 	sk_common_release(sk);
 }
