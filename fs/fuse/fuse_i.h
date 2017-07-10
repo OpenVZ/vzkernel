ace77e29bbda6516de52390f1517d7e2417eedd4
Maxim Patlasov <mpatlasov@virtuozzo.com>
2017-07-10 16:10:30 +0400

    fuse: ioctl(FIEMAP) support
    It is quite tricky due to necessity to split very large
    extent sets.
    
    https://jira.sw.ru/browse/PSBM-68226
    
    Signed-off-by: Alexey Kuznetsov <kuznet@virtuozzo.com>
    Signed-off-by: Maxim Patlasov <mpatlasov@virtuozzo.com>
    
    +++
    fuse: memory leakage
    
    Another consequence of pseudoimpovement in RHEL8.
    
    mFixes: 776b93dd7993 ("fuse: ioctl(FIEMAP) support")
    In the scope of https://jira.sw.ru/browse/PSBM-129289
    
    Signed-off-by: Alexey Kuznetsov <kuznet@acronis.com>
    
    (cherry picked from commit 8b66737fb2b0d02b1ff6554567c2c775e4d14ba8)
    Signed-off-by: Liu Kui <Kui.Liu@acronis.com>
    Feature: vStorage

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -295,7 +295,7 @@ struct fuse_args {
 	bool may_block:1;
 	bool is_ext:1;
 	struct fuse_in_arg in_args[3];
-	struct fuse_arg out_args[2];
+	struct fuse_arg out_args[3];
 	void (*end)(struct fuse_mount *fm, struct fuse_args *args, int error);
 	struct fuse_req *req;
 
@@ -851,6 +851,9 @@ struct fuse_conn {
 	/** Handle wrong FUSE_NOTIFY_INVAL_FILES from old fused */
 	unsigned compat_inval_files:1;
 
+	/** No ioctl(FIEMAP) */
+	unsigned no_fiemap:1;
+
 	/** The number of requests waiting for completion */
 	atomic_t num_waiting;
 
@@ -1337,6 +1340,9 @@ struct posix_acl;
 struct posix_acl *fuse_get_acl(struct inode *inode, int type);
 int fuse_set_acl(struct user_namespace *mnt_userns, struct inode *inode,
 		 struct posix_acl *acl, int type);
+
+int fuse_fiemap(struct inode *inode, struct fiemap_extent_info *fieinfo,
+		__u64 start, __u64 len);
 
 /* readdir.c */
 int fuse_readdir(struct file *file, struct dir_context *ctx);
