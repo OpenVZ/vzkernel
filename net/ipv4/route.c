fe2da04012df57532892bebce061cd7647fd39d6
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-07-21 12:44:39 +0400

    ve/proc/net: virtualize all the network proc entries
    Patchset description:
    proc/net: rework containerization
    
    This is a precursor series in scope of
    https://jira.sw.ru/browse/PSBM-58574
    
    ======================================================
    This patch description:
    
    Use proc_net_create() and proc_net_create_data() for all the per-net dentries.
    We need this to expose per-net proc dentries once commit
    e5990221bde54d757a5b5d85e81b2535064570ad is reverted
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    
    S_ISGID is set for procfs only in proc_net.c, so this looks good to me,
    Reviewed-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    [VvS REHL7.8 rebase] context changes

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -388,19 +388,19 @@ static int __net_init ip_rt_do_proc_init(struct net *net)
 {
 	struct proc_dir_entry *pde;
 
-	pde = proc_create("rt_cache", S_IRUGO, net->proc_net,
+	pde = proc_net_create("rt_cache", S_IRUGO, net->proc_net,
 			  &rt_cache_seq_fops);
 	if (!pde)
 		goto err1;
 
-	pde = proc_net_create_data("rt_cache", S_IRUGO,
-			  net->proc_net_stat, &rt_cpu_seq_fops, NULL);
+	pde = proc_net_create("rt_cache", S_IRUGO,
+			  net->proc_net_stat, &rt_cpu_seq_fops);
 	if (!pde)
 		goto err2;
 
 #ifdef CONFIG_IP_ROUTE_CLASSID
-	pde = proc_net_create_data("rt_acct", 0,
-			net->proc_net, &rt_acct_proc_fops, NULL);
+	pde = proc_net_create("rt_acct", 0,
+			net->proc_net, &rt_acct_proc_fops);
 	if (!pde)
 		goto err3;
 #endif
