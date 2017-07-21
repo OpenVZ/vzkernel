bf49077545fbe387c4efb45f4f0de76eb021b913
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-07-21 12:44:39 +0400

    ve/proc/net: virtualize all the network proc entries
    Patchset description:
    proc/net: rework containerization
    
    This is a precursor series in scope of
    https://jira.sw.ru/browse/PSBM-58574
    
    ======================================================
    This patch description:
    
    Use proc_net_create() and proc_net_create_data() for all the per-net dentries.
    We need this to expose per-net proc dentries once commit
    e5990221bde54d757a5b5d85e81b2535064570ad is reverted
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    
    S_ISGID is set for procfs only in proc_net.c, so this looks good to me,
    Reviewed-by: Dmitry Safonov <dsafonov@virtuozzo.com>
    [VvS REHL7.8 rebase] context changes

--- net/l2tp/l2tp_ppp.c
+++ net/l2tp/l2tp_ppp.c
@@ -1760,7 +1760,7 @@ static __net_init int pppol2tp_init_net(struct net *net)
 	struct proc_dir_entry *pde;
 	int err = 0;
 
-	pde = proc_create("pppol2tp", S_IRUGO, net->proc_net,
+	pde = proc_net_create("pppol2tp", S_IRUGO, net->proc_net,
 			  &pppol2tp_proc_fops);
 	if (!pde) {
 		err = -ENOMEM;
