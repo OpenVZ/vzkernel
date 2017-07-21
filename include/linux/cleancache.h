c785edb5abf26cd46de12c0140a1aed4cb8cb862
Andrey Ryabinin <aryabinin@virtuozzo.com>
2017-07-21 12:23:33 +0400

    ve/mm/cleancache: count only successful put in the cleancache_puts counter
    Current semantic of the cleancache_puts counter (/sys/kernel/debug/cleancache/puts)
    is to count the number of the attempts to put into cleancache. Let's change
    this to the number of the successful puts, as this is less confusing.
    
    https://jira.sw.ru/browse/PSBM-63964
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- include/linux/cleancache.h
+++ include/linux/cleancache.h
@@ -30,7 +30,7 @@ struct cleancache_ops {
 	int (*init_shared_fs)(char *uuid, size_t);
 	int (*get_page)(int, struct cleancache_filekey,
 			pgoff_t, struct page *);
-	void (*put_page)(int, struct cleancache_filekey,
+	int (*put_page)(int, struct cleancache_filekey,
 			pgoff_t, struct page *);
 	void (*invalidate_page)(int, struct cleancache_filekey, pgoff_t);
 	void (*invalidate_inode)(int, struct cleancache_filekey);
