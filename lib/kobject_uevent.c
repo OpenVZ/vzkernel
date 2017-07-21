ab14494ba29caf688ba38a519cbe12552294714c
Andrey Ryabinin <aryabinin@virtuozzo.com>
2017-07-21 12:24:48 +0400

    kobj, uevent: allow to broadcast event to ve0 from any ve
    Commit 732eb5c2ac0a ("ve/kobj: Send events per VE instead of
    all net-namespaces broadcasting") forbid to send uevents to any another
    ve. So when container stumps on FS-corruption and sends uevent about that,
    the host uevents won't see it. Let's allow to broadcast any events to
    the ve0 from other ve's to fix this
    
    https://jira.sw.ru/browse/PSBM-68710
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Reviewed-by: Cyrill Gorcunov <gorcunov@openvz.org>

--- lib/kobject_uevent.c
+++ lib/kobject_uevent.c
@@ -259,11 +259,13 @@ int kobject_uevent_env_one(struct kobject *kobj, enum kobject_action action,
 		struct sock *uevent_sock = ue_sk->sk;
 		struct sk_buff *skb;
 		size_t len;
+		struct ve_struct *owner_ve;
 
 		if (!netlink_has_listeners(uevent_sock, 1))
 			continue;
 
-		if (sock_net(uevent_sock)->owner_ve != get_exec_env())
+		owner_ve = sock_net(uevent_sock)->owner_ve;
+		if (!ve_is_super(owner_ve) && owner_ve != get_exec_env())
 			continue;
 
 		/* allocate message with the maximum possible size */
