44d32cbfa246983342d5768267c2e721b906ea9b
Dmitry Monakhov <dmonakhov@openvz.org>
2017-07-25 16:18:41 +0400

    ext4: send abort uevent on ext4 journal abort
    Currenlty error from device result in ext4_abort, but uevent not generated because
    ext4_abort() caller's context do not allow GFP_KERNEL memory allocation.
    Let's relax submission context requirement and deffer actual uevent submission
    to work_queue.  It can be any workqueue I've pick rsv_conversion_wq because it is
    already exists.
    
    New uevent "ABORT" is introduced.
    
    https://jira.sw.ru/browse/PSBM-68848
    
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>
    
    khorenko@: "system_wq" does not fit here because at the moment of
    work execution sb can be already destroyed.
    "EXT4_SB(sb)->rsv_conversion_wq" is flushed before sb is destroyed.

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -1399,6 +1399,7 @@ struct ext4_sb_info {
 	__u32 s_csum_seed;
 
 	bool s_err_event_sent;
+	bool s_abrt_event_sent;
 
 	/* Reclaim extents from extent status tree */
 	struct shrinker s_es_shrinker;
@@ -2791,6 +2792,7 @@ enum ext4_event_type {
      EXT4_UA_UMOUNT,
      EXT4_UA_REMOUNT,
      EXT4_UA_ERROR,
+     EXT4_UA_ABORT,
      EXT4_UA_FREEZE,
      EXT4_UA_UNFREEZE,
 };
