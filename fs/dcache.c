d5154d4f1480e3b6b0404c1da5a4e923fa1c8313
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-08-07 14:47:34 +0400

    fs: Revert "MS/FS/DCACHE: move vfs caches to headers"
    This reverts commit c66a040 ("MS/FS/DCACHE: move vfs caches to headers"), which
    is leftover from vz6 and it's not need anymore.
    
    After the commit, dentry_cache will be statically declared again.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- fs/dcache.c
+++ fs/dcache.c
@@ -92,7 +92,7 @@ __cacheline_aligned_in_smp DEFINE_SEQLOCK(rename_lock);
 
 EXPORT_SYMBOL(rename_lock);
 
-struct kmem_cache *dentry_cache __read_mostly;
+static struct kmem_cache *dentry_cache __read_mostly;
 
 /*
  * This is the single most critical data structure when it comes
