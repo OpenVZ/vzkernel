59b11dfef0f9e457c4c7f9d539bef251376741f7
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-08-31 17:40:33 +0300

    proc connector: use generic event helper for exec event
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Reviewed-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- drivers/connector/cn_proc.c
+++ drivers/connector/cn_proc.c
@@ -132,31 +132,17 @@ void proc_fork_connector(struct task_struct *task)
 	proc_event_connector(task, PROC_EVENT_FORK, 0, fill_fork_event);
 }
 
-void proc_exec_connector(struct task_struct *task)
+static bool fill_exec_event(struct proc_event *ev, struct task_struct *task,
+			    int unused)
 {
-	struct cn_msg *msg;
-	struct proc_event *ev;
-	struct timespec ts;
-	__u8 buffer[CN_PROC_MSG_SIZE] __aligned(8);
-
-	if (atomic_read(&proc_event_num_listeners) < 1)
-		return;
-
-	msg = buffer_to_cn_msg(buffer);
-	ev = (struct proc_event *)msg->data;
-	memset(&ev->event_data, 0, sizeof(ev->event_data));
-	get_seq(&msg->seq, &ev->cpu);
-	ktime_get_ts(&ts); /* get high res monotonic timestamp */
-	ev->timestamp_ns = timespec_to_ns(&ts);
-	ev->what = PROC_EVENT_EXEC;
 	ev->event_data.exec.process_pid = task->pid;
 	ev->event_data.exec.process_tgid = task->tgid;
+	return true;
+}
 
-	memcpy(&msg->id, &cn_proc_event_id, sizeof(msg->id));
-	msg->ack = 0; /* not used */
-	msg->len = sizeof(*ev);
-	msg->flags = 0; /* not used */
-	cn_netlink_send(msg, CN_IDX_PROC, GFP_KERNEL);
+void proc_exec_connector(struct task_struct *task)
+{
+	proc_event_connector(task, PROC_EVENT_EXEC, 0, fill_exec_event);
 }
 
 void proc_id_connector(struct task_struct *task, int which_id)
