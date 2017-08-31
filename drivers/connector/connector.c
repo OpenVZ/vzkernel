fe296adafd7f7a1c16b55336a9a5e954aa686401
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-08-31 17:40:28 +0300

    connector: introduce VE-aware get_cdev() helper
    Once containerized, device won't be one and for all.
    Thus make a helper template and use it instead of direct device object access.
    Use ve0 for now.
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Reviewed-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- drivers/connector/connector.c
+++ drivers/connector/connector.c
@@ -42,6 +42,11 @@ static struct cn_dev cdev;
 
 static int cn_already_initialized;
 
+static struct cn_dev *get_cdev(struct ve_struct *ve)
+{
+	return &cdev;
+}
+
 /*
  * Sends mult (multiple) cn_msg at a time.
  *
@@ -78,7 +83,7 @@ int cn_netlink_send_mult(struct cn_msg *msg, u16 len, u32 portid, u32 __group,
 	struct sk_buff *skb;
 	struct nlmsghdr *nlh;
 	struct cn_msg *data;
-	struct cn_dev *dev = &cdev;
+	struct cn_dev *dev = get_cdev(get_ve0());
 	u32 group = 0;
 	int found = 0;
 
@@ -144,7 +149,7 @@ static int cn_call_callback(struct sk_buff *skb)
 {
 	struct nlmsghdr *nlh;
 	struct cn_callback_entry *i, *cbq = NULL;
-	struct cn_dev *dev = &cdev;
+	struct cn_dev *dev = get_cdev(get_ve0());
 	struct cn_msg *msg = nlmsg_data(nlmsg_hdr(skb));
 	struct netlink_skb_parms *nsp = &NETLINK_CB(skb);
 	int err = -ENODEV;
@@ -210,7 +215,7 @@ int cn_add_callback(struct cb_id *id, const char *name,
 				     struct netlink_skb_parms *))
 {
 	int err;
-	struct cn_dev *dev = &cdev;
+	struct cn_dev *dev = get_cdev(get_ve0());
 
 	if (!cn_already_initialized)
 		return -EAGAIN;
@@ -233,7 +238,7 @@ EXPORT_SYMBOL_GPL(cn_add_callback);
  */
 void cn_del_callback(struct cb_id *id)
 {
-	struct cn_dev *dev = &cdev;
+	struct cn_dev *dev = get_cdev(get_ve0());
 
 	cn_queue_del_callback(dev->cbdev, id);
 }
@@ -241,7 +246,7 @@ EXPORT_SYMBOL_GPL(cn_del_callback);
 
 static int cn_proc_show(struct seq_file *m, void *v)
 {
-	struct cn_queue_dev *dev = cdev.cbdev;
+	struct cn_queue_dev *dev = get_cdev(get_ve0())->cbdev;
 	struct cn_callback_entry *cbq;
 
 	seq_printf(m, "Name            ID\n");
@@ -262,7 +267,7 @@ static int cn_proc_show(struct seq_file *m, void *v)
 
 static int cn_init(void)
 {
-	struct cn_dev *dev = &cdev;
+	struct cn_dev *dev = get_cdev(get_ve0());
 	struct netlink_kernel_cfg cfg = {
 		.groups	= CN_NETLINK_USERS + 0xf,
 		.input	= cn_rx_skb,
@@ -287,7 +292,7 @@ static int cn_init(void)
 
 static void cn_fini(void)
 {
-	struct cn_dev *dev = &cdev;
+	struct cn_dev *dev = get_cdev(get_ve0());
 
 	cn_already_initialized = 0;
 
