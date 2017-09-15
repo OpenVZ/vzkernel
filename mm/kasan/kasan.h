da5a9b2ae1844f5a6b994bd9c42ed9f0458ffa86
Andrey Ryabinin <aryabinin@virtuozzo.com>
2017-09-15 17:27:26 +0300

    mm/kasan: get rid of ->alloc_size in struct kasan_alloc_meta
    Size of slab object already stored in cache->object_size.
    
    Note, that kmalloc() internally rounds up size of allocation, so
    object_size may be not equal to alloc_size, but, usually we don't need
    to know the exact size of allocated object.  In case if we need that
    information, we still can figure it out from the report.  The dump of
    shadow memory allows to identify the end of allocated memory, and
    thereby the exact allocation size.
    
    Link: http://lkml.kernel.org/r/1470062715-14077-4-git-send-email-aryabinin@virtuozzo.com
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Dmitry Vyukov <dvyukov@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    https://jira.sw.ru/browse/PSBM-69081
    (cherry picked from commit 47b5c2a0f021e90a79845d1a1353780e5edd0bce)
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- mm/kasan/kasan.h
+++ mm/kasan/kasan.h
@@ -76,9 +76,7 @@ struct kasan_track {
 
 struct kasan_alloc_meta {
 	struct kasan_track track;
-	u32 state : 2;	/* enum kasan_state */
-	u32 alloc_size : 30;
-	u32 reserved;
+	u32 state;
 };
 
 struct qlist_node {
