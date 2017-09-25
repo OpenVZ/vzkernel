5783ed278d62952a8fa2e23f54099daad34003c6
Wanpeng Li <wanpeng.li@hotmail.com>
2017-09-25 13:44:58 +0000

    KVM: async_pf: Add L1 guest async_pf #PF vmexit handler
    This patch adds the L1 guest async page fault #PF vmexit handler, such
    by L1 similar to ordinary async page fault.
    
    Cc: Paolo Bonzini <pbonzini@redhat.com>
    Cc: Radim Krčmář <rkrcmar@redhat.com>
    Signed-off-by: Wanpeng Li <wanpeng.li@hotmail.com>
    [Passed insn parameters to kvm_mmu_page_fault().]
    Signed-off-by: Radim Krčmář <rkrcmar@redhat.com>
    (cherry picked from commit 1261bfa326f5e903166498628a1894edce0caabc)
    fix #PSBM-56498
    Signed-off-by: Denis Plotnikov <dplotnikov@virtuozzo.com>
    
    Signed-off-by: Denis Plotnikov <dplotnikov@virtuozzo.com>

--- arch/x86/kvm/mmu.h
+++ arch/x86/kvm/mmu.h
@@ -74,6 +74,9 @@ enum {
 int handle_mmio_page_fault(struct kvm_vcpu *vcpu, u64 addr, bool direct);
 void kvm_init_shadow_mmu(struct kvm_vcpu *vcpu);
 void kvm_init_shadow_ept_mmu(struct kvm_vcpu *vcpu, bool execonly);
+int kvm_handle_page_fault(struct kvm_vcpu *vcpu, u64 error_code,
+				u64 fault_address, char *insn, int insn_len,
+				bool need_unprotect);
 
 static inline unsigned int kvm_mmu_available_pages(struct kvm *kvm)
 {
