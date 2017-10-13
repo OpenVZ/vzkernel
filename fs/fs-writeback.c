823b105c8aa6d012e8efee4b061c1d92d7f0f007
Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
2017-10-13 15:40:02 +0300

    ms/trylock_super(): replacement for grab_super_passive()
    We have a softlockup in wb_do_writeback -> wb_writeback ->
    __writeback_inodes_wb stack, as we iterate over works in
    wb->bdi->work_list and for each work we move wb->b_dirty inodes to
    wb->b_io(in queue_io), and later in __writeback_inodes_wb() we try
    to write them, we trylock inode->i_sb->s_umount rw_semaphore and
    fail, we put each inode back to wb->b_dirty, and on each inode we
    take sb_lock - which can be long. We do aproximately ~100.000.000
    iterations, witch takes more than softlockup threshold(20sec).
    The above s_umount lock is taken by sync() in container in
    sync_filesystems_ve() wich sleeps for 50 sec.
    
    Removing sb_lock from the equation should remove most of contention
    and all iterations should pass much faster.
    
    https://jira.sw.ru/browse/PSBM-73370
    ms commit: eb6ef3d ("trylock_super(): replacement for grab_super_passive()")
    
    original commit message:
    
    I've noticed significant locking contention in memory reclaimer around
    sb_lock inside grab_super_passive(). Grab_super_passive() is called from
    two places: in icache/dcache shrinkers (function super_cache_scan) and
    from writeback (function __writeback_inodes_wb). Both are required for
    progress in memory allocator.
    
    Grab_super_passive() acquires sb_lock to increment sb->s_count and check
    sb->s_instances. It seems sb->s_umount locked for read is enough here:
    super-block deactivation always runs under sb->s_umount locked for write.
    Protecting super-block itself isn't a problem: in super_cache_scan() sb
    is protected by shrinker_rwsem: it cannot be freed if its slab shrinkers
    are still active. Inside writeback super-block comes from inode from bdi
    writeback list under wb->list_lock.
    
    This patch removes locking sb_lock and checks s_instances under s_umount:
    generic_shutdown_super() unlinks it under sb->s_umount locked for write.
    New variant is called trylock_super() and since it only locks semaphore,
    callers must call up_read(&sb->s_umount) instead of drop_super(sb) when
    they're done.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@yandex-team.ru>
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    Suggested-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- fs/fs-writeback.c
+++ fs/fs-writeback.c
@@ -797,9 +797,9 @@ static long __writeback_inodes_wb(struct bdi_writeback *wb,
 		if (time_is_before_jiffies(start_time + 15* HZ))
 			trace = 1;
 
-		if (!grab_super_passive(sb)) {
+		if (!trylock_super(sb)) {
 			/*
-			 * grab_super_passive() may fail consistently due to
+			 * trylock_super() may fail consistently due to
 			 * s_umount being grabbed by someone else. Don't use
 			 * requeue_io() to avoid busy retrying the inode/sb.
 			 */
@@ -810,7 +810,7 @@ static long __writeback_inodes_wb(struct bdi_writeback *wb,
 			continue;
 		}
 		wrote += writeback_sb_inodes(sb, wb, work);
-		drop_super(sb);
+		up_read(&sb->s_umount);
 
 		/* refer to the same tests at the end of writeback_sb_inodes */
 		if (wrote) {
