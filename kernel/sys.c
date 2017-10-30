5e2954477dbafd2e68b9019046f3645d18dc5a7f
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2017-10-30 19:24:48 +0300

    ve/sysinfo: don't clean uptime, procs and loads in containers
    Have diff-ve-sysinfo-dont-clean-uptime-procs-and-loads-in-containers
    in rh6, but in rh7 we moved only
       si_meminfo(info);
       si_swapinfo(info);
    part, so also move virtinfo_notifier_call part.
    
    Note: bc_fill_sysinfo() does not clear sysinfo for ve0.
    
    rh6 commit message:
    Subject: fix sysinfo in containers: don't clean uptime, procs and loads
    Date: Fri, 27 Apr 2012 19:37:14 +0400
    
    virtinfo_notifier_call calls bc_fill_sysinfo, which stores for later
    calculations totalram and totalswap and then clears entire
    sysinfo structure.
    
    Let's fill uptime, procs and loads after call virtinfo_notifier_call.
    
    Fix for http://bugzilla.openvz.org/show_bug.cgi?id=2051
    
    Signed-off-by: Dmitry Guryanov <dguryanov@parallels.com>
    
    Acked-by: Pavel Emelyanov <xemul@parallels.com>
    
    https://jira.sw.ru/browse/PSBM-62094
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -2661,6 +2661,12 @@ static int do_sysinfo(struct sysinfo *info)
 
 	memset(info, 0, sizeof(struct sysinfo));
 
+#ifdef CONFIG_BEANCOUNTERS
+	if (virtinfo_notifier_call(VITYPE_GENERAL, VIRTINFO_SYSINFO, info)
+			& NOTIFY_FAIL)
+		return -ENOMSG;
+#endif
+
 	get_monotonic_boottime(&tp);
 	info->uptime = tp.tv_sec + (tp.tv_nsec ? 1 : 0);
 
