daac328c0c3aee96967b8665bdeb1e5050f9ff6c
Andrey Ryabinin <aryabinin@virtuozzo.com>
2017-11-07 12:59:17 +0300

    ms/mm: introduce kv[mz]alloc helpers
    This only small part of the upstream commit
    a7c3e901a46ff54c016d040847eda598a9e3e653. I backported only
    part that introduces kv[mz]alloc helpers.
    
    Description of the original patch:
    
    commit a7c3e901a46ff54c016d040847eda598a9e3e653
    Author: Michal Hocko <mhocko@suse.com>
    Date:   Mon May 8 15:57:09 2017 -0700
    
        mm: introduce kv[mz]alloc helpers
    
        Patch series "kvmalloc", v5.
    
        There are many open coded kmalloc with vmalloc fallback instances in the
        tree.  Most of them are not careful enough or simply do not care about
        the underlying semantic of the kmalloc/page allocator which means that
        a) some vmalloc fallbacks are basically unreachable because the kmalloc
        part will keep retrying until it succeeds b) the page allocator can
        invoke a really disruptive steps like the OOM killer to move forward
        which doesn't sound appropriate when we consider that the vmalloc
        fallback is available.
    
        As it can be seen implementing kvmalloc requires quite an intimate
        knowledge if the page allocator and the memory reclaim internals which
        strongly suggests that a helper should be implemented in the memory
        subsystem proper.
    
        Most callers, I could find, have been converted to use the helper
        instead.  This is patch 6.  There are some more relying on __GFP_REPEAT
        in the networking stack which I have converted as well and Eric Dumazet
        was not opposed [2] to convert them as well.
    
        [1] http://lkml.kernel.org/r/20170130094940.13546-1-mhocko@kernel.org
        [2] http://lkml.kernel.org/r/1485273626.16328.301.camel@edumazet-glaptop3.roam.corp.google.com
    
        This patch (of 9):
    
        Using kmalloc with the vmalloc fallback for larger allocations is a
        common pattern in the kernel code.  Yet we do not have any common helper
        for that and so users have invented their own helpers.  Some of them are
        really creative when doing so.  Let's just add kv[mz]alloc and make sure
        it is implemented properly.  This implementation makes sure to not make
        a large memory pressure for > PAGE_SZE requests (__GFP_NORETRY) and also
        to not warn about allocation failures.  This also rules out the OOM
        killer as the vmalloc is a more approapriate fallback than a disruptive
        user visible action.
    
        This patch also changes some existing users and removes helpers which
        are specific for them.  In some cases this is not possible (e.g.
        ext4_kvmalloc, libcfs_kvzalloc) because those seems to be broken and
        require GFP_NO{FS,IO} context which is not vmalloc compatible in general
        (note that the page table allocation is GFP_KERNEL).  Those need to be
        fixed separately.
    
        While we are at it, document that __vmalloc{_node} about unsupported gfp
        mask because there seems to be a lot of confusion out there.
        kvmalloc_node will warn about GFP_KERNEL incompatible (which are not
        superset) flags to catch new abusers.  Existing ones would have to die
        slowly.
    
    https://jira.sw.ru/browse/PSBM-76752
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- security/apparmor/include/apparmor.h
+++ security/apparmor/include/apparmor.h
@@ -64,8 +64,6 @@ extern int apparmor_initialized __initdata;
 /* fn's in lib */
 char *aa_split_fqname(char *args, char **ns_name);
 void aa_info_message(const char *str);
-void *kvmalloc(size_t size);
-void kvfree(void *buffer);
 
 
 /**
