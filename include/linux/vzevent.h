57ce8093b0f1c8530d731a3e899bf37c40bc5345
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-11-21 16:13:19 +0300

    vzevent: initial patch
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    (cherry picked from vz8 commit a5d0c9b8757da6a01bd4a7620e1c432fbecb79ca)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Feature: ve: vzevents - useful events related to Container status

---
+++ include/linux/vzevent.h
@@ -0,0 +1,29 @@
+/*
+ *  include/linux/vzevent.h
+ *
+ *  Copyright (c) 2010-2015 Parallels IP Holdings GmbH
+ *  Copyright (c) 2017-2021 Virtuozzo International GmbH. All rights reserved.
+ *
+ */
+
+#ifndef __LINUX_VZ_EVENT_H__
+#define __LINUX_VZ_EVENT_H__
+
+#if defined(CONFIG_VZ_EVENT) || defined(CONFIG_VZ_EVENT_MODULE)
+extern int vzevent_send(int msg, const char *attrs_fmt, ...);
+#else
+static inline int vzevent_send(int msg, const char *attrs_fmt, ...)
+{
+	return 0;
+}
+#endif
+
+enum {
+	VE_EVENT_MOUNT,
+	VE_EVENT_UMOUNT,
+	VE_EVENT_START,
+	VE_EVENT_STOP,
+	VE_EVENT_REBOOT,
+};
+
+#endif /* __LINUX_VZ_EVENT_H__ */
