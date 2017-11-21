f8317323d1f240d7ab8166ee877efd59d482307f
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-11-21 16:14:29 +0300

    vzdev: initial patch
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    
    (cherry picked from vz8 commit 1cd6755cef20e5db2fa631426c84265f6ddb122c)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Feature: ve: ve generic structures

---
+++ include/linux/vzctl.h
@@ -0,0 +1,29 @@
+/*
+ *  include/linux/vzctl.h
+ *
+ *  Copyright (c) 2005-2008 SWsoft
+ *  Copyright (c) 2009-2015 Parallels IP Holdings GmbH
+ *  Copyright (c) 2017-2021 Virtuozzo International GmbH. All rights reserved.
+ *
+ */
+
+#ifndef _LINUX_VZCTL_H
+#define _LINUX_VZCTL_H
+
+#include <linux/list.h>
+
+struct module;
+struct inode;
+struct file;
+struct vzioctlinfo {
+	unsigned type;
+	int (*ioctl)(struct file *, unsigned int, unsigned long);
+	int (*compat_ioctl)(struct file *, unsigned int, unsigned long);
+	struct module *owner;
+	struct list_head list;
+};
+
+extern void vzioctl_register(struct vzioctlinfo *inf);
+extern void vzioctl_unregister(struct vzioctlinfo *inf);
+
+#endif
