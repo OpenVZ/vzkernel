d3fa0aa31974a783f7fb30787986e3b06f67b0b8
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-11-22 11:36:32 +0300

    veip: initial patch
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

---
+++ include/linux/veip.h
@@ -0,0 +1,35 @@
+/*
+ *
+ *  Copyright (c) 2000-2017 Virtuozzo International GmbH.
+ *  All rights reserved.
+ *
+ */
+
+#ifndef __VE_IP_H_
+#define __VE_IP_H_
+
+#include <linux/list.h>
+
+struct veip_struct
+{
+	struct list_head	src_lh;
+	struct list_head	dst_lh;
+	struct list_head	ip_lh;
+	struct list_head	list;
+	struct list_head	ext_lh;
+	envid_t			veid;
+	struct rcu_head		rcu;
+};
+
+struct ve_addr_struct {
+	int family;
+	__u32 key[4];
+};
+
+struct sockaddr;
+
+extern void veaddr_print(char *, int, struct ve_addr_struct *);
+extern int sockaddr_to_veaddr(struct sockaddr __user *uaddr, int addrlen,
+		struct ve_addr_struct *veaddr);
+
+#endif
