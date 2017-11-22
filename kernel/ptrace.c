b52f81b2edba9cd8e5a248ad55d1f499d038e36c
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-11-22 23:18:43 +0300

    ve/mm/trace: introduce vps_dumpable flag
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    mm/vps_dumpable: naming and description for task->mm->vps_dumpable values
    
    We have task->mm->vps_dumpable field that has magic values. It's time to name
    them and describe. This patch does this conversion:
    
    vps_dumpable=0 -> VD_VE_ENTER_TASK	- mark tasks entered to VE from host,
    					  no ptrace and no coredump allowed
    vps_dumpable=1 -> VD_PTRACE_COREDUMP	- mark tasks with ptrace and
    					  coredump allowed
    vps_dumpable=2 -> VD_LICDATA_ACCESS	- mark tasks that accesses container
    					  license sensitive information,
    					  ptrace and coredump prohibited
    
    Changes v3:
    More readable defines comments.
    Check ->vps_dumpable first to avoid releasing creds in do_coredump().
    
    Changes v2:
    Moved vps_dumpable values below struct mm_struct.
    
    https://jira.sw.ru/browse/PSBM-20666
    
    Signed-off-by: Andrey Smetanin <asmetanin@parallels.com>

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -238,6 +238,8 @@ int ___ptrace_may_access(struct task_struct *tracer,
 	 * or halting the specified task is impossible.
 	 */
 	int dumpable = 0;
+	int vps_dumpable = 0;
+
 	/* Don't let security modules deny introspection */
 	if (task == tracer)
 		return 0;
@@ -265,11 +267,17 @@ int ___ptrace_may_access(struct task_struct *tracer,
 ok:
 	rcu_read_unlock();
 	smp_rmb();
-	if (task->mm)
+	if (task->mm) {
 		dumpable = get_dumpable(task->mm);
+		vps_dumpable = (task->mm->vps_dumpable == VD_PTRACE_COREDUMP);
+	}
 	rcu_read_lock();
 	if (dumpable != SUID_DUMP_USER &&
 	    !ptrace_has_cap(__task_cred(task)->user_ns, mode)) {
+		rcu_read_unlock();
+		return -EPERM;
+	}
+	if (!vps_dumpable && !ve_is_super(get_exec_env())) {
 		rcu_read_unlock();
 		return -EPERM;
 	}
@@ -332,6 +340,10 @@ static int ptrace_attach(struct task_struct *task, long request,
 
 	task_lock(task);
 	retval = __ptrace_may_access(task, PTRACE_MODE_ATTACH);
+	if (!retval) {
+		if (!task->mm || task->mm->vps_dumpable == VD_LICDATA_ACCESS)
+			retval = -EACCES;
+	}
 	task_unlock(task);
 	if (retval)
 		goto unlock_creds;
