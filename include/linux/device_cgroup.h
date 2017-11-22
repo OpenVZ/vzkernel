d0476144de3827ff6af0bb3bb1445f0fe9aa170d
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-11-22 12:50:24 +0300

    devcgroup: initial patch
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ve/devcgroup: fix mknod permissions - allow to create any dev inside a CT
    
    DEV_ALL is not handled properly by 3.9 kernel, because it is not
    supposed to be found in exception tables => substitute it with
    DEV_CHAR and DEV_BLOCK entries to allow mknod any device.
    
    https://jira.sw.ru/browse/PSBM-21979
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    +++
    ve/cgroup: devices -- Modify exception list for docker sake
    
    When docker runs up it modifies nested device cgroups. The devices it needs
    to operate with are almost the same we've had in our exception list already
    except:
    
     1) Add ACC_MKNOD for every device we have
    
    	This is harmless operation simply to make docker happy.
    
     2) Add setting up ACC_MKNOD for devices created for container
        via set_device_perms_ve. At the moment this is important
        for VT use inside container.
    
     3) Add MISC_MAJOR:200 for tun device
    
    	Tun/tap is safe to use inside container as far as I know.
    
        p.s. khorenko@ approved this kind of change in pcs7.
    
     4) For some reason docker requires write access to /dev/random,
        grand it (since we're prohibiting writing to /dev/random
        from inside of ve on kernel level, it's safe to do).
    
    v2:
     - Use ns_capable(CAP_VE_SYS_ADMIN) instead of plain capable(CAP_SYS_ADMIN)
       for docker sake. Note the vanilla kernel no longer has any can_attach
       helper, but to make the patch smaller lets keep it. ns_capable should
       be enough for security, after all the user in container may attach own
       tasks only.
    
    v3:
     - Use nsown_capable.
    
    v4:
     - Switch back to plain capable test. It turned out that vanilla
       kernel has no cap test in devcgroup_can_attach (neither it
       has this helper), while nsown_capable looks like be too relaxed.
       So I think we could use plain capable() as we do in PCS6 kernel
       same time requiring CAP_VE_SYS_ADMIN to present inside container.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@odin.com>
    Acked-by: Konstantin Khorenko <khorenko@odin.com>
    
    CC: Vladimir Davydov <vdavydov@odin.com>
    CC: Pavel Emelyanov <xemul@odin.com>
    CC: Andrey Vagin <avagin@odin.com>
    
    +++
    ve: device cgroup -- Implement devcgroup_seq_show_ve
    
    In PCS7 cgroups are configured from user space, so there is
    no longer connection from ve to device cgroup via css as
    it was in PCS6. Instead we should open device cgroup explicitly.
    
    https://jira.sw.ru/browse/PSBM-33555
    
    v2 (by vdavydov@):
     - use ve::ve_name because we're switching to UUID based containers
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@odin.com>
    Reviewed-by: Vladimir Davydov <vdavydov@parallels.com>
    
    CC: Konstantin Khorenko <khorenko@odin.com>
    CC: Andrey Vagin <avagin@odin.com>
    
    +++
    Drop VZCTL_ENV_CREATE
    
    It's getting too difficult to support it. Since we've been using cgroup
    interface for creating VE for quite a while, let's drop it.
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- include/linux/device_cgroup.h
+++ include/linux/device_cgroup.h
@@ -11,9 +11,24 @@ static inline int devcgroup_inode_permission(struct inode *inode, int mask)
 		return 0;
 	return __devcgroup_inode_permission(inode, mask);
 }
+
+extern int devcgroup_device_permission(umode_t mode, dev_t dev, int mask);
+extern int devcgroup_device_visible(umode_t mode, int major,
+		int start_minor, int nr_minors);
+
+struct cgroup;
+int devcgroup_set_perms_ve(struct cgroup *cgroup, unsigned, dev_t, unsigned);
+struct ve_struct;
+int devcgroup_seq_show_ve(struct cgroup *devices_root, struct ve_struct *ve, struct seq_file *m);
+
 #else
 static inline int devcgroup_inode_permission(struct inode *inode, int mask)
 { return 0; }
 static inline int devcgroup_inode_mknod(int mode, dev_t dev)
+{ return 0; }
+static inline int devcgroup_device_permission(umode_t mode, dev_t dev, int mask)
+{ return 0; }
+static inline int devcgroup_device_visible(umode_t mode, int major,
+		int start_minor, int nr_minors)
 { return 0; }
 #endif
