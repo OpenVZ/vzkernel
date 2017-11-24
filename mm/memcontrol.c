0fa84764c6b8b9f411cff4a8cf82e7fbe7bed8cb
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-11-25 01:50:33 +0300

    ms/memcg: do not call high reclaim if !__GFP_WAIT
    Sent to ms, not applied yet:
    https://lkml.org/lkml/2015/5/28/528
    
    When trimming memcg consumption excess (see memory.high), we call
    try_to_free_mem_cgroup_pages without checking if we are allowed to sleep
    in the current context, which can result in a deadlock. Fix this.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@suse.cz>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -2951,6 +2951,8 @@ static int mem_cgroup_do_charge(struct mem_cgroup *memcg, gfp_t gfp_mask,
 	return CHARGE_NOMEM;
 
 done:
+	if (!(gfp_mask & __GFP_WAIT))
+		goto out;
 	/*
 	 * If the hierarchy is above the normal consumption range,
 	 * make the charging task trim their excess contribution.
@@ -2960,6 +2962,7 @@ done:
 			continue;
 		try_to_free_mem_cgroup_pages(memcg, nr_pages, gfp_mask, false);
 	} while ((memcg = parent_mem_cgroup(memcg)));
+out:
 	return CHARGE_OK;
 }
 
