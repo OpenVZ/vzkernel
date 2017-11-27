607714b14330d23749880ab2556c1c8128620bfe
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-27 20:10:46 +0300

    ve: Use ve_printk() for often kernel messages about user actions
    Move messages such as program segfault etc out of system dmesg.
    
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- net/core/sock.c
+++ net/core/sock.c
@@ -412,8 +412,8 @@ static void sock_warn_obsolete_bsdism(const char *name)
 	static char warncomm[TASK_COMM_LEN];
 	if (strcmp(warncomm, current->comm) && warned < 5) {
 		strcpy(warncomm,  current->comm);
-		pr_warn("process `%s' is using obsolete %s SO_BSDCOMPAT\n",
-			warncomm, name);
+		ve_printk(VE_LOG, KERN_WARNING "process `%s' is using obsolete "
+			"%s SO_BSDCOMPAT\n", warncomm, name);
 		warned++;
 	}
 }
