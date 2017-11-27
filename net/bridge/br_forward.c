e11af1b2ff921eeba77781e1e855737d325b9943
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-27 19:52:14 +0300

    net: Add NETIF_F_VENET netdev feature and optimized skb forwarding
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    [VvS REHL7.8 rebase] context changes

--- net/bridge/br_forward.c
+++ net/bridge/br_forward.c
@@ -36,7 +36,8 @@ static inline int should_deliver(const struct net_bridge_port *p,
 int br_dev_queue_push_xmit(struct sock *sk, struct sk_buff *skb)
 {
 	skb_push(skb, ETH_HLEN);
-	if (!is_skb_forwardable(skb->dev, skb))
+	if (!(skb->dev->features & NETIF_F_VENET) &&
+	    !is_skb_forwardable(skb->dev, skb))
 		goto drop;
 
 	br_drop_fake_rtable(skb);
