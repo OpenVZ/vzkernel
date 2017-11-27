9575776dd0e8fec0bd8020259602f7b58cec4346
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-27 17:29:18 +0300

    sysctl: Change sysctl::show_unhandled_signals default
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -1048,7 +1048,7 @@ spurious_fault(unsigned long error_code, unsigned long address)
 	return ret;
 }
 
-int show_unhandled_signals = 1;
+int show_unhandled_signals = 0;
 
 static inline int
 access_error(unsigned long error_code, struct vm_area_struct *vma)
