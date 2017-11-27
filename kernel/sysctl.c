26f77244a5e8870fb1c0001cf5d5700b8f30f29a
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-27 22:33:05 +0300

    ve: Implement per ve randomize_va_space
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -64,6 +64,7 @@
 #include <linux/sched/sysctl.h>
 #include <linux/kexec.h>
 #include <linux/mount.h>
+#include <linux/ve.h>
 
 #include "../lib/kstrtox.h"
 
@@ -1022,9 +1023,11 @@ static struct ctl_table kern_table[] = {
 #if defined(CONFIG_MMU)
 	{
 		.procname	= "randomize_va_space",
-		.data		= &randomize_va_space,
+		.data		= &_randomize_va_space,
+		.extra1		= (void *)offsetof(struct ve_struct,
+							_randomize_va_space),
 		.maxlen		= sizeof(int),
-		.mode		= 0644,
+		.mode		= 0644 | S_ISVTX,
 		.proc_handler	= proc_dointvec,
 	},
 #endif
