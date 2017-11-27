b766f396e2c0f1a727b83a2d362c59f5d79ad3c2
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-27 20:23:00 +0300

    ub: Show ub memory on sysrq mem show
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- drivers/tty/sysrq.c
+++ drivers/tty/sysrq.c
@@ -47,6 +47,9 @@
 #include <linux/rcupdate.h>
 #include <linux/ve.h>
 
+#include <bc/oom_kill.h>
+#include <bc/vmpages.h>
+
 #include <asm/ptrace.h>
 #include <asm/irq_regs.h>
 
@@ -318,6 +321,13 @@ static struct sysrq_key_op sysrq_ftrace_dump_op = {
 
 static void sysrq_handle_showmem(int key)
 {
+	struct user_beancounter *ub;
+
+	rcu_read_lock();
+	for_each_beancounter(ub)
+		show_ub_mem(ub);
+	rcu_read_unlock();
+
 	show_mem(0);
 }
 static struct sysrq_key_op sysrq_showmem_op = {
