5917ae8afce46ba6be81baf36b58ab3ec02313f4
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-27 22:55:51 +0300

    ub: Add address_space::dirtied_ub
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -584,6 +584,7 @@ struct address_space {
 	spinlock_t		private_lock;	/* for use by the address_space */
 	struct list_head	private_list;	/* ditto */
 	void			*private_data;	/* ditto */
+	struct user_beancounter *dirtied_ub;
 } __attribute__((aligned(sizeof(long))));
 	/*
 	 * On most architectures that alignment is already the case; but
