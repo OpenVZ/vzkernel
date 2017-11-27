4a8649e14d60c824a220b0b6e7c4d77eb6624257
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-27 18:47:11 +0300

    pid_ns: Virtualize pid_max
    Introduce per pid namespace pid_max.
    
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    +++
    pid: Use proper ns in proc_dointvec_pidmax()
    
    Use current task pid ns instead of pid ns for future children
    https://jira.sw.ru/browse/PSBM-121530
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- include/linux/threads.h
+++ include/linux/threads.h
@@ -25,6 +25,7 @@
  * This controls the default maximum pid allocated to a process
  */
 #define PID_MAX_DEFAULT (CONFIG_BASE_SMALL ? 0x1000 : 0x8000)
+#define PID_MAX_NS_DEFAULT	(PID_MAX_DEFAULT)
 
 /*
  * A maximum of 4 million PIDs should be enough for a while.
