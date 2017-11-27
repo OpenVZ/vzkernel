46e41a0bc3142cc189c9b66f788d94a3e83314d6
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-27 20:10:46 +0300

    ve: Use ve_printk() for often kernel messages about user actions
    Move messages such as program segfault etc out of system dmesg.
    
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- lib/nlattr.c
+++ lib/nlattr.c
@@ -231,8 +231,9 @@ int nla_parse(struct nlattr **tb, int maxtype, const struct nlattr *head,
 	}
 
 	if (unlikely(rem > 0))
-		pr_warn_ratelimited("netlink: %d bytes leftover after parsing attributes in process `%s'.\n",
-				    rem, current->comm);
+		ve_pr_warn_ratelimited(VE_LOG,
+			"netlink: %d bytes leftover after parsing attributes in process `%s'.\n",
+			rem, current->comm);
 
 	err = 0;
 errout:
