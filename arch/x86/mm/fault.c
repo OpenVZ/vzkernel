0cfaafd1d6a96d6df90d27e8ef5954699898727c
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-27 20:10:46 +0300

    ve: Use ve_printk() for often kernel messages about user actions
    Move messages such as program segfault etc out of system dmesg.
    
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    (cherry picked from vz8 commit 8d73ec829805acacb51d2657253808d4c85742d1)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Feature: print: enhance logging

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -778,7 +778,7 @@ show_signal_msg(struct pt_regs *regs, unsigned long error_code,
 	if (!printk_ratelimit())
 		return;
 
-	printk("%s%s[%d]: segfault at %lx ip %px sp %px error %lx",
+	ve_printk(VE_LOG, "%s%s[%d]: segfault at %lx ip %px sp %px error %lx",
 		loglvl, tsk->comm, task_pid_nr(tsk), address,
 		(void *)regs->ip, (void *)regs->sp, error_code);
 
