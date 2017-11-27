fa48e3b983bcab95f812ec7711cb1992ce0734d6
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-27 18:36:17 +0300

    net: Add sysctl_tcp_use_sg to disable NETIF_F_SG sk route capability
    Allow to disable paged skbs.
    
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -277,6 +277,7 @@ extern unsigned int sysctl_tcp_notsent_lowat;
 extern int sysctl_tcp_min_tso_segs;
 extern int sysctl_tcp_autocorking;
 extern int sysctl_tcp_invalid_ratelimit;
+extern int sysctl_tcp_use_sg;
 
 extern atomic_long_t tcp_memory_allocated;
 extern struct percpu_counter tcp_sockets_allocated;
