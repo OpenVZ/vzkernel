b5e6a51b98f584e44d5bc612e99a46bf5769f12b
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-11-27 10:55:36 +0300

    ve/net/cred: add ve_capable to check capabilities relative to the current VE (v2)
    We want to allow a few operations in VE. Currently we use nsown_capable,
    but it's wrong, because in this case we allow these operations in any
    user namespace.
    
    v2: take ve0->cred if the currect ve isn't running
    
    https://jira.sw.ru/browse/PSBM-39077
    
    Signed-off-by: Andrew Vagin <avagin@virtuozzo.com>
    Reviewed-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- net/ipv6/sit.c
+++ net/ipv6/sit.c
@@ -310,7 +310,7 @@ static int ipip6_tunnel_get_prl(struct ip_tunnel *t,
 	/* For simple GET or for root users,
 	 * we try harder to allocate.
 	 */
-	kp = (cmax <= 1 || capable(CAP_NET_ADMIN)) ?
+	kp = (cmax <= 1 || ve_capable(CAP_NET_ADMIN)) ?
 		kcalloc(cmax, sizeof(*kp), GFP_KERNEL | __GFP_NOWARN) :
 		NULL;
 
