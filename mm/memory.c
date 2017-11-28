88f451e5232e748c2e4f0f745755354cf4a9c352
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-28 17:56:28 +0300

    ub: Add dirty page accounting
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    [VvS RHEL7.8 rebase] context changes

--- mm/memory.c
+++ mm/memory.c
@@ -1240,7 +1240,7 @@ again:
 			if (!PageAnon(page)) {
 				if (pte_dirty(ptent)) {
 					force_flush = 1;
-					set_page_dirty(page);
+					set_page_dirty_mm(page, mm);
 				}
 				if (pte_young(ptent) &&
 				    likely(!VM_SequentialReadHint(vma)))
@@ -1615,7 +1615,7 @@ static int insert_page(struct vm_area_struct *vma, unsigned long addr,
 	/* Ok, finally just insert the thing.. */
 	get_page(page);
 	inc_mm_counter_fast(mm, mm_counter_file(page));
-	page_add_file_rmap(page);
+	page_add_file_rmap(page, mm);
 	set_pte_at(mm, addr, pte, mk_pte(page, prot));
 
 	retval = 0;
@@ -2904,7 +2904,8 @@ static int do_swap_page(struct vm_fault *vmf, pte_t orig_pte)
 	mem_cgroup_commit_charge_swapin(page, ptr);
 
 	swap_free(entry);
-	if (vm_swap_full() || (vmf->vma->vm_flags & VM_LOCKED) || PageMlocked(page))
+	if (vm_swap_full() || ub_swap_full(mm->mm_ub) ||
+			(vmf->vma->vm_flags & VM_LOCKED) || PageMlocked(page))
 		try_to_free_swap(page);
 	unlock_page(page);
 	if (page != swapcache) {
@@ -2982,7 +2983,6 @@ static int do_anonymous_page(struct vm_fault *vmf)
 		goto setpte;
 	}
 
-	/* Allocate our own private page. */
 	if (unlikely(anon_vma_prepare(vma)))
 		goto oom;
 	page = alloc_zeroed_user_highpage_movable(vma, address);
@@ -3089,7 +3089,7 @@ static void do_set_pte(struct vm_area_struct *vma, unsigned long address,
 		page_add_new_anon_rmap(page, vma, address);
 	} else {
 		inc_mm_counter_fast(vma->vm_mm, mm_counter_file(page));
-		page_add_file_rmap(page);
+		page_add_file_rmap(page, vma->vm_mm);
 	}
 	set_pte_at(vma->vm_mm, address, pte, entry);
 
