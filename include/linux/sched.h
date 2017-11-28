bd19f27e2a177033f0323b5cabcd6908ec69bb3d
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-28 17:26:15 +0300

    sched: Add cpulimit base interfaces
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    +++
    ve/sched: take nr_cpus and cpu_rate from ve root task group
    
    Patchset description:
    
    ve: properly handle nr_cpus and cpu_rate for nested cgroups
    
    https://jira.sw.ru/browse/PSBM-69678
    
    Pavel Tikhomirov (3):
      cgroup: remove rcu_read_lock from cgroup_get_ve_root
      cgroup: make cgroup_get_ve_root visible in kernel/sched/core.c
      sched: take nr_cpus and cpu_rate from ve root task group
    
    =============================================================
    This patch description:
    
    Cpu view in container should depend only from root cpu cgroup
    nr_cpus/rate configuration. So replace tg->xxx references by
    tg_xxx(tg) helpers to get xxx from root ve cgroup. We still
    allow set/read rate and nr_cpus directly in nested cgroups,
    but they are just converted to corresponding cfs_period and
    cfs_quota setup, and does _not_ influence in container view
    of cpus and their stats.
    
    Also remove excessive rcu_read_lock/unlock as we have no rcu
    dereference in between, looks like some leftover for task_group()
    which differs in VZ6 and VZ7.
    
    https://jira.sw.ru/browse/PSBM-69678
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Reviewed-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    +++
    ve/sched: remove no ve root cgroup warning
    
    We can get to ve_root_tg() from host's cgroup so it is expected
    to have no ve root cgroup for it. Call stack on task wakeup:
    
    wake_up_process -> try_to_wake_up -> select_task_rq_fair
    -> select_runnable_cpu -> check_cpulimit_spread -> tg_cpu_rate
    -> ve_root_tg
    
    Fixes: e661261 ("ve/sched: take nr_cpus and cpu_rate from ve root task group")

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -3150,6 +3150,30 @@ static inline void set_task_cpu(struct task_struct *p, unsigned int cpu)
 
 #endif /* CONFIG_SMP */
 
+#ifdef CONFIG_CFS_CPULIMIT
+extern unsigned int task_nr_cpus(struct task_struct *p);
+extern unsigned int task_vcpu_id(struct task_struct *p);
+extern unsigned int sysctl_sched_cpulimit_scale_cpufreq;
+extern unsigned int sched_cpulimit_scale_cpufreq(unsigned int freq);
+#else
+static inline unsigned int task_nr_cpus(struct task_struct *p)
+{
+	return num_online_cpus();
+}
+
+static inline unsigned int task_vcpu_id(struct task_struct *p)
+{
+	return task_cpu(p);
+}
+
+static inline unsigned int sched_cpulimit_scale_cpufreq(unsigned int freq)
+{
+	return freq;
+}
+#endif
+
+#define num_online_vcpus() task_nr_cpus(current)
+
 extern long sched_setaffinity(pid_t pid, const struct cpumask *new_mask);
 extern long sched_getaffinity(pid_t pid, struct cpumask *mask);
 
