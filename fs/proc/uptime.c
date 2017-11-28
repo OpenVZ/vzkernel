95e61c0fef8139c4e4d10faa9587a9263cb4bed7
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-28 13:39:21 +0300

    ve/time: Customize VE uptime
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    (cherry picked from commit eb7a72d2db97a84ba20b136569c7870fe9743b19)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ve/time: Fix VE uptime virtualization to use u64 start_time
    
    mFixes: a3c4d1d8f383 ("ve/time: Customize VE uptime")
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ve: Use @real_start_timespec in uptime_proc_show
    
    uptime_proc_show uses bootbased clocks so we should use
    @real_start_timespec here instead. Seems was a typo while
    converting from pcs6 code.
    
    In scope of
    https://jira.sw.ru/browse/PSBM-41406
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    Reviewed-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    
    vdavydov@:
    This hunk was a part of
      diff-cpt-record-ct-boot-based-start-time-to-show-correct-uptime
    which was skipped during rebase to RH7 because it was considered cpt-related.
    
    (cherry picked from vz7 commit 55b9202e39282f2a21773fd1fd99317bc6e07ddd)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    khorenko@: extracted hunks related to idle time virtualization and
    moved them into commit
      ("ve/time/stat: idle time virtualization in /proc/loadavg")
    
    khorenko@: rebase to RHEL8.4 notes:
    - offset type changed: timespec -> timespec64

--- fs/proc/uptime.c
+++ fs/proc/uptime.c
@@ -7,10 +7,12 @@
 #include <linux/time.h>
 #include <linux/time_namespace.h>
 #include <linux/kernel_stat.h>
+#include <linux/cgroup.h>
+#include <linux/ve.h>
 
 static int uptime_proc_show(struct seq_file *m, void *v)
 {
-	struct timespec64 uptime;
+	struct timespec64 uptime, offset;
 	struct timespec64 idle;
 	u64 nsec;
 	u32 rem;
@@ -25,6 +27,14 @@ static int uptime_proc_show(struct seq_file *m, void *v)
 
 	idle.tv_sec = div_u64_rem(nsec, NSEC_PER_SEC, &rem);
 	idle.tv_nsec = rem;
+#ifdef CONFIG_VE
+	if (!ve_is_super(get_exec_env())) {
+		offset = ns_to_timespec64(get_exec_env()->real_start_time);
+		set_normalized_timespec64(&uptime,
+					  uptime.tv_sec - offset.tv_sec,
+					  uptime.tv_nsec - offset.tv_nsec);
+	}
+#endif
 	seq_printf(m, "%lu.%02lu %lu.%02lu\n",
 			(unsigned long) uptime.tv_sec,
 			(uptime.tv_nsec / (NSEC_PER_SEC / 100)),
