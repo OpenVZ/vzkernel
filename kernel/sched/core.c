52f73b22a4e05c56aec4512df11a3d26e0f47e78
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-28 15:46:14 +0300

    sched: Account cfs_rq::nr_iowait
    TODO: Extract time accounting. Currently impossible,
    as this will break binary identity.
    
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1846,6 +1846,12 @@ try_to_wake_up(struct task_struct *p, unsigned int state, int wake_flags)
 	 */
 	smp_rmb();
 
+	if (p->in_iowait && p->sched_class->nr_iowait_dec) {
+		struct rq *rq = __task_rq_lock(p);
+		p->sched_class->nr_iowait_dec(p);
+		__task_rq_unlock(rq);
+	}
+
 	p->sched_contributes_to_load = !!task_contributes_to_load(p);
 	p->state = TASK_WAKING;
 
@@ -6570,6 +6576,7 @@ migration_call(struct notifier_block *nfb, unsigned long action, void *hcpu)
 
 			set_rq_online(rq);
 		}
+		start_cfs_idle_time_accounting(cpu);
 		raw_spin_unlock_irqrestore(&rq->lock, flags);
 		break;
 
@@ -6584,6 +6591,7 @@ migration_call(struct notifier_block *nfb, unsigned long action, void *hcpu)
 		}
 		migrate_tasks(cpu);
 		BUG_ON(rq->nr_running != 1); /* the migration thread */
+		stop_cfs_idle_time_accounting(cpu);
 		raw_spin_unlock_irqrestore(&rq->lock, flags);
 		break;
 
@@ -9047,6 +9055,10 @@ void sched_move_task(struct task_struct *tsk)
 	if (queued)
 		dequeue_task(rq, tsk, DEQUEUE_SAVE | DEQUEUE_MOVE);
 	else {
+		if (!(tsk->state & TASK_WAKING) && tsk->in_iowait &&
+				tsk->sched_class->nr_iowait_dec)
+			tsk->sched_class->nr_iowait_dec(tsk);
+
 		if (task_contributes_to_load(tsk))
 			task_cfs_rq(tsk)->nr_unint--;
 
@@ -9078,6 +9090,10 @@ void sched_move_task(struct task_struct *tsk)
 	if (queued)
 		enqueue_task(rq, tsk, ENQUEUE_RESTORE | ENQUEUE_MOVE);
 	else {
+		if (!(tsk->state & TASK_WAKING) && tsk->in_iowait &&
+				tsk->sched_class->nr_iowait_inc)
+			tsk->sched_class->nr_iowait_inc(tsk);
+
 		if (task_contributes_to_load(tsk))
 			task_cfs_rq(tsk)->nr_unint++;
 
