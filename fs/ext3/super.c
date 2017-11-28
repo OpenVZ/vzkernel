94e38ce08a06efb817540bcc2638dd992fc43799
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-28 17:35:34 +0300

    fs: Mask appropriate filesystems FS_VIRTUALIZED
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- fs/ext3/super.c
+++ fs/ext3/super.c
@@ -3062,7 +3062,7 @@ static struct file_system_type ext3_fs_type = {
 	.name		= "ext3",
 	.mount		= ext3_mount,
 	.kill_sb	= kill_block_super,
-	.fs_flags	= FS_REQUIRES_DEV,
+	.fs_flags	= FS_REQUIRES_DEV | FS_VIRTUALIZED,
 };
 MODULE_ALIAS_FS("ext3");
 
