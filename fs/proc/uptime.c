aad92f3e49aafc8d5721a1a951656f21846363fa
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-28 13:39:21 +0300

    ve/time: Customize VE uptime
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    (cherry picked from commit eb7a72d2db97a84ba20b136569c7870fe9743b19)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/proc/uptime.c
+++ fs/proc/uptime.c
@@ -6,11 +6,11 @@
 #include <linux/seq_file.h>
 #include <linux/time.h>
 #include <linux/kernel_stat.h>
+#include <linux/cgroup.h>
+#include <linux/ve.h>
 
-static int uptime_proc_show(struct seq_file *m, void *v)
+static inline void get_ve0_idle(struct timespec64 *idle)
 {
-	struct timespec uptime;
-	struct timespec64 idle;
 	u64 nsec;
 	u32 rem;
 	int i;
@@ -19,9 +19,50 @@ static int uptime_proc_show(struct seq_file *m, void *v)
 	for_each_possible_cpu(i)
 		nsec += (__force u64) kcpustat_cpu(i).cpustat[CPUTIME_IDLE];
 
+	idle->tv_sec = div_u64_rem(nsec, NSEC_PER_SEC, &rem);
+	idle->tv_nsec = rem;
+}
+
+static inline void get_veX_idle(struct timespec *idle, struct cgroup* cgrp)
+{
+#if 0
+FIXME:	to be reworked anyway in
+	"Use ve init task's css instead of opening cgroup via vfs"
+
+	struct kernel_cpustat kstat;
+
+	cpu_cgroup_get_stat(cgrp, &kstat);
+	*idle = ns_to_timespec(kstat.cpustat[CPUTIME_IDLE]);
+#endif
+}
+
+static int uptime_proc_show(struct seq_file *m, void *v)
+{
+	struct timespec uptime;
+	struct timespec64 idle;
+
+	if (ve_is_super(get_exec_env()))
+		get_ve0_idle(&idle);
+	else {
+		get_ve0_idle(&idle);
+#if 0
+FIXME:  to be reworked anyway in
+        "Use ve init task's css instead of opening cgroup via vfs"
+
+		rcu_read_lock();
+		get_veX_idle(&idle, task_cgroup(current, cpu_cgroup_subsys_id));
+		rcu_read_unlock();
+#endif
+	}
+
 	get_monotonic_boottime(&uptime);
-	idle.tv_sec = div_u64_rem(nsec, NSEC_PER_SEC, &rem);
-	idle.tv_nsec = rem;
+#ifdef CONFIG_VE
+	if (!ve_is_super(get_exec_env())) {
+		set_normalized_timespec(&uptime,
+			uptime.tv_sec - get_exec_env()->start_timespec.tv_sec,
+			uptime.tv_nsec - get_exec_env()->start_timespec.tv_nsec);
+	}
+#endif
 	seq_printf(m, "%lu.%02lu %lu.%02lu\n",
 			(unsigned long) uptime.tv_sec,
 			(uptime.tv_nsec / (NSEC_PER_SEC / 100)),
