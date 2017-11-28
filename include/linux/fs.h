8310488eb1e699aca39a373808a77a3c5d16615f
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-28 12:07:17 +0300

    fs: Restrict ve sync methods
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -64,6 +64,7 @@ extern struct inodes_stat_t inodes_stat;
 extern int leases_enable, lease_break_time;
 extern int sysctl_protected_symlinks;
 extern int sysctl_protected_hardlinks;
+extern int sysctl_fsync_enable;
 
 struct buffer_head;
 typedef int (get_block_t)(struct inode *inode, sector_t iblock,
