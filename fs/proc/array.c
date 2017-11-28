e0fe2cb0513d4f1d89c80a9d8a21fde2c2595049
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-28 17:37:32 +0300

    ve/proc: Show vcpu in do_task_stat()
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    (cherry picked from commit 78002f240e10fec47aaa4a727b5fdebba3a3e27a)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/proc/array.c
+++ fs/proc/array.c
@@ -436,6 +436,7 @@ static int do_task_stat(struct seq_file *m, struct pid_namespace *ns,
 	u64 cgtime, gtime;
 	unsigned long rsslim = 0;
 	unsigned long flags;
+	int is_super = ve_is_super(get_exec_env());
 
 	state = *get_task_state(task);
 	vsize = eip = esp = 0;
@@ -582,6 +583,7 @@ static int do_task_stat(struct seq_file *m, struct pid_namespace *ns,
 	seq_put_decimal_ull(m, " ", 0);
 	seq_put_decimal_ll(m, " ", task->exit_signal);
 	seq_put_decimal_ll(m, " ", task_cpu(task));
+	seq_put_decimal_ll(m, " ", is_super ? task_cpu(task) : task_vcpu_id(task));
 	seq_put_decimal_ull(m, " ", task->rt_priority);
 	seq_put_decimal_ull(m, " ", task->policy);
 	seq_put_decimal_ull(m, " ", delayacct_blkio_ticks(task));
