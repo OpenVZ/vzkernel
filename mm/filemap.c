4cb9e1661fd32965b6460bd6bc2f77e92619cd78
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-28 12:15:31 +0300

    ve: Add virtinfo subsys
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    +++
    vziolimit: add blktrace hooks
    
    This hooks allow us to use standard blktrace tools to trace vziolimits
    functionality.
    
    https://jira.sw.ru/browse/PSBM-20104
    
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>

--- mm/filemap.c
+++ mm/filemap.c
@@ -46,6 +46,8 @@
 
 #include <asm/mman.h>
 
+#include <linux/virtinfo.h>
+
 /*
  * Shared mappings implemented 30.11.1994. It's not fully working yet,
  * though.
@@ -1857,6 +1859,8 @@ page_ok:
 		goto out;
 
 page_not_up_to_date:
+		virtinfo_notifier_call(VITYPE_IO, VIRTINFO_IO_PREPARE, NULL);
+
 		/* Get exclusive access to the page ... */
 		error = lock_page_killable(page);
 		if (unlikely(error))
@@ -1924,6 +1928,8 @@ readpage_error:
 		goto out;
 
 no_cached_page:
+		virtinfo_notifier_call(VITYPE_IO, VIRTINFO_IO_PREPARE, NULL);
+
 		/*
 		 * Ok, it wasn't cached, so we need to create a new
 		 * page..
@@ -2155,6 +2161,8 @@ static int page_cache_read(struct file *file, pgoff_t offset, gfp_t gfp_mask)
 	struct page *page; 
 	int ret;
 
+	virtinfo_notifier_call(VITYPE_IO, VIRTINFO_IO_PREPARE, NULL);
+
 	do {
 		page = __page_cache_alloc(gfp_mask|__GFP_COLD);
 		if (!page)
@@ -2278,6 +2286,11 @@ int filemap_fault(struct vm_area_struct *vma, struct vm_fault *vmf)
 		 * waiting for the lock.
 		 */
 		do_async_mmap_readahead(vma, ra, file, page, offset);
+
+		if (unlikely(!PageUptodate(page)))
+			virtinfo_notifier_call(VITYPE_IO,
+					VIRTINFO_IO_PREPARE, NULL);
+
 	} else if (!page) {
 		/* No page in the page cache at all */
 		do_sync_mmap_readahead(vma, ra, file, offset);
@@ -3041,6 +3054,8 @@ generic_file_buffered_write(struct kiocb *iocb, const struct iovec *iov,
 	struct file *file = iocb->ki_filp;
 	ssize_t status;
 	struct iov_iter i;
+
+	virtinfo_notifier_call(VITYPE_IO, VIRTINFO_IO_PREPARE, NULL);
 
 	iov_iter_init(&i, iov, nr_segs, count, written);
 	status = generic_perform_write(file, &i, pos);
