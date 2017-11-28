04c4108359bd4050d4a099a9a41060cb00916575
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-28 17:56:28 +0300

    ub: Add dirty page accounting
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- include/linux/rmap.h
+++ include/linux/rmap.h
@@ -177,7 +177,7 @@ void page_add_anon_rmap(struct page *, struct vm_area_struct *, unsigned long);
 void do_page_add_anon_rmap(struct page *, struct vm_area_struct *,
 			   unsigned long, int);
 void page_add_new_anon_rmap(struct page *, struct vm_area_struct *, unsigned long);
-void page_add_file_rmap(struct page *);
+void page_add_file_rmap(struct page *, struct mm_struct *);
 void page_remove_rmap(struct page *);
 
 void hugepage_add_anon_rmap(struct page *, struct vm_area_struct *,
