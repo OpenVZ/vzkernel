cfe3549d89dd12197a1c15f69910fc845fcf42c6
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-11-28 12:15:31 +0300

    ve: Add virtinfo subsys
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    +++
    vziolimit: add blktrace hooks
    
    This hooks allow us to use standard blktrace tools to trace vziolimits
    functionality.
    
    https://jira.sw.ru/browse/PSBM-20104
    
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -37,6 +37,7 @@
 #include <linux/timer.h>
 #include <linux/sched/rt.h>
 #include <linux/mm_inline.h>
+#include <linux/virtinfo.h>
 #include <trace/events/writeback.h>
 
 #include "internal.h"
@@ -1550,6 +1551,9 @@ pause:
 	if (!dirty_exceeded && bdi->dirty_exceeded)
 		bdi->dirty_exceeded = 0;
 
+	virtinfo_notifier_call(VITYPE_IO, VIRTINFO_IO_BALANCE_DIRTY,
+			       (void*)pages_dirtied);
+
 	if (writeback_in_progress(bdi))
 		return;
 
@@ -1903,6 +1907,8 @@ int write_cache_pages(struct address_space *mapping,
 			}
 
 			done_index = page->index;
+
+			virtinfo_notifier_call(VITYPE_IO, VIRTINFO_IO_PREPARE, NULL);
 
 			lock_page(page);
 
