8fb2e2440fe9dddb801abf44d8b9e7cb75ef781a
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-12-04 13:34:02 +0300

    vzeth: bits and pieces of our interface
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Signed-off-by: Stanislav Kinsbursky <skinsbursky@parallels.com>
    Signed-off-by: Andrew Vagin <avagin@openvz.org>
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>

--- include/linux/netdev_features.h
+++ include/linux/netdev_features.h
@@ -80,6 +80,7 @@ enum {
 
 	NETIF_F_VIRTUAL_BIT,		/* can be registered inside VE */
 	NETIF_F_VENET_BIT,		/* device is venet device */
+	NETIF_F_FIXED_ADDR_BIT,
 
 	/*
 	 * Add your fresh new feature above and remember to update
@@ -148,6 +149,7 @@ enum {
 
 #define NETIF_F_VIRTUAL		__NETIF_F(VIRTUAL)
 #define	NETIF_F_VENET		__NETIF_F(VENET)
+#define	NETIF_F_FIXED_ADDR	__NETIF_F(VENET)
 
 #define for_each_netdev_feature(mask_addr, bit)	\
 	for_each_set_bit(bit, (unsigned long *)mask_addr, NETDEV_FEATURE_COUNT)
