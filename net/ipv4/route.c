97934ff0320d2f6551f2b804e049b225195380aa
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-12-04 18:16:00 +0300

    ve/proc: restricted proc-entries scope
    This patch adds lightweight proc virtualization: entries with S_ISVTX bit are
    visible from all instances, entries without S_ISVTX are visible only via proc
    mounted from init-pid-namespace.
    
    https://jira.sw.ru/browse/PSBM-18019
    
    Plus fixes for :
    https://jira.sw.ru/browse/PSBM-18021
    https://jira.sw.ru/browse/PSBM-40359
    https://jira.sw.ru/browse/PSBM-58574
    https://jira.sw.ru/browse/PSBM-55920
    https://bugs.openvz.org/browse/OVZ-6834
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -393,13 +393,14 @@ static int __net_init ip_rt_do_proc_init(struct net *net)
 	if (!pde)
 		goto err1;
 
-	pde = proc_create("rt_cache", S_IRUGO,
-			  net->proc_net_stat, &rt_cpu_seq_fops);
+	pde = proc_net_create_data("rt_cache", S_IRUGO,
+			  net->proc_net_stat, &rt_cpu_seq_fops, NULL);
 	if (!pde)
 		goto err2;
 
 #ifdef CONFIG_IP_ROUTE_CLASSID
-	pde = proc_create("rt_acct", 0, net->proc_net, &rt_acct_proc_fops);
+	pde = proc_net_create_data("rt_acct", 0,
+			net->proc_net, &rt_acct_proc_fops, NULL);
 	if (!pde)
 		goto err3;
 #endif
