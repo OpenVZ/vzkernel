1c6b6fe2023ba275ab9e6834b810d6e2c12be0a7
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-12-04 18:16:00 +0300

    ve/proc: restricted proc-entries scope
    This patch adds lightweight proc virtualization: entries with S_ISVTX bit are
    visible from all instances, entries without S_ISVTX are visible only via proc
    mounted from init-pid-namespace.
    
    https://jira.sw.ru/browse/PSBM-18019
    
    Plus fixes for :
    https://jira.sw.ru/browse/PSBM-18021
    https://jira.sw.ru/browse/PSBM-40359
    https://jira.sw.ru/browse/PSBM-58574
    https://jira.sw.ru/browse/PSBM-55920
    https://bugs.openvz.org/browse/OVZ-6834
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    (cherry picked from commit 886a1c051dab5a162199ee4e0c995a9b112cdabe)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ve/proc/netfilter: Get rid of per-CT iptables mask
    
    https://jira.sw.ru/browse/PSBM-127787
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- net/netfilter/x_tables.c
+++ net/netfilter/x_tables.c
@@ -1712,6 +1712,7 @@ int xt_proto_init(struct net *net, u_int8_t af)
 	struct proc_dir_entry *proc;
 	kuid_t root_uid;
 	kgid_t root_gid;
+	int mode;
 #endif
 
 	if (af >= ARRAY_SIZE(xt_prefix))
@@ -1719,12 +1720,14 @@ int xt_proto_init(struct net *net, u_int8_t af)
 
 
 #ifdef CONFIG_PROC_FS
+	mode = 0440 | S_ISVTX;
+
 	root_uid = make_kuid(net->user_ns, 0);
 	root_gid = make_kgid(net->user_ns, 0);
 
 	strlcpy(buf, xt_prefix[af], sizeof(buf));
 	strlcat(buf, FORMAT_TABLES, sizeof(buf));
-	proc = proc_net_create_net_data(buf, 0440, net->proc_net, &xt_table_seq_ops,
+	proc = proc_net_create_net_data(buf, mode, net->proc_net, &xt_table_seq_ops,
 			sizeof(struct seq_net_private),
 			(void *)(unsigned long)af);
 	if (!proc)
@@ -1734,7 +1737,7 @@ int xt_proto_init(struct net *net, u_int8_t af)
 
 	strlcpy(buf, xt_prefix[af], sizeof(buf));
 	strlcat(buf, FORMAT_MATCHES, sizeof(buf));
-	proc = proc_net_create_seq_private(buf, 0440, net->proc_net,
+	proc = proc_net_create_seq_private(buf, mode, net->proc_net,
 			&xt_match_seq_ops, sizeof(struct nf_mttg_trav),
 			(void *)(unsigned long)af);
 	if (!proc)
@@ -1744,7 +1747,7 @@ int xt_proto_init(struct net *net, u_int8_t af)
 
 	strlcpy(buf, xt_prefix[af], sizeof(buf));
 	strlcat(buf, FORMAT_TARGETS, sizeof(buf));
-	proc = proc_net_create_seq_private(buf, 0440, net->proc_net,
+	proc = proc_net_create_seq_private(buf, mode, net->proc_net,
 			 &xt_target_seq_ops, sizeof(struct nf_mttg_trav),
 			 (void *)(unsigned long)af);
 	if (!proc)
