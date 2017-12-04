ebe62fbe9c096081952c8229fe5bfe211422bb52
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-12-04 20:09:54 +0300

    ve/sched: port vcpu hotslice
    This patch ports
    
    diff-sched-make-vcpus-sticky
    diff-sched-cfs-dec-nr_cpus_active-immediately-if-not-going-to-sleep
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    ================
    * diff-sched-make-vcpus-sticky
    Author: Vladimir Davydov
    Email: vdavydov@parallels.com
    Subject: sched: make vcpus sticky
    Date: Thu, 6 Dec 2012 11:35:20 +0400
    
    If a vcpu goes to sleep, don't migrate it for some time (5 ms by default)
    to improve cache locality.
    
    The delay is tunable via sysctl kernel.sched_vcpu_hotslice
    
    https://jira.sw.ru/browse/PSBM-16913
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Acked-by: Pavel Emelyanov <xemul@parallels.com>
    
    * diff-sched-cfs-dec-nr_cpus_active-immediately-if-not-going-to-sleep
    Author: Vladimir Davydov
    Email: vdavydov@parallels.com
    Subject: sched: cfs: dec nr_cpus_active immediately if not going to sleep
    Date: Mon, 4 Mar 2013 20:06:15 +0400
    
    nr_cpus_active is used to control task group parallelism: it counts the
    number of cpus the task group is currently executing on and the
    scheduler tries to gather tasks together if nr_cpus_active > nr_cpus
    where nr_cpus is the cpulimit set by the admin.
    
    Some time ago nr_cpus_active updates were made lazy for 'vcpus' to
    become sticky (see PSBM-16913): if the last task of a task group on a
    particular cpu goes to sleep, a timer is scheduled to decrement
    nr_cpus_active instead of decrementing it immediately. The idea was to
    improve cache locality by forcing a sleeping task to use the cpu it was
    executing last time when it awakes, and it did help.
    
    However, the improvement described above has one flaw: it postpones the
    decrement even if no tasks go to sleep, but the 'vcpu' is migrated by
    the load balancer.  Obviously we should exclude this case and decrement
    nr_cpus_active right away, otherwise the scheduler will go mad thinking
    about how many cpus a task groups is using and how many it may use. This
    patch fixes this drawback.
    
    https://jira.sw.ru/browse/PSBM-17399
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -504,6 +504,14 @@ static struct ctl_table kern_table[] = {
 #endif
 #ifdef CONFIG_CFS_CPULIMIT
 	{
+		.procname	= "sched_vcpu_hotslice",
+		.data		= &sysctl_sched_vcpu_hotslice,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= &zero,
+	},
+	{
 		.procname	= "sched_cpulimit_scale_cpufreq",
 		.data		= &sysctl_sched_cpulimit_scale_cpufreq,
 		.maxlen		= sizeof(unsigned int),
