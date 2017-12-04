b717c9a9d298477962fc0c80e9bbcfce649b187a
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-12-04 18:16:00 +0300

    ve/proc: restricted proc-entries scope
    This patch adds lightweight proc virtualization: entries with S_ISVTX bit are
    visible from all instances, entries without S_ISVTX are visible only via proc
    mounted from init-pid-namespace.
    
    https://jira.sw.ru/browse/PSBM-18019
    
    Plus fixes for :
    https://jira.sw.ru/browse/PSBM-18021
    https://jira.sw.ru/browse/PSBM-40359
    https://jira.sw.ru/browse/PSBM-58574
    https://jira.sw.ru/browse/PSBM-55920
    https://bugs.openvz.org/browse/OVZ-6834
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    (cherry picked from commit 886a1c051dab5a162199ee4e0c995a9b112cdabe)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ve/proc/netfilter: Get rid of per-CT iptables mask
    
    https://jira.sw.ru/browse/PSBM-127787
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/proc/stat.c
+++ fs/proc/stat.c
@@ -222,7 +222,7 @@ static const struct file_operations proc_stat_operations = {
 
 static int __init proc_stat_init(void)
 {
-	proc_create("stat", 0, NULL, &proc_stat_operations);
+	proc_create("stat", S_ISVTX, NULL, &proc_stat_operations);
 	return 0;
 }
 fs_initcall(proc_stat_init);
