c1e872abc915d864e4259d259a9aec98ba8e7465
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-12-04 19:30:37 +0300

    sched: Port diff-sched-make-nr_cpus-limit-support-hierarchies
    Author: Vladimir Davydov
    Email: vdavydov@parallels.com
    Subject: sched: make nr_cpus limit support hierarchies
    Date: Wed, 26 Nov 2014 17:29:31 +0300
    
    Before the patch nr_cpus limiter was completely unaware of hierarchies,
    i.e. creation of a cpu sub-cgroup in a container would result in its
    tasks being spread over all physical cpus irrespective of the
    container's nr_cpus limit.
    
    This patch changes this. It makes sub-cgroups created inside a
    container's cpu cgroup respect the containers nr_cpus limit. For
    example, if a container has nr_cpus=2, all its tasks should be running
    on 2 physical cpus most of time even if some of the tasks were moved
    into a cpu sub-cgroup.
    
    However, nr_cpus inside a cpu sub-cgroup of a container still do not
    limit parallelism, only the total cpu time granted to the sub-cgroup,
    because implementation of fully hierarchical nr_cpus limit will be
    likely to impact performance much.
    
    Related to https://jira.sw.ru/browse/PSBM-33642
    
    Plus fixes for:
    https://jira.sw.ru/browse/PSBM-35082
    https://jira.sw.ru/browse/PSBM-35251
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Signed-off-by: Kirill Tkhai <ktkhai@odin.com>
    
    +++
    sched: make check_cpulimit_spread accept tg instead of cfs_rq
    
    It only needs cfs_rq->tg, so let's pass it directly. This eases further
    modifications.
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1269,6 +1269,7 @@ static const u32 prio_to_wmult[40] = {
 
 #define DEQUEUE_SLEEP		0x01
 #define DEQUEUE_SAVE		0x02
+#define DEQUEUE_TASK_SLEEP	0x04
 
 struct sched_class {
 	const struct sched_class *next;
