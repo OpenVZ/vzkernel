98b80ae959b22863b6c7f3b04cbe2e989a4ae69b
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-12-04 18:16:00 +0300

    ve/proc: restricted proc-entries scope
    This patch adds lightweight proc virtualization: entries with S_ISVTX bit are
    visible from all instances, entries without S_ISVTX are visible only via proc
    mounted from init-pid-namespace.
    
    https://jira.sw.ru/browse/PSBM-18019
    
    Plus fixes for :
    https://jira.sw.ru/browse/PSBM-18021
    https://jira.sw.ru/browse/PSBM-40359
    https://jira.sw.ru/browse/PSBM-58574
    https://jira.sw.ru/browse/PSBM-55920
    https://bugs.openvz.org/browse/OVZ-6834
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- fs/locks.c
+++ fs/locks.c
@@ -2676,7 +2676,7 @@ static const struct file_operations proc_locks_operations = {
 
 static int __init proc_locks_init(void)
 {
-	proc_create("locks", 0, NULL, &proc_locks_operations);
+	proc_create("locks", S_ISVTX, NULL, &proc_locks_operations);
 	return 0;
 }
 module_init(proc_locks_init);
