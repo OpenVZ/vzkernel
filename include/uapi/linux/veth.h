3b9a7f3b75e7bcb05f8d0f6d1403d5104e698fc4
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-12-04 13:34:02 +0300

    vzeth: initial patch
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    VZETH: cleanup owner_env references
    
    Use owner_ve from struct net
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    
    +++
    VENET: cleanup owner_env references
    
    Use net->owner_ve
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    
    +++
    VE/NET: port of VZ VETH device
    
    v2: change driver name to "vz-veth"
    
    This patch is a port of vzveth device to 3.6 kernel.
    Main changes are in ethtool operations (all functionality has been ported),
    disable of CPT code and fix obvious errors.
    
    Signed-off-by: Stanislav Kinsbursky <skinsbursky@parallels.com>
    
    +++
    VE/VZETHDEV: pass ve_struct in veth_dev_start
    
    Before ve_struct is got by get_exec_env(). Currently we try to
    avoid using set_exec_env().
    
    Signed-off-by: Andrew Vagin <avagin@openvz.org>
    
    +++
    VE/NET: port diff-net-substitute-ve_hook-by-pernet_operations-in-veth (v2)
    
    Once again. Now for 3.9.
    
    https://jira.sw.ru/browse/PSBM-20668
    
    The original comment:
    
    https://jira.sw.ru/browse/PSBM-18176
    
    Vladimir Davydov (3):
      Port diff-net-improve-batching-of-nf_conntrack-net-exit
      Port diff-net-substitute-ve_hook-by-pernet_operations-in-venet
      Port diff-net-substitute-ve_hook-by-pernet_operations-in-veth
    
    net: substitute ve_hook by pernet_operations in veth
    
    This improves batching.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    +++
    veth: Make it carry NETIF_F_VIRTUAL
    
    We need this device being allowed in containers, basically
    due to of 2.6.32 -> 3.9 migration.
    
    When container has been checkpointed with vzeth device assigned,
    we change it to vanilla veth device while converting checkpoint
    image, since the new kernel should not use vzeth at all.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Acked-by: Pavel Emelyanov <xemul@parallels.com>
    CC: Andrew Vagin <avagin@openvz.org>
    CC: Konstantin Khorenko <khorenko@openvz.org>
    
    +++
    vz/veth: remove vzethdev
    
    We are going to use the upstream veth
    
    Signed-off-by: Andrew Vagin <avagin@openvz.org>
    Reviewed-by: Kirill Tkhai <ktkhai@odin.com>

--- include/uapi/linux/veth.h
+++ include/uapi/linux/veth.h
@@ -1,3 +1,12 @@
+/*
+ *  include/linux/veth.h
+ *
+ *  Copyright (C) 2007  SWsoft
+ *  All rights reserved.
+ *
+ *  Licensing governed by "linux/COPYING.SWsoft" file.
+ *
+ */
 #ifndef __NET_VETH_H_
 #define __NET_VETH_H_
 
@@ -8,5 +17,8 @@ enum {
 	__VETH_INFO_MAX
 #define VETH_INFO_MAX	(__VETH_INFO_MAX - 1)
 };
+
+#define SIOCSVENET	(SIOCDEVPRIVATE + 0xf)
+#define SIOCSFIXEDADDR	(SIOCDEVPRIVATE + 0xe)
 
 #endif
