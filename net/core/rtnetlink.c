c6fdf5df4b5d321899f3f3345b6d6e839faf1b4c
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-12-04 13:48:25 +0300

    ve/net: introduce vz_security_*_check checks
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>

--- net/core/rtnetlink.c
+++ net/core/rtnetlink.c
@@ -39,6 +39,7 @@
 #include <linux/if_vlan.h>
 #include <linux/pci.h>
 #include <linux/etherdevice.h>
+#include <linux/ve.h>
 
 #include <asm/uaccess.h>
 
@@ -2781,6 +2782,7 @@ static int rtnl_dump_all(struct sk_buff *skb, struct netlink_callback *cb)
 {
 	int idx;
 	int s_idx = cb->family;
+	struct net *net = sock_net(skb->sk);
 
 	if (s_idx == 0)
 		s_idx = 1;
@@ -2790,6 +2792,8 @@ static int rtnl_dump_all(struct sk_buff *skb, struct netlink_callback *cb)
 			continue;
 		if (rtnl_msg_handlers[idx] == NULL ||
 		    rtnl_msg_handlers[idx][type].dumpit == NULL)
+			continue;
+		if (vz_security_family_check(net, idx))
 			continue;
 		if (idx > s_idx) {
 			memset(&cb->args[0], 0, sizeof(cb->args));
@@ -4103,6 +4107,9 @@ static int rtnetlink_rcv_msg(struct sk_buff *skb, struct nlmsghdr *nlh)
 		return 0;
 
 	family = ((struct rtgenmsg *)nlmsg_data(nlh))->rtgen_family;
+	if (vz_security_family_check(net, family))
+		return -EAFNOSUPPORT;
+
 	sz_idx = type>>2;
 	kind = type&3;
 
