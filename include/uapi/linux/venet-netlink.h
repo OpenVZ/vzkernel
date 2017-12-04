f641d65b1f5263b4e51364b8902915abf5b29be5
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-12-04 13:06:30 +0300

    drivers/net/ve: venet network device introduced
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

---
+++ include/uapi/linux/venet-netlink.h
@@ -0,0 +1,32 @@
+/*
+ *
+ *  Copyright (c) 2000-2017 Virtuozzo International GmbH.
+ *  All rights reserved.
+ *
+ */
+
+#ifndef __NET_VENET_H_
+#define __NET_VENET_H_
+
+#include <linux/types.h>
+
+enum {
+	VENET_INFO_UNSPEC,
+	VENET_INFO_CMD,
+
+	__VENET_INFO_MAX
+#define VENET_INFO_MAX   (__VENET_INFO_MAX - 1)
+};
+
+enum {
+	VENET_IP_ADD,
+	VENET_IP_DEL,
+};
+
+struct venetaddrmsg {
+	__u8		va_family;
+	__u8		va_cmd;
+	__u32		va_addr[4];
+};
+
+#endif
