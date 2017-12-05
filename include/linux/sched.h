23fc04951efd2136577e92b0bb57bc6b59e1ab37
Andrey Ryabinin <aryabinin@virtuozzo.com>
2017-12-05 15:12:48 +0300

    mm/vmscan: Introduce PF_MEMCG_RECLAIM task flag to identify memcg reclaim.
    In tcache and tswap we need to distinguish per-memcg reclaim from a
    global one. PF_MEMCG_RECLAIM flag will be used for that.
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2111,6 +2111,7 @@ extern void thread_group_cputime_adjusted(struct task_struct *p, cputime_t *ut, 
 #define PF_KTHREAD	0x00200000	/* I am a kernel thread */
 #define PF_RANDOMIZE	0x00400000	/* randomize virtual address space */
 #define PF_SWAPWRITE	0x00800000	/* Allowed to write to swap */
+#define PF_MEMCG_RECLAIM  0x01000000	/* We are in memcg reclaim */
 #define PF_NO_SETAFFINITY 0x04000000	/* Userland is not allowed to meddle with cpus_allowed */
 #define PF_MCE_EARLY    0x08000000      /* Early kill for mce process policy */
 #define PF_MEMPOLICY	0x10000000	/* Non-default NUMA mempolicy */
