633a774805304e00f72393d5a6eac518bc28e5bc
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-12-11 14:13:49 +0300

    ve/fs/sync: per containter sync and syncfs
    The implementation is straightforward, since we already have per container
    writeback. Sync just get user beancounter and launches writeback work for it.
    
    https://jira.sw.ru/browse/PSBM-39583
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Reviewed-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    
    +++
    ve/fs/sync: don't skip dirty metadata on filtered sync
    
    Port of diff-ubc-dont-skip-dirty-metadata-on-filtered-sync patch
    from PCS6.
    
    Don't skip dirty metadata during per container sync[fs].
    
    	date: 2012/01/11 19:59:46; author: xemul; state: Exp;
    	Author: Konstantin Khlebnikov
    	Email: khlebnikov@openvz.org
    	Subject: ubc: disable filtered sync for too dirty inodes
    	Date: Tue, 10 Jan 2012 17:45:25 +0400
    
    	Filter only inodes with clean meta-data.
    
    	We cannot filter writes in this case. Actually we only move writes to
    	the journal thread, and it submit them in not very effective way.
    
    	Found-by: Alexey Kuznetsov <kuznet@parallels.com>
    	Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    
    https://jira.sw.ru/browse/PSBM-39583
    
    And the issue below is caused because we don't account metadata =>
    inodes with dirty metadata could be detached from UB
    (mapping->dirtied_ub == NULL). So sync() will skip such inodes.
    With this patch sync() will skip inodes with dirty data only.
    
    https://jira.sw.ru/browse/PSBM-44125
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Reviewed-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    
    khorenko@: we still have 2 cases:
    1) bindmount from host into a CT => dirtied_ub will be set, but not "ours",
    but we still want to sync inodes with dirty metadata because of the single
    journal
    
    2) we've dropped vzfs, but still have simfs case: again a single journal =>
    we should commit inodes with dirty metadata even in case they (inodes) do not
    belong to "our" Container.
    
    ============================================================
    
    ve/fs/sync: fix CT's mountpoints traversal
    
    Currently sync reads 've->root_path.mnt' mount and iterate over it childs.
    This doesn't work, as not all in-container mounts could be reached that way.
    
    This patch slightly rework's mounts traversal. Now sync just iterates over all
    mounts of ve's mount namespace (actually we iterate over all mounts that
    seen in container's /proc/mounts). This approach seems much more correct and
    also simpler.
    
    https://jira.sw.ru/browse/PSBM-44125
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    Note: this patch does not cover traversing sub mount namespaces
    and we are ok with that for now.
    https://jira.sw.ru/browse/PSBM-44684
    
    Reviewed-by: Vladimir Davydov <vdavydov@virtuozzo.com>

--- fs/mount.h
+++ fs/mount.h
@@ -93,6 +93,8 @@ static inline int is_mounted(struct vfsmount *mnt)
 	return !IS_ERR_OR_NULL(real_mount(mnt)->mnt_ns);
 }
 
+extern struct rw_semaphore namespace_sem;
+
 extern struct mount *__lookup_mnt(struct vfsmount *, struct dentry *);
 
 extern int __legitimize_mnt(struct vfsmount *, unsigned);
