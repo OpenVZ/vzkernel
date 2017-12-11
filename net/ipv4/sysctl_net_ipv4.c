16ac8b2e27871e67b308347df2137bdd5ff33269
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-12-12 00:18:05 +0300

    ve/sysctl/ipc: mark as virtualized
    These sysctls arealready virtualized, thus we can permit writes from a container.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    
    +++
    VE/SYSCTL: fixup virtualized sysctls
    
    switch sysctls to new api
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -411,7 +411,7 @@ static struct ctl_table ipv4_table[] = {
 		.data		= &sysctl_tcp_syncookies,
 		.maxlen		= sizeof(int),
 		.mode		= 0644 | S_ISVTX,
-		.proc_handler	= proc_dointvec
+		.proc_handler	= proc_dointvec_immutable,
 	},
 #endif
 	{
