57ef8bdc7ccca2980d554a6ea0e2d9c0bacf68a3
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-12-11 20:32:36 +0300

    headers: inclusion

--- mm/fremap.c
+++ mm/fremap.c
@@ -17,6 +17,7 @@
 #include <linux/syscalls.h>
 #include <linux/mmu_notifier.h>
 #include <linux/userfaultfd_k.h>
+#include <linux/module.h>
 
 #include <asm/mmu_context.h>
 #include <asm/cacheflush.h>
