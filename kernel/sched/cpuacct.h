8d128e57bb072a9ad71f396c853fce023c3bba90
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-12-11 23:40:07 +0300

    sched: Account task_group::cpustat,taskstats,avenrun
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    +++
    sched: use cpuacct->cpustat for showing cpu stats
    
    In contrast to RH6, where tg->cpustat was used, now cpu stats are
    accounted in the cpuacct cgroup. So zap tg->cpustat and use
    cpuacct->cpustat for showing cpu.proc.stat instead. Fortunately cpu and
    cpuacct cgroups are always mounted together (even by systemd by
    default), so this will work.
    
    Related to https://jira.sw.ru/browse/PSBM-33642
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    +++
    ve/sched: Do not use kstat_glb_lock to update kstat_glob::nr_unint_avg
    
    kstat_glob::nr_unint_avg can't be updated in parallel on two or
    more cpus, so on modifications we have to protect against readers
    only.
    
    So, avoid using global kstat_glb_lock here, to minimize its
    sharing with another counters it protects.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- kernel/sched/cpuacct.h
+++ kernel/sched/cpuacct.h
@@ -3,6 +3,9 @@
 extern void cpuacct_charge(struct task_struct *tsk, u64 cputime);
 extern void cpuacct_account_field(struct task_struct *p, int index, u64 val);
 
+struct cgroup;
+extern struct kernel_cpustat *cpuacct_cpustat(struct cgroup *cgrp, int cpu);
+
 #else
 
 static inline void cpuacct_charge(struct task_struct *tsk, u64 cputime)
