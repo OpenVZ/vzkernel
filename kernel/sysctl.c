4c363c6f8a61975eb7d29bc238c9b1ba5363fd68
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-12-12 00:53:52 +0300

    ve/kmod: Port autoloading from CT
    Patchset description:
    
    Port autoloading of netfilter modules functuonality
    
    https://jira.sw.ru/browse/PSBM-28910
    
    Signed-off-by: Kirill Tkhai <ktkhai@parallels.com>
    
    Kirill Tkhai (4):
          kmod: Move check of VE permitions from __call_usermodehelper_exec() to upper functions
          kmod: Port autoloading from CT
          netfilter: Add autoloading of sockopt modules
          netfilter: Check for permittions while looking for target and match
    
    This patch description:
    
    Change __request_module() to be able to analyze
    if we want to load module inside CT. If it's allowed
    we autoload requested module from ve0 fs space.
    
    +++
    ve/kmod: fix out-of-bounds access in call_modprobe()
    
    Commit 18f83b2460e2 ("ve/kmod: Port autoloading from CT") extended
    argv array for one more element, however it wasn't extended
    on allocation site.
    
    https://jira.sw.ru/browse/PSBM-38666
    
    mFixes: 18f83b2460e2 ("ve/kmod: Port autoloading from CT")
    Signed-off-by: Andrey Ryabinin <aryabinin@odin.com>
    Cc: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    Signed-off-by: Andrey Ryabinin <aryabinin@odin.com>
    Acked-by: Kirill Tkhai <ktkhai@odin.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -117,6 +117,9 @@ extern int sysctl_nr_open_min, sysctl_nr_open_max;
 extern int sysctl_nr_trim_pages;
 #endif
 
+int ve_allow_module_load = 1;
+EXPORT_SYMBOL(ve_allow_module_load);
+
 /* Constants used for minimum and  maximum */
 #ifdef CONFIG_LOCKUP_DETECTOR
 static int sixty = 60;
@@ -901,6 +904,17 @@ static struct ctl_table kern_table[] = {
 		.proc_handler	= proc_dointvec_minmax_sysadmin,
 		.extra1		= &zero,
 		.extra2		= &two,
+	},
+#endif
+#ifdef CONFIG_VE
+        {
+		.procname       = "ve_allow_module_load",
+		.data           = &ve_allow_module_load,
+		.maxlen         = sizeof(int),
+		.mode           = 0644,
+		.proc_handler   = &proc_dointvec_minmax,
+		.extra1		= &zero,
+		.extra2		= &one,
 	},
 #endif
 	{
