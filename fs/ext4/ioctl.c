9b078c669dad61e25e8f1b7aa8e540753649cdf6
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-12-11 20:49:33 +0300

    ext4: Provide a balloon nipple for management
    When the fs is mounted with active balloon someone will have to
    inflate/blow off one. To make it possible there will be a special
    ioctl for obtaining the fd.
    
    Not very elegant solution maybe, but it's OK for PVC containers.
    
    +++
    ext4: fix file allocation check in ext4_open_balloon
    
    Function alloc_file() doesn't return NULL (unlike in 2.6.32-x).
    It returns error pointer. File structure allocation may fail before
    file->f_ep_links is initialized, which may lead to crash in
    eventpoll_release_file().
    
    https://jira.sw.ru/browse/PSBM-41222
    
    Fixes: 9cea7449aa589f325fff378e7256a3c2fc8f048d
    "ext4: Provide a balloon nipple for management"
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- fs/ext4/ioctl.c
+++ fs/ext4/ioctl.c
@@ -197,6 +197,60 @@ journal_err_out:
 	return err;
 }
 
+static int ext4_open_balloon(struct super_block *sb, struct vfsmount *mnt)
+{
+	struct inode *balloon_ino;
+	int err, fd;
+	struct file *filp;
+	struct dentry *de;
+	struct path path;
+	fmode_t mode;
+
+	balloon_ino = EXT4_SB(sb)->s_balloon_ino;
+	err = -ENOENT;
+	if (balloon_ino == NULL)
+		goto err;
+
+	err = fd = get_unused_fd();
+	if (err < 0)
+		goto err_fd;
+
+	__iget(balloon_ino);
+	de = d_obtain_alias(balloon_ino);
+	err = PTR_ERR(de);
+	if (IS_ERR(de))
+		goto err_de;
+
+	path.dentry = de;
+	path.mnt = mntget(mnt);
+	err = mnt_want_write(path.mnt);
+	if (err)
+		mode = FMODE_READ;
+	else
+		mode = FMODE_READ | FMODE_WRITE;
+	filp = alloc_file(&path, mode,
+			&ext4_file_operations.kabi_fops);
+	if (mode & FMODE_WRITE)
+		mnt_drop_write(path.mnt);
+	if (IS_ERR(filp)) {
+		err = PTR_ERR(filp);
+		goto err_filp;
+	}
+
+	filp->f_flags |= O_LARGEFILE;
+	fd_install(fd, filp);
+	return fd;
+
+err_filp:
+	path_put(&path);
+err_de:
+	put_unused_fd(fd);
+err_fd:
+	/* nothing */
+err:
+	return err;
+}
+
 long ext4_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
 	struct inode *inode = file_inode(filp);
@@ -639,6 +693,12 @@ resizefs_out:
 	}
 	case EXT4_IOC_PRECACHE_EXTENTS:
 		return ext4_ext_precache(inode);
+
+	case EXT4_IOC_OPEN_BALLOON:
+		if (!capable(CAP_SYS_ADMIN))
+			return -EACCES;
+
+		return ext4_open_balloon(inode->i_sb, filp->f_path.mnt);
 
 	default:
 		return -ENOTTY;
