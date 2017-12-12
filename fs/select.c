6b8af8a23405b797b350730ccb9d0967a3cced39
Konstantin Khorenko <khorenko@virtuozzo.com>
2017-12-12 09:26:00 +0300

    memcg: charge kmem allocations accounted to UBC in PCS6 to memcg
    First patch description:
    ms/kmemcg: account certain kmem allocations to memcg
    
    Mark those kmem allocations that are known to be easily triggered from
    userspace as __GFP_ACCOUNT/SLAB_ACCOUNT, which makes them accounted to
    memcg.  For the list, see below:
    
     - threadinfo
     - task_struct
     - task_delay_info
     - pid
     - cred
     - mm_struct
     - vm_area_struct and vm_region (nommu)
     - anon_vma and anon_vma_chain
     - signal_struct
     - sighand_struct
     - fs_struct
     - files_struct
     - fdtable and fdtable->full_fds_bits
     - dentry and external_name
     - inode for all filesystems. This is the most tedious part, because
       most filesystems overwrite the alloc_inode method.
    
    The list is far from complete, so feel free to add more objects.
    Nevertheless, it should be close to "account everything" approach and
    keep most workloads within bounds.  Malevolent users will be able to
    breach the limit, but this was possible even with the former "account
    everything" approach (simply because it did not account everything in
    fact).
    
    [akpm@linux-foundation.org: coding-style fixes]
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Greg Thelen <gthelen@google.com>
    Cc: Christoph Lameter <cl@linux.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    (cherry picked from commit 5d097056c9a017a3b720849efb5432f37acabbac)
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    
    Conflicts:
    	drivers/staging/lustre/lustre/llite/super25.c
    	fs/dcache.c
    	fs/f2fs/super.c
    	fs/file.c
    	kernel/fork.c
    
    +++
    memcg: charge kmem allocations accounted to UBC in PCS6 to memcg
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    
    +++
    memcg/tty: charge tty kmem allocations
    
    To be merged into c2b05d7bf7f3c73d8377796f0212d9b65127a016
    ("memcg: charge kmem allocations accounted to UBC in PCS6 to memcg")
    
    https://jira.sw.ru/browse/PSBM-54928
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    tty,vtty: Fix building procedure
    
     - after rebase no __alloc_tty_struct needed, the
       alloc_tty_struct accounts for memory itself
     - need to fix release and master open routines
    
    https://jira.sw.ru/browse/PSBM-54928
    
    To be merged with f89c673a386b2ab7a23158d748d0e69a2c2c0f21
    ("memcg/tty: charge tty kmem allocations") and further.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@openvz.org>

--- fs/select.c
+++ fs/select.c
@@ -583,7 +583,7 @@ int core_sys_select(int n, fd_set __user *inp, fd_set __user *outp,
 			goto out_nofds;
 
 		alloc_size = 6 * size;
-		bits = kvmalloc(alloc_size, GFP_KERNEL);
+		bits = kvmalloc(alloc_size, GFP_KERNEL_ACCOUNT);
 		if (!bits)
 			goto out_nofds;
 	}
@@ -906,7 +906,7 @@ int do_sys_poll(struct pollfd __user *ufds, unsigned int nfds,
 
 		len = min(todo, POLLFD_PER_PAGE);
 		size = sizeof(struct poll_list) + sizeof(struct pollfd) * len;
-		walk = walk->next = kmalloc(size, GFP_KERNEL);
+		walk = walk->next = kmalloc(size, GFP_KERNEL_ACCOUNT);
 		if (!walk) {
 			err = -ENOMEM;
 			goto out_fds;
