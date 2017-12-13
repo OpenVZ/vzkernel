f0b38be6fc50979f019ba25807d83bacbf8c63cd
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-12-13 15:09:06 +0300

    ve: Link net and ve_struct
    Network primitives want to have access to ve, which
    created the network namespace.
    
    This patch adds net::owner_ve pointer and makes ve_struct
    not to be freed before pernet operations are finished.
    
    The same for ve_struct: it increments counter of ve_ns,
    and ve_ns->net_ns can't be released before ve_drop_context()
    decrements counter.
    
    In addition, this patch adds ve_net_lock() and ve_net_unlock()
    as repalcement for ve_netns.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- include/linux/ve.h
+++ include/linux/ve.h
@@ -32,6 +32,8 @@ struct ve_struct {
 	u64			start_jiffies;		/* Deprecated */
 
 	struct nsproxy __rcu	*ve_ns;
+	/* Please, use ve_net_lock() and ve_net_unlock() instead of ve_netns */
+#define ve_netns		[:|||||||:]
 	struct cred		*init_cred;
 };
 
@@ -50,6 +52,17 @@ static inline struct ve_struct *css_to_ve(struct cgroup_subsys_state *css)
 }
 
 extern struct cgroup_subsys_state *ve_get_init_css(struct ve_struct *ve, int subsys_id);
+
+static inline struct net *ve_net_lock(struct ve_struct *ve)
+{
+	rcu_read_lock();
+	return rcu_dereference(ve->ve_ns)->net_ns;
+}
+
+static inline void ve_net_unlock(struct ve_struct *ve)
+{
+	rcu_read_unlock();
+}
 #else	/* CONFIG_VE */
 #define get_ve(ve)	(NULL)
 #define put_ve(ve)	do { } while (0)
