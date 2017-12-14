876e7a783286b4f187a5d04fb2cfb3c19935935d
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-12-14 18:31:42 +0300

    ve: Add veowner: skeleton for vz specific sysctl and /proc/ entries
    Register /proc/vz directory (proc_vz_dir variable),
    and fake /proc/fairsched and /proc/fairsched2.
    
    Merged: "ve/fairsched: Resurrect proc fairsched files"
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    Dropped /proc/vz/fairsched empty directory.
    
    Added /proc/bc empty directory because systemd detects
    /proc/vz presence and without /proc/bc considers it runs inside
    a Container and node does not boot.
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ve: remove /proc/fairsched(,2) and /proc/vz/fairsched #PSBM-79882
    
    These files are legacy and are not used anymore. Dropping.
    
    https://jira.sw.ru/browse/PSBM-79882
    Signed-off-by: "Denis V. Lunev" <den@openvz.org>
    
    +++
    ve: Explain the need for /proc/bc directory
    
    Can't easily drop "/proc/bc" directory: without dropping /proc/vz dir
    systemd considers it's in a Container and Node does not boot.
    
    https://jira.sw.ru/browse/PSBM-127913
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    (cherry picked from vz8 commit 4d9889856c8fb4aaa7da8c527f9e76b7dbc14eae)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Feature: ve: ve generic structures

--- init/main.c
+++ init/main.c
@@ -101,6 +101,7 @@
 #include <linux/init_syscalls.h>
 #include <linux/stackdepot.h>
 #include <net/net_namespace.h>
+#include <linux/veowner.h>
 
 #include <asm/io.h>
 #include <asm/bugs.h>
@@ -1410,6 +1411,7 @@ static void __init do_initcalls(void)
  */
 static void __init do_basic_setup(void)
 {
+	init_ve_system();
 	cpuset_init_smp();
 	driver_init();
 	init_irq_proc();
