c03c6a4a3cd9b8af54099266eb7acd2c9fa6bda4
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-12-15 18:41:05 +0300

    ve/cgroup: Add pseudosuper state for restore sake
    Pseudouser state is the only case, when mounting and configuring
    cgroups from inside CT is allowed.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Acked-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    +++
    ve: add a comment about possible pseudosuper race
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    (cherry-picked from vz7 commit fdac6d1dd408 ("ve: add a comment about possible
    pseudosuper race"))
    
    https://jira.sw.ru/browse/PSBM-127848
    Signed-off-by: Valeriy Vdovin <valeriy.vdovin@virtuozzo.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -57,6 +57,7 @@
 #include <linux/sched/cputime.h>
 #include <linux/psi.h>
 #include <net/sock.h>
+#include <linux/ve.h>
 
 #include <linux/rh_features.h>
 
@@ -2058,6 +2059,12 @@ static struct dentry *cgroup_mount(struct file_system_type *fs_type,
 	struct dentry *dentry;
 	int ret;
 
+#ifdef CONFIG_VE
+	if (!ve_is_super(get_exec_env()) && !(flags & MS_KERNMOUNT)) {
+		if (!get_exec_env()->is_pseudosuper)
+			return ERR_PTR(-EACCES);
+	}
+#endif
 	get_cgroup_ns(ns);
 
 	/* Check if the caller has permission to mount. */
