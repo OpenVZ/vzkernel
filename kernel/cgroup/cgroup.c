a77672d99add2843486b6877c08a87f37132a73f
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-12-15 17:38:12 +0300

    cgroups: export some helpers for future use by venet
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -4013,6 +4013,7 @@ int cgroup_rm_cftypes(struct cftype *cfts)
 	mutex_unlock(&cgroup_mutex);
 	return ret;
 }
+EXPORT_SYMBOL_GPL(cgroup_rm_cftypes);
 
 /**
  * cgroup_add_cftypes - add an array of cftypes to a subsystem
@@ -4086,6 +4087,7 @@ int cgroup_add_legacy_cftypes(struct cgroup_subsys *ss, struct cftype *cfts)
 		cft->flags |= __CFTYPE_NOT_ON_DFL;
 	return cgroup_add_cftypes(ss, cfts);
 }
+EXPORT_SYMBOL_GPL(cgroup_add_legacy_cftypes);
 
 /**
  * cgroup_file_notify - generate a file modified event for a cgroup_file
