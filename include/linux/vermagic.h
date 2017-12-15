5c8bb63b2abbd25f559da826566c851903759f33
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-12-15 13:15:39 +0300

    printf: Add VZVERSION and task_ve_name() to dump_stack_print_info()
    Extracted from initial patch.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    (cherry picked from vz8 commit 0748b422a590e15a8db413fc61d81a62c26cbe62)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Feature: print: enhance logging

--- include/linux/vermagic.h
+++ include/linux/vermagic.h
@@ -3,7 +3,7 @@
 #define _LINUX_VERMAGIC_H
 
 #ifndef INCLUDE_VERMAGIC
-#error "This header can be included from kernel/module.c or *.mod.c only"
+#error "This header can be included from kernel/module.c, *.mod.c or lib/dump_stack.c only"
 #endif
 
 #include <generated/utsrelease.h>
