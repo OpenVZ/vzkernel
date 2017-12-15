a12f87fb74974192f7913ecc928960c30250fe75
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-12-15 19:23:31 +0300

    kernel/ve: Add support of VE features mask
    Add "features" variable and per-net helper, plus cgroup dentry.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@odin.com>
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    
    (cherry picked from vz8 commit f04cfc268f1686f1105988c1d7a5549fb06ac758)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Feature: ve: per-CT features management

--- include/linux/ve.h
+++ include/linux/ve.h
@@ -36,6 +36,9 @@ struct ve_struct {
 
 	struct nsproxy __rcu	*ve_ns;
 	struct cred		*init_cred;
+
+	/* see vzcalluser.h for VE_FEATURE_XXX definitions */
+	__u64			features;
 };
 
 extern int nr_ve;
@@ -54,12 +57,18 @@ static inline struct ve_struct *css_to_ve(struct cgroup_subsys_state *css)
 
 extern struct cgroup_subsys_state *ve_get_init_css(struct ve_struct *ve, int subsys_id);
 
+#define ve_feature_set(ve, f)			\
+	!!((ve)->features & VE_FEATURE_##f)
+
 #else	/* CONFIG_VE */
 #define get_ve(ve)	((void)(ve), NULL)
 #define put_ve(ve)	do { (void)(ve); } while (0)
 
 static inline void ve_stop_ns(struct pid_namespace *ns) { }
 static inline void ve_exit_ns(struct pid_namespace *ns) { }
+
+#define ve_feature_set(ve, f)		{ true; }
+
 #endif	/* CONFIG_VE */
 
 #endif /* _LINUX_VE_H */
