6813a6600194e96eb90ff563ec18eb008b6b8121
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-12-15 18:41:05 +0300

    ve/cgroup: Add pseudosuper state for restore sake
    Pseudouser state is the only case, when mounting and configuring
    cgroups from inside CT is allowed.
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Acked-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- kernel/cgroup/cgroup.c
+++ kernel/cgroup/cgroup.c
@@ -56,6 +56,7 @@
 #include <linux/file.h>
 #include <linux/sched/cputime.h>
 #include <net/sock.h>
+#include <linux/ve.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/cgroup.h>
@@ -2064,6 +2065,12 @@ static struct dentry *cgroup_mount(struct file_system_type *fs_type,
 	struct dentry *dentry;
 	int ret;
 
+#ifdef CONFIG_VE
+	if (!ve_is_super(get_exec_env()) && !(flags & MS_KERNMOUNT)) {
+		if (!get_exec_env()->is_pseudosuper)
+			return ERR_PTR(-EACCES);
+	}
+#endif
 	get_cgroup_ns(ns);
 
 	/* Check if the caller has permission to mount. */
