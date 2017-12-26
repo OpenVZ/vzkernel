1ff75f29841be65649c03883042a84fe6b8d6c64
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2017-12-26 18:36:40 +0300

    ve/net: Track netfilter modules per net-namespace
    Previously in rhel6 we've been not allowing to create nested net namespaces
    so all netfilter modules tracking has been done via VE structure
    
    	struct ve_struct {
    		...
    		__u64			ipt_mask;
    		__u64			_iptables_modules;
    		...
    	}
    
    Here @ipt_mask stands for features bits which are set up from VE
    configutarion and represent netfilter modules which are allowed
    to use inside VE (might be situation when modules are loaded on
    the node but forbidden inside VE).
    
    When some particular module is allowed inside VE and get loaded we're
    setting up an appropriate bit in @_iptables_modules and clear it back
    upon module unload: net_ipt_module_set() helper is doing that usually
    in module net-init() call. Same time this bit is signalling that
    module specific resource allocated for VE use should be freed once
    module is unloaded, iow net-exit() is called.
    
    For example
    
    	| static int __net_init iptable_mangle_net_init(struct net *net)
    	| {
    	| 	if (!net_ipt_permitted(net, VE_IP_MANGLE))
    	| 		return 0;
    	|
    	| 	/* Register table */
    	| 	net->ipv4.iptable_mangle =
    	| 		ipt_register_table(net, &packet_mangler, &initial_table.repl);
    	| 	if (IS_ERR(net->ipv4.iptable_mangle))
    	| 		return PTR_ERR(net->ipv4.iptable_mangle);
    	|
    	| 	net_ipt_module_set(net, VE_IP_MANGLE);
    	| 	return 0;
    	| }
    	|
    	| static void __net_exit iptable_mangle_net_exit(struct net *net)
    	| {
    	| 	if (!net_is_ipt_module_set(net, VE_IP_MANGLE))
    	| 		return;
    	| }
    	|
    	| static void __net_exit iptable_mangle_net_exit(struct net *net)
    	| {
    	| 	if (!net_is_ipt_module_set(net, VE_IP_MANGLE))
    	| 		return;
    	|
    	| 	ipt_unregister_table(net->ipv4.iptable_mangle);
    	|
    	| 	net_ipt_module_clear(net, VE_IP_MANGLE);
    	| }
    	|
    	| static struct pernet_operations iptable_mangle_net_ops = {
    	| 	.init = iptable_mangle_net_init,
    	| 	.exit = iptable_mangle_net_exit,
    	| };
    
    here we allocate @net->ipv4.iptable_mangle which should be cleaned up
    on exit, moreover the net-namespace engine is designed that way: if
    somehting if failed in init() call then exit() is called and must
    clear all data if been allocated.
    
    This worked well until we have allowed nested net-namespaces. Every
    nested net-namespace share same @owner_ve (ie the VE which is a keeper
    of it). Thus once nested namespace called net_ipt_module_clear()
    the shared @owner_ve get @_iptables_modules bit clear and finally
    the primary net namespace no longer see this bit set in @_iptables_modules.
    
    Lets move @_iptables_modules to struct net instead. Toplevel VE's
    net is referred via @ve_netns member which exist all the container's
    lifetime. Also we've to move it into @init_net thus node's net would
    be tracked same way automatically.
    
    https://jira.sw.ru/browse/PSBM-31451
    
    Signed-off-by: Cyrill Gorcunov <gorcunov@parallels.com>
    Signed-off-by: Kirill Tkhai <ktkhai@odin.com>
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- include/linux/netfilter.h
+++ include/linux/netfilter.h
@@ -440,4 +440,32 @@ extern struct nfnl_ct_hook __rcu *nfnl_ct_hook;
  */
 DECLARE_PER_CPU(bool, nf_skb_duplicated);
 
+#ifdef CONFIG_VE_IPTABLES
+#include <linux/vziptable_defs.h>
+
+#define net_ipt_permitted(netns, ipt)					\
+	(mask_ipt_allow((netns)->owner_ve->ipt_mask, ipt))
+
+#define net_ipt_module_set(netns, ipt)					\
+	({								\
+		(netns)->_iptables_modules |= ipt##_MOD;	\
+	})
+
+#define net_ipt_module_clear(netns, ipt)				\
+	({								\
+		(netns)->_iptables_modules &= ~ipt##_MOD;	\
+	})
+
+#define net_is_ipt_module_set(netns, ipt)				\
+	((netns)->_iptables_modules & (ipt##_MOD))
+
+#else /* CONFIG_VE_IPTABLES */
+
+#define net_ipt_permitted(netns, ipt)		(1)
+#define net_is_ipt_module_set(netns, ipt)	(1)
+#define net_ipt_module_set(netns, ipt)
+#define net_ipt_module_clear(netns, ipt)
+
+#endif /* CONFIG_VE_IPTABLES */
+
 #endif /*__LINUX_NETFILTER_H*/
