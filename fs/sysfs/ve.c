7b5b03ce9a2a0bce3f7ebda16888c06fcc267cba
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2018-01-05 18:26:10 +0300

    ve/sysfs: add dentries visibility filter
    This patch adds sysfs_ve_perms_set set (stored on kerfs_super_info) and per-ve
    sysfs_perms_key, allowing to set nodes permissions and hide sysfs entries or
    change their access permissions.
    
    This patch adds two attributes to VE cgroup to set sysfs dentries visibility:
      ve.default_sysfs_permissions
    in root cgroup and
      ve.sysfs_permissions
    in each nested cgroup.
    
    Permissions in "ve.sysfs_permissions" overrides
    "ve.default_sysfs_permissions".
    
    An example of default config for sysfs:
    
    / rx
    block rx
    class rx
    class/block rx
    class/net rx
    class/tty rx
    class/mem rx
    devices rx
    devices/virtual rx
    devices/virtual/tty rx
    devices/virtual/tty/tty rx
    devices/virtual/tty/tty/uevent rw
    devices/virtual/tty/tty/dev r
    devices/virtual/tty/console rx
    devices/virtual/tty/console/uevent rw
    devices/virtual/tty/console/dev r
    devices/virtual/tty/ptmx rx
    devices/virtual/tty/ptmx/uevent rw
    devices/virtual/tty/ptmx/dev r
    devices/virtual/tty/tty0 rx
    devices/virtual/tty/tty0/uevent rw
    devices/virtual/tty/tty0/dev r
    devices/virtual/tty/tty1 rx
    devices/virtual/tty/tty1/uevent rw
    devices/virtual/tty/tty1/dev r
    devices/virtual/tty/tty2 rx
    devices/virtual/tty/tty2/uevent rw
    devices/virtual/tty/tty2/dev r
    devices/virtual/mem rx
    devices/virtual/mem/null rx
    devices/virtual/mem/null/uevent rw
    devices/virtual/mem/null/dev r
    devices/virtual/mem/zero rx
    devices/virtual/mem/zero/uevent rw
    devices/virtual/mem/zero/dev r
    devices/virtual/mem/full rx
    devices/virtual/mem/full/uevent rw
    devices/virtual/mem/full/dev r
    devices/virtual/mem/random rx
    devices/virtual/mem/random/uevent rw
    devices/virtual/mem/random/dev r
    devices/virtual/mem/urandom rx
    devices/virtual/mem/urandom/uevent rw
    devices/virtual/mem/urandom/dev r
    devices/virtual/net rx
    devices/system rx
    devices/system/cpu rx
    devices/system/cpu/cpu0 rx
    devices/system/cpu/cpu1 rx
    dev rx
    dev/block rx
    dev/char rx
    fs rx
    fs/cgroup rx
    kernel rx
    kernel/uevent_seqnum r
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    
    https://jira.sw.ru/browse/PSBM-83607
    (cherry picked from stable-4.14 branch commit
    d6b6aaecca8236962b242d765fae6d29e069730d)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    sysfs: provide proper ve0 instead of NULL to kernfs_perms_write()
    
    kernfs_perms_write() provides sysfs_perms_key taken from ve_struct,
    so initialize 've' properly, don't just use NULL if ve_is_super().
    
    Also deeper kernfs_perms_set() checks for ve_is_super(ve) as well,
    so NULL definitely does not work here.
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    sysfs/ve: fix output cgroup/ve/ve.default_sysfs_permissions
    
    sysfs_perms_open() replaces ve0 with NULL which results in always empty
    output from cgroup/ve/ve.default_sysfs_permissions file.
    
    Fix that by passing correct ve0 address.
    
    mFixes: 7ce278f50962 ("ve/sysfs: add dentries visibility filter")
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

---
+++ fs/sysfs/ve.c
@@ -0,0 +1,145 @@
+/*
+ *  fs/sysfs/ve.c
+ *
+ *  Copyright (c) 2000-2017 Virtuozzo International GmbH.
+ *  All rights reserved.
+ *
+ */
+
+#include <linux/seq_file.h>
+#include <linux/module.h>
+#include <linux/uaccess.h>
+#include <linux/ve.h>
+#include "sysfs.h"
+
+#include "linux/kernfs-ve.h"
+
+struct kmapset_set sysfs_ve_perms_set;
+
+static DEFINE_MUTEX(sysfs_perms_mutex);
+
+void sysfs_set_ve_perms(struct dentry *root)
+{
+	kernfs_set_ve_perms(root, offsetof(struct ve_struct,
+					   sysfs_perms_key));
+}
+
+int sysfs_init_ve_perms(struct kernfs_root *root)
+{
+	return kernfs_init_ve_perms(root, &sysfs_ve_perms_set);
+}
+
+static void *sysfs_perms_start(struct seq_file *m, loff_t *ppos)
+{
+	struct ve_struct *ve = m->private;
+
+	mutex_lock(&sysfs_perms_mutex);
+	return kernfs_perms_start(m, ppos, sysfs_root_kn, &ve->sysfs_perms_key);
+}
+
+static void *sysfs_perms_next(struct seq_file *m, void *v, loff_t *ppos)
+{
+	struct ve_struct *ve = m->private;
+
+	return kernfs_perms_next(m, v, ppos, &ve->sysfs_perms_key);
+}
+
+static void sysfs_perms_stop(struct seq_file *m, void *v)
+{
+	kernfs_perms_stop(m, v);
+	mutex_unlock(&sysfs_perms_mutex);
+}
+
+static int sysfs_perms_show(struct seq_file *m, void *v)
+{
+	struct ve_struct *ve = m->private;
+
+	return kernfs_perms_show(m, v, &ve->sysfs_perms_key);
+}
+
+static ssize_t sysfs_perms_write(struct cgroup *cgrp,
+		struct cftype *cftype, struct file * file,
+		const char __user *buf, size_t count, loff_t *ppos)
+{
+	struct ve_struct *ve = cgroup_ve(file->f_dentry->d_parent->d_fsdata);
+	char *page;
+	ssize_t ret, len;
+
+	page = (unsigned char *)__get_free_page(GFP_KERNEL);
+	if (!page)
+		return -ENOMEM;
+	len = min(count, PAGE_SIZE - 1);
+	ret = copy_from_user(page, buf, len);
+	if (ret)
+		goto err;
+
+	page[len] = '\0';
+
+	mutex_lock(&sysfs_perms_mutex);
+	ret = kernfs_perms_write(ve, page, len, *ppos,
+				 sysfs_root_kn, &ve->sysfs_perms_key);
+	mutex_unlock(&sysfs_perms_mutex);
+err:
+	free_page((unsigned long)page);
+	return ret;
+}
+
+struct seq_operations sysfs_perms_sops = {
+	.start = sysfs_perms_start,
+	.stop = sysfs_perms_stop,
+	.next = sysfs_perms_next,
+	.show = sysfs_perms_show,
+};
+
+static int sysfs_perms_open(struct inode *inode, struct file *file)
+{
+	struct ve_struct *ve = cgroup_ve(file->f_dentry->d_parent->d_fsdata);
+	struct seq_file *m;
+	int ret;
+
+	ret = seq_open(file, &sysfs_perms_sops);
+	if (!ret) {
+		m = file->private_data;
+		m->private = ve;
+	}
+	return ret;
+}
+
+static ssize_t sysfs_perms_read(struct cgroup *cgrp, struct cftype *cft,
+	struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
+{
+	return seq_read(file, buf, nbytes, ppos);
+}
+
+static int sysfs_perms_release(struct inode *inode, struct file *file)
+{
+	return seq_release(inode, file);
+}
+
+static struct cftype sysfs_ve_cftypes[] = {
+	{
+		.name = "default_sysfs_permissions",
+		.flags = CFTYPE_ONLY_ON_ROOT,
+		.open = sysfs_perms_open,
+		.read = sysfs_perms_read,
+		.write = sysfs_perms_write,
+		.release = sysfs_perms_release,
+		.mode = S_IRUGO | S_IWUSR,
+	},
+	{
+		.name = "sysfs_permissions",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.open = sysfs_perms_open,
+		.read = sysfs_perms_read,
+		.write = sysfs_perms_write,
+		.release = sysfs_perms_release,
+		.mode = S_IRUGO | S_IWUSR,
+	},
+	{ },
+};
+
+static int init_sysfve_perms(void)
+{
+	return cgroup_add_cftypes(&ve_subsys, sysfs_ve_cftypes);
+}
+module_init(init_sysfve_perms);
