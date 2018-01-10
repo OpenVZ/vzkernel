6a2a0a75fdcd24447c2624e90733da8084b45fb2
Andrey Ryabinin <aryabinin@virtuozzo.com>
2018-01-10 19:44:32 +0300

    mm, cgroup, writeback: Enable per-cgroup writeback for v1 cgroup.
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -465,7 +465,7 @@ struct cgroup_subsys_state *mem_cgroup_css_from_page(struct page *page)
 
 	memcg = page->mem_cgroup;
 
-	if (!memcg || !cgroup_subsys_on_dfl(memory_cgrp_subsys))
+	if (!memcg || 0 /*!cgroup_subsys_on_dfl(memory_cgrp_subsys))*/)
 		memcg = root_mem_cgroup;
 
 	return &memcg->css;
