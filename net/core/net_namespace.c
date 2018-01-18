fdae8e80513a019f48c88a799df4bb3b85065c38
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2018-01-18 19:21:23 +0300

    ve/netns: fix counting of network namespaces limits
    If net_alloc fails we leak netns_avail_nr counter when it should be
    incremented back.
    
    Fixes commit a408265ce710 ("ve/netns: limit number of network namespaces
    per container")
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- net/core/net_namespace.c
+++ net/core/net_namespace.c
@@ -397,6 +397,7 @@ struct net *copy_net_ns(unsigned long flags,
 	net = net_alloc();
 	if (!net) {
 		dec_net_namespaces(ucounts);
+		atomic_inc(&ve->netns_avail_nr);
 		return ERR_PTR(-ENOMEM);
 	}
 
