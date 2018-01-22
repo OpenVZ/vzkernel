84f366909b6b90d00f420ebcff634b351d8ce367
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2018-01-22 16:39:26 +0300

    ve/kernfs: implement ve-based permissions
    Here are the main ideas:
    1) Store pointer to 've' on kernfs superblock info.
    2) Keep on each kernfs node kmapset mapping (ve -> permissions).
    3) Store kmapset key on each ve (used to check dentry visibility).
    4) Store key offset in ve_struct on each kernfs superblock info to be able to
    get the key pointer by current VE.
    
    And do not inherit ve permissions from parent.
    Otherwise when a new ploop is created, all containers that have access
    to devices/virtual/block will gain access to the new ploop too, which is
    a security breach.
    
    https://jira.sw.ru/browse/PSBM-20892
    https://jira.sw.ru/browse/PSBM-34682
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    
    https://jira.sw.ru/browse/PSBM-83607
    (cherry picked from stable-4.14 branch commit
    85f107442ecd3a629643d1e48664c317863ed342)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

---
+++ fs/kernfs/kernfs-ve.h
@@ -0,0 +1,54 @@
+/*
+ *  fs/kernfs/kernfs-ve.h
+ *
+ *  Copyright (c) 2000-2017 Virtuozzo International GmbH.
+ *  All rights reserved.
+ *
+ */
+
+#ifndef __KERNFS_VE_H
+#define __KERNFS_VE_H
+
+struct kernfs_root;
+struct kernfs_super_info;
+struct kernfs_node;
+struct kmapset;
+
+#ifdef CONFIG_VE
+
+int kernfs_test_ve(struct kernfs_super_info *sb_info,
+		   struct kernfs_super_info *info);
+
+void kernfs_get_ve_perms(struct kernfs_node *kn);
+void kernfs_put_ve_perms(struct kernfs_node *kn);
+
+int kernfs_ve_permission(struct kernfs_node *kn,
+			 struct kernfs_super_info *info, int mask);
+
+int kernfs_ve_allowed(struct kernfs_node *kn);
+
+#else // CONFIG_VE
+
+static inline int kernfs_test_ve(struct kernfs_super_info *sb_info,
+				 struct kernfs_super_info *info)
+{
+	return 0;
+}
+
+void kernfs_get_ve_perms(struct kernfs_node *kn) { }
+void kernfs_put_ve_perms(struct kernfs_node *kn) { }
+
+static inline int kernfs_ve_permission(struct kernfs_node *kn,
+				 struct kernfs_super_info *info, int mask)
+{
+	return 0;
+}
+
+static inline int kernfs_ve_allowed(void)
+{
+	return 1;
+}
+
+#endif
+
+#endif
