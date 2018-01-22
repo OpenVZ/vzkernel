5aef937f93c2905389fa156cb8cd5f78a53e8d13
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2018-01-22 16:39:26 +0300

    ve/kernfs: implement ve-based permissions
    Here are the main ideas:
    1) Store pointer to 've' on kernfs superblock info.
    2) Keep on each kernfs node kmapset mapping (ve -> permissions).
    3) Store kmapset key on each ve (used to check dentry visibility).
    4) Store key offset in ve_struct on each kernfs superblock info to be able to
    get the key pointer by current VE.
    
    And do not inherit ve permissions from parent.
    Otherwise when a new ploop is created, all containers that have access
    to devices/virtual/block will gain access to the new ploop too, which is
    a security breach.
    
    https://jira.sw.ru/browse/PSBM-20892
    https://jira.sw.ru/browse/PSBM-34682
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

--- fs/kernfs/dir.c
+++ fs/kernfs/dir.c
@@ -17,6 +17,7 @@
 #include <linux/hash.h>
 
 #include "kernfs-internal.h"
+#include "kernfs-ve.h"
 
 DEFINE_MUTEX(kernfs_mutex);
 static DEFINE_SPINLOCK(kernfs_rename_lock);	/* kn->parent and ->name */
@@ -537,6 +538,7 @@ void kernfs_put(struct kernfs_node *kn)
 						kn->iattr->ia_secdata_len);
 		simple_xattrs_free(&kn->iattr->xattrs);
 	}
+	kernfs_put_ve_perms(kn);
 	kfree(kn->iattr);
 	spin_lock(&kernfs_idr_lock);
 	idr_remove(&root->ino_idr, kn->id.ino);
@@ -782,6 +784,7 @@ int kernfs_add_one(struct kernfs_node *kn)
 		ktime_get_real_ts64(&ps_iattrs->ia_ctime);
 		ps_iattrs->ia_mtime = ps_iattrs->ia_ctime;
 	}
+	kernfs_get_ve_perms(kn);
 
 	mutex_unlock(&kernfs_mutex);
 
