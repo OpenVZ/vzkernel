926ce2f139d0d51dd2a017a4b6af1d7d5212d738
Vladimir Davydov <vdavydov@virtuozzo.com>
2018-02-05 15:47:32 +0300

    mm: memcontrol: add stats for reclaimable and unreclaimable stats
    Required by vcmmd for estimating the size of available memory, which in
    turn is used for calculating the lower boundary for memory limit.
    
    Also, these counters will be used by the following patches.
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1141,6 +1141,8 @@ static const unsigned int memcg1_stats[] = {
 	NR_FILE_MAPPED,
 	NR_FILE_DIRTY,
 	NR_WRITEBACK,
+	NR_SLAB_RECLAIMABLE,
+	NR_SLAB_UNRECLAIMABLE,
 	MEMCG_SWAP,
 };
 
@@ -1152,6 +1154,8 @@ static const char *const memcg1_stat_names[] = {
 	"mapped_file",
 	"dirty",
 	"writeback",
+	"slab_reclaimable",
+	"slab_unreclaimable",
 	"swap",
 };
 
