c9d60ce866086f3c75355420feb38856e1fb2479
Andrei Vagin <avagin@openvz.org>
2018-02-15 17:34:46 +0300

    fuse: add a new async operation to unmap regions
    The fuse interface allows to run any operation asynchronously, because
    the kernel redirect all operations to an user daemon and then waits an
    answer.
    
    In ploop, we want to handle discard requests via fallocate and
    a simplest way to do this is to run fallocate(FALLOC_FL_PUNCH_HOLE)
    asynchronously like the write command.
    
    This patch adds a new async command IOCB_CMD_UNMAP_ITER, which sends
    fallocate(FALLOC_FL_PUNCH_HOLE) to a fuse user daemon.
    
    Signed-off-by: Andrei Vagin <avagin@openvz.org>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -365,6 +365,9 @@ struct fuse_req {
 			struct fuse_write_in in;
 			struct fuse_write_out out;
 		} write;
+		struct {
+			struct fuse_fallocate_in in;
+		} fallocate;
 		struct fuse_notify_retrieve_in retrieve_in;
 		struct fuse_lk_in lk_in;
 	} misc;
