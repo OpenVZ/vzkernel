d2e40087df1962ba465e8c334539c80ef8c5ea7a
Dmitry Monakhov <dmonakhov@openvz.org>
2018-02-19 14:16:28 +0300

    fuse: add kdirect io engine v0.1
    Add fast path API for fuse.
    Idea is simple: Divide IO-path from (signaling/metadata)-path.
    
    metadata operations always require strong consistency and it is hard to make it
    fast, but sustaned IO may( and should) be superfast and scalable.
    In fact it can be done quite easily by adding hook to request_send which may
    interpect requests right before submission to userspace thread, and forward
    it to fast-path is possible.
    
    patch status:
    - [X] Draft skeleton
    - [ ] More intitive patch split
        - move fuse functions to helpers first
        - pure API patch
    - [ ] io_inode seems is redundant. I need to initialize req->inode for each request
    
    https://jira.sw.ru/browse/PSBM-80680
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>
    
    +++
    fuse kio: Revert fuse_request_free() changes
    
    Currently, it's a dead code, so there is no functional
    changes. But I do this for next patches, to make the code
    more predictable.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Reviewed-by: Pavel Butsykin <pbutsykin@virtuozzo.com>
    
    +++
    fuse: fix occasional vstorage-mount abort
    
    Call conventions for fuse_release_common() changed in RHEL8
    
    Signed-off-by: Alexey Kuznetsov <kuznet@acronis.com>
    
    +++
    fuse: Fix fuse_parse_param() kio_name handling #PSBM-131962
    
    https://jira.sw.ru/browse/PSBM-131962
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    +++
    fuse: fix junk at in kio name
    
    Patch "fuse: Fix fuse_parse_param() kio_name handling #PSBM-131962" has
    a silly mistake.
    
    Affects: #VSTOR-45882
    https://pmc.acronis.com/browse/VSTOR-45882
    
    Signed-off-by: Alexey Kuznetsov <kuznet@acronis.com>
    Reviewed-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    lkui@: starting with rebase of this first one of fuse kio patches, the build
    process will be broken temporarily. We will fix all issues at once after
    all fuse kio patches have been rebased.
    (rebased from 9fcc293a328e2005bd724aea1e8b506f34d02545S)
    
    Signed-off-by: Liu Kui <Kui.Liu@acronis.com>
    Feature: vStorage

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -623,11 +623,19 @@ static int fuse_create_open(struct inode *dir, struct dentry *entry,
 	if (err) {
 		fi = get_fuse_inode(inode);
 		fuse_sync_release(fi, ff, flags);
-	} else {
-		file->private_data = ff;
-		fuse_finish_open(inode, file);
+		return err;
 	}
-	return err;
+
+	file->private_data = ff;
+	fuse_finish_open(inode, file);
+	if (fm->fc->kio.op && fm->fc->kio.op->file_open &&
+	    fm->fc->kio.op->file_open(file, inode)) {
+		if (err) {
+			fput(file);
+			return  err;
+		}
+	}
+	return 0;
 
 out_free_ff:
 	fuse_file_free(ff);
@@ -1611,6 +1619,7 @@ static void fuse_setattr_fill(struct fuse_conn *fc, struct fuse_args *args,
 			      struct fuse_attr_out *outarg_p)
 {
 	args->opcode = FUSE_SETATTR;
+	args->io_inode = inode;
 	args->nodeid = get_node_id(inode);
 	args->in_numargs = 1;
 	args->in_args[0].size = sizeof(*inarg_p);
