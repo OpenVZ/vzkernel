f5574d505e76d8b7e3a74156993080313f03ffa5
Dmitry Monakhov <dmonakhov@openvz.org>
2018-02-19 14:22:33 +0300

    fuse kio: Add pcs engine combo v0.8
    https://jira.sw.ru/browse/PSBM-80680
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>
    
    +++
    fuse kio: Move map_chunk_start() and map_chunk_end() checks
    
    The both functions are valid in case of map is not dead.
    Otherwise, they dereference zero m->mapping pointer.
    Fix that.
    
    mFixes: 66e16d47b6f4 ("fuse kio: Add pcs engine combo v0.8")
    
    https://jira.sw.ru/browse/PSBM-87665
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Reviewed-by: Pavel Butsykin <pbutsykin@virtuozzo.com>
    
    +++
    fuse kio: initialize batch in pcs_map shrinker
    
    Shinker memory is allocated in process_pcs_init_reply() via kvmalloc()
    (thus not zeroed) and "batch" field not initialized later in
    pcs_mapset_init(), so fit it and initialize to default batch value.
    
    mFixes: 9623c8c78601 ("fuse kio: Add pcs engine combo v0.8")
    https://pmc.acronis.com/browse/VSTOR-15801
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Feature: vStorage

---
+++ fs/fuse/kio/pcs/pcs_flow_detect.h
@@ -0,0 +1,14 @@
+/*
+ *  fs/fuse/kio/pcs/pcs_flow_detect.h
+ *
+ *  Copyright (c) 2018-2021 Virtuozzo International GmbH. All rights reserved.
+ *
+ */
+
+#ifndef _PCS_FLOW_DETECT_H_
+#define _PCS_FLOW_DETECT_H_ 1
+
+/* TODO:!!! this is stump for  flow_detection */
+#include "pcs_flow_detect_stub.h"
+
+#endif /* _PCS_FLOW_DETECT_H_ */
