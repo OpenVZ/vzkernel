42a8fddf2308dc0118adb08c1edbe5da761956ee
Dmitry Monakhov <dmonakhov@openvz.org>
2018-02-19 14:22:33 +0300

    fuse kio: Add pcs engine combo v0.8
    https://jira.sw.ru/browse/PSBM-80680
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>
    
    +++
    fuse kio: Move map_chunk_start() and map_chunk_end() checks
    
    The both functions are valid in case of map is not dead.
    Otherwise, they dereference zero m->mapping pointer.
    Fix that.
    
    mFixes: 66e16d47b6f4 ("fuse kio: Add pcs engine combo v0.8")
    
    https://jira.sw.ru/browse/PSBM-87665
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Reviewed-by: Pavel Butsykin <pbutsykin@virtuozzo.com>
    
    +++
    fuse kio: initialize batch in pcs_map shrinker
    
    Shinker memory is allocated in process_pcs_init_reply() via kvmalloc()
    (thus not zeroed) and "batch" field not initialized later in
    pcs_mapset_init(), so fit it and initialize to default batch value.
    
    mFixes: 9623c8c78601 ("fuse kio: Add pcs engine combo v0.8")
    https://pmc.acronis.com/browse/VSTOR-15801
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>

---
+++ fs/fuse/kio/pcs/fuse_io.c
@@ -0,0 +1,172 @@
+/*
+ *  fs/fuse/kio/pcs/fuse_io.c
+ *
+ *  Copyright (c) 2018-2021 Virtuozzo International GmbH. All rights reserved.
+ *
+ */
+
+#include <net/sock.h>
+#include <linux/net.h>
+#include <linux/kthread.h>
+#include <linux/types.h>
+#include <linux/pagemap.h>
+
+#include "pcs_types.h"
+#include "pcs_sock_io.h"
+#include "pcs_rpc.h"
+#include "pcs_sock_io.h"
+#include "pcs_req.h"
+#include "pcs_map.h"
+#include "pcs_cs.h"
+#include "pcs_cluster.h"
+#include "log.h"
+
+#include "../../fuse_i.h"
+
+static void intreq_complete(struct pcs_int_request *ireq)
+{
+	pcs_api_iorequest_t *req = ireq->apireq.req;
+
+	BUG_ON(ireq->type != PCS_IREQ_API);
+
+	if (pcs_if_error(&ireq->error)) {
+		req->flags |= PCS_REQ_F_ERROR;
+		if (ireq->error.value == PCS_ERR_NO_STORAGE ||
+		    ireq->error.value == PCS_ERR_CSD_LACKING)
+			req->flags |= PCS_REQ_F_NOSPACE;
+	}
+	req->complete(req);
+}
+
+static void on_read_done(struct pcs_fuse_req *r, size_t size)
+{
+	struct pcs_fuse_cluster *pfc = cl_from_req(r);
+
+	DTRACE("do fuse_request_end req:%p op:%d err:%d\n", &r->req, r->req.in.h.opcode, r->req.out.h.error);
+	r->req.args->out_args[0].size = size;
+	fuse_request_end(pfc->fc, &r->req);
+}
+
+static void on_sync_done(struct pcs_fuse_req *r)
+{
+	struct pcs_fuse_cluster *pfc = cl_from_req(r);
+
+	DTRACE("do fuse_request_end req:%p op:%d err:%d\n", &r->req, r->req.in.h.opcode, r->req.out.h.error);
+	fuse_request_end(pfc->fc, &r->req);
+}
+
+static void on_write_done(struct pcs_fuse_req *r, off_t pos, size_t size)
+{
+	struct fuse_args *args = r->req.args;
+	struct fuse_io_args *ia = container_of(args, typeof(*ia), ap.args);
+	struct fuse_write_out *out = &ia->write.out;
+	struct pcs_fuse_cluster *pfc = cl_from_req(r);
+
+	out->size = size;
+
+	DTRACE("do fuse_request_end req:%p op:%d err:%d\n", &r->req, r->req.in.h.opcode, r->req.out.h.error);
+	fuse_request_end(pfc->fc, &r->req);
+}
+
+static void req_get_iter(void *data, unsigned int offset, struct iov_iter *it, unsigned int direction)
+{
+	struct pcs_fuse_req *r = data;
+
+	iov_iter_bvec(it, direction, r->exec.io.bvec, r->exec.io.num_bvecs, r->exec.io.req.size);
+	iov_iter_advance(it, offset);
+}
+
+static inline void set_io_buff(struct pcs_fuse_req *r, off_t offset, size_t size,
+			       int zeroing)
+{
+	struct fuse_args *args = r->req.args;
+	struct fuse_io_args *ia = container_of(args, typeof(*ia), ap.args);
+	int i;
+	size_t count = 0;
+	r->exec.io.bvec = r->exec.io.inline_bvec;
+	r->exec.io.num_bvecs = ia->ap.num_pages;
+	for (i = 0; i < ia->ap.num_pages && count < size; i++) {
+		r->exec.io.bvec[i].bv_page = ia->ap.pages[i];
+		r->exec.io.bvec[i].bv_offset = ia->ap.descs[i].offset;
+		r->exec.io.bvec[i].bv_len = ia->ap.descs[i].length;
+		count += r->exec.io.bvec[i].bv_len;
+	}
+	count = 0;
+	for (i = 0; i < r->exec.io.num_bvecs; i++) {
+		count += r->exec.io.bvec[i].bv_len;
+		if (zeroing && r->exec.io.bvec[i].bv_len < PAGE_SIZE)
+			clear_highpage(r->exec.io.bvec[i].bv_page);
+	}
+	BUG_ON(size > count);
+	r->exec.io.req.pos = offset;
+	r->exec.io.req.size = size;
+}
+
+static void prepare_io_(struct pcs_fuse_req *r, unsigned short type, off_t offset, size_t size,
+		       void (*complete)(struct _pcs_api_iorequest_t *))
+{
+	/* Use inline request structure */
+	struct pcs_int_request *ireq = &r->exec.ireq;
+
+	TRACE("INIT r(%p) ireq:%p {%ld, %ld}\n", r, ireq, offset, size);
+
+	/* Initialize IO request */
+	switch (type)
+	{
+	case PCS_REQ_T_READ:
+		set_io_buff(r, offset, size, r->req.args->page_zeroing);
+		break;
+	case PCS_REQ_T_WRITE:
+		set_io_buff(r, offset, size, 0);
+		break;
+	}
+
+	r->exec.io.req.type = type;
+	r->exec.io.req.datasource = r;
+	r->exec.io.req.get_iter = req_get_iter;
+	r->exec.io.req.complete = complete;
+
+	/* Initialize internal request structure */
+	ireq->type = PCS_IREQ_API;
+	ireq->apireq.req = &r->exec.io.req;
+	ireq->complete_cb = intreq_complete;
+	ireq->completion_data.parent = 0;
+	ireq->completion_data.ctx = r;
+	ireq->completion_data.priv = r;
+}
+
+static void ioreq_complete(pcs_api_iorequest_t *ioreq)
+{
+	struct pcs_fuse_req *r = ioreq->datasource;
+
+	BUG_ON(ioreq != &r->exec.io.req);
+
+	if (ioreq->flags & PCS_REQ_F_ERROR) {
+		if (ioreq->flags & PCS_REQ_F_NOSPACE)
+			r->req.out.h.error = -ENOSPC;
+		else
+			r->req.out.h.error = -EIO;
+	} else {
+		r->req.out.h.error = 0;
+	}
+
+	switch (ioreq->type) {
+	case PCS_REQ_T_READ:
+		on_read_done(r, ioreq->size);
+		break;
+	case PCS_REQ_T_WRITE:
+		on_write_done(r, ioreq->pos, ioreq->size);
+		break;
+	case PCS_REQ_T_SYNC:
+		on_sync_done(r);
+		break;
+	default:
+		BUG();
+	}
+
+}
+
+void pcs_fuse_prep_io(struct pcs_fuse_req *r, unsigned short type, off_t offset, size_t size)
+{
+	prepare_io_(r, type, offset, size, ioreq_complete);
+}
