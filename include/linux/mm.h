e12931457bf1892918586b3829bcf9ab12dd7d22
Konstantin Khorenko <khorenko@virtuozzo.com>
2018-03-22 13:28:19 +0300

    mm: introduce kvmalloc_check() for safe calls to kvmalloc()
    Sometimes a function which allocates high order pages is called with different
    flags while kvmalloc() should be called with at least GFP_KERNEL.
    
    kvmalloc_check() function uses kvmalloc() when possible depending on flags
    provided.
    
    https://jira.sw.ru/browse/PSBM-82593
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Reviewed-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    [VvS RHEL77b rebase]

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -529,6 +529,7 @@ static inline void *kvcalloc(size_t n, size_t size, gfp_t flags)
 	return kvmalloc_array(n, size, flags | __GFP_ZERO);
 }
 
+extern void *kvmalloc_check(size_t size, gfp_t flags);
 extern void kvfree(const void *addr);
 
 static inline void compound_lock(struct page *page)
