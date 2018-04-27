b17ccea08e7534e669a260ae871a4c0b5900ccf1
Alexey Kuznetsov <kuznet@virtuozzo.com>
2018-04-27 12:07:53 +0300

    fuse/kio_pcs: adapt prometheus statistics to current vstorage state
    Signed-off-by: Alexey Kuznetsov <kuznet@virtuozzo.com>
    Signed-off-by: Dmitry Monakhov <dmonakhov@openvz.org>

--- fs/fuse/kio/pcs/fuse_prometheus_prot.h
+++ fs/fuse/kio/pcs/fuse_prometheus_prot.h
@@ -1,23 +1,30 @@
 #ifndef __FUSE_PROMETHEUS_PROT__
 #define __FUSE_PROMETHEUS_PROT__ 1
 
-#define KFUSE_OP_READ		0
-#define KFUSE_OP_WRITE		1
-#define KFUSE_OP_FSYNC		2
-#define KFUSE_OP_FALLOCATE	3
-#define KFUSE_OP_MAX		4
+#define KFUSE_OP_CS_READ	0
+#define KFUSE_OP_CS_WRITE	1
+#define KFUSE_OP_CS_SYNC	2
+#define KFUSE_OP_CS_WRITE_HOLE	3
+#define KFUSE_OP_CS_WRITE_ZERO	4
+#define KFUSE_OP_CS_FIEMAP	5
 
-#define KFUSE_PROM_MAX		(9*5 + 1)
+#define KFUSE_OP_READ		6
+#define KFUSE_OP_WRITE		7
+#define KFUSE_OP_FSYNC		8
+#define KFUSE_OP_FALLOCATE	9
+#define KFUSE_OP_MAX		10
+
+#define KFUSE_PROM_MAX		(9*5 + 2)
 
 struct kfuse_stat_rec
 {
-	u64	value;
-	u64	count;
+	u64	buckets[KFUSE_PROM_MAX];
+	u64	sum;
 };
 
 struct kfuse_histogram
 {
-	struct kfuse_stat_rec	buckets[KFUSE_OP_MAX][KFUSE_PROM_MAX+1];
+	struct kfuse_stat_rec	metrics[KFUSE_OP_MAX];
 };
 
 #endif /* __FUSE_PROMETHEUS_PROT__ */
