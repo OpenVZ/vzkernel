67bfa622b310cff4b2b99975d759d378da11ea9f
Oleg Babin <obabin@virtuozzo.com>
2018-05-16 11:09:25 +0300

    tty: use kvmalloc for screenbuffer allocation
    Console driver allocates a screenbuffer with kmalloc(). The
    size of the buffer can be up to 32768 bytes which is the 3rd
    memory order.
    
    Use kvmalloc() for screenbuffer allocation to fallback to vmalloc()
    in case of high order page is not available at the moment.
    
    https://jira.sw.ru/browse/HCI-53
    Signed-off-by: Oleg Babin <obabin@virtuozzo.com>
    [VvS RHEL7.8 rebase] context changes

--- drivers/tty/vt/vt.c
+++ drivers/tty/vt/vt.c
@@ -812,7 +812,7 @@ int vc_allocate(unsigned int currcons)	/* return 0 on success */
 	if (!*vc->vc_uni_pagedir_loc)
 		con_set_default_unimap(vc);
 
-	vc->vc_screenbuf = kmalloc(vc->vc_screenbuf_size, GFP_KERNEL);
+	vc->vc_screenbuf = kvmalloc(vc->vc_screenbuf_size, GFP_KERNEL);
 	if (!vc->vc_screenbuf)
 		goto err_free;
 
@@ -828,7 +828,7 @@ int vc_allocate(unsigned int currcons)	/* return 0 on success */
 	return 0;
 err_free:
 	visual_deinit(vc);
-	kfree(vc);
+	kvfree(vc);
 	vc_cons[currcons].d = NULL;
 	return -ENOMEM;
 }
@@ -900,7 +900,7 @@ static int vc_do_resize(struct tty_struct *tty, struct vc_data *vc,
 
 	if (new_screen_size > (4 << 20))
 		return -EINVAL;
-	newscreen = kmalloc(new_screen_size, GFP_USER);
+	newscreen = kvmalloc(new_screen_size, GFP_USER);
 	if (!newscreen)
 		return -ENOMEM;
 
@@ -912,7 +912,7 @@ static int vc_do_resize(struct tty_struct *tty, struct vc_data *vc,
 
 	err = resize_screen(vc, new_cols, new_rows, user);
 	if (err) {
-		kfree(newscreen);
+		kvfree(newscreen);
 		return err;
 	}
 
@@ -959,7 +959,7 @@ static int vc_do_resize(struct tty_struct *tty, struct vc_data *vc,
 	if (new_scr_end > new_origin)
 		scr_memsetw((void *)new_origin, vc->vc_video_erase_char,
 			    new_scr_end - new_origin);
-	kfree(vc->vc_screenbuf);
+	kvfree(vc->vc_screenbuf);
 	vc->vc_screenbuf = newscreen;
 	vc->vc_screenbuf_size = new_screen_size;
 	set_origin(vc);
@@ -1041,7 +1041,7 @@ struct vc_data *vc_deallocate(unsigned int currcons)
 		vcs_remove_sysfs(currcons);
 		visual_deinit(vc);
 		put_pid(vc->vt_pid);
-		kfree(vc->vc_screenbuf);
+		kvfree(vc->vc_screenbuf);
 		vc_cons[currcons].d = NULL;
 	}
 	return vc;
