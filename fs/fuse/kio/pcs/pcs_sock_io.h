ed1404a3d82e31bf370c30d945d5f1cb258c1013
Kirill Tkhai <ktkhai@virtuozzo.com>
2018-05-28 18:38:57 +0300

    fuse kio: Declare pcs_sockio::parent type
    It's always struct pcs_rpc, so there is no a reason
    to keep it unnamed.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Reviewed-by: Pavel Butsykin <pbutsykin@virtuozzo.com>
    Feature: vStorage

--- fs/fuse/kio/pcs/pcs_sock_io.h
+++ fs/fuse/kio/pcs/pcs_sock_io.h
@@ -125,7 +125,7 @@ struct pcs_sockio
 	struct list_head	write_queue;
 	int			write_queue_len;
 	spinlock_t		q_lock;
-	void			*parent;
+	struct pcs_rpc		*parent;
 
 	pcs_error_t		error;
 	int			send_timeout;
