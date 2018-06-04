2633bdc8a12a436208d4d516e0f138f4687c8ed3
Konstantin Khorenko <khorenko@virtuozzo.com>
2018-06-04 16:50:56 +0300

    ve/kernfs: fix lookup for kernfs superblock
    sysfs (kernfs) mount looks up for already existing superblock,
    but uses non-inited kernfs_super_info->ve field for comparison.
    
    Fill the ve field before existing superblock lookup.
    
    https://jira.sw.ru/browse/PSBM-85052
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    [VvS RHEL7.8 rebase] context changes

--- fs/kernfs/mount.c
+++ fs/kernfs/mount.c
@@ -162,6 +162,7 @@ struct dentry *kernfs_mount_ns(struct file_system_type *fs_type, int flags,
 	info->root = root;
 	info->ns = ns;
 	INIT_LIST_HEAD(&info->node);
+	info->ve = get_exec_env();
 
 	sb = sget_userns(fs_type, kernfs_test_super, kernfs_set_super, flags,
 			 &init_user_ns, info);
