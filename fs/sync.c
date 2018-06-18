f9883f082c075b3a4897e7bbf06176892197b43e
Vasily Averin <vvs@virtuozzo.com>
2018-06-18 12:02:03 +0300

    ms/fs: convert sync_file_range to use errseq_t based error-tracking
    sync_file_range doesn't call down into the filesystem directly at all.
    It only kicks off writeback of pagecache pages and optionally waits
    on the result.
    
    Convert sync_file_range to use errseq_t based error tracking, under the
    assumption that most users will prefer this behavior when errors occur.
    
    Reviewed-by: Jan Kara <jack@suse.cz>
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    
    https://pmc.acronis.com/browse/VSTOR-10912
    (cherry picked from commit 6454568d961bc5de316014548838398174a128c4)
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>

--- fs/sync.c
+++ fs/sync.c
@@ -567,7 +567,7 @@ SYSCALL_DEFINE4(sync_file_range, int, fd, loff_t, offset, loff_t, nbytes,
 
 	ret = 0;
 	if (flags & SYNC_FILE_RANGE_WAIT_BEFORE) {
-		ret = filemap_fdatawait_range(mapping, offset, endbyte);
+		ret = file_fdatawait_range(f.file, offset, endbyte);
 		if (ret < 0)
 			goto out_acct;
 	}
@@ -579,7 +579,7 @@ SYSCALL_DEFINE4(sync_file_range, int, fd, loff_t, offset, loff_t, nbytes,
 	}
 
 	if (flags & SYNC_FILE_RANGE_WAIT_AFTER)
-		ret = filemap_fdatawait_range(mapping, offset, endbyte);
+		ret = file_fdatawait_range(f.file, offset, endbyte);
 
 out_acct:
 	ub_percpu_inc(ub, frsync_done);
