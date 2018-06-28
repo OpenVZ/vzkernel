f58198e6c6fadad75584cee41e1377af5d68b3c3
Konstantin Khorenko <khorenko@virtuozzo.com>
2018-06-28 12:36:15 +0300

    ve/fs/overlay: allow overlayfs to be mounted in non-root userns
    We need overlayfs to be mounted inside Container and RHEL7.5 requires a
    special flag to be set on fs which are allowed to be mounted inside
    non-root user namespaces.
    
    Fixes: e381a0e538de ve/fs/overlay: allow overlayfs to be used inside a Container
    https://jira.sw.ru/browse/PSBM-86153
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -1260,7 +1260,8 @@ static struct file_system_type ovl_fs_type = {
 	.name		= "overlay",
 	.mount		= ovl_mount,
 	.kill_sb	= kill_anon_super,
-	.fs_flags	= FS_HAS_DOPS_WRAPPER | FS_VIRTUALIZED,
+	.fs_flags	= FS_HAS_DOPS_WRAPPER |
+			  FS_VIRTUALIZED | FS_USERNS_MOUNT,
 };
 MODULE_ALIAS_FS("overlay");
 
