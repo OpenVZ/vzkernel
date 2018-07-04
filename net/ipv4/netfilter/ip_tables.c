ce65be93d94bf94e39c80073c7c7f05278d4455e
Florian Westphal <fw@strlen.de>
2018-07-04 20:25:32 +0200

    ms/netfilter: x_tables: set module owner for icmp(6) matches
    nft_compat relies on xt_request_find_match to increment
    refcount of the module that provides the match/target.
    
    The (builtin) icmp matches did't set the module owner so it
    was possible to rmmod ip(6)tables while icmp extensions were still in use.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    https://jira.sw.ru/browse/PSBM-99593
    
    (cherry picked from commit d376bef9c29b3c65aeee4e785fffcd97ef0a9a81)
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>

--- net/ipv4/netfilter/ip_tables.c
+++ net/ipv4/netfilter/ip_tables.c
@@ -2102,6 +2102,7 @@ static struct xt_match ipt_builtin_mt[] __read_mostly = {
 		.checkentry = icmp_checkentry,
 		.proto      = IPPROTO_ICMP,
 		.family     = NFPROTO_IPV4,
+		.me	    = THIS_MODULE,
 	},
 };
 
