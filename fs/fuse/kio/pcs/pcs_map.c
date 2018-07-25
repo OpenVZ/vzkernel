dd97bf93e9601894c64748ad07a461afa85221f8
Pavel Butsykin <pbutsykin@virtuozzo.com>
2018-07-25 18:25:54 +0300

    fs/fuse kio_pcs: fix NULL pointer dereference in pcs_flushreq_complete()
    To access ireq->cc->fc, we should use sreq instead of ireq (which in fact is
    the parent of sreq) because the parent can be NULL.
    
    https://pmc.acronis.com/browse/VSTOR-12570
    
    Signed-off-by: Pavel Butsykin <pbutsykin@virtuozzo.com>

--- fs/fuse/kio/pcs/pcs_map.c
+++ fs/fuse/kio/pcs/pcs_map.c
@@ -2849,7 +2849,7 @@ static void pcs_flushreq_complete(struct pcs_int_request * sreq)
 
 	if (!pcs_if_error(&sreq->error)) {
 		if (sync_is_finished(sreq->flushreq.msg, m)) {
-			FUSE_KTRACE(ireq->cc->fc, "finished");
+			FUSE_KTRACE(sreq->cc->fc, "finished");
 			goto done_dirty;
 		}
 		sreq->error.value = PCS_ERR_CSD_STALE_MAP;
@@ -2859,7 +2859,7 @@ static void pcs_flushreq_complete(struct pcs_int_request * sreq)
 
 	if (ireq && !pcs_if_error(&ireq->error)) {
 		if (ireq_check_redo(sreq)) {
-			FUSE_KTRACE(ireq->cc->fc, "restart after flush error %d", sreq->error.value);
+			FUSE_KTRACE(sreq->cc->fc, "restart after flush error %d", sreq->error.value);
 			if (map_version_compare(&ioh->map_version, &m->version) < 0)
 				sreq->flags &= ~IREQ_F_ONCE;
 			spin_unlock(&m->lock);
@@ -2875,7 +2875,7 @@ static void pcs_flushreq_complete(struct pcs_int_request * sreq)
 				ireq_delay(sreq);
 			return;
 		}
-		FUSE_KTRACE(ireq->cc->fc, "flush error %d", sreq->error.value);
+		FUSE_KTRACE(sreq->cc->fc, "flush error %d", sreq->error.value);
 		pcs_copy_error(&ireq->error, &sreq->error);
 		notify_error = 1;
 	}
