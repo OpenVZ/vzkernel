c6d012a1c3e28d49b595ed573916f05ad5a45835
Oleg Babin <obabin@virtuozzo.com>
2018-07-26 18:14:51 +0300

    ext4: replace ext4_kvmalloc() with kvmalloc()
    ext4_kvmalloc() is used to allocate the table of group descritors
    blocks. It is called in GFP_NOFS context which is not vmalloc()
    compatible so it cannot be directly replaced with kvmalloc().
    
    In order to use kvmalloc() with GFP_KERNEL flag the memory allocation
    is moved from add_new_gdb() / add_new_gdb_meta_bg() functions to the
    beginning of the resize process before any journaling is started and
    any FS locks are taken.
    
    After this we do not need ext4_kvmalloc/ext4_kvfree functions any more.
    
    https://jira.sw.ru/browse/PSBM-83044
    
    Signed-off-by: Oleg Babin <obabin@virtuozzo.com>
    Signed-off-by: Jan Dakinevich <jan.dakinevich@virtuozzo.com>
    
    +++
    ext4: fix out of bounds access in ext4_alloc_group_desc_bh_array()
    
    https://jira.sw.ru/browse/PSBM-87413
    
    mFixes: d695abe ("ext4: replace ext4_kvmalloc() with kvmalloc()")
    Signed-off-by: Jan Dakinevich <jan.dakinevich@virtuozzo.com>
    [VvS RHEL7.8 rebase] context changes
    
    +++
    ext4: Fix high probable use-after-free
    
    Here we have even worse race than in mainstream.
    
    https://jira.sw.ru/browse/PSBM-101798
    
    mFixes: 86521524314e "ext4: replace ext4_kvmalloc() with kvmalloc()"
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -2217,6 +2217,37 @@ int ext4_alloc_flex_bg_array(struct super_block *sb, ext4_group_t ngroup)
 	return 0;
 }
 
+/*
+ * Allocate the top-level s_group_desc array for the specified number
+ * of groups. As the memory is allocated before the journaling is started
+ * we can safely use kvmalloc() with GFP_KERNEL flag here.
+ */
+int ext4_alloc_group_desc_bh_array(struct super_block *sb, ext4_group_t ngroup)
+{
+	struct ext4_sb_info *sbi = EXT4_SB(sb);
+	unsigned long num_desc = DIV_ROUND_UP(ngroup,  EXT4_DESC_PER_BLOCK(sb));
+	struct buffer_head **o_group_desc, **n_group_desc;
+
+	if (num_desc <= sbi->s_gdb_count)
+		return 0;
+
+	n_group_desc = kvmalloc(num_desc * sizeof(struct buffer_head *),
+				GFP_KERNEL);
+	if (!n_group_desc) {
+		ext4_warning(sb, "not enough memory for %lu groups", num_desc);
+		return -ENOMEM;
+	}
+
+	o_group_desc = sbi->s_group_desc;
+	memcpy(n_group_desc, o_group_desc,
+	       sbi->s_gdb_count * sizeof(struct buffer_head *));
+	WRITE_ONCE(sbi->s_group_desc, n_group_desc);
+
+	/* FIXME: rcu is needed here. See ms commit 1d0c3924a92e */
+	kvfree(o_group_desc);
+	return 0;
+}
+
 static int ext4_fill_flex_info(struct super_block *sb)
 {
 	struct ext4_sb_info *sbi = EXT4_SB(sb);
