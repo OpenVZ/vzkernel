OpenVZ/vzkernel

commit d821108a8a1af8e99dd61af874f1bc5900087591

ubc: initial patch
VE: virtualize cgroups

Cgroup of cgroups. Oh well..

https://jira.sw.ru/browse/PSBM-20665
https://jira.sw.ru/browse/PSBM-20540

Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>

+++
VE: fix couple compilation warnings

Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>

+++
cgroup: cgroup->top_cgroup => &cgroup->root->top_cgroup

After the patchset applied the kernel compiles (if CONFIG_FUSE_FS is off),
boots and allows starting containers. Fixing broken fuse bits will be done
a bit later.

Vladimir Davydov (13):
  Trivial compilation fixes
  sched: move declaration of cgroup related functions to global header
  khleper: fix compilation
  bc/proc: fix compilation
  cpuset: remove unused stuff and fix compilation
  ve/cgroup: fix compilation
  cgroup: cgroup->top_cgroup => &cgroup->root->top_cgroup
  cgroup/cpuset/fairsched: get rid of cgroup_lock
  Remove include/net/netlink_sock.h
  fairsched: use proc_mkdir for proc dir creation
  ve: use proc_mkdir for proc dir creation
  proc: mark only net dirs as virtualized by default
  proc-net: mark all files in proc-net directories as virtualized

This patch description:
The top_cgroup field was removed from the cgroup struct, because it always
equals &cgroup->root->top_cgroup. The patch fixes vz-related code according to
this.

Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>

+++
ve/cgroups: add functions ve_cgroup_open and ve_cgroup_remove

Mount cgroup hierarchies with the same subsystem sets as systemd
in rhel7. There are two types of problem with that:

If we mount our hierarchy before systemd (fairsched, beancounter),
systemd fails to mount them and may work incorrectly.

If we mount our hierarchy after systemd - we fail to mount, as
it happens in vecalls.c

This patchset make our hierarchies the same as systemd ones.

    ve: add functions ve_cgroup_open and ve_cgroup_remove
    ve: don't mount freezer subsystem
    ve: don't use common root cgroup in vecalls.c

include/bc/misc.h
@@ -1,0 +1,46 @@
+/*
+ *  include/bc/misc.h
+ *
+ *  Copyright (C) 2005  SWsoft
+ *  All rights reserved.
+ *  
+ *  Licensing governed by "linux/COPYING.SWsoft" file.
+ *
+ */
+
+#ifndef __BC_MISC_H_
+#define __BC_MISC_H_
+
+#include <bc/decl.h>
+
+struct tty_struct;
+struct file;
+struct file_lock;
+struct sigqueue;
+
+UB_DECLARE_FUNC(int, ub_file_charge(struct file *f))
+UB_DECLARE_VOID_FUNC(ub_file_uncharge(struct file *f))
+UB_DECLARE_FUNC(int, ub_flock_charge(struct file_lock *fl, int hard))
+UB_DECLARE_VOID_FUNC(ub_flock_uncharge(struct file_lock *fl))
+UB_DECLARE_FUNC(int, ub_siginfo_charge(struct sigqueue *q,
+			struct user_beancounter *ub, gfp_t gfp_mask))
+UB_DECLARE_VOID_FUNC(ub_siginfo_uncharge(struct sigqueue *q))
+UB_DECLARE_FUNC(int, ub_task_charge(struct user_beancounter *ub))
+UB_DECLARE_VOID_FUNC(ub_task_uncharge(struct user_beancounter *ub))
+UB_DECLARE_VOID_FUNC(ub_task_get(struct user_beancounter *ub,
+			struct task_struct *task))
+UB_DECLARE_VOID_FUNC(ub_task_put(struct task_struct *task))
+UB_DECLARE_FUNC(int, ub_pty_charge(struct tty_struct *tty))
+UB_DECLARE_VOID_FUNC(ub_pty_uncharge(struct tty_struct *tty))
+
+#ifdef CONFIG_BEANCOUNTERS
+#define set_flock_charged(fl)	do { (fl)->fl_charged = 1; } while (0)
+#define unset_flock_charged(fl)	do {		\
+		WARN_ON((fl)->fl_charged == 0);	\
+		(fl)->fl_charged = 0;		\
+	} while (0)
+#else
+#define set_flock_charged(fl)	do { } while (0)
+#define unset_flock_charged(fl)	do { } while (0)
+#endif
+#endif

kernel/bc/misc.c
@@ -1,0 +1,170 @@
+/*
+ *  kernel/bc/misc.c
+ *
+ *  Copyright (C) 2005  SWsoft
+ *  All rights reserved.
+ *  
+ *  Licensing governed by "linux/COPYING.SWsoft" file.
+ *
+ */
+
+#include <linux/tty.h>
+#include <linux/tty_driver.h>
+#include <linux/signal.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
+#include <linux/file.h>
+#include <linux/sched.h>
+#include <linux/module.h>
+
+#include <bc/beancounter.h>
+#include <bc/proc.h>
+
+/*
+ * Task staff
+ */
+
+int ub_task_charge(struct user_beancounter *ub)
+{
+	return charge_beancounter_fast(ub, UB_NUMPROC, 1, UB_HARD);
+}
+
+void ub_task_uncharge(struct user_beancounter *ub)
+{
+	uncharge_beancounter_fast(ub, UB_NUMPROC, 1);
+}
+
+void ub_task_get(struct user_beancounter *ub, struct task_struct *task)
+{
+	struct task_beancounter *new_bc = &task->task_bc;
+
+	new_bc->task_ub = get_beancounter(ub);
+	new_bc->exec_ub = get_beancounter(ub);
+}
+
+void ub_task_put(struct task_struct *task)
+{
+	struct task_beancounter *task_bc;
+
+	task_bc = &task->task_bc;
+
+	put_beancounter(task_bc->exec_ub);
+	put_beancounter(task_bc->task_ub);
+
+	task_bc->exec_ub = (struct user_beancounter *)0xdeadbcbc;
+	task_bc->task_ub = (struct user_beancounter *)0xdead100c;
+}
+
+int ub_file_charge(struct file *f)
+{
+	struct user_beancounter *ub = get_exec_ub();
+	int err;
+
+	err = charge_beancounter_fast(ub, UB_NUMFILE, 1, UB_HARD);
+	if (unlikely(err))
+		goto no_file;
+
+	f->f_ub = get_beancounter(ub);
+
+	return 0;
+
+no_file:
+	return err;
+}
+
+void ub_file_uncharge(struct file *f)
+{
+	struct user_beancounter *ub = f->f_ub;
+
+	uncharge_beancounter_fast(ub, UB_NUMFILE, 1);
+	put_beancounter(ub);
+}
+
+int ub_flock_charge(struct file_lock *fl, int hard)
+{
+	struct user_beancounter *ub;
+	int err;
+
+	ub = fl->fl_ub;
+	if (ub == NULL)
+		return 0;
+
+	err = charge_beancounter(ub, UB_NUMFLOCK, 1, hard ? UB_HARD : UB_SOFT);
+	if (!err)
+		fl->fl_charged = 1;
+	return err;
+}
+
+void ub_flock_uncharge(struct file_lock *fl)
+{
+	struct user_beancounter *ub;
+
+	ub = fl->fl_ub;
+	if (ub == NULL || !fl->fl_charged)
+		return;
+
+	uncharge_beancounter(ub, UB_NUMFLOCK, 1);
+	fl->fl_charged = 0;
+}
+
+/*
+ * Signal handling
+ */
+
+int ub_siginfo_charge(struct sigqueue *sq, struct user_beancounter *ub,
+			gfp_t gfp_mask)
+{
+	if (charge_beancounter_fast(ub, UB_NUMSIGINFO, 1, UB_HARD))
+		goto out_num;
+
+	sq->sig_ub = get_beancounter(ub);
+	return 0;
+
+out_num:
+	return -ENOMEM;
+}
+EXPORT_SYMBOL(ub_siginfo_charge);
+
+void ub_siginfo_uncharge(struct sigqueue *sq)
+{
+	struct user_beancounter *ub;
+
+	ub = sq->sig_ub;
+	sq->sig_ub = NULL;
+	uncharge_beancounter_fast(ub, UB_NUMSIGINFO, 1);
+	put_beancounter(ub);
+}
+
+/*
+ * PTYs
+ */
+
+int ub_pty_charge(struct tty_struct *tty)
+{
+	struct user_beancounter *ub = get_exec_ub();
+	int retval;
+
+	retval = 0;
+	if (ub && tty->driver->subtype == PTY_TYPE_MASTER &&
+			!test_bit(TTY_CHARGED, &tty->flags)) {
+		retval = charge_beancounter(ub, UB_NUMPTY, 1, UB_HARD);
+		if (!retval) {
+			set_bit(TTY_CHARGED, &tty->flags);
+			tty->ub = get_beancounter(ub);
+		}
+	}
+	return retval;
+}
+
+void ub_pty_uncharge(struct tty_struct *tty)
+{
+	struct user_beancounter *ub;
+
+	ub = tty->ub;
+	if (ub && tty->driver->subtype == PTY_TYPE_MASTER &&
+			test_bit(TTY_CHARGED, &tty->flags)) {
+		uncharge_beancounter(ub, UB_NUMPTY, 1);
+		clear_bit(TTY_CHARGED, &tty->flags);
+		put_beancounter(ub);
+	}
+}

kernel/exit.c
@@ -57,7 +57,6 @@
 #include <linux/userfaultfd_k.h>
 
 #include <bc/misc.h>
-#include <bc/oom_kill.h>
 
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
@@ -214,7 +213,7 @@ repeat:
 	qwrite_unlock_irq(&tasklist_lock);
 	cgroup_pids_release(p);
 	release_thread(p);
-	ub_task_uncharge(p->task_bc.task_ub);
+	ub_task_uncharge(get_task_ub(p));
 	call_rcu(&p->rcu, delayed_put_task_struct);
 
 	p = leader;

kernel/fork.c
@@ -89,7 +89,7 @@
 #include <asm/tlbflush.h>
 
 #include <bc/misc.h>
-#include <bc/oom_kill.h>
+#include <bc/vmpages.h>
 
 #include <trace/events/sched.h>
 
@@ -111,6 +111,7 @@ DEFINE_PER_CPU(unsigned long, process_counts) = 0;
 __attribute__((__section__(".data..cacheline_aligned")))
 static atomic_t tasklist_waiters = ATOMIC_INIT(0);
 __cacheline_aligned DEFINE_QRWLOCK(tasklist_lock);  /* outer */
+EXPORT_SYMBOL(tasklist_lock);
 
 void tasklist_write_lock_irq(void)
 {
@@ -444,6 +445,10 @@ static int dup_mmap(struct mm_struct *mm, struct mm_struct *oldmm)
 			continue;
 		}
 		charge = 0;
+		if (ub_memory_charge(mm, mpnt->vm_end - mpnt->vm_start,
+					mpnt->vm_flags & ~VM_LOCKED,
+					mpnt->vm_file, UB_HARD))
+			goto fail_noch;
 		if (mpnt->vm_flags & VM_ACCOUNT) {
 			unsigned long len = vma_pages(mpnt);
 
@@ -536,6 +541,9 @@ fail_nomem_anon_vma_fork:
 fail_nomem_policy:
 	kmem_cache_free(vm_area_cachep, tmp);
 fail_nomem:
+	ub_memory_uncharge(mm, mpnt->vm_end - mpnt->vm_start,
+			mpnt->vm_flags & ~VM_LOCKED, mpnt->vm_file);
+fail_noch:
 	retval = -ENOMEM;
 	vm_unacct_memory(charge);
 	goto out;
@@ -573,12 +581,12 @@ __cacheline_aligned_in_smp DEFINE_SPINLOCK(mmlist_lock);
 
 static inline void set_mm_ub(struct mm_struct *mm, struct user_beancounter *ub)
 {
-	mm->mm_ub = get_beancounter_longterm(ub);
+	mm->mm_ub = get_beancounter(ub);
 }
 
 static inline void put_mm_ub(struct mm_struct *mm)
 {
-	put_beancounter_longterm(mm->mm_ub);
+	put_beancounter(mm->mm_ub);
 	mm->mm_ub = NULL;
 }
 
@@ -722,8 +730,6 @@ void mmput(struct mm_struct *mm)
 		}
 		if (mm->binfmt)
 			module_put(mm->binfmt->module);
-		if (mm->global_oom || mm->ub_oom)
-			ub_oom_mm_dead(mm);
 		put_mm_ub(mm);
 		mmdrop(mm);
 	}
@@ -960,8 +966,6 @@ struct mm_struct *dup_mm(struct task_struct *tsk)
 		goto fail_nomem;
 
 	memcpy(mm, oldmm, sizeof(*mm));
-	mm->global_oom = 0;
-	mm->ub_oom = 0;
 	mm_init_cpumask(mm);
 
 #if defined(CONFIG_TRANSPARENT_HUGEPAGE) && !USE_SPLIT_PMD_PTLOCKS

commit 93f3499e067721f1012cee24e00b40ab76818118

bc: roll back beancounters-based numer of processes accounting
ubc param "numproc" is handled ub in our home brew beancounters code,
while stock kernel (even RHEL7 one) now has pids cgroup which
does approximately the same.

So drop original "numproc" ubc limit code, next patch adds similar
funtionality basing on pids cgroup.

Note: the difference between approaches:
- ubc code accounts and limits processes (thus you can create a lot of
  threads in a Container with "numproc" limit)
- pids cgroup accounts and limits "pids", thus including threads

At the same time we already configure pids cgroup for a Container and
set up "pids.max" equal to ubc "numproc" value, so the semantic has been
already changed for some period.

Fixes: d821108a8a1a ("ubc: initial patch")
https://jira.sw.ru/browse/PSBM-86044

Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
Reviewed-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

======================================================
Patchset description:
bc: rework "numproc" ubc limit to be based on pids cgroup

It's time to rework our home brew ubc "numproc" limit to be based on stock pids
cgroup.

Note: the semantic will change a bit: now we limit number of possible threads
in a Container while old ubc "numproc" limited number of tasks.

Note1: vzctl update is required.

https://jira.sw.ru/browse/PSBM-86044

Konstantin Khorenko (2):
  bc: roll back beancounters-based numer of processes accounting
  bc: implement ubc "numproc" limit based on pids cgroup

include/bc/misc.h
@@ -23,8 +23,6 @@ UB_DECLARE_VOID_FUNC(ub_flock_uncharge(struct file_lock *fl))
 UB_DECLARE_FUNC(int, ub_siginfo_charge(struct sigqueue *q,
 			struct user_beancounter *ub, gfp_t gfp_mask))
 UB_DECLARE_VOID_FUNC(ub_siginfo_uncharge(struct sigqueue *q))
-UB_DECLARE_FUNC(int, ub_task_charge(struct user_beancounter *ub))
-UB_DECLARE_VOID_FUNC(ub_task_uncharge(struct user_beancounter *ub))
 UB_DECLARE_VOID_FUNC(ub_task_get(struct user_beancounter *ub,
 			struct task_struct *task))
 UB_DECLARE_VOID_FUNC(ub_task_put(struct task_struct *task))

kernel/bc/beancounter.c
@@ -76,7 +76,6 @@ const char *ub_rnames[] = {
 /* default maximum perpcu resources precharge */
 int ub_resource_precharge[UB_RESOURCES] = {
 	[UB_PRIVVMPAGES]= 256,
-	[UB_NUMPROC]	= 4,
 	[UB_NUMSIGINFO]	= 4,
 	[UB_NUMFILE]	= 8,
 };
@@ -1089,7 +1088,6 @@ void __init ub_init_early(void)
 	memset(&current->task_bc, 0, sizeof(struct task_beancounter));
 	(void)set_exec_ub(ub);
 	current->task_bc.task_ub = get_beancounter(ub);
-	__charge_beancounter_locked(ub, UB_NUMPROC, 1, UB_FORCE);
 	init_mm.mm_ub = get_beancounter(ub);
 
 	list_add(&ub->ub_list, &ub_list_head);

kernel/bc/misc.c
@@ -22,16 +22,6 @@
  * Task staff
  */
 
-int ub_task_charge(struct user_beancounter *ub)
-{
-	return charge_beancounter_fast(ub, UB_NUMPROC, 1, UB_HARD);
-}
-
-void ub_task_uncharge(struct user_beancounter *ub)
-{
-	uncharge_beancounter_fast(ub, UB_NUMPROC, 1);
-}
-
 void ub_task_get(struct user_beancounter *ub, struct task_struct *task)
 {
 	struct task_beancounter *new_bc = &task->task_bc;

kernel/exit.c
@@ -214,7 +214,6 @@ repeat:
 	qwrite_unlock_irq(&tasklist_lock);
 	cgroup_pids_release(p);
 	release_thread(p);
-	ub_task_uncharge(get_task_ub(p));
 	call_rcu(&p->rcu, delayed_put_task_struct);
 
 	p = leader;

kernel/fork.c
@@ -1394,12 +1394,9 @@ static struct task_struct *copy_process(unsigned long clone_flags,
 		goto fork_out;
 
 	retval = -ENOMEM;
-	if (ub_task_charge(get_exec_ub()))
-		goto fork_out;
-
 	p = dup_task_struct(current, node);
 	if (!p)
-		goto bad_fork_uncharge;
+		goto fork_out;
 
 	ub_task_get(get_exec_ub(), p);
 
@@ -1771,8 +1768,6 @@ bad_fork_cleanup_count:
 bad_fork_free:
 	ub_task_put(p);
 	free_task(p);
-bad_fork_uncharge:
-	ub_task_uncharge(get_exec_ub());
 fork_out:
 	return ERR_PTR(retval);
 }