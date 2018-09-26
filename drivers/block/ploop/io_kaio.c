55cd092c3cf6e15fa56e6c37798a362452226534
Vasily Averin <vvs@virtuozzo.com>
2018-09-26 17:43:48 +0300

    ploop: kthread_create() errors processing
    kthread_create() returns not NULL but ERR_PTR in case of internal failures.
    
    https://jira.sw.ru/browse/PSBM-88561
    
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    Reviewed-by: Evgenii Shatokhin <eshatokhin@virtuozzo.com>
    
    v2: extra netfilter hunks was removed.

--- drivers/block/ploop/io_kaio.c
+++ drivers/block/ploop/io_kaio.c
@@ -887,9 +887,11 @@ static int kaio_open(struct ploop_io * io)
 		io->fsync_thread = kthread_create(kaio_fsync_thread,
 						  io, "ploop_fsync%d",
 						  delta->plo->index);
-		if (io->fsync_thread == NULL) {
+		if (IS_ERR(io->fsync_thread)) {
+			err = PTR_ERR(io->fsync_thread);
+			io->fsync_thread = NULL;
 			ploop_kaio_close(io->files.mapping, 0);
-			return -ENOMEM;
+			return err;
 		}
 
 		wake_up_process(io->fsync_thread);
@@ -985,8 +987,9 @@ static int kaio_prepare_merge(struct ploop_io * io, struct ploop_snapdata *sd)
 	io->fsync_thread = kthread_create(kaio_fsync_thread,
 					  io, "ploop_fsync%d",
 					  io->plo->index);
-	if (io->fsync_thread == NULL) {
-		err = -ENOMEM;
+	if (IS_ERR(io->fsync_thread)) {
+		err = PTR_ERR(io->fsync_thread);
+		io->fsync_thread = NULL;
 		goto prep_merge_done;
 	}
 
