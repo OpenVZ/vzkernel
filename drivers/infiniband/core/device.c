0c1b23e5c362d5ae299162ffeb816bdbd7a995d2
Jan Dakinevich <jan.dakinevich@virtuozzo.com>
2018-09-28 12:24:02 +0300

    IB/core: use kvzalloc to allocate ib_device
    The allocator ib_alloc_device() is used to allocate both ib_device and
    device specific instance. This allocations sometimes quite big and
    takes tens KiB, causing high page orders.
    
    Meanwhile, the memory is not intended for DMA and doesn't require to be
    physically contiguous.
    
    https://jira.sw.ru/browse/HCI-129
    Signed-off-by: Jan Dakinevich <jan.dakinevich@virtuozzo.com>

--- drivers/infiniband/core/device.c
+++ drivers/infiniband/core/device.c
@@ -202,7 +202,7 @@ static void ib_device_release(struct device *device)
 		ib_cache_release_one(dev);
 		kfree(dev->port_immutable);
 	}
-	kfree(dev);
+	kvfree(dev);
 }
 
 static int ib_device_uevent(struct device *device,
@@ -243,7 +243,7 @@ struct ib_device *ib_alloc_device(size_t size)
 	if (WARN_ON(size < sizeof(struct ib_device)))
 		return NULL;
 
-	device = kzalloc(size, GFP_KERNEL);
+	device = kvzalloc(size, GFP_KERNEL);
 	if (!device)
 		return NULL;
 
