f41bccbad5efb4999265df65e25eb342571a824c
Pavel Butsykin <pbutsykin@virtuozzo.com>
2018-10-03 17:08:53 +0300

    fs/fuse kio_pcs: fix race between req->end() and request_wait_answer()
    There is already a patch for this issue:
    commit c76d20779550
    Author: Dmitry Monakhov <dmonakhov@openvz.org>
    Date:   Mon Feb 19 14:06:59 2018 +0300
    
       fuse: fix ->end() vs ->waitq ordering
    
    But there's still a small window when waiter can be notified about request
    completion before req->end() callback executed. This can happen because
    synchronous requests can use interruptible wait and such wait can be completed
    based only on FR_FINISHED flag, although req->end() may not be executed yet.
    
    Signed-off-by: Pavel Butsykin <pbutsykin@virtuozzo.com>
    =======================================================
    Patchset description:
    
    RPC connection double close fixes
    
    https://pmc.acronis.com/browse/VSTOR-15378
    
    Pavel Butsykin (3):
      fs/fuse kio_pcs: fix race between req->end() and request_wait_answer()
      fs/fuse kio_pcs: remove fuse destroy request
      fs/fuse kio_pcs: drop rpc connection close in pcs_rpc_engine_fini()
    Feature: vStorage

--- fs/fuse/dev.c
+++ fs/fuse/dev.c
@@ -298,9 +298,12 @@ void fuse_request_end(struct fuse_req *req)
 	struct fuse_mount *fm = req->fm;
 	struct fuse_conn *fc = fm->fc;
 	struct fuse_iqueue *fiq = req->args->fiq;
+	bool bg;
 
 	if (test_and_set_bit(FR_FINISHED, &req->flags))
 		goto put_request;
+
+	bg = test_bit(FR_BACKGROUND, &req->flags);
 
 	/*
 	 * test_and_set_bit() implies smp_mb() between bit
@@ -314,7 +317,7 @@ void fuse_request_end(struct fuse_req *req)
 	}
 	WARN_ON(test_bit(FR_PENDING, &req->flags));
 	WARN_ON(test_bit(FR_SENT, &req->flags));
-	if (test_bit(FR_BACKGROUND, &req->flags)) {
+	if (bg) {
 		spin_lock(&fc->bg_lock);
 		clear_bit(FR_BACKGROUND, &req->flags);
 		if (fc->num_background == fc->max_background) {
@@ -341,8 +344,11 @@ void fuse_request_end(struct fuse_req *req)
 		spin_unlock(&fc->bg_lock);
 	}
 
-	if (test_bit(FR_ASYNC, &req->flags))
+	if (test_bit(FR_ASYNC, &req->flags)) {
 		req->args->end(fm, req->args, req->out.h.error);
+		if (!bg)
+			req->args->end = NULL;
+	}
 	/* Wake up waiter sleeping in request_wait_answer() */
 	wake_up(&req->waitq);
 put_request:
@@ -387,7 +393,7 @@ static void request_wait_answer(struct fuse_req *req)
 	if (!fc->no_interrupt) {
 		/* Any signal may interrupt this */
 		err = wait_event_interruptible(req->waitq,
-					test_bit(FR_FINISHED, &req->flags));
+				test_bit(FR_FINISHED, &req->flags) && !req->args->end);
 		if (!err)
 			return;
 
@@ -401,7 +407,7 @@ static void request_wait_answer(struct fuse_req *req)
 	if (!test_bit(FR_FORCE, &req->flags)) {
 		/* Only fatal signals may interrupt this */
 		err = wait_event_killable(req->waitq,
-					test_bit(FR_FINISHED, &req->flags));
+				test_bit(FR_FINISHED, &req->flags) && !req->args->end);
 		if (!err)
 			return;
 
