87a344ad70e0081185681c4e4717265323bc2faf
Kirill Tkhai <ktkhai@virtuozzo.com>
2018-10-04 12:00:46 +0300

    mm: Fix int overflow in callers of do_shrink_slab()
    do_shrink_slab() returns unsigned long value, and
    the placing into int variable cuts high bytes off.
    Then we compare ret and 0xfffffffe (since SHRINK_EMPTY
    is converted to ret type).
    
    Thus, big number of objects returned by do_shrink_slab()
    may be interpreted as SHRINK_EMPTY, if low bytes of
    their value are equal to 0xfffffffe. Fix that
    by declaration ret as unsigned long in these functions.
    
    Reported-by: Cyrill Gorcunov <gorcunov@openvz.org>
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Acked-by: Cyrill Gorcunov <gorcunov@gmail.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -437,8 +437,8 @@ static unsigned long shrink_slab_memcg(gfp_t gfp_mask, int nid,
 			struct mem_cgroup *memcg, int priority)
 {
 	struct memcg_shrinker_map *map;
-	unsigned long freed = 0;
-	int ret, i;
+	unsigned long ret, freed = 0;
+	int i;
 
 	if (!memcg_kmem_enabled())
 		return 0;
@@ -537,9 +537,8 @@ static unsigned long shrink_slab(gfp_t gfp_mask, int nid,
 				 int priority,
 				 bool for_drop_caches)
 {
+	unsigned long ret, freed = 0;
 	struct shrinker *shrinker;
-	unsigned long freed = 0;
-	int ret;
 
 	if (unlikely(test_tsk_thread_flag(current, TIF_MEMDIE)))
 		return 0;
