900dfa6ccb9b701641a45a1544861f290576d623
Kirill Tkhai <ktkhai@virtuozzo.com>
2018-10-16 15:37:11 +0300

    fuse kio: Differ fuse_pcs_kdirect_claim_op() return values
    Fail open in case of fuse_get_req() was failed, and leave it successful in case
    of vstorage-mount said we can't claim file by kio (e.g., file will be served by
    userspace).
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Reviewed-by: Pavel Butsykin <pbutsykin@virtuozzo.com>
    Feature: vStorage

--- fs/fuse/kio/pcs/pcs_fuse_kdirect.c
+++ fs/fuse/kio/pcs/pcs_fuse_kdirect.c
@@ -294,7 +294,7 @@ static int fuse_pcs_kdirect_claim_op(struct fuse_conn *fc, struct file *file,
 	if (err || outarg->result) {
 		TRACE("%s:%d h.err:%d result:%d\n", __FUNCTION__, __LINE__,
 		      err, outarg->result);
-		err = err ? : outarg->result;
+		err = -EOPNOTSUPP;
 	}
 
 	return err;
@@ -347,7 +347,7 @@ static int kpcs_do_file_open(struct fuse_conn *fc, struct file *file, struct ino
 		pcs_mapping_deinit(&di->mapping);
 		kfree(di);
 		/* Claim error means we cannot claim, just that */
-		return 0;
+		return (ret == -EOPNOTSUPP ? 0: ret);
 	}
 	/* TODO: Propper initialization of dentry should be here!!! */
 	fi->private = di;
