e5990350edce9a03ca9197f1c9e47f7b62cb1b7c
Konstantin Khorenko <khorenko@virtuozzo.com>
2018-10-26 17:50:40 +0300

    fs/inode: move cond_resched() at the end of prune_icache_sb()
    We don't want to put cond_resched() inside the block
    KSTAT_PERF_ENTER .. KSTAT_PERF_LEAVE and we can't add it
    before KSTAT_PERF_ENTER because compiler complains on mixed declaration
    and code, thus just move it after the KSTAT block.
    
    https://jira.sw.ru/browse/PSBM-89684
    
    Fixes: f7b3e463cd53 ("vzstat: initial patch")
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/inode.c
+++ fs/inode.c
@@ -774,13 +774,13 @@ long prune_icache_sb(struct super_block *sb, struct shrink_control *sc)
 	LIST_HEAD(freeable);
 	long freed;
 
-	cond_resched();
-
 	KSTAT_PERF_ENTER(shrink_icache);
 	freed = list_lru_shrink_walk(&sb->s_inode_lru, sc,
 				     inode_lru_isolate, &freeable);
 	dispose_list(&freeable);
 	KSTAT_PERF_LEAVE(shrink_icache);
+
+	cond_resched();
 	return freed;
 }
 
