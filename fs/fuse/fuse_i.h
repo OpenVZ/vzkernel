00570f7bbb2c77a5387474342e8618d0b2eadfd4
Kirill Tkhai <ktkhai@virtuozzo.com>
2018-11-09 13:33:22 +0300

    ms/fuse: Introduce fi->lock to protect write related fields
    ML commit f15ecfef058d
    
    To minimize contention of fc->lock, this patch introduces a new spinlock
    for protection fuse_inode metadata:
    
    fuse_inode:
    	writectr
    	writepages
    	write_files
    	queued_writes
    	attr_version
    
    inode:
    	i_size
    	i_nlink
    	i_mtime
    	i_ctime
    
    Also, it protects the fields changed in fuse_change_attributes_common()
    (too many to list).
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -96,7 +96,7 @@ struct fuse_inode {
 	/** Version of last attribute change */
 	u64 attr_version;
 
-	/** Files usable in writepage.  Protected by fc->lock */
+	/** Files usable in writepage.  Protected by fi->lock */
 	struct list_head write_files;
 
 	/** Writepages pending on truncate or fsync */
@@ -114,6 +114,9 @@ struct fuse_inode {
 
 	/** Miscellaneous bits describing inode state */
 	unsigned long state;
+
+	/** Lock to protect write related fields */
+	spinlock_t lock;
 };
 
 /** FUSE inode state bits */
