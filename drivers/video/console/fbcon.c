0841a0062e2d720e9d4e305428566af66d1d3817
Konstantin Khorenko <khorenko@virtuozzo.com>
2018-11-19 18:14:53 +0300

    fbcon: use kvmalloc() for scrollback buffer
    Scrollback frame buffer is rather big - 32K,
    so it requires 3rd order page, so let's use kvmalloc() instead of
    ordinary kmalloc() for it.
    
    https://pmc.acronis.com/browse/VSTOR-17781
    https://jira.sw.ru/browse/HCI-188
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- drivers/video/console/fbcon.c
+++ drivers/video/console/fbcon.c
@@ -995,7 +995,7 @@ static const char *fbcon_startup(void)
 			if (!softback_buf) {
 				softback_buf =
 				    (unsigned long)
-				    kmalloc(fbcon_softback_size,
+				    kvmalloc(fbcon_softback_size,
 					    GFP_KERNEL);
 				if (!softback_buf) {
 					fbcon_softback_size = 0;
@@ -1004,7 +1004,7 @@ static const char *fbcon_startup(void)
 			}
 		} else {
 			if (softback_buf) {
-				kfree((void *) softback_buf);
+				kvfree((void *) softback_buf);
 				softback_buf = 0;
 				softback_top = 0;
 			}
@@ -3567,7 +3567,7 @@ static void fbcon_exit(void)
 	if (fbcon_has_exited)
 		return;
 
-	kfree((void *)softback_buf);
+	kvfree((void *)softback_buf);
 	softback_buf = 0UL;
 
 	for (i = 0; i < FB_MAX; i++) {
