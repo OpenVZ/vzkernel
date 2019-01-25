cf688ac6c79fb4d78967f2ead80badfcfe104bed
Kirill Tkhai <ktkhai@virtuozzo.com>
2019-01-25 11:09:34 +0000

    ms/ipvs: remove an annoying printk in netns init
    ms commit 864e91ca981e
    
    At most it is used for debugging purpose, but I don't think
    it is even useful for debugging, just remove it.
    
    Signed-off-by: Cong Wang <xiyou.wangcong@gmail.com>
    Signed-off-by: Simon Horman <horms@verge.net.au>
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- net/netfilter/ipvs/ip_vs_core.c
+++ net/netfilter/ipvs/ip_vs_core.c
@@ -2178,8 +2178,6 @@ static int __net_init __ip_vs_init(struct net *net)
 	if (ip_vs_sync_net_init(net) < 0)
 		goto sync_fail;
 
-	printk(KERN_INFO "IPVS: Creating netns size=%zu id=%d\n",
-			 sizeof(struct netns_ipvs), ipvs->gen);
 	return 0;
 /*
  * Error handling
