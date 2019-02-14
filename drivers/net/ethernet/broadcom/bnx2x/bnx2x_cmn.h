49058d6e9a37ad996bed15cf2d56712fccb26173
Konstantin Khorenko <khorenko@virtuozzo.com>
2019-02-14 17:23:45 +0300

    bnx2x: suppress warnings on DMA memory allocation - addon
    Fixes: 33951eb784ed ("bnx2x: suppress warnings on DMA memory allocation")
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- drivers/net/ethernet/broadcom/bnx2x/bnx2x_cmn.h
+++ drivers/net/ethernet/broadcom/bnx2x/bnx2x_cmn.h
@@ -53,7 +53,7 @@ extern int bnx2x_num_queues;
 #define BNX2X_PCI_ALLOC(y, size)					\
 ({									\
 	void *x = dma_zalloc_coherent(&bp->pdev->dev, size, y,		\
-				      GFP_KERNEL | __GFP_NOWARN);	\
+				      GFP_KERNEL | __GFP_ORDER_NOWARN);	\
 	if (x)								\
 		DP(NETIF_MSG_HW,					\
 		   "BNX2X_PCI_ALLOC: Physical %Lx Virtual %p\n",	\
@@ -64,7 +64,7 @@ extern int bnx2x_num_queues;
 #define BNX2X_PCI_FALLOC(y, size)					\
 ({									\
 	void *x = dma_alloc_coherent(&bp->pdev->dev, size, y,		\
-				     GFP_KERNEL | __GFP_NOWARN);	\
+				     GFP_KERNEL | __GFP_ORDER_NOWARN);	\
 	if (x) {							\
 		memset(x, 0xff, size);					\
 		DP(NETIF_MSG_HW,					\
