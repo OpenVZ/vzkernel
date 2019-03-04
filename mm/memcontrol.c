4ec2a8efec60c4638275d9d8dd7cfcf68b8769b6
Vladimir Davydov <vdavydov@virtuozzo.com>
2019-03-04 17:52:23 +0300

    ms/mm: workingset: make shadow node shrinker memcg aware
    Workingset code was recently made memcg aware, but shadow node shrinker
    is still global.  As a result, one small cgroup can consume all memory
    available for shadow nodes, possibly hurting other cgroups by reclaiming
    their shadow nodes, even though reclaim distances stored in its shadow
    nodes have no effect.  To avoid this, we need to make shadow node
    shrinker memcg aware.
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    https://pmc.acronis.com/browse/VSTOR-19037
    (cherry picked from commit 0a6b76dd23fa08c5fd7b68acdb55018a37afd4aa)
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1333,7 +1333,7 @@ mem_cgroup_zone_nr_lru_pages(struct mem_cgroup *memcg, int nid, int zid,
 	return ret;
 }
 
-static unsigned long
+unsigned long
 mem_cgroup_node_nr_lru_pages(struct mem_cgroup *memcg,
 			int nid, unsigned int lru_mask)
 {
