2d9ac8be0c76c00ec277ab4883a52792aab3d95d
Andrey Ryabinin <aryabinin@virtuozzo.com>
2019-03-06 14:08:28 +0300

    mm/swap: activate swapped in pages on fault
    Move swapped in anon pages directly to active list. This should
    help us to prevent anon thrashing. Recently swapped in pages
    has more chances to stay in memory.
    
    https://pmc.acronis.com/browse/VSTOR-20859
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    [VvS RHEL7.8 rebase] context changes

--- mm/zswap.c
+++ mm/zswap.c
@@ -449,7 +449,7 @@ static int zswap_get_swap_cache_page(swp_entry_t entry,
 	bool page_was_allocated;
 
 	*retpage = __read_swap_cache_async(entry, GFP_KERNEL,
-			NULL, 0, &page_was_allocated);
+			NULL, 0, &page_was_allocated, false);
 	if (page_was_allocated)
 		return ZSWAP_SWAPCACHE_NEW;
 	if (!*retpage)
