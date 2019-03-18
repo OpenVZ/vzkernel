346b389e0ce50dcf110576c42057abe4a8f43257
Kirill Tkhai <ktkhai@virtuozzo.com>
2019-03-18 16:29:04 +0300

    ploop: Prohibit discard in case of multi-delta case
    We can't send discard in case of there are more than
    on delta. Otherewise it is considered as REQ_WRITE
    request and forces block allocation, which leads
    to crash since bio list is NULL on discard.
    
    Strange thing, crash dis -l shows me, that problem
    address is on the bottoms branch of this function,
    but really we go thru
      delta && delta != top_delta && whole_block() branch.
    
    https://jira.sw.ru/browse/PSBM-92785
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- drivers/block/ploop/dev.c
+++ drivers/block/ploop/dev.c
@@ -2550,6 +2550,9 @@ static bool ploop_can_issue_discard(struct ploop_device *plo,
 	if (test_bit(PLOOP_S_NO_FALLOC_DISCARD, &plo->state))
 		return false;
 
+	if (!list_is_singular(&plo->map.delta_list))
+		return false;
+
 	return whole_block(plo, preq);
 }
 
