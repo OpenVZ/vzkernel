d81ae844b3b05d827d94cb020ccf53bb94fe90b9
Pavel Butsykin <pbutsykin@virtuozzo.com>
2019-03-26 12:49:03 +0300

    fs/fuse kio: fix double free of pfc in process_pcs_init_reply()
    Remove redundant kvfree(pfc) in process_pcs_init_reply() to fix it.
    
    This branch can only be taken if an abortion occurs during the mount, that is
    quite a rare event.
    
    https://pmc.acronis.com/browse/VSTOR-20922
    
    Signed-off-by: Pavel Butsykin <pbutsykin@virtuozzo.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- fs/fuse/kio/pcs/pcs_fuse_kdirect.c
+++ fs/fuse/kio/pcs/pcs_fuse_kdirect.c
@@ -127,7 +127,6 @@ static void process_pcs_init_reply(struct fuse_conn *fc, struct fuse_req *req)
 	if (pfc) {
 		fuse_ktrace_remove(fc);
 		pcs_cluster_fini(pfc);
-		kvfree(pfc);
 	}
 out:
 	if (fc->conn_error)
