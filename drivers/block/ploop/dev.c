bc8bd3c2a82d0d8df58d4c029bad644b7dadd7d5
Konstantin Khorenko <khorenko@virtuozzo.com>
2019-03-30 11:10:05 +0300

    Revert "ploop: Prohibit PLOOP_IOC_UPDATE_INDEX on singular list"
    This reverts commit 17042e7b7c70ffd84433e3ac14d283738e58c3f7.
    
    Temporary revert due to
    https://jira.sw.ru/browse/PSBM-93243

--- drivers/block/ploop/dev.c
+++ drivers/block/ploop/dev.c
@@ -4277,14 +4277,7 @@ static int ploop_index_update_ioc(struct ploop_device *plo, unsigned long arg)
 	struct reloc_map *map;
 	int i;
 
-	/*
-	 * Currently this is used by userspace for relocation
-	 * of start blocks before expanding BAT of !top delta.
-	 * In case of someone wants to do that on a singular
-	 * list, he should expand holes bitmap too.
-	 */
-	if (list_empty(&plo->map.delta_list) ||
-	    list_is_singular(&plo->map.delta_list))
+	if (list_empty(&plo->map.delta_list))
 		return -ENOENT;
 
 	if (copy_from_user(&ctl, (void*)arg,
