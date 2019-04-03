ffce1aef80d82512a0afb45c78fa29a91311dcdb
Konstantin Khorenko <khorenko@virtuozzo.com>
2019-04-03 11:42:07 +0300

    Revert "ploop: Introduce ploop_submit_alloc() helper"
    This reverts commit 1e9da1ecbb1580d94faf6093042d9da3dd70deac.
    
    Dropping ploop discard support feature for now.
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- drivers/block/ploop/io_direct.c
+++ drivers/block/ploop/io_direct.c
@@ -732,14 +732,24 @@ out:
 
 static int
 dio_submit_alloc(struct ploop_io *io, struct ploop_request * preq,
-		 struct bio_list * sbl, unsigned int size, iblock_t iblk)
+		 struct bio_list * sbl, unsigned int size)
 {
 	int err;
+	iblock_t iblk = io->alloc_head++;
+
+	trace_submit_alloc(preq);
+
+	if (!(io->files.file->f_mode & FMODE_WRITE)) {
+		PLOOP_FAIL_REQUEST(preq, -EBADF);
+		return -1;
+	}
 
 	err = cached_submit(io, iblk, preq, sbl, size, true);
 	if (err) {
+		if (err == -ENOSPC)
+			io->alloc_head--;
 		PLOOP_FAIL_REQUEST(preq, err);
-		return err;
+		return -1;
 	}
 	preq->eng_state = PLOOP_E_DATA_WBI;
 	return 1;
