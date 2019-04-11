b61fd0112b93353f93d05df60902d4705d52dce3
Pavel Butsykin <pbutsykin@virtuozzo.com>
2019-04-11 14:41:52 +0300

    fs/fuse kio: export fastpath protocol version
    In order to transfer the logic of the fallback decision to user-space,
    let's add export fastpath version.
    
    https://jira.sw.ru/browse/PSBM-93637
    
    Signed-off-by: Pavel Butsykin <pbutsykin@virtuozzo.com>

--- fs/fuse/kio/pcs/pcs_prot_types.h
+++ fs/fuse/kio/pcs/pcs_prot_types.h
@@ -73,9 +73,12 @@ typedef u64 PCS_CHUNK_UID_T;
 typedef u64 PCS_LEASE_GEN_T;
 typedef u32 PCS_POLICY_GEN_T;
 
-typedef struct {
-	u32 major;
-	u32 minor;
+typedef union {
+	struct {
+		u32 major;
+		u32 minor;
+	};
+	u64 full;
 } PCS_FAST_PATH_VERSION_T;
 
 /*
