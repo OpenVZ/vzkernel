6e7f839d55e787d5ffb51d6be4f8c84e64bb9b58
Andrey Ryabinin <aryabinin@virtuozzo.com>
2019-05-21 18:34:55 +0300

    mm/workingset: fix workingset_activation() with memcgroups enabled
    workingset_activation() mistakenly does nothing when memory cgroups are
    enabled. Commit f6a8b015027e ("ms/mm: workingset: per-cgroup cache
    thrash detection") also changed mem_cgroup_begin_update_page_stat()
    to return memcg. But commit didn't make it right, memcg returns only
    when memcg is moving and NULL otherwise. Instead of mocking with
    mem_cgroup_begin_update_page_stat() obtain memcg from page in
    workingset_activation() directly.
    
    Fixes: f6a8b015027e ("ms/mm: workingset: per-cgroup cache thrash detection")
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -2731,7 +2731,7 @@ cleanup:
  * account and taking the move_lock in the slowpath.
  */
 
-struct mem_cgroup *__mem_cgroup_begin_update_page_stat(struct page *page,
+void __mem_cgroup_begin_update_page_stat(struct page *page,
 				bool *locked, unsigned long *flags)
 {
 	struct mem_cgroup *memcg;
@@ -2741,7 +2741,8 @@ struct mem_cgroup *__mem_cgroup_begin_update_page_stat(struct page *page,
 again:
 	memcg = pc->mem_cgroup;
 	if (unlikely(!memcg || !PageCgroupUsed(pc)))
-		return NULL;
+		return;
+
 	/*
 	 * If this memory cgroup is not under account moving, we don't
 	 * need to take move_lock_mem_cgroup(). Because we already hold
@@ -2749,7 +2750,7 @@ again:
 	 * rcu_read_unlock() if mem_cgroup_stolen() == true.
 	 */
 	if (!mem_cgroup_stolen(memcg))
-		return NULL;
+		return;
 
 	move_lock_mem_cgroup(memcg, flags);
 	if (memcg != pc->mem_cgroup || !PageCgroupUsed(pc)) {
@@ -2757,7 +2758,7 @@ again:
 		goto again;
 	}
 	*locked = true;
-	return memcg;
+	return;
 }
 
 void __mem_cgroup_end_update_page_stat(struct page *page, unsigned long *flags)
