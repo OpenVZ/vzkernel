c6857a3301a3e2ca4cd4fbdb7f80ee97842a03cd
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2019-05-21 18:11:19 +0300

    net: fix false-positive deadlock detection for sk_receive_queue lock
    Here is the code path of false-positive deadlock:
    
    CPU1                                    CPU2
    
    __udp_enqueue_schedule_skb
     spin_lock(sk->sk_receive_queue.lock) <- udp
    
                                            ipoib_start_xmit
                                             spin_lock_irqsave(((struct rdma_netdev *)netdev_priv(net_device))->clnt_priv->lock)
                                             path_rec_start
                                              ...
                                               __netlink_sendskb
                                                skb_queue_tail
                                                 spin_lock_irqsave(sk->sk_receive_queue.lock) <- netlink
    
    do_IRQ
     ...
      ipoib_cm_rx_event_handler
       spin_lock_irqsave(((struct rdma_netdev *)netdev_priv(net_device))->clnt_priv->lock)
    
    For sockets of different protocols these sk->sk_receive_queue.lock's
    are different on the right and on the left, thus no real deadlock will
    happen as CPU2 will continue execution instead of waiting for lock.
    
    So mark the udp lock with separate lockdep class.
    
    https://jira.sw.ru/browse/HCI-223
    https://pmc.acronis.com/browse/VSTOR-18951
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -1640,7 +1640,8 @@ static inline void skb_queue_head_init_class(struct sk_buff_head *list,
 		struct lock_class_key *class)
 {
 	skb_queue_head_init(list);
-	lockdep_set_class(&list->lock, class);
+	if (class)
+		lockdep_set_class(&list->lock, class);
 }
 
 /*
