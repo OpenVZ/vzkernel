b111826d074c595f75d91fcd4d3ce8f4bcb51e4c
Pavel Butsykin <pbutsykin@virtuozzo.com>
2019-05-21 18:51:25 +0300

    fs/fuse kio: implement cs statistics accounting
    This is preparation patch that adds the implementation of calculating statistics
    for cs objects. Backport from user-mod vstorage client.
    
    Signed-off-by: Pavel Butsykin <pbutsykin@virtuozzo.com>
    
    =====================
    Patchset description:
    
    Statistics for vstorage kernel fast-path
    
    Vstorage provides plenty of statistics information via 'vstorage -c cl mnt-top',
    but when it comes kernel fast-path, it doesn't work. All because mnt-top
    command collects information from .vstorage.info directory, where vstorage-mount
    provides a lot of different statistical information in the form of files, but
    it was not implemented for for fast-path.
    
    This patch-set is aimed to implementation of the support of some statistics
    inforamtion files from .vstorage.info:
      cs_stats
      fstat
      fstat_lat
      iostat
      requests
    Which will be located at "/sys/fs/fuse/connections/*mnt_id*/kio_stat/". This
    will be enough to maintain vstorage mnt-top command for fast-path mount points.
    
    https://pmc.acronis.com/browse/VSTOR-20979
    
    Pavel Butsykin (15):
      fs/fuse: add conn_ctl to fuse_conn struct
      fs/fuse kio: create sysfs stat directory
      fs/fuse kio: implement iostat
      fs/fuse kio: make common interface pcs_kio_file_list() for listing kio files
      fs/fuse kio: make common interface pcs_kio_req_list() for listing kio reqs
      fs/fuse kio: add retry counter for kio requests
      fs/fuse kio: implement pcs_strerror()
      fs/fuse kio: implement requests statistics
      fs/fuse kio: implement fstat statistics info
      fs/fuse kio: implement fstat_lat statistics info
      fs/fuse kio: remove useless pcs_cs initialization
      fs/fuse kio: implement cs statistics accounting
      fs/fuse kio: convert rpc state id to string
      fs/fuse kio: implement cs_stats statistics info
      fs/fuse kio: add locked cs_get_avg_in_flight()

--- fs/fuse/kio/pcs/pcs_cs.h
+++ fs/fuse/kio/pcs/pcs_cs.h
@@ -96,6 +96,7 @@ struct pcs_cs {
 		struct pcs_perf_rate_cnt read_ops_rate;
 		struct pcs_perf_rate_cnt write_ops_rate;
 		struct pcs_perf_rate_cnt sync_ops_rate;
+		spinlock_t lock;
 	} stat;
 };
 
@@ -161,14 +162,13 @@ void pcs_cs_update_stat(struct pcs_cs *cs, u32 iolat, u32 netlat, int op_type);
 
 static inline void pcs_cs_stat_up(struct pcs_cs *cs)
 {
-#if 0
-	/* TODO: temproraly disable perf counters */
+	spin_lock(&cs->stat.lock);
 	pcs_perfcounter_stat_up(&cs->stat.iolat);
 	pcs_perfcounter_stat_up(&cs->stat.netlat);
 	pcs_perfcounter_up_rate(&cs->stat.write_ops_rate);
 	pcs_perfcounter_up_rate(&cs->stat.read_ops_rate);
 	pcs_perfcounter_up_rate(&cs->stat.sync_ops_rate);
-#endif
+	spin_unlock(&cs->stat.lock);
 }
 
 static inline bool cs_is_blacklisted(struct pcs_cs *cs)
