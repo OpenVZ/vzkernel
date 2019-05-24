ac5623103d8d2a4a304fec1cf6b5d190a8c052a2
Konstantin Khorenko <khorenko@virtuozzo.com>
2019-05-24 11:22:04 +0300

    ve/time: introduce CT feature to allow setting date/time
    Sometimes people would like to run ntp server inside trusted Containers,
    so let's introduce an appropriate CT feature for that.
    
    Note: time is NOT vistualized, so Container changes date/time of the
    whole Node.
    
    https://jira.sw.ru/browse/PSBM-94635
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- include/uapi/linux/vzcalluser.h
+++ include/uapi/linux/vzcalluser.h
@@ -122,6 +122,7 @@ struct env_create_param3 {
 #define VE_FEATURE_IPGRE	(1ULL << 6)	/* deprecated */
 #define VE_FEATURE_BRIDGE	(1ULL << 7)
 #define VE_FEATURE_NFSD		(1ULL << 8)
+#define VE_FEATURE_TIME		(1ULL << 9)
 
 #define VE_FEATURES_OLD		(VE_FEATURE_SYSFS)
 #define VE_FEATURES_DEF		(VE_FEATURE_SYSFS | VE_FEATURE_DEF_PERMS)
