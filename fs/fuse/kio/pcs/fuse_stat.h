67e832fab15327ea6caec3f75d3f8bc0a8b73875
Pavel Butsykin <pbutsykin@virtuozzo.com>
2019-05-29 17:24:41 +0300

    fs/fuse kio: implement iostat statistics info
    This statistical information contains general information about all
    kio requests for the last STAT_TIMER_PERIOD time. Made by analogy
    with /.vstorage.info/iostat statistics of user-mod client.
    
    example:
    read         109      445645      26415104         4096    4096    4096
    write        0        0           0                0       0       0
    sync         0                    0
    
    Signed-off-by: Pavel Butsykin <pbutsykin@virtuozzo.com>
    
    =====================
    Patchset description:
    
    Statistics for vstorage kernel fast-path
    
    Vstorage provides plenty of statistics information via 'vstorage -c cl mnt-top',
    but when it comes kernel fast-path, it doesn't work. All because mnt-top
    command collects information from .vstorage.info directory, where vstorage-mount
    provides a lot of different statistical information in the form of files, but
    it was not implemented for for fast-path.
    
    This patch-set is aimed to implementation of the support of some statistics
    inforamtion files from .vstorage.info:
      cs_stats
      fstat
      fstat_lat
      iostat
      requests
    Which will be located at "/sys/fs/fuse/connections/*mnt_id*/kio_stat/". This
    will be enough to maintain vstorage mnt-top command for fast-path mount points.
    
    https://pmc.acronis.com/browse/VSTOR-20979
    
    Acked-by: Alexey Kuznetsov <kuznet@virtuozzo.com>
    
    Pavel Butsykin (14):
      fs/fuse: add conn_ctl to fuse_conn struct
      fs/fuse kio: create sysfs stat directory
      fs/fuse kio: implement iostat statistics info
      fs/fuse kio: make interface pcs_kio_file_list() for listing kio files
      fs/fuse kio: make interface pcs_kio_req_list() for listing kio reqs
      fs/fuse kio: add retry counter for kio requests
      fs/fuse kio: implement pcs_strerror()
      fs/fuse kio: implement requests statistics info
      fs/fuse kio: implement fstat statistics info
      fs/fuse kio: implement fstat_lat statistics info
      fs/fuse kio: remove useless pcs_cs initialization
      fs/fuse kio: implement cs statistics accounting
      fs/fuse kio: convert rpc state id to string
      fs/fuse kio: implement cs_stats statistics info

--- fs/fuse/kio/pcs/fuse_stat.h
+++ fs/fuse/kio/pcs/fuse_stat.h
@@ -1,11 +1,46 @@
 #ifndef _FUSE_STAT_H_
 #define _FUSE_STAT_H_ 1
 
+#define STAT_TIMER_PERIOD 5
+
+struct pcs_msg;
+struct pcs_int_request;
+
+struct fuse_val_stat {
+	u64 events;
+	u64 val_total;
+	u64 val_min;
+	u64 val_max;
+};
+
+struct pcs_fuse_io_stat {
+	struct fuse_val_stat read_bytes;
+	struct fuse_val_stat write_bytes;
+	struct fuse_val_stat flush_cnt;
+} ____cacheline_aligned;
+
+struct pcs_fuse_io_stat_sync {
+	struct pcs_fuse_io_stat glob;
+	struct pcs_fuse_io_stat __percpu *period[2];
+	atomic_t idx;
+	seqlock_t seqlock;
+	spinlock_t lock;
+};
+
 struct pcs_fuse_stat {
+	struct pcs_fuse_io_stat_sync io;
+	struct delayed_work work;
+
 	struct dentry *kio_stat;
+	struct dentry *iostat;
 };
 
 void pcs_fuse_stat_init(struct pcs_fuse_stat *stat);
 void pcs_fuse_stat_fini(struct pcs_fuse_stat *stat);
+
+void pcs_fuse_stat_io_count(struct pcs_int_request *ireq, struct pcs_msg *resp);
+
+int pcs_fuse_io_stat_alloc(struct pcs_fuse_io_stat_sync *iostat);
+void pcs_fuse_io_stat_free(struct pcs_fuse_io_stat_sync *iostat);
 
 #endif /* _FUSE_STAT_H_ */
