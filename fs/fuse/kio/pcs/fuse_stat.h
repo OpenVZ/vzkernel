f51ad60b29ed3e9f8d433967ecbd2ea8472aab7a
Pavel Butsykin <pbutsykin@virtuozzo.com>
2019-05-29 17:24:50 +0300

    fs/fuse kio: implement fstat_lat statistics info
    This statistic shows latency of requests for each open kio file. Latency is
    displayed for 99 and 95 percentile in the following time ranges: 20ms, 50ms,
    100ms, 200ms, 500ms. In turn, the request latency is detailed in three parts:
    pending_lat - time spent in the queue
    net_lat     - time spent in the network
    io_lat      - disk operation time
    
    example:
    # rw open/age inactive  net_lat                  net_lat_global           io_lat                   io_lat_global            plat                     plat_global              path
    rw      2/448    0      (95% <= 20, 99% <= 20)   (95% <= 20, 99% <= 20)   (95% <= 50, 99% <= 100)  (95% <= 20, 99% <= 50)   (95% <= 20, 99% <= 20)   (95% <= 20, 99% <= 20)   /fio_test/randasyncread_jobs24_4k.1.0
    rw      2/448    0      (95% <= 20, 99% <= 20)   (95% <= 20, 99% <= 20)   (95% <= 50, 99% <= 50)   (95% <= 20, 99% <= 50)   (95% <= 20, 99% <= 20)   (95% <= 20, 99% <= 20)   /fio_test/randasyncread_jobs24_4k.0.0
    
    Signed-off-by: Pavel Butsykin <pbutsykin@virtuozzo.com>
    
    =====================
    Patchset description:
    
    Statistics for vstorage kernel fast-path
    
    Vstorage provides plenty of statistics information via 'vstorage -c cl mnt-top',
    but when it comes kernel fast-path, it doesn't work. All because mnt-top
    command collects information from .vstorage.info directory, where vstorage-mount
    provides a lot of different statistical information in the form of files, but
    it was not implemented for for fast-path.
    
    This patch-set is aimed to implementation of the support of some statistics
    inforamtion files from .vstorage.info:
      cs_stats
      fstat
      fstat_lat
      iostat
      requests
    Which will be located at "/sys/fs/fuse/connections/*mnt_id*/kio_stat/". This
    will be enough to maintain vstorage mnt-top command for fast-path mount points.
    
    https://pmc.acronis.com/browse/VSTOR-20979
    
    Acked-by: Alexey Kuznetsov <kuznet@virtuozzo.com>
    
    Pavel Butsykin (14):
      fs/fuse: add conn_ctl to fuse_conn struct
      fs/fuse kio: create sysfs stat directory
      fs/fuse kio: implement iostat statistics info
      fs/fuse kio: make interface pcs_kio_file_list() for listing kio files
      fs/fuse kio: make interface pcs_kio_req_list() for listing kio reqs
      fs/fuse kio: add retry counter for kio requests
      fs/fuse kio: implement pcs_strerror()
      fs/fuse kio: implement requests statistics info
      fs/fuse kio: implement fstat statistics info
      fs/fuse kio: implement fstat_lat statistics info
      fs/fuse kio: remove useless pcs_cs initialization
      fs/fuse kio: implement cs statistics accounting
      fs/fuse kio: convert rpc state id to string
      fs/fuse kio: implement cs_stats statistics info

--- fs/fuse/kio/pcs/fuse_stat.h
+++ fs/fuse/kio/pcs/fuse_stat.h
@@ -35,19 +35,55 @@ struct pcs_fuse_stat {
 	struct dentry *iostat;
 	struct dentry *requests;
 	struct dentry *fstat;
+	struct dentry *fstat_lat;
+};
+
+enum {
+	LAT_ORDER1 = 0,
+	LAT_ORDER2 = 1,
+	LAT_ORDER3 = 2,
+	LAT_ORDER4 = 3,
+	LAT_ORDER5 = 4,
+	LAT_ORDER_OTHER = 5,
+};
+#define LATENCY_ORDER_MAX (LAT_ORDER_OTHER + 1)
+
+struct fuse_lat_stat {
+	u64 lat[LATENCY_ORDER_MAX];
+	u64 count;
+};
+
+struct pcs_fuse_io_lat {
+	struct fuse_lat_stat io_lat;
+	struct fuse_lat_stat net_lat;
+	struct fuse_lat_stat pending_lat;
+} ____cacheline_aligned;
+
+struct pcs_fuse_io_lat_sync {
+	struct pcs_fuse_io_lat glob;
+	struct pcs_fuse_io_lat __percpu *period[2];
+	atomic_t idx;
+	seqlock_t seqlock;
+	spinlock_t lock;
 };
 
 struct fuse_io_cnt {
 	struct pcs_fuse_io_stat_sync io;
+	struct pcs_fuse_io_lat_sync lat;
+
 	abs_time_t created_ts;
 };
 
 void pcs_fuse_stat_init(struct pcs_fuse_stat *stat);
 void pcs_fuse_stat_fini(struct pcs_fuse_stat *stat);
 
-void pcs_fuse_stat_io_count(struct pcs_int_request *ireq, struct pcs_msg *resp);
+void pcs_fuse_stat_io_count(struct pcs_int_request *ireq, struct pcs_msg *resp,
+			    u32 io_lat, u32 net_lat);
 
 int pcs_fuse_io_stat_alloc(struct pcs_fuse_io_stat_sync *iostat);
 void pcs_fuse_io_stat_free(struct pcs_fuse_io_stat_sync *iostat);
+
+int pcs_fuse_fstat_alloc(struct pcs_fuse_io_lat_sync *lat);
+void pcs_fuse_fstat_free(struct pcs_fuse_io_lat_sync *lat);
 
 #endif /* _FUSE_STAT_H_ */
