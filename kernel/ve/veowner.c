87ee455d41b368e5d7c639a434ee85524ba719de
Konstantin Khorenko <khorenko@virtuozzo.com>
2019-06-10 15:19:40 +0300

    kernel/ve/veowner.c: copyright update
    https://jira.sw.ru/browse/PSBM-94979
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    (cherry picked from commit 1e6b98c1ece20daea21d67de9b79b87978b0b008)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- kernel/ve/veowner.c
+++ kernel/ve/veowner.c
@@ -1,7 +1,9 @@
 /*
  *  kernel/ve/veowner.c
  *
- *  Copyright (c) 2000-2017 Virtuozzo International GmbH.  All rights reserved.
+ *  Copyright (c) 2000-2008 SWsoft
+ *  Copyright (c) 2009-2015 Parallels IP Holdings GmbH
+ *  Copyright (c) 2017-2021 Virtuozzo International GmbH. All rights reserved.
  *
  */
 
