cd9edd03285b1ac96f8c0392f988839e7b4bc178
Konstantin Khorenko <khorenko@virtuozzo.com>
2019-06-10 15:19:45 +0300

    kernel/ve/vznetstat/ip6_vznetstat.c: copyright update
    https://jira.sw.ru/browse/PSBM-94979
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- kernel/ve/vznetstat/ip6_vznetstat.c
+++ kernel/ve/vznetstat/ip6_vznetstat.c
@@ -2,6 +2,7 @@
  *  kernel/ve/vznetstat/ip6_vznetstat.c
  *
  *  Copyright (c) 2010-2015 Parallels IP Holdings GmbH
+ *  Copyright (c) 2017-2019 Virtuozzo International GmbH. All rights reserved.
  *
  */
 
