04baafaec85766e503b78d0345491e2f78529760
Konstantin Khorenko <khorenko@virtuozzo.com>
2019-06-10 15:18:52 +0300

    drivers/block/ploop/ploop_events.h: copyright update
    https://jira.sw.ru/browse/PSBM-94979
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- drivers/block/ploop/ploop_events.h
+++ drivers/block/ploop/ploop_events.h
@@ -2,6 +2,7 @@
  *  drivers/block/ploop/ploop_events.h
  *
  *  Copyright (c) 2010-2015 Parallels IP Holdings GmbH
+ *  Copyright (c) 2017-2019 Virtuozzo International GmbH. All rights reserved.
  *
  */
 
