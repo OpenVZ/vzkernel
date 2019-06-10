c933069c091a8dd7affa2d08f87b3a908ef77494
Konstantin Khorenko <khorenko@virtuozzo.com>
2019-06-10 15:19:53 +0300

    net/ipv4/netfilter/ip_vzprivnet.c: copyright update
    https://jira.sw.ru/browse/PSBM-94979
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- net/ipv4/netfilter/ip_vzprivnet.c
+++ net/ipv4/netfilter/ip_vzprivnet.c
@@ -2,6 +2,7 @@
  *  net/ipv4/netfilter/ip_vzprivnet.c
  *
  *  Copyright (c) 2010-2015 Parallels IP Holdings GmbH
+ *  Copyright (c) 2017-2019 Virtuozzo International GmbH. All rights reserved.
  *
  */
 
