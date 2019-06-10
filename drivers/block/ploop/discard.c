00eadbae8bc6d19c82cce7fd588d29fb8309d6d2
Konstantin Khorenko <khorenko@virtuozzo.com>
2019-06-10 15:18:41 +0300

    drivers/block/ploop/discard.c: copyright update
    https://jira.sw.ru/browse/PSBM-94979
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- drivers/block/ploop/discard.c
+++ drivers/block/ploop/discard.c
@@ -2,6 +2,7 @@
  *  drivers/block/ploop/discard.c
  *
  *  Copyright (c) 2010-2015 Parallels IP Holdings GmbH
+ *  Copyright (c) 2017-2019 Virtuozzo International GmbH. All rights reserved.
  *
  */
 
