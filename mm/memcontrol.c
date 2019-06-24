a2833b53fdca1e5d265b5bb633d30b4091a370ec
Andrey Ryabinin <aryabinin@virtuozzo.com>
2019-06-24 18:26:02 +0300

    mm/memcontrol: warn if memcg reparenting failed to succeed in 20 min
    If mem_cgroup_reparent_charges() does not succeed in 20 minutes,
    - stop trying (and release cgroup_mutex by the way)
    - print a warning
    
    https://jira.sw.ru/browse/PSBM-96003
    https://pmc.acronis.com/browse/VSTOR-24241
    https://jira.sw.ru/browse/PSBM-96533
    https://jira.sw.ru/browse/PSBM-96889
    https://pmc.acronis.com/browse/VSTOR-25631
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    This patch is a merge of following patches:
    
    +++
    mm/memcontrol: don't reparent forever if counter leaked.
    
    In case of leaked memcg->memory counter mem_cgroup_reparent_charges()
    may stuck forever under global cgroup_mutex making the whole
    system unusable. I don't see reson for 'while ((memory - kmem) > 0)'
    loop, IMO memory should be equal to kmem after the first iteration.
    And if not it's a leak, but just in case make several attempts.
    
    +++
    mm/memcontrol: warn about leaking memory on reparenting after 100 attempts
    
    We know that 1 iteration could be not enough for complete reparenting of
    memcg, so warn in case we don't succeed in 100 iterations.
    If we ever see this warning, we better get rid of reparenting at all as
    it is planned in https://jira.sw.ru/browse/PSBM-94117.
    
    +++
    mm/memcg: Fix reparent warning again.
    
    In case of unsuccessful reparenting reparent_attempts counter
    will be -1, so intended warning will not trigger. Check diff
    of ->memory - ->kmem counters instead. It's correct and easier
    to read.
    
    +++
    mm/memcg: use timeout instead of iteration count to determine leak.
    
    100 reparent attempts seems too low, and hits too often.
    Try to use 2min timeout instead.
    
    +++
    mm/memcg: increase reparent timeout
    
    Make it 20 min.
    
    +++
    mm/memcg: correct timeout check.
    
    We should use time_before() instead of time_after(), since we want
    true value to repeat the loop before timeout reaches.

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4370,6 +4370,9 @@ static void mem_cgroup_force_empty_list(struct mem_cgroup *memcg,
 static void mem_cgroup_reparent_charges(struct mem_cgroup *memcg)
 {
 	int node, zid;
+	/* Protection from leaked memcg->memory counter. */
+	unsigned long start_time = jiffies;
+	unsigned long timeout = start_time + HZ*1200;
 
 	do {
 		/* This is for making all *used* pages to be on LRU. */
@@ -4401,8 +4404,17 @@ static void mem_cgroup_reparent_charges(struct mem_cgroup *memcg)
 		 * right after the check. RES_USAGE should be safe as we always
 		 * charge before adding to the LRU.
 		 */
-	} while (page_counter_read(&memcg->memory) -
-		 page_counter_read(&memcg->kmem) > 0);
+	} while ((page_counter_read(&memcg->memory) -
+		 page_counter_read(&memcg->kmem) > 0) && time_before(jiffies, timeout));
+
+	WARN_ONCE((page_counter_read(&memcg->memory) -
+			page_counter_read(&memcg->kmem) > 0),
+		  "memcg 0x%p leak suspected: "
+		  "memory=%lu, kmem=%lu start_time=%lx timeout=%lx jiffies=%lx",
+		  memcg,
+		  page_counter_read(&memcg->memory),
+		  page_counter_read(&memcg->kmem),
+		  start_time, timeout, jiffies);
 }
 
 /*
