fe2b6768d1b12dd99cf1c6378fe644de0251d596
Konstantin Khorenko <khorenko@virtuozzo.com>
2019-06-27 14:39:10 +0300

    scsi: aacraid: resurrect correct arc ctrl checks for Series-6
    This partially reverts ms commit
    395e5df79a95 ("scsi: aacraid: Remove reference to Series-9")
    
    The patch above not anly drops Series-9 cards checks but also
    changes logic for Series-6 controllers which lead to controller
    hungs/resets under high io load.
    
    So revert original arc ctrl checks for Series-6 controllers.
    
    https://jira.sw.ru/browse/PSBM-95736
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- drivers/scsi/aacraid/linit.c
+++ drivers/scsi/aacraid/linit.c
@@ -1664,7 +1664,9 @@ static void __aac_shutdown(struct aac_dev * aac)
 	aac_adapter_disable_int(aac);
 
 	cpu = cpumask_first(cpu_online_mask);
-	if (aac_is_src(aac)) {
+	if (aac->pdev->device == PMC_DEVICE_S6 ||
+	    aac->pdev->device == PMC_DEVICE_S7 ||
+	    aac->pdev->device == PMC_DEVICE_S8) {
 		if (aac->max_msix > 1) {
 			for (i = 0; i < aac->max_msix; i++) {
 				if (irq_set_affinity_hint(
@@ -1954,7 +1956,8 @@ static int aac_acquire_resources(struct aac_dev *dev)
 	aac_adapter_enable_int(dev);
 
 
-	if (aac_is_src(dev))
+	if (dev->pdev->device == PMC_DEVICE_S7 ||
+	    dev->pdev->device == PMC_DEVICE_S8)
 		aac_define_int_mode(dev);
 
 	if (dev->msi_enabled)
