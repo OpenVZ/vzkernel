adb5f6d0c0384fc92052af45be14bc85d9f69fd5
Kirill Tkhai <ktkhai@virtuozzo.com>
2019-08-06 13:25:11 +0300

    ploop: Check that nobody writes to ZERO_PAGE()
    ploop juggles this aux_bio, reuses it from time to time,
    so it's easy to make a mistake and to reuse a zero page.
    Say, we use zero page in ploop_entry_nullify_req() after
    fill_zero_bio(), and later PLOOP_E_RELOC_NULLIFY state
    may become PLOOP_E_ENTRY, which is just generic state
    of all requests.
    
    For beter debug and detection of such the cases, add
    the debug check to be safe against the above mistakes.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    +++
    ploop: Fix the check that nobody writes to ZERO_PAGE()
    
    The check must verify the destination page, not the source one.
    
    mFixes: 3a44b99ce15e ("ploop: Check that nobody writes to ZERO_PAGE()")
    https://jira.sw.ru/browse/PSBM-97043
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    --
    v2: move the check in bcopy_from_blist() out of the cycle

--- drivers/block/ploop/io_direct.c
+++ drivers/block/ploop/io_direct.c
@@ -344,6 +344,8 @@ static void bcopy_from_blist(struct page *page, int dst_off, /* dst */
 {
 	u8 *kdst = kmap_atomic(page);
 
+	WARN_ON_ONCE(page == ZERO_PAGE(0));
+
 	while (copy_len > 0) {
 		u8 *ksrc;
 		int copy = MIN(copy_len, biter->bv->bv_len - biter->off);
