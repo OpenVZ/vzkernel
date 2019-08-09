4411da45c729178b35d7a3ab0d07afb7f9b2d238
Konstantin Khorenko <khorenko@virtuozzo.com>
2019-08-09 17:48:06 +0300

    Revert "Revert "rh/tcp: tcp_fragment() should apply sane memory limits""
    This reverts commit 1da9fefca8fdeb173c81b6e7b8c3ce9f6f67dec9.
    
    So let's try to return back to RedHat version of the fix for
      CVE-2019-11478: Kernel: tcp: excessive resource consumption while
      processing SACK blocks allows remote denial of service
    
    https://jira.sw.ru/browse/PSBM-95578
    https://jira.sw.ru/browse/PSBM-95669
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -1128,6 +1128,12 @@ int tcp_fragment(struct sock *sk, struct sk_buff *skb, u32 len,
 	if (nsize < 0)
 		nsize = 0;
 
+	if (unlikely((sk->sk_wmem_queued >> 1) > sk->sk_sndbuf) ||
+	    skb_queue_len(&sk->sk_write_queue) > 2048) {
+		NET_INC_STATS(sock_net(sk), LINUX_MIB_TCPWQUEUETOOBIG);
+		return -ENOMEM;
+	}
+
 	if (skb_unclone(skb, GFP_ATOMIC))
 		return -ENOMEM;
 
