3ee0a075e6870334e5026f939888cc413177d638
Peter Zijlstra <peterz@infradead.org>
2019-10-03 18:38:09 +0300

    ms/x86/cpu: Sanitize FAM6_ATOM naming
    Going primarily by:
    
      https://en.wikipedia.org/wiki/List_of_Intel_Atom_microprocessors
    
    with additional information gleaned from other related pages; notably:
    
     - Bonnell shrink was called Saltwell
     - Moorefield is the Merriefield refresh which makes it Airmont
    
    The general naming scheme is: FAM6_ATOM_UARCH_SOCTYPE
    
      for i in `git grep -l FAM6_ATOM` ; do
    	sed -i  -e 's/ATOM_PINEVIEW/ATOM_BONNELL/g'		\
    		-e 's/ATOM_LINCROFT/ATOM_BONNELL_MID/'		\
    		-e 's/ATOM_PENWELL/ATOM_SALTWELL_MID/g'		\
    		-e 's/ATOM_CLOVERVIEW/ATOM_SALTWELL_TABLET/g'	\
    		-e 's/ATOM_CEDARVIEW/ATOM_SALTWELL/g'		\
    		-e 's/ATOM_SILVERMONT1/ATOM_SILVERMONT/g'	\
    		-e 's/ATOM_SILVERMONT2/ATOM_SILVERMONT_X/g'	\
    		-e 's/ATOM_MERRIFIELD/ATOM_SILVERMONT_MID/g'	\
    		-e 's/ATOM_MOOREFIELD/ATOM_AIRMONT_MID/g'	\
    		-e 's/ATOM_DENVERTON/ATOM_GOLDMONT_X/g'		\
    		-e 's/ATOM_GEMINI_LAKE/ATOM_GOLDMONT_PLUS/g' ${i}
      done
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Arnaldo Carvalho de Melo <acme@redhat.com>
    Cc: Jiri Olsa <jolsa@redhat.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Vince Weaver <vincent.weaver@maine.edu>
    Cc: dave.hansen@linux.intel.com
    Cc: len.brown@intel.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    
    (cherry picked from commit f2c4db1bd80720cd8cb2a5aa220d9bc9f374f04e)
    Most of this patch was applied on recent RedHat update:
    
      0dfda84 "rh7: import RHEL7 kernel-3.10.0-957.21.3.el7"
    
    Signed-off-by: Jan Dakinevich <jan.dakinevich@virtuozzo.com>

--- arch/x86/events/intel/core.c
+++ arch/x86/events/intel/core.c
@@ -4447,9 +4447,9 @@ __init int intel_pmu_init(void)
 
 	case INTEL_FAM6_ATOM_BONNELL:
 	case INTEL_FAM6_ATOM_BONNELL_MID:
+	case INTEL_FAM6_ATOM_SALTWELL:
 	case INTEL_FAM6_ATOM_SALTWELL_MID:
 	case INTEL_FAM6_ATOM_SALTWELL_TABLET:
-	case INTEL_FAM6_ATOM_SALTWELL:
 		memcpy(hw_cache_event_ids, atom_hw_cache_event_ids,
 		       sizeof(hw_cache_event_ids));
 
@@ -4464,7 +4464,9 @@ __init int intel_pmu_init(void)
 
 	case INTEL_FAM6_ATOM_SILVERMONT:
 	case INTEL_FAM6_ATOM_SILVERMONT_X:
+	case INTEL_FAM6_ATOM_SILVERMONT_MID:
 	case INTEL_FAM6_ATOM_AIRMONT:
+	case INTEL_FAM6_ATOM_AIRMONT_MID:
 		memcpy(hw_cache_event_ids, slm_hw_cache_event_ids,
 			sizeof(hw_cache_event_ids));
 		memcpy(hw_cache_extra_regs, slm_hw_cache_extra_regs,
