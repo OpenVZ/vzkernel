796130a1f77adeab59350ead7b2e08cb77d0194e
Vasily Averin <vvs@virtuozzo.com>
2019-10-18 15:28:23 +0300

    ms/netfilter: nf_tables: avoid global info storage
    ML commit 2a43ecf96ba6a6eed70dbcd99d0888fc0ad3b82b
    Author: Florian Westphal <fw@strlen.de>
    Date:   Wed Jul 11 13:45:13 2018 +0200
    
        netfilter: nf_tables: avoid global info storage
    
    This works because all accesses are currently serialized by nfnl
    nf_tables subsys mutex.
    
    If we want to have per-netns locking, we need to make this scratch
    area pernetns or allocate it on demand.
    
    This does the latter, its ~28kbyte but we can fallback to vmalloc
    so it should be fine.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>
    
    VvS: backported to vz7.114.x kernels to fix high-order-allocation warning
    https://jira.sw.ru/browse/PSBM-98682
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    [VvS RHEL7.8 rebase] context changes

--- net/netfilter/nf_tables_api.c
+++ net/netfilter/nf_tables_api.c
@@ -2028,14 +2028,13 @@ static void nf_tables_rule_destroy(const struct nft_ctx *ctx,
 
 #define NFT_RULE_MAXEXPRS	128
 
-static struct nft_expr_info *info;
-
 static int nf_tables_newrule(struct net *net, struct sock *nlsk,
 			     struct sk_buff *skb, const struct nlmsghdr *nlh,
 			     const struct nlattr * const nla[])
 {
 	const struct nfgenmsg *nfmsg = nlmsg_data(nlh);
 	struct nft_af_info *afi;
+	struct nft_expr_info *info = NULL;
 	struct nft_table *table;
 	struct nft_chain *chain;
 	struct nft_rule *rule, *old_rule = NULL;
@@ -2096,6 +2095,12 @@ static int nf_tables_newrule(struct net *net, struct sock *nlsk,
 	n = 0;
 	size = 0;
 	if (nla[NFTA_RULE_EXPRESSIONS]) {
+		info = kvmalloc_array(NFT_RULE_MAXEXPRS,
+				      sizeof(struct nft_expr_info),
+				      GFP_KERNEL);
+		if (!info)
+			return -ENOMEM;
+
 		nla_for_each_nested(tmp, nla[NFTA_RULE_EXPRESSIONS], rem) {
 			err = -EINVAL;
 			if (nla_type(tmp) != NFTA_LIST_ELEM)
@@ -2177,6 +2182,7 @@ static int nf_tables_newrule(struct net *net, struct sock *nlsk,
 		err = -ENOMEM;
 		goto err3;
 	}
+	kvfree(info);
 	chain->use++;
 	return 0;
 
@@ -2189,6 +2195,7 @@ err1:
 		if (info[i].ops != NULL)
 			module_put(info[i].ops->type->owner);
 	}
+	kvfree(info);
 	return err;
 }
 
@@ -4756,20 +4763,13 @@ static int __init nf_tables_module_init(void)
 {
 	int err;
 
-	info = kmalloc(sizeof(struct nft_expr_info) * NFT_RULE_MAXEXPRS,
-		       GFP_KERNEL);
-	if (info == NULL) {
-		err = -ENOMEM;
-		goto err1;
-	}
-
 	err = nf_tables_core_module_init();
 	if (err < 0)
-		goto err2;
+		return err;
 
 	err = register_pernet_subsys(&nf_tables_net_ops);
 	if (err < 0)
-		goto err3;
+		goto err;
 
 	/* must be last */
 	err = nfnetlink_subsys_register(&nf_tables_subsys);
@@ -4779,11 +4779,8 @@ static int __init nf_tables_module_init(void)
 	return err;
 err4:
 	unregister_pernet_subsys(&nf_tables_net_ops);
-err3:
+err:
 	nf_tables_core_module_exit();
-err2:
-	kfree(info);
-err1:
 	return err;
 }
 
@@ -4793,7 +4790,6 @@ static void __exit nf_tables_module_exit(void)
 	unregister_pernet_subsys(&nf_tables_net_ops);
 	rcu_barrier();
 	nf_tables_core_module_exit();
-	kfree(info);
 }
 
 module_init(nf_tables_module_init);
