2eef305cef2724609fe974e2c97401f4656f50e4
Ildar Ismagilov <ildar.ismagilov@virtuozzo.com>
2019-10-21 14:12:55 +0300

    fs/fuse kio: don't wait read requests in case of fsync/flush
    In this patch, the KIO requests are divided into two types: read and write.
    And in case of fsync/flush we only wait for completion write requests.
    
    https://pmc.acronis.com/browse/VSTOR-11372
    
    Signed-off-by: Ildar Ismagilov <ildar.ismagilov@virtuozzo.com>
    Acked-by: Alexey Kuznetsov <kuznet@acronis.com>

--- fs/fuse/dir.c
+++ fs/fuse/dir.c
@@ -1638,7 +1638,7 @@ void fuse_set_nowrite(struct inode *inode)
 	BUG_ON(fi->writectr < 0);
 	fi->writectr += FUSE_NOWRITE;
 	spin_unlock(&fi->lock);
-	inode_dio_wait(inode);
+	fuse_write_dio_wait(fi);
 	wait_event(fi->page_waitq, fi->writectr == FUSE_NOWRITE);
 }
 
@@ -1778,6 +1778,7 @@ int fuse_do_setattr(struct inode *inode, struct iattr *attr,
 
 	if (is_truncate) {
 		fuse_set_nowrite(inode);
+		fuse_read_dio_wait(fi);
 		set_bit(FUSE_I_SIZE_UNSTABLE, &fi->state);
 		if (trust_local_cmtime && attr->ia_size != inode->i_size)
 			attr->ia_valid |= ATTR_MTIME | ATTR_CTIME;
