4f5a541bfeb7dc42b8a9526389a39f8f266ff6c1
Laura Abbott <labbott@redhat.com>
2019-10-22 15:59:33 -0400

    [redhat] Update CI to bring changelog back to internal
    Because of the way ARK flows, the changelog only gets committed
    to the master branch. Since master gets overwritten each rebase,
    we lose the history we need. Automate bringing the information
    for the changelog back to internal.
    
    Signed-off-by: Laura Abbott <labbott@redhat.com>

---
+++ redhat/update-internal.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+GIT_DEPTH=$1
+
+set -x
+
+git fetch --depth=$GIT_DEPTH origin
+git checkout -b new_clog_$RANDOM origin/internal
+git checkout origin/master -- redhat/kernel.changelog-8.99
+git add redhat/kernel.changelog-8.99
+git checkout origin/master -- redhat/marker
+git add redhat/marker
+git config user.name "CKI@GitLab"
+git config user.email "cki-project@redhat.com"
+
+# Did anything change?
+LINES_CHANGED=$(git diff --cached | wc -l)
+if [ "${LINES_CHANGED}" != "0" ]; then
+    git commit -m "Updated changelog"
+    git remote add gitlab git@gitlab.com:cki-project/kernel-ark.git
+    ssh-keyscan -H gitlab.com >> ~/.ssh/known_hosts
+    git push -o merge_request.create \
+                -o merge_request.target=internal \
+                -o merge_request.title="Changelog Update" \
+                -o merge_request.remove_source_branch \
+                gitlab
+fi
