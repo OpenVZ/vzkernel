91d8c3e8a140fd53ae9b0a3840a8cb8a663bd10c
Konstantin Khorenko <khorenko@virtuozzo.com>
2019-10-22 16:24:41 +0300

    Revert "fs: use original vfsmount for touch_atime"
    This reverts commit c03ccabe433a3cf811477358975e9d0a4bd0c144.
    
    The patch being reverted had been introduced in the scope of
    https://jira.sw.ru/browse/PSBM-51009
    
    Now it's reverted in the scope of
    https://jira.sw.ru/browse/PSBM-78863
    
    because Red Hat had backported 4bacc9c9234c ("overlayfs: Make f_path always
    point to the overlay and f_inode to the underlay") long ago and now
    xfstests should work fine on overlayfs (atime part).
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1059,7 +1059,6 @@ struct file {
 		struct rcu_head 	fu_rcuhead;
 	} f_u;
 	struct path		f_path;
-	struct path		f_original_path;
 #define f_dentry	f_path.dentry
 	struct inode		*f_inode;	/* cached value */
 	const struct file_operations	*f_op;
@@ -2345,8 +2344,7 @@ extern void touch_atime(struct path *);
 static inline void file_accessed(struct file *file)
 {
 	if (!(file->f_flags & O_NOATIME))
-		touch_atime(file->f_original_path.mnt ?
-			    &file->f_original_path : &file->f_path);
+		touch_atime(&file->f_path);
 }
 
 int sync_inode(struct inode *inode, struct writeback_control *wbc);
