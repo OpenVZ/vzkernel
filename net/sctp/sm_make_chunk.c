6ed1d09ce2012d3cf49b70bd565abcf00fe92fe3
Konstantin Khorenko <khorenko@virtuozzo.com>
2019-10-31 12:50:31 +0300

    sctp: suppress warnings on high order memory allocation on sctp chunk creation
    _sctp_make_chunk() could allocate skbs up to 65K in size
    (4th order page), so we get complains about high order page
    allocations from our detection mechanizm while running sctp
    testsuite.
    
    It's not evident that substituting alloc_skb() with
    alloc_skb_with_frags() speeds up the sender performance.
    On another hand if the node's memory is highly fragmented
    and allocation of high order pages has noticable latency,
    it influences the sender most, not the node as a whole.
    
    And taking into account that sctp is not widely used,
    let's just shut up the warning in this place.
    
    https://jira.sw.ru/browse/PSBM-93798
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- net/sctp/sm_make_chunk.c
+++ net/sctp/sm_make_chunk.c
@@ -1412,7 +1412,7 @@ static struct sctp_chunk *_sctp_make_chunk(const struct sctp_association *asoc,
 		goto nodata;
 
 	/* No need to allocate LL here, as this is only a chunk. */
-	skb = alloc_skb(chunklen, gfp);
+	skb = alloc_skb(chunklen, gfp | __GFP_ORDER_NOWARN);
 	if (!skb)
 		goto nodata;
 
