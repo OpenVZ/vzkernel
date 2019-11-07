3ee2c0236369fc4bfb4bda127127809e0b1a8ed2
Sergey Lysanov <slysanov@virtuozzo.com>
2019-11-07 16:49:33 +0300

    fs/fuse kio: share bandwith/IOPS for prometheus stats
    Pass the following counters from KIO to prometheus through sysfs:
     - reads_total
     - read_bytes_total
     - writes_total
     - write_bytes_total
     - flushes_total
    
    The compatability with previous version of prometheus proto was saved -
    size of histograms wasn't changed.
    
    https://pmc.acronis.com/browse/VSTOR-20601
    
    Signed-off-by: Sergey Lysanov <slysanov@virtuozzo.com>
    Reviewed-by: Ildar Ismagilov <ildar.ismagilov@virtuozzo.com>

--- fs/fuse/kio/pcs/fuse_prometheus_prot.h
+++ fs/fuse/kio/pcs/fuse_prometheus_prot.h
@@ -8,23 +8,38 @@
 #define KFUSE_OP_CS_WRITE_ZERO	4
 #define KFUSE_OP_CS_FIEMAP	5
 
-#define KFUSE_OP_READ		6
-#define KFUSE_OP_WRITE		7
-#define KFUSE_OP_FSYNC		8
-#define KFUSE_OP_FALLOCATE	9
-#define KFUSE_OP_MAX		10
+#define KFUSE_OP_READ				6
+#define KFUSE_OP_WRITE				7
+#define KFUSE_OP_FSYNC				8
+#define KFUSE_OP_FALLOCATE			9
+#define KFUSE_OP_UNALIGNED_WRITE	10
+#define KFUSE_OP_UNALIGNED_READ		11
+#define KFUSE_OP_MAX				12
 
+/* Histograms contain latencies of all operations except unaligned
+ * writes and reads
+ */
+#define KFUSE_HISTOGRAM_MAX	10
 #define KFUSE_PROM_MAX		(9*5 + 2)
 
-struct kfuse_stat_rec
-{
+struct kfuse_histogram {
 	u64	buckets[KFUSE_PROM_MAX];
 	u64	sum;
 };
 
-struct kfuse_histogram
-{
-	struct kfuse_stat_rec	metrics[KFUSE_OP_MAX];
+struct kfuse_counter {
+	u64 events;
+	u64 val_total;
+};
+
+struct kfuse_metrics {
+	/* Histograms are compatible with old version of proto
+	 * between userspace and kio where the counters were skipped.
+	 */
+	struct kfuse_histogram	hists[KFUSE_HISTOGRAM_MAX];
+
+	/* Counters were added in 3.5 release */
+	struct kfuse_counter	cnts[KFUSE_OP_MAX];
 };
 
 #endif /* __FUSE_PROMETHEUS_PROT__ */
