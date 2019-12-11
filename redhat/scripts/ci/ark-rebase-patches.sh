5a64dab1d4c7fe02550c2f2329504d677d488f65
Jeremy Cline <jcline@redhat.com>
2019-12-11 15:43:34 -0500

    [redhat] Add a script to automatically rebase ark-patches
    This script rebases the ark-patches against the latest master branch. If
    patches cannot be rebased, issues are filed in the GitLab issue tracker.
    
    This script is really only intended to be run in CI on a regular
    schedule (nightly, most likely). A sample .gitlab-ci.yml would be:
    
    ark_patches_rebase:
      script:
        - dnf -y install python3-gitlab
        - git config user.name "CKI@GitLab"
        - git config user.email "cki-project@redhat.com"
        - redhat/scripts/ci/ark-rebase-patches.sh "master" $CI_PROJECT_ID
        - git push -f origin ark-patches
    
    Signed-off-by: Jeremy Cline <jcline@redhat.com>

---
+++ redhat/scripts/ci/ark-rebase-patches.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/bash
+#
+# Automatically rebase the kernel patches in ark-patches.
+#
+# Any patches that do not apply cleanly during the rebase are dropped, and an
+# issue is filed to track rebasing that patch.
+#
+# This assumes you have python3-gitlab installed and a configuration file set
+# up in ~/.python-gitlab.cfg or /etc/python-gitlab.cfg. An example configuration
+# can be found at https://python-gitlab.readthedocs.io/en/stable/cli.html. If
+# the configuration is not in one of the above locations, the path can be set
+# with the PYTHON_GITLAB_CONFIG environment variable.
+#
+# Arguments:
+#   1) The commit/tag/branch to rebase onto.
+#   2) The Gitlab project ID to file issues against. See the project page on
+#      Gitlab for the ID. For example, https://gitlab.com/cki-project/kernel-ark/
+#      is project ID 13604247
+set -e
+
+UPSTREAM_REF=$1
+PROJECT_ID=$2
+
+ISSUE_TEMPLATE="During an automated rebase of ark-patches, commit %s failed to rebase.
+
+The commit in question is:
+~~~
+%s
+~~~
+
+To fix this issue:
+
+1. \`git rebase upstream ark-patches\`
+2. Use your soft, squishy brain to resolve the conflict as you see fit. If it is
+   non-trivial and has an \"Upstream Status: RHEL only\" tag, contact the author
+   and ask them to rebase the patch.
+3. \`if git tag -v $UPSTREAM_REF; then git branch ark/patches/$UPSTREAM_REF && git push upstream ark/patches/$UPSTREAM_REF; fi\`
+4. \`git push -f upstream ark-patches\`
+"
+
+if [ -z "$PYTHON_GITLAB_CONFIG" ]; then
+	GITLAB_CONFIG_OPT=""
+else
+	GITLAB_CONFIG_OPT="-c $PYTHON_GITLAB_CONFIG"
+fi
+
+if git show "$UPSTREAM_REF" > /dev/null 2>&1; then
+	printf "Rebasing ark-patches onto %s...\n" "$UPSTREAM_REF"
+else
+	printf "No such git object \"%s\" in tree\n" "$UPSTREAM_REF"
+	exit 1
+fi
+
+if [ -n "$PROJECT_ID" ] && [ "$PROJECT_ID" -eq "$PROJECT_ID" ] 2> /dev/null; then
+	printf "Filing issues against GitLab project ID %s\n" "$PROJECT_ID"
+else
+	printf "No Gitlab project ID specified; halting!\n"
+	exit 1
+fi
+
+CLEAN_REBASE=true
+if git rebase "$UPSTREAM_REF" ark-patches; then
+	printf "Cleanly rebased all patches\n"
+else
+	while true; do
+		CLEAN_REBASE=false
+		CONFLICT=$(git am --show-current-patch)
+		COMMIT=$(git am --show-current-patch | head -n1 | awk '{print $2}' | cut -c 1-12)
+		TITLE=$(printf "Unable to automatically rebase commit %s" "$COMMIT")
+		DESC=$(printf "$ISSUE_TEMPLATE" "$COMMIT" "$CONFLICT")
+		OPEN_ISSUES=$(gitlab $GITLAB_CONFIG_OPT project-issue list --project-id "$PROJECT_ID" --search "$TITLE")
+		if [ -n "$OPEN_ISSUES" ]; then
+			echo "Skipping filing an issue about commit $COMMIT; already exists as $OPEN_ISSUES"
+			continue
+		fi
+
+		if gitlab $GITLAB_CONFIG_OPT project-issue create --project-id "$PROJECT_ID" \
+			--title "$TITLE" --description "$DESC" --labels "Patch Rebase"; then
+			if git rebase --skip; then
+				printf "Finished dropping patches that fail to rebase\n"
+				break
+			else
+				continue
+			fi
+		else
+			printf "Halting rebase because an issue cannot be filed for a conflict\n"
+			exit 1
+		fi
+	done
+fi
+
+if $CLEAN_REBASE; then
+	printf "You can safely update ark-patches with 'git push -f <remote> ark-patches'\n"
+else
+	printf "Some patches could not be rebased, fix up ark-patches as necessary"
+	printf " before pushing the branch."
+	exit 2
+fi
+
+if git tag -v "$UPSTREAM_REF" > /dev/null 2>&1; then
+	printf "Creating branch \"ark/patches/%s\"\n" "$UPSTREAM_REF"
+	git branch ark/patches/"$UPSTREAM_REF"
+	printf "Don't forget to run 'git push <remote> ark/patches/%s'\n" "$UPSTREAM_REF"
+fi
