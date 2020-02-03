b6d35aa59fd792b3bd1545ef461468cd61addf21
Kirill Tkhai <ktkhai@virtuozzo.com>
2020-02-03 14:01:12 +0000

    ploop: Disable holes on io_direct raw images
    The problem is raw images may have ploop1 snapshots
    on top of them. When base image contains a hole and
    there is a read bio covering both the hole and some
    existing extent, a part of the bio must be zoroes,
    while another part must go to existing extent.
    Ploop driver stack does not support this.
    
    In case of there is no a snapshot, we used to fallocate
    a hole on-flight as workaround, but this works only
    for R/W mounted ploop. So, we completely disable holes
    there.
    
    https://jira.sw.ru/browse/PSBM-101189
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- drivers/block/ploop/fmt_raw.c
+++ drivers/block/ploop/fmt_raw.c
@@ -56,6 +56,9 @@ raw_open(struct ploop_delta * delta)
 	pos += (1 << (cluster_log + 9)) - 1;
 	delta->io.alloc_head = pos >> (cluster_log + 9);
 
+	if (delta->io.ops->id == PLOOP_IO_DIRECT)
+		set_bit(PLOOP_S_NO_FALLOC_DISCARD, &delta->plo->state);
+
 	/* no more allocations at all */
 	delta->flags |= PLOOP_FMT_PREALLOCATED;
 
