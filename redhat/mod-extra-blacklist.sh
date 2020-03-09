a9d00d64b9529227ca5bd2b5bc66f0d49748eebf
Jeremy Cline <jcline@redhat.com>
2020-03-09 14:34:04 -0400

    [redhat] Sync ARK latest mod-extra-blacklist.sh with Fedora
    Fedora includes a blacklist for the floppy driver[0]. Bring this into
    ARK.
    
    [0] https://bugzilla.redhat.com/show_bug.cgi?id=1789155
    
    Signed-off-by: Jeremy Cline <jcline@redhat.com>

--- redhat/mod-extra-blacklist.sh
+++ redhat/mod-extra-blacklist.sh
@@ -46,3 +46,11 @@ foreachp()
 [ -d "$buildroot/etc/modprobe.d/" ] || mkdir -p "$buildroot/etc/modprobe.d/"
 find "$buildroot/$kernel_base/extra" -name "*.ko*" | \
 	foreachp check_blacklist
+
+# Many BIOS-es export a PNP-id which causes the floppy driver to autoload
+# even though most modern systems don't have a 3.5" floppy driver anymore
+# this replaces the old die_floppy_die.patch which removed the PNP-id from
+# the module
+if [ -f $buildroot/$kernel_base/extra/drivers/block/floppy.ko* ]; then
+	blacklist "floppy"
+fi
