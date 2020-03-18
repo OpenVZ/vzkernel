76ee5e1237437bdd5fd37d1a371aff33ceeb989c
Neil Horman <nhorman@tuxdriver.com>
2020-03-18 10:10:54 -0400

    Fix xz memory usage issue
    The create-tarball.sh script can occasionally hang non 64 bit systems
    due to extreeme memory usage.  Build farms will occasionally use older
    32 bit systems to do arch independent work (like assembling
    srpms/tarballs/etc), and when xz runs on those systems, it will, given a
    sufficiently large input file, allocate more than the available 3G of
    memory address space allocated to a user space process, causing swap
    storms, and build failures due to resultant job timeouts.
    
    Fix it by detecting if we are on a 64 bit arch or not, and setting the
    xz memory usage limit accordingly
    
    Signed-off-by: Neil Horman <nhorman@redhat.com>

--- redhat/scripts/create-tarball.sh
+++ redhat/scripts/create-tarball.sh
@@ -6,6 +6,14 @@ DIR=$3
 
 XZ_THREADS=`rpm --eval %{_smp_mflags} | sed -e 's!^-j!--threads !'`
 
+ARCH=`arch`
+XZ_OPTIONS=""
+
+if [ "$ARCH" != "x86_64" ]
+then
+        XZ_OPTIONS="-M 3G"
+fi
+
 if [ -f ${TARBALL} ]; then
 	TARID=`( xzcat -qq ${TARBALL} | git get-tar-commit-id ) 2>/dev/null`
 	GITID_NORMALIZE=`git log --max-count=1 --pretty=format:%H ${GITID}`
@@ -19,4 +27,4 @@ fi
 echo "Creating `basename ${TARBALL}`..."
 trap 'rm -vf ${TARBALL}' INT
 cd ../ &&
-  git archive --prefix=${DIR}/ --format=tar ${GITID} | xz ${XZ_THREADS} > ${TARBALL};
+  git archive --prefix=${DIR}/ --format=tar ${GITID} | xz ${XZ_OPTIONS} ${XZ_THREADS} > ${TARBALL};
