2d7e00adca8f52191eff08240ac10c77644e77a6
Don Zickus <dzickus@redhat.com>
2020-03-31 13:26:17 -0400

    Migrate blacklisting floppy.ko to mod-blacklist.sh
    Leftover chunk wasn't included in the initial RHEL blacklist changes.
    Convert the final piece.
    
    Signed-off-by: Don Zickus <dzickus@redhat.com>

--- redhat/mod-blacklist.sh
+++ redhat/mod-blacklist.sh
@@ -143,6 +143,14 @@ if [ ! -n "$Dest" ]; then
 	foreachp check_blacklist < $List
 fi
 
+# Many BIOS-es export a PNP-id which causes the floppy driver to autoload
+# even though most modern systems don't have a 3.5" floppy driver anymore
+# this replaces the old die_floppy_die.patch which removed the PNP-id from
+# the module
+if [ -f $RpmDir/$ModDir/extra/drivers/block/floppy.ko* ]; then
+	blacklist "floppy"
+fi
+
 # avoid an empty kernel-extra package
 echo "$ModDir/$OverrideDir" >> $RpmDir/$ListName
 
--- redhat/mod-extra-blacklist.sh
+++
@@ -1,8 +0,0 @@
-
-# Many BIOS-es export a PNP-id which causes the floppy driver to autoload
-# even though most modern systems don't have a 3.5" floppy driver anymore
-# this replaces the old die_floppy_die.patch which removed the PNP-id from
-# the module
-if [ -f $buildroot/$kernel_base/extra/drivers/block/floppy.ko* ]; then
-	blacklist "floppy"
-fi
