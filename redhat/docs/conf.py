7274dd1ff994a4fa62644a46a3630185058a2d38
Jeremy Cline <jcline@redhat.com>
2020-05-18 15:34:48 -0400

    Drop the static path configuration for the Sphinx docs
    There are no static files at this time. I don't know the first thing
    about CSS and rely on my elders and betters to make the documentation
    look presentable. Configuring a static directory also generates a Sphinx
    warning when it isn't present (which it isn't on clean checkouts because
    it's empty) so just remove it.
    
    Signed-off-by: Jeremy Cline <jcline@redhat.com>

--- redhat/docs/conf.py
+++ redhat/docs/conf.py
@@ -40,8 +40,3 @@ html_theme_options = {
     "show_related": True,
     "sidebar_collapse": True,
 }
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
