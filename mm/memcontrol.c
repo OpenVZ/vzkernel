5843747dbd8192447cdfb93eebe3a8a0cad826e8
Andrey Ryabinin <aryabinin@virtuozzo.com>
2020-07-21 17:59:50 +0300

    mm/memcg: Use per-cpu stock charges for ->kmem and ->cache counters
    Currently we use per-cpu stocks to do precharges of the ->memory and ->memsw
    counters. Do this for the ->kmem and ->cache as well to decrease contention
    on these counters as well.
    
    https://jira.sw.ru/browse/PSBM-101300
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    ++++
    memcg leak due to missing css_put on last kmem uncharge
    
    When mem_cgroup is removes mem_cgroup_css_offline() calls
    memcg_deactivate_kmem() which disables kmem accounting.
    If memcg still have some charged kmem final css_put is not called,
    and delayed till last kmem will be uncharged.
    
    Usually kmem is uncharged by using memcg_uncharge_kmem() which have
    according checks and if required calls final css_put().
    
    Though patch added to vz7.162.14 kernel
    "mm/memcg: Use per-cpu stock charges for ->kmem and ->cache counters"
    enabled kmem charge/uncharge in refill_stock()/drain_stock()
    without using of memcg_uncharge_kmem(), as result nobody called
    final css_put() after last kmem uncharge.
    
    This patch adds css_get/put for safe access to memcg in drain_stock(),
    and calls an additional css_put() after last kmem uncharge.
    
    Fixes: "mm/memcg: Use per-cpu stock charges for ->kmem and ->cache counters"
    https://bugs.openvz.org/browse/OVZ-7250
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    
    ++++
    memcg: force css_put after last kmem uncharge in uncharge_batch
    
    This patch calls css_put() after last kmem uncharge in uncharge_batch.
    and releases a reference taken in memcg_deactivate_kmem.
    
    https://bugs.openvz.org/browse/OVZ-7250
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    
    ++++
    memcg: improvements for "memcg leak due to missing css_put on last kmem uncharge"
    
    - inlined memcg_kmem_release_css() to improve function reference in css_stack
    - removed css_get/css_put extra calls in refill_stock/drain_stock
    
    https://bugs.openvz.org/browse/OVZ-7250
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    [vz7.182.x rebase] minor context changes

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -436,6 +436,8 @@ enum {
 	KMEM_ACCOUNTED_DEAD, /* dead memcg with pending kmem charges */
 };
 
+static inline void memcg_kmem_release_css(struct mem_cgroup *memcg);
+
 static struct mem_cgroup_per_node *
 mem_cgroup_nodeinfo(struct mem_cgroup *memcg, int nid)
 {
@@ -2827,6 +2829,8 @@ void mem_cgroup_update_page_stat(struct page *page,
 struct memcg_stock_pcp {
 	struct mem_cgroup *cached; /* this never be root cgroup */
 	unsigned int nr_pages;
+	unsigned int cache_nr_pages;
+	unsigned int kmem_nr_pages;
 	struct work_struct work;
 	unsigned long flags;
 #define FLUSHING_CACHED_CHARGE	0
@@ -2845,7 +2849,8 @@ static DEFINE_MUTEX(percpu_charge_mutex);
  *
  * returns true if successful, false otherwise.
  */
-static bool consume_stock(struct mem_cgroup *memcg, unsigned int nr_pages)
+static bool consume_stock(struct mem_cgroup *memcg, unsigned int nr_pages,
+			bool cache, bool kmem)
 {
 	struct memcg_stock_pcp *stock;
 	bool ret = false;
@@ -2854,9 +2859,19 @@ static bool consume_stock(struct mem_cgroup *memcg, unsigned int nr_pages)
 		return ret;
 
 	stock = &get_cpu_var(memcg_stock);
-	if (memcg == stock->cached && stock->nr_pages >= nr_pages) {
-		stock->nr_pages -= nr_pages;
-		ret = true;
+	if (memcg == stock->cached) {
+		if (cache && stock->cache_nr_pages >= nr_pages) {
+			stock->cache_nr_pages -= nr_pages;
+			ret = true;
+		}
+		if (kmem && stock->kmem_nr_pages >= nr_pages) {
+			stock->kmem_nr_pages -= nr_pages;
+			ret = true;
+		}
+		if (!cache && !kmem && stock->nr_pages >= nr_pages) {
+			stock->nr_pages -= nr_pages;
+			ret = true;
+		}
 	}
 	put_cpu_var(memcg_stock);
 	return ret;
@@ -2868,13 +2883,27 @@ static bool consume_stock(struct mem_cgroup *memcg, unsigned int nr_pages)
 static void drain_stock(struct memcg_stock_pcp *stock)
 {
 	struct mem_cgroup *old = stock->cached;
+	unsigned long nr_pages = stock->nr_pages + stock->cache_nr_pages + stock->kmem_nr_pages;
+	u64 kmem = 1;
 
-	if (stock->nr_pages) {
-		page_counter_uncharge(&old->memory, stock->nr_pages);
+	if (!old)
+		return;
+
+	if (stock->cache_nr_pages)
+		page_counter_uncharge(&old->cache, stock->cache_nr_pages);
+	if (stock->kmem_nr_pages)
+		kmem = page_counter_uncharge(&old->kmem, stock->kmem_nr_pages);
+
+	if (nr_pages) {
+		page_counter_uncharge(&old->memory, nr_pages);
 		if (do_swap_account)
-			page_counter_uncharge(&old->memsw, stock->nr_pages);
+			page_counter_uncharge(&old->memsw, nr_pages);
 		stock->nr_pages = 0;
+		stock->kmem_nr_pages = 0;
+		stock->cache_nr_pages = 0;
 	}
+	if (kmem == 0)
+		memcg_kmem_release_css(old);
 	stock->cached = NULL;
 }
 
@@ -2904,7 +2933,8 @@ static void __init memcg_stock_init(void)
  * Cache charges(val) to local per_cpu area.
  * This will be consumed by consume_stock() function, later.
  */
-static void refill_stock(struct mem_cgroup *memcg, unsigned int nr_pages)
+static void refill_stock(struct mem_cgroup *memcg, unsigned int nr_pages,
+			bool cache, bool kmem)
 {
 	struct memcg_stock_pcp *stock = &get_cpu_var(memcg_stock);
 
@@ -2912,7 +2942,13 @@ static void refill_stock(struct mem_cgroup *memcg, unsigned int nr_pages)
 		drain_stock(stock);
 		stock->cached = memcg;
 	}
-	stock->nr_pages += nr_pages;
+
+	if (cache)
+		stock->cache_nr_pages += nr_pages;
+	else if (kmem)
+		stock->kmem_nr_pages += nr_pages;
+	else
+		stock->nr_pages += nr_pages;
 	put_cpu_var(memcg_stock);
 }
 
@@ -2935,7 +2971,7 @@ static void drain_all_stock(struct mem_cgroup *root_memcg, bool sync)
 
 		rcu_read_lock();
 		memcg = stock->cached;
-		if (memcg && stock->nr_pages &&
+		if (memcg && (stock->nr_pages + stock->kmem_nr_pages + stock->cache_nr_pages) &&
 		    __mem_cgroup_same_or_subtree(root_memcg, memcg))
 			flush = true;
 		rcu_read_unlock();
@@ -3104,21 +3140,13 @@ static int try_charge(struct mem_cgroup *memcg, gfp_t gfp_mask, bool kmem_charge
 	bool drained = false;
 
 	if (mem_cgroup_is_root(memcg))
-		goto done;
+		return 0;
 retry:
 	flags = 0;
 
-	if (consume_stock(memcg, nr_pages)) {
-		if (kmem_charge && !page_counter_try_charge(
-				&memcg->kmem, nr_pages, &counter)) {
-			refill_stock(memcg, nr_pages);
-			goto charge;
-		}
-
+	if (consume_stock(memcg, nr_pages, cache_charge, kmem_charge))
 		goto done;
-	}
 
-charge:
 	mem_over_limit = NULL;
 	if (page_counter_try_charge(&memcg->memory, batch, &counter)) {
 		if (do_swap_account && !page_counter_try_charge(
@@ -3144,9 +3172,9 @@ charge:
 		mem_over_limit = mem_cgroup_from_counter(counter, memory);
 
 	if (!mem_over_limit && kmem_charge) {
-		if (!page_counter_try_charge(&memcg->kmem, nr_pages, &counter)) {
+		if (!page_counter_try_charge(&memcg->kmem, batch, &counter)) {
 			if (gfp_mask & __GFP_NOFAIL) {
-				page_counter_charge(&memcg->kmem, nr_pages);
+				page_counter_charge(&memcg->kmem, batch);
 			} else {
 				flags |= MEM_CGROUP_RECLAIM_KMEM;
 				mem_over_limit = mem_cgroup_from_counter(counter, kmem);
@@ -3266,12 +3294,13 @@ done_restock:
 		goto bypass;
 	}
 
-	if (batch > nr_pages)
-		refill_stock(memcg, batch - nr_pages);
-done:
 	if (cache_charge)
-		page_counter_charge(&memcg->cache, nr_pages);
+		page_counter_charge(&memcg->cache, batch);
 
+	if (batch > nr_pages)
+		refill_stock(memcg, batch - nr_pages, cache_charge, kmem_charge);
+
+done:
 	/*
 	 * If the hierarchy is above the normal consumption range, schedule
 	 * reclaim on returning to userland.  We can perform reclaim here
@@ -3523,10 +3552,12 @@ void memcg_uncharge_kmem(struct mem_cgroup *memcg,
 	if (do_swap_account)
 		page_counter_uncharge(&memcg->memsw, nr_pages);
 
-	/* Not down to 0 */
-	if (kmem)
-		return;
+	if (kmem == 0)
+		memcg_kmem_release_css(memcg);
+}
 
+static inline void memcg_kmem_release_css(struct mem_cgroup *memcg)
+{
 	/*
 	 * Releases a reference taken in memcg_deactivate_kmem in case
 	 * this last uncharge is racing with the offlining code or it is
@@ -7749,10 +7780,11 @@ static void uncharge_batch(struct mem_cgroup *memcg, unsigned long pgpgout,
 			   unsigned long nr_shmem, struct page *dummy_page)
 {
 	unsigned long flags;
+	u64 kmem = 1;
 
 	if (!mem_cgroup_is_root(memcg)) {
 		if (nr_kmem)
-			page_counter_uncharge(&memcg->kmem, nr_kmem);
+			kmem = page_counter_uncharge(&memcg->kmem, nr_kmem);
 		if (nr_mem + nr_kmem)
 			page_counter_uncharge(&memcg->memory, nr_mem + nr_kmem);
 		if (nr_memsw + nr_kmem)
@@ -7772,6 +7804,8 @@ static void uncharge_batch(struct mem_cgroup *memcg, unsigned long pgpgout,
 	__this_cpu_add(memcg->stat->nr_page_events, nr_anon + nr_file);
 	memcg_check_events(memcg, dummy_page);
 	local_irq_restore(flags);
+	if (kmem == 0)
+		memcg_kmem_release_css(memcg);
 }
 
 static void uncharge_list(struct list_head *page_list)
