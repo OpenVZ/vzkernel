2a9bdf64b220a6508de5b7e77a01317ded3b870e
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2020-07-21 07:44:34 +0300

    kernfs/sysfs: add ioctl to get fd network namespace tag
    Sysfs mounts save current netns when mounting and show different set of
    network devices in /sys/class/net based on it. But there is currently no
    simple way to find out to that netns the sysfs mount is tagged to.
    
    Lets add an ioctl so that we can get open file descriptor to this
    namespace on nsfs. Next we would be able to readlink this fd to get
    network namespace id.
    
    Sysfs holds only "passive" reference to the net namespace, so the only
    guaranty we have is that struct net is not freed yet. So lets add a
    helper maybe_get_net_ns to get net only if it's fully alive.
    
    v2: fix bad net namespace referencing
    https://jira.sw.ru/browse/PSBM-105161
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- net/socket.c
+++ net/socket.c
@@ -1097,6 +1097,18 @@ struct ns_common *get_net_ns(struct ns_common *ns)
 }
 EXPORT_SYMBOL_GPL(get_net_ns);
 
+struct ns_common *maybe_get_net_ns(struct ns_common *ns)
+{
+	struct net *net;
+
+	net = maybe_get_net(container_of(ns, struct net, ns));
+	if (!net)
+		return ERR_PTR(-ENXIO);
+
+	return &net->ns;
+}
+EXPORT_SYMBOL_GPL(maybe_get_net_ns);
+
 static long sock_ioctl(struct file *file, unsigned cmd, unsigned long arg)
 {
 	struct socket *sock;
