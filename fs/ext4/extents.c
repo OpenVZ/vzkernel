ef68e54f5c8555412ef7bba62c07ec46898d1820
Kirill Tkhai <ktkhai@virtuozzo.com>
2020-08-17 09:52:43 +0300

    ext4: Fix fiemap() with FIEMAP_FLAG_CACHE flag
    This flag says to cache extents in extent tree before iterations over
    them, but the combination of these two actions does not work.
    
    ext4_ext_precache() populates cache, but extent iteration can't occur,
    since fiemap_check_flags() does not know this flag.
    
    So, the result of this flag is:
      1)cache is populated,
      2)-EBADR is returned instead of extents array.
    
    This looks like a BUG in mainstream, which was blindly fixed in commit
    bb5835edcdf8bf7 "ext4: add new ioctl EXT4_IOC_GET_ES_CACHE".
    
    This patch makes the flag to work as expected.
    https://jira.sw.ru/browse/PSBM-105347
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Acked-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    (cherry picked from commit df8daecb4f65ab2c7b5447500f6afa5522c113d2)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -5159,6 +5159,7 @@ int ext4_fiemap(struct inode *inode, struct fiemap_extent_info *fieinfo,
 		error = ext4_ext_precache(inode);
 		if (error)
 			return error;
+		fieinfo->fi_flags &= ~FIEMAP_FLAG_CACHE;
 	}
 
 	/* fallback to generic here if not in extents fmt */
