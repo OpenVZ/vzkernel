b44541a0b8d3b41a924010a783d7e417e945b333
Vasily Averin <vvs@virtuozzo.com>
2020-09-10 16:36:28 +0300

    fixup rh fs/dcache: Don't set DCACHE_REFERENCED on dentries when first put into LRU
    Restores RHEL7 patch
    "fs/dcache: Don't set DCACHE_REFERENCED on dentries when first put into LRU"
    reverted during re-base to RHEL7.7
    It changes dcache LRU schrink to the upstream way.
    
    To be merged with "Revert RHBZ#1489573 patches"
    https://jira.sw.ru/browse/PSBM-104223
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>

--- fs/dcache.c
+++ fs/dcache.c
@@ -496,6 +496,8 @@ static void dentry_lru_add(struct dentry *dentry)
 {
 	if (unlikely(!(dentry->d_flags & DCACHE_LRU_LIST)))
 		d_lru_add(dentry);
+	else
+		dentry->d_flags |= DCACHE_REFERENCED;
 }
 
 /*
@@ -770,7 +772,6 @@ repeat:
 			goto kill_it;
 	}
 
-	dentry->d_flags |= DCACHE_REFERENCED;
 	dentry_lru_add(dentry);
 
 	dentry->d_lockref.count--;
