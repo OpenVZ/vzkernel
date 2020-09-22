d64c6358efec5c13a55bc4dddf9d614634d6d892
David Howells <dhowells@redhat.com>
2020-09-22 18:56:57 +0300

    ms/vfs: syscall: Add move_mount(2) to move mounts around
    Patchset description:
    These syscalls were added as preparation step for new mount api (fsopen,
    fsconfig, fsmount and fspick will be ported separately).
    
    We can use them to implement "cross-namespace bind-mounting" like this:
    
    fd = open_tree(AT_FDCWD, "/mnt", OPEN_TREE_CLONE);
    setns(nsfd, CLONE_NEWNS);
    move_mount(fd, "", AT_FDCWD, "/mnt2", MOVE_MOUNT_F_EMPTY_PATH);
    
    This will allow us implementing feature of adding bindmounts to runing
    container instead of having unreliable external propagations.
    
    Version for VZ8 is slightly different from VZ7 version.
    
    https://jira.sw.ru/browse/PSBM-107263
    
    Current patch description:
    From: David Howells <dhowells@redhat.com>
    
    Add a move_mount() system call that will move a mount from one place to
    another and, in the next commit, allow to attach an unattached mount tree.
    
    The new system call looks like the following:
    
        int move_mount(int from_dfd, const char *from_path,
    		   int to_dfd, const char *to_path,
    		   unsigned int flags);
    
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: linux-api@vger.kernel.org
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    
    vfs: syscall: Add move_mount(2) to move mounts around
    (cherry-picked from commit 2db154b3ea8e14b04fee23e3fdfd5e9d17fbc6ae)
    uapi, x86: Fix the syscall numbering of the mount API syscalls [ver #2]
    (cherry-picked from commit 9c8ad7a2ff0bfe58f019ec0abc1fb965114dde7d)
    selinux: fix regression introduced by move_mount(2) syscall
    (cherry-picked from commit 98aa00345de54b8340dc2ddcd87f446d33387b5e)
    
    https://jira.sw.ru/browse/PSBM-107263
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- include/uapi/linux/fs.h
+++ include/uapi/linux/fs.h
@@ -165,6 +165,17 @@ struct inodes_stat_t {
 #define OPEN_TREE_CLOEXEC	O_CLOEXEC	/* Close the file on execve() */
 
 /*
+ * move_mount() flags.
+ */
+#define MOVE_MOUNT_F_SYMLINKS		0x00000001 /* Follow symlinks on from path */
+#define MOVE_MOUNT_F_AUTOMOUNTS		0x00000002 /* Follow automounts on from path */
+#define MOVE_MOUNT_F_EMPTY_PATH		0x00000004 /* Empty from path permitted */
+#define MOVE_MOUNT_T_SYMLINKS		0x00000010 /* Follow symlinks on to path */
+#define MOVE_MOUNT_T_AUTOMOUNTS		0x00000020 /* Follow automounts on to path */
+#define MOVE_MOUNT_T_EMPTY_PATH		0x00000040 /* Empty to path permitted */
+#define MOVE_MOUNT__MASK		0x00000077
+
+/*
  * Structure for FS_IOC_FSGETXATTR[A] and FS_IOC_FSSETXATTR.
  */
 struct fsxattr {
