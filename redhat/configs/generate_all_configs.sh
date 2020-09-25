c4358a16552a7fb6ed361aaf7d6abbce7149201c
Ben Crocker <bcrocker@redhat.com>
2020-09-24 23:16:03 -0400

    generate_all_configs.sh: Fix syntax flagged by shellcheck
    SC2006: Use $(...) notation instead of legacy backticked `...`.
    SC2039: In POSIX sh, == in place of = is undefined.  (NOTE: works fine in bash.)
    SC2086: Double quote to prevent globbing and word splitting.
    SC2231: Quote expansions in this for-loop glob to prevent wordsplitting, e.g. "$dir"/*.txt .
    
    Signed-off-by: Ben Crocker <bcrocker@redhat.com>

--- redhat/configs/generate_all_configs.sh
+++ redhat/configs/generate_all_configs.sh
@@ -9,7 +9,7 @@
 PRIMARY=$1
 DEBUGBUILDSENABLED=$2
 
-if [ -z $2 ]; then
+if [ -z "$2" ]; then
 	exit 1
 fi
 
@@ -17,24 +17,24 @@ if [ -z "$PRIMARY" ]; then
 	PRIMARY=rhel
 fi
 
-if [ "$PRIMARY" == "fedora" ]; then
+if [ "$PRIMARY" = "fedora" ]; then
 	SECONDARY=rhel
 else
 	SECONDARY=fedora
 fi
 
-for i in kernel-*-$PRIMARY.config; do
-	NEW=kernel-$VERSION-`echo $i | cut -d - -f2- | sed s/-$PRIMARY//`
+for i in kernel-*-"$PRIMARY".config; do
+	NEW=kernel-"$VERSION"-$(echo "$i" | cut -d - -f2- | sed s/-"$PRIMARY"//)
 	#echo $NEW
-	mv $i $NEW
+	mv "$i" "$NEW"
 done
 
-rm -f kernel-*-$SECONDARY.config
+rm -f kernel-*-"$SECONDARY".config
 
-if [ $DEBUGBUILDSENABLED -eq 0 ]; then
+if [ "$DEBUGBUILDSENABLED" -eq 0 ]; then
 	for i in kernel-*debug*.config; do
-		base=`echo $i | sed -r s/-?debug//g`
-		NEW=kernel-`echo $base | cut -d - -f2-`
-		mv $i $NEW
+		base=$(echo "$i" | sed -r s/-?debug//g)
+		NEW=kernel-$(echo "$base" | cut -d - -f2-)
+		mv "$i" "$NEW"
 	done
 fi
