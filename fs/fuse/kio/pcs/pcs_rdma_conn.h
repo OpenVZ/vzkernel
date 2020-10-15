102a3eaf46c97c32e652d1a4ef846c56512b866b
Ildar Ismagilov <ildar.ismagilov@virtuozzo.com>
2020-10-15 10:37:35 +0300

    fs/fuse kio: implement support RDMA transport
    This patch adds support of RDMA transport for RPC.
    
    For RDMA read requests is possible to enable/disable usage of prealloceted dma memory region
    through sysfs '/sys/module/fuse_kio_pcs/parameters/rdmaio_use_dma_mr_for_rdma_rw'
    
    This patch provides the ability to enable maping a list of msg pages
    to rdma memory region through sysfs '/sys/module/fuse_kio_pcs/parameters/rdmaio_use_map_for_mr',
    instead of copy pages to/from preallocated and registred region.
    
    Also possible to configure RDMA io queue depth through sysfs
    '/sys/module/fuse_kio_pcs/parameters/rdmaio_queue_depth'
    
    https://pmc.acronis.com/browse/VSTOR-4310
    
    Signed-off-by: Ildar Ismagilov <ildar.ismagilov@virtuozzo.com>
    
    +++
    fs/fuse kio: resolve name conflicts with ib_core
    
    Rename ib_mr_pool_{init,destroy,get,set} to avoid conflicts
    with names from include/rdma/mr_pool.h
    
    mFixes: 867c0e15fddf (fs/fuse kio: implement support RDMA transport)
    
    Signed-off-by: Ildar Ismagilov <ildar.ismagilov@virtuozzo.com>
    
    +++
    fs/fuse kio: add dependency from infiniband to Kconfig
    
    In previous patches RDMA transport support was added but
    dependency from INFINIBAND is missed.
    
    mFixes: 867c0e15fddf (fs/fuse kio: implement support RDMA transport)
    
    Signed-off-by: Ildar Ismagilov <ildar.ismagilov@virtuozzo.com>
    Signed-off-by: Ildar Ismagilov <Ildar.Ismagilov@acronis.com>

---
+++ fs/fuse/kio/pcs/pcs_rdma_conn.h
@@ -0,0 +1,13 @@
+/*
+ *  fs/fuse/kio/pcs/pcs_rdma_conn.h
+ *
+ *  Copyright (c) 2018-2021 Virtuozzo International GmbH. All rights reserved.
+ *
+ */
+
+#ifndef _PCS_RMDA_CONN_H_
+#define _PCS_RMDA_CONN_H_ 1
+
+void pcs_rdmaconnect_start(struct pcs_rpc *ep);
+
+#endif /* _PCS_RMDA_CONN_H_ */
