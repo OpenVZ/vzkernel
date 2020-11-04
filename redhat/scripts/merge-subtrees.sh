a0cdf2a41071f51b57855f21445f5edea93b4138
Ben Crocker <bcrocker@redhat.com>
2020-11-04 11:58:12 -0500

    [redhat] merge-subtrees.sh: Fix syntax flagged by shellcheck
    SC2162: read without -r will mangle backslashes
    SC2086: Double quote to prevent globbing and word splitting
    
    Rewrote main loop so it will work.
    
    Signed-off-by: Ben Crocker <bcrocker@redhat.com>
    
    merge-subtrees.h: fix
    
    Signed-off-by: Ben Crocker <bcrocker@redhat.com>

--- redhat/scripts/merge-subtrees.sh
+++ redhat/scripts/merge-subtrees.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 # In newer versions of git-subtree the git repo is not explicitly required,
 # however, given the wide variance of git versions we need to include it.
 
@@ -7,6 +6,7 @@ entries="
 	redhat/rhdocs git://git.host.prod.eng.bos.redhat.com/rhkernel-docs
 	"
 
-echo $entries | while read entry; do
-	git subtree pull --prefix=$entry  master
+echo "$entries" | while read -r name url; do
+	[ -z "$name" ] && continue
+	git subtree pull --prefix="$name" "$url"  master
 done
