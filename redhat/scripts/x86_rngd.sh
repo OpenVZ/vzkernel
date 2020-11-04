4ceb39cebe9cf8c3aeef0bb6c7bbfe0042da7d99
Ben Crocker <bcrocker@redhat.com>
2020-11-03 23:30:32 -0500

    [redhat] x86_rngd.sh: Fix syntax flagged by shellcheck
    SC2039: bash-isms
    SC2181: Check exit code directly with e.g. 'if mycmd;', not indirectly with $?.
    
    Signed-off-by: Ben Crocker <bcrocker@redhat.com>

--- redhat/scripts/x86_rngd.sh
+++ redhat/scripts/x86_rngd.sh
@@ -1,15 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 #
 # this script is a helper script for i686 and x86_64 builds.  It gets the
 # random number generator running.
 
 echo -n "Trying hardware random number generator ..."
-rngd -r /dev/hwrandom >& /dev/null
-if [ $? -ne 0 ]; then
+if ! rngd -r /dev/hwrandom >& /dev/null; then
 	echo "failed"
 	# try the pseudo-random number generator
-	echo "Using psuedo-random number instead"
+	echo "Using pseudo-random number instead"
 	rngd -r /dev/urandom >& /dev/null
 else
-	echo "succeded"
+	echo "succeeded"
 fi
