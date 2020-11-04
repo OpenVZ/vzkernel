f0169a781ad81e56dd937d1ab0ff38f8858a2678
Ben Crocker <bcrocker@redhat.com>
2020-11-03 23:35:26 -0500

    [redhat] update_scripts.sh: Fix syntax flagged by shellcheck
    SC2231: Quote expansions in this for loop glob to prevent wordsplitting.
    SC2086: Double quote to prevent globbing and word splitting.
    
    Signed-off-by: Ben Crocker <bcrocker@redhat.com>

--- redhat/update_scripts.sh
+++ redhat/update_scripts.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
 	exit 1
 fi
 
-TARGET=$1
+TARGET="$1"
 
-for i in $RPM_SOURCE_DIR/*.$TARGET; do
+for i in "$RPM_SOURCE_DIR"/*."$TARGET"; do
 	NEW=${i%.$TARGET}
-	cp $i $NEW
+	cp "$i" "$NEW"
 done
