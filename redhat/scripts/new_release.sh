e32b352bbf7c0c26fe77a257469640e493a65b58
Ben Crocker <bcrocker@redhat.com>
2020-11-03 23:02:15 -0500

    [redhat] new_release.sh: Fix syntax flagged by shellcheck
    SC2007: Use $((..)) instead of deprecated $[..]
    SC2086: Double quote to prevent globbing and word splitting.
    SC2166: Prefer [ p ] || [ q ] as [ p -o q ] is not well defined.
    EVEN BETTER: use [[ p || q ]].
    
    Signed-off-by: Ben Crocker <bcrocker@redhat.com>

--- redhat/scripts/new_release.sh
+++ redhat/scripts/new_release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [ -z "$1" -o -z "$2" ]; then
-	echo "$(basename $0) <redhat path> <zstream? no/yes/branch>" >&2;
+if [[ -z $1 || -z $2 ]]; then
+	echo "$(basename "$0") <redhat path> <zstream? no/yes/branch>" >&2;
 	exit 1;
 fi
 
@@ -13,28 +13,28 @@ if [ -s "$RHPATH/linux-kernel-test.patch" ]; then
 	exit 1;
 fi
 
-RELEASE=$(sed -n -e 's/^RHEL_RELEASE\ =\ \(.*\)/\1/p' $RHPATH/../Makefile.rhelver)
+RELEASE=$(sed -n -e 's/^RHEL_RELEASE\ =\ \(.*\)/\1/p' "$RHPATH"/../Makefile.rhelver)
 
-YVER=$(echo $RELEASE | cut -d "." -f 1)
-YVER=${YVER:=$RELEASE}
-ZMAJ=$(echo $RELEASE | cut -s -d "." -f 2)
+YVER=$(echo "$RELEASE" | cut -d "." -f 1)
+YVER=${YVER:="$RELEASE"}
+ZMAJ=$(echo "$RELEASE" | cut -s -d "." -f 2)
 ZMAJ=${ZMAJ:=0}
-ZMIN=$(echo $RELEASE | cut -s -d "." -f 3)
+ZMIN=$(echo "$RELEASE" | cut -s -d "." -f 3)
 ZMIN=${ZMIN:=0}
 
 if [ "$ZSTREAM_FLAG" == "no" ]; then
 	if [ "$YSTREAM_FLAG" == "yes" ]; then
-		NEW_RELEASE="$[RELEASE + 1]";
+		NEW_RELEASE="$((RELEASE + 1))";
 	else
-		EARLY_YBUILD=$(sed -n -e 's/^EARLY_YBUILD:=\(.*\)/\1/p' $RHPATH/../Makefile.rhelver);
-		EARLY_YRELEASE=$(sed -n -e 's/^EARLY_YRELEASE:=\(.*\)/\1/p' $RHPATH/../Makefile.rhelver);
+		EARLY_YBUILD=$(sed -n -e 's/^EARLY_YBUILD:=\(.*\)/\1/p' "$RHPATH"/../Makefile.rhelver);
+		EARLY_YRELEASE=$(sed -n -e 's/^EARLY_YRELEASE:=\(.*\)/\1/p' "$RHPATH"/../Makefile.rhelver);
 		if [ "$EARLY_YBUILD" != "$RELEASE" ]; then
 			NEW_EARLY_YRELEASE=1;
 		else
-			NEW_EARLY_YRELEASE="$[EARLY_YRELEASE + 1]";
+			NEW_EARLY_YRELEASE="$((EARLY_YRELEASE + 1))";
 		fi
-		sed -i -e "s/^EARLY_YBUILD:=$EARLY_YBUILD/EARLY_YBUILD:=$RELEASE/" $RHPATH/../Makefile.rhelver;
-		sed -i -e "s/^EARLY_YRELEASE:=$EARLY_YRELEASE/EARLY_YRELEASE:=$NEW_EARLY_YRELEASE/" $RHPATH/../Makefile.rhelver;
+		sed -i -e "s/^EARLY_YBUILD:=$EARLY_YBUILD/EARLY_YBUILD:=$RELEASE/" "$RHPATH"/../Makefile.rhelver;
+		sed -i -e "s/^EARLY_YRELEASE:=$EARLY_YRELEASE/EARLY_YRELEASE:=$NEW_EARLY_YRELEASE/" "$RHPATH"/../Makefile.rhelver;
 		NEW_RELEASE=$RELEASE;
 	fi
 elif [ "$ZSTREAM_FLAG" == "yes" ]; then
@@ -42,9 +42,9 @@ elif [ "$ZSTREAM_FLAG" == "yes" ]; then
 elif [ "$ZSTREAM_FLAG" == "branch" ]; then
 	NEW_RELEASE=$YVER.$ZMAJ.$((ZMIN+1));
 else
-	echo "$(basename $0) invalid <zstream> value, allowed [no|yes|branch]" >&2;
+	echo "$(basename "$0") invalid <zstream> value, allowed [no|yes|branch]" >&2;
 	exit 1;
 fi
 
-sed -i -e "s/RHEL_RELEASE\ =.*/RHEL_RELEASE\ =\ $NEW_RELEASE/" $RHPATH/../Makefile.rhelver;
+sed -i -e "s/RHEL_RELEASE\ =.*/RHEL_RELEASE\ =\ $NEW_RELEASE/" "$RHPATH"/../Makefile.rhelver;
 
