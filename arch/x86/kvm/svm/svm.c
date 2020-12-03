875d5dc5933f6386c228cdd31b065ad1f73d83f5
Paolo Bonzini <pbonzini@redhat.com>
2020-12-03 10:15:16 -0500

    ms/KVM: x86: reinstate vendor-agnostic check on SPEC_CTRL cpuid bits #PSBM-120787 #PSBM-123538
    Until commit e7c587da1252 ("x86/speculation: Use synthetic bits for IBRS/IBPB/STIBP",
    2018-05-17), KVM was testing both Intel and AMD CPUID bits before allowing the
    guest to write MSR_IA32_SPEC_CTRL and MSR_IA32_PRED_CMD.  Testing only Intel bits
    on VMX processors, or only AMD bits on SVM processors, fails if the guests are
    created with the "opposite" vendor as the host.
    
    While at it, also tweak the host CPU check to use the vendor-agnostic feature bit
    X86_FEATURE_IBPB, since we only care about the availability of the MSR on the host
    here and not about specific CPUID bits.
    
    mFixes: e7c587da1252 ("x86/speculation: Use synthetic bits for IBRS/IBPB/STIBP")
    Cc: stable@vger.kernel.org
    Reported-by: Denis V. Lunev <den@openvz.org>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    https://jira.sw.ru/browse/PSBM-123538
    
    ========================================
    The patch is a replacement for vz patch:
    
     commit 39d637ddbcf876f897e01c737bbb351461921df0
     Author: Denis V. Lunev <den@openvz.org>
     Date:   Wed Oct 28 19:25:57 2020 +0300
    
        kvm: fix AMD IBRS/IBPB/STIBP/SSBD reporting #PSBM-120787
    
        We should report these bits in 80000008 EBX on AMD only, i.e. when AMD
        specific feature bits are enabled.
    
        https://jira.sw.ru/browse/PSBM-120787
    
        Signed-off-by: Denis V. Lunev <den@openvz.org>
        CC: Vasily Averin <vvs@virtuozzo.com>
        CC: Konstantin Khorenko <khorenko@virtuozzo.com>
    ========================================

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -2547,12 +2547,13 @@ static int svm_set_msr(struct kvm_vcpu *vcpu, struct msr_data *msr)
 		break;
 	case MSR_IA32_PRED_CMD:
 		if (!msr->host_initiated &&
+		    !guest_cpuid_has(vcpu, X86_FEATURE_SPEC_CTRL) &&
 		    !guest_cpuid_has(vcpu, X86_FEATURE_AMD_IBPB))
 			return 1;
 
 		if (data & ~PRED_CMD_IBPB)
 			return 1;
-		if (!boot_cpu_has(X86_FEATURE_AMD_IBPB))
+		if (!boot_cpu_has(X86_FEATURE_IBPB))
 			return 1;
 		if (!data)
 			break;
