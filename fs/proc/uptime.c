192cc3671dcfabbfcc1362901df25039abe8004d
Konstantin Khorenko <khorenko@virtuozzo.com>
2020-12-07 15:51:07 +0300

    ve/time/stat: idle time virtualization in /proc/loadavg
    The patch is based on following vz7 commits:
      a58fb58bff1c ("Use ve init task's css instead of opening cgroup via vfs")
      75fc174adc36 ("sched: Port cpustat related patches")
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Reviewed-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- fs/proc/uptime.c
+++ fs/proc/uptime.c
@@ -10,10 +10,8 @@
 #include <linux/cgroup.h>
 #include <linux/ve.h>
 
-static int uptime_proc_show(struct seq_file *m, void *v)
+static inline void get_ve0_idle(struct timespec64 *idle)
 {
-	struct timespec64 uptime, offset;
-	struct timespec64 idle;
 	u64 nsec;
 	u32 rem;
 	int i;
@@ -22,11 +20,31 @@ static int uptime_proc_show(struct seq_file *m, void *v)
 	for_each_possible_cpu(i)
 		nsec += (__force u64) kcpustat_cpu(i).cpustat[CPUTIME_IDLE];
 
+	idle->tv_sec = div_u64_rem(nsec, NSEC_PER_SEC, &rem);
+	idle->tv_nsec = rem;
+}
+
+static inline void get_veX_idle(struct ve_struct *ve, struct timespec64 *idle)
+{
+	struct kernel_cpustat kstat;
+
+	ve_get_cpu_stat(ve, &kstat);
+	*idle = ns_to_timespec64(kstat.cpustat[CPUTIME_IDLE]);
+}
+
+static int uptime_proc_show(struct seq_file *m, void *v)
+{
+	struct timespec64 uptime, offset;
+	struct timespec64 idle;
+	struct ve_struct *ve = get_exec_env();
+
+	if (ve_is_super(ve))
+		get_ve0_idle(&idle);
+	else
+		get_veX_idle(ve, &idle);
+
 	ktime_get_boottime_ts64(&uptime);
 	timens_add_boottime(&uptime);
-
-	idle.tv_sec = div_u64_rem(nsec, NSEC_PER_SEC, &rem);
-	idle.tv_nsec = rem;
 #ifdef CONFIG_VE
 	if (!ve_is_super(get_exec_env())) {
 		offset = ns_to_timespec64(get_exec_env()->real_start_time);
