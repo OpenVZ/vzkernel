58b2cd949131965c57ddd7f584144645cf97fafe
Andrey Ryabinin <aryabinin@virtuozzo.com>
2020-12-21 19:42:59 +0300

    proc,memcg: use memcg limits for showing oom_score inside CT
    Use memcg's limits of task to show /proc/<pid>/oom_score.
    
    Note: in vz7 we had different behavior. It showed 'oom_score'
    based on 've->memcg' limits of process reading oom_score.
    
    Now we look at memcg of <pid> process and don't care about the
    current one. It seems more correct behaviour.
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -526,8 +526,14 @@ static const struct file_operations proc_lstats_operations = {
 static int proc_oom_score(struct seq_file *m, struct pid_namespace *ns,
 			  struct pid *pid, struct task_struct *task)
 {
-	unsigned long totalpages = totalram_pages + total_swap_pages;
+	unsigned long totalpages;
 	unsigned long points = 0;
+	struct mem_cgroup *memcg;
+
+	rcu_read_lock();
+	memcg = mem_cgroup_from_task(task);
+	totalpages = mem_cgroup_total_pages(memcg);
+	rcu_read_unlock();
 
 	points = oom_badness(task, NULL, NULL, totalpages, NULL) *
 					1000 / totalpages;
