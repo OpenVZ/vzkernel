66bbe4261c53cfcb98c4f2715f12f73d7d1bece3
Jan Kara <jack@suse.cz>
2021-02-05 14:30:24 +0300

    ms/writeback: Export inode_io_list_del()
    Ext4 needs to remove inode from writeback lists after it is out of
    visibility of its journalling machinery (which can still dirty the
    inode). Export inode_io_list_del() for it.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Link: https://lore.kernel.org/r/20200421085445.5731-3-jack@suse.cz
    Signed-off-by: Theodore Ts'o <tytso@mit.edu>
    
    (cherry picked from commit 4301efa4c7cca11556dd89899eee066d49b47bf7)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- include/linux/writeback.h
+++ include/linux/writeback.h
@@ -116,6 +116,7 @@ void wakeup_flusher_threads(long nr_pages, enum wb_reason reason);
 void wakeup_flusher_threads_ub(long nr_pages, struct user_beancounter *ub,
 			enum wb_reason reason);
 void inode_wait_for_writeback(struct inode *inode);
+void inode_io_list_del(struct inode *inode);
 
 /* writeback.h requires fs.h; it, too, is not included from here. */
 static inline void wait_on_inode(struct inode *inode)
