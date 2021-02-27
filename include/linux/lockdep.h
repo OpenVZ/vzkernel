d07bfc49c7c704ffec7d848751a77054ab17d4e5
Shuah Khan <skhan@linuxfoundation.org>
2021-02-26 17:06:59 -0700

    ms/lockdep: Add lockdep lock state defines
    Adds defines for lock state returns from lock_is_held_type() based on
    Johannes Berg's suggestions as it make it easier to read and maintain
    the lock states. These are defines and a enum to avoid changes to
    lock_is_held_type() and lockdep_is_held() return types.
    
    Updates to lock_is_held_type() and  __lock_is_held() to use the new
    defines.
    
    Signed-off-by: Shuah Khan <skhan@linuxfoundation.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/linux-wireless/871rdmu9z9.fsf@codeaurora.org/
    
    (cherry picked from ms commit f8cfa46608f8aa5ca5421ce281ab314129c15411)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- include/linux/lockdep.h
+++ include/linux/lockdep.h
@@ -336,6 +336,11 @@ extern void lock_acquire(struct lockdep_map *lock, unsigned int subclass,
 extern void lock_release(struct lockdep_map *lock, int nested,
 			 unsigned long ip);
 
+/* lock_is_held_type() returns */
+#define LOCK_STATE_UNKNOWN	-1
+#define LOCK_STATE_NOT_HELD	0
+#define LOCK_STATE_HELD		1
+
 /*
  * Same "read" as for lock_acquire(), except -1 means any.
  */
@@ -368,11 +373,13 @@ extern void lockdep_trace_alloc(gfp_t mask);
 #define lockdep_depth(tsk)	(debug_locks ? (tsk)->lockdep_depth : 0)
 
 #define lockdep_assert_held(l)	do {					\
-		WARN_ON(debug_locks && lockdep_is_held(l) == 0);	\
+		WARN_ON(debug_locks &&					\
+			lockdep_is_held(l) == LOCK_STATE_NOT_HELD);	\
 	} while (0)
 
 #define lockdep_assert_not_held(l)	do {				\
-		WARN_ON(debug_locks && lockdep_is_held(l) == 1);	\
+		WARN_ON(debug_locks &&					\
+			lockdep_is_held(l) == LOCK_STATE_HELD);		\
 	} while (0)
 
 #define lockdep_assert_held_exclusive(l)	do {			\
