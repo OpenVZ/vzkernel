66893d511b71b8ad4085169c3c763111f7013820
Kirill Tkhai <ktkhai@virtuozzo.com>
2021-03-18 11:12:23 +0300

    ve: Add option allowing ioctl(FITRIM) from inside CT
    https://jira.sw.ru/browse/PSBM-127082
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- kernel/ve/ve.c
+++ kernel/ve/ve.c
@@ -70,6 +70,12 @@ struct module no_module = { .state = MODULE_STATE_GOING };
 EXPORT_SYMBOL(no_module);
 #endif
 
+bool ve_allow_ioctl_fitrim = 0;
+module_param_named(allow_ioctl_fitrim, ve_allow_ioctl_fitrim, bool, 0644);
+MODULE_PARM_DESC(ve_allow_ioctl_fitrim,
+		 "Allow ioctl(FITRIM) from inside VE. Only ext4 is supported now");
+EXPORT_SYMBOL(ve_allow_ioctl_fitrim);
+
 static DEFINE_PER_CPU(struct kstat_lat_pcpu_snap_struct, ve0_lat_stats);
 
 struct ve_struct ve0 = {
