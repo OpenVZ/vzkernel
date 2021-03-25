63c0f64e7d7e5ab6459cbbb74bce9a0482b6afe3
Hangbin Liu <haliu@redhat.com>
2021-03-25 10:12:23 +0800

    wireguard: mark as Tech Preview
    Bugzilla: https://bugzilla.redhat.com/show_bug.cgi?id=1613522
    Upstream: RHEL only
    
    WireGuard is Tech Preview in RHEL 9.0.
    
    Signed-off-by: Hangbin Liu <haliu@redhat.com>

--- drivers/net/wireguard/main.c
+++ drivers/net/wireguard/main.c
@@ -48,6 +48,7 @@ static int __init mod_init(void)
 	pr_info("WireGuard " WIREGUARD_VERSION " loaded. See www.wireguard.com for information.\n");
 	pr_info("Copyright (C) 2015-2019 Jason A. Donenfeld <Jason@zx2c4.com>. All Rights Reserved.\n");
 
+	mark_tech_preview("WireGuard", THIS_MODULE);
 	return 0;
 
 err_netlink:
