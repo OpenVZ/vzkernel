1bf26d054c3171e28a00128f3b77a5c27fe2bdea
Vasily Averin <vvs@virtuozzo.com>
2021-04-14 12:50:45 +0300

    memcg: disable accounting in netdev_create_hash()
    netdev_create_hash() is called twice per netns and
    allocates 1 page only. I think it is too small to account this.
    
    https://jira.sw.ru/browse/PSBM-120694
    To_merge: 06fac184ac6b ("memcg: charge kmem allocations accounted to UBC in PCS6
    to memcg")
    
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -9854,7 +9854,7 @@ static struct hlist_head * __net_init netdev_create_hash(void)
 	int i;
 	struct hlist_head *hash;
 
-	hash = kmalloc_array(NETDEV_HASHENTRIES, sizeof(*hash), GFP_KERNEL_ACCOUNT);
+	hash = kmalloc_array(NETDEV_HASHENTRIES, sizeof(*hash), GFP_KERNEL);
 	if (hash != NULL)
 		for (i = 0; i < NETDEV_HASHENTRIES; i++)
 			INIT_HLIST_HEAD(&hash[i]);
