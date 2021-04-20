885d9ecc8877a31f1ee4d3171de551331ac3046e
David Ward <david.ward@ll.mit.edu>
2021-04-19 22:15:03 -0400

    Replace "flavour" where "variant" is meant instead
    In the SPEC file, "flavour" is used when referring to the kernel "variant"
    instead (such as debug, lpae, or zfcpdump). In the Fedora/ARK documentation
    and scripts, "flavor" refers to a configuration directory (fedora or ark)
    underneath redhat/configs/ in the os-build branch.
    
    Signed-off-by: David Ward <david.ward@ll.mit.edu>

--- redhat/mod-sign.sh
+++ redhat/mod-sign.sh
@@ -2,7 +2,7 @@
 
 # The modules_sign target checks for corresponding .o files for every .ko that
 # is signed. This doesn't work for package builds which re-use the same build
-# directory for every flavour, and the .config may change between flavours.
+# directory for every variant, and the .config may change between variants.
 # So instead of using this script to just sign lib/modules/$KernelVer/extra,
 # sign all .ko in the buildroot.
 
