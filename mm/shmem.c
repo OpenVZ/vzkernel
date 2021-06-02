1b74259fc551fdf565ffc975614e5c8b1b336241
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2021-06-02 14:02:43 +0300

    shmem/ve: virtualize tmpfs default size
    Set default size to half of physpages from ve memory cgroup. User in ct
    would get -ENOSPC on 1/2 memory limit reached like we do on host.
    
    https://jira.sw.ru/browse/PCLIN-26841
    https://jira.sw.ru/browse/PSBM-127785
    
    (extracted from vz7 commit dce154c32257 ("ub/memcg: introduce
    ub_total_pages"))
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    +++
    shmem/ve: Keep host's totalram_pages intact during CT tmpfs default size calculation
    
    During tmpfs default size virtualization we occasionally
    overwrite 'totalram_pages' global value used for host.
    
    Fix it.
    
    mFixes: b563045c1998 ("shmem/ve: virtualize tmpfs default size")
    https://jira.sw.ru/browse/PSBM-130518
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- mm/shmem.c
+++ mm/shmem.c
@@ -35,6 +35,7 @@
 #include <linux/uio.h>
 #include <linux/khugepaged.h>
 #include <linux/hugetlb.h>
+#include <linux/ve.h>
 
 #include <asm/tlbflush.h> /* for arch/microblaze update_mmu_cache() */
 
@@ -106,14 +107,31 @@ struct shmem_falloc {
 };
 
 #ifdef CONFIG_TMPFS
+static unsigned long tmpfs_ram_pages(void)
+{
+	struct ve_struct *ve = get_exec_env();
+	struct cgroup_subsys_state *css;
+	unsigned long ve_ram_pages;
+
+	if (ve_is_super(ve))
+		return totalram_pages;
+
+	css = ve_get_init_css(ve, memory_cgrp_id);
+	ve_ram_pages = min(totalram_pages,
+			mem_cgroup_total_pages(mem_cgroup_from_css(css)));
+	css_put(css);
+
+	return ve_ram_pages;
+}
+
 static unsigned long shmem_default_max_blocks(void)
 {
-	return totalram_pages / 2;
+	return tmpfs_ram_pages() / 2;
 }
 
 static unsigned long shmem_default_max_inodes(void)
 {
-	return min(totalram_pages - totalhigh_pages, totalram_pages / 2);
+	return min(totalram_pages - totalhigh_pages, tmpfs_ram_pages() / 2);
 }
 #endif
 
