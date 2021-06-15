d8f543d8e68194f5d18b0150887cda8178e38c4d
Kirill Tkhai <ktkhai@virtuozzo.com>
2021-06-15 19:27:02 +0300

    ext4: Care about reserves in case of IO thread
    Kthread has root permittions, and it may exceed
    ext4 reserves (see ext4_has_free_clusters(),
    also see tune2fs -r).
    
    Introduce a new flag PF_IO_THREAD, which indicates
    a thread of *loop blocks devices, and care about
    it in ext4_has_free_clusters().
    
    Loop is the first user.
    
    Note, that ms kernel (06.2021) still has free PF_xxx
    bits, so I choosed to use one in 4.18.
    
    https://jira.sw.ru/browse/PSBM-127225
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- drivers/block/loop.c
+++ drivers/block/loop.c
@@ -942,7 +942,7 @@ static void loop_unprepare_queue(struct loop_device *lo)
 
 static int loop_kthread_worker_fn(void *worker_ptr)
 {
-	current->flags |= PF_LOCAL_THROTTLE | PF_MEMALLOC_NOIO;
+	current->flags |= PF_LOCAL_THROTTLE | PF_MEMALLOC_NOIO | PF_IO_THREAD;
 	return kthread_worker_fn(worker_ptr);
 }
 
