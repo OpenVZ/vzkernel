0df1bb146279db80d8cf054e64280a825e5a9678
Kirill Tkhai <ktkhai@virtuozzo.com>
2021-06-17 19:01:06 +0300

    ploop: Save level before submitting pio
    Introduce a new field and place it there.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    =====================
    Patchset description:
    
    ploop: Allow to resubmit partially completed request
    
    This allows to continue submitting partially completed requests.
    
    https://jira.sw.ru/browse/PSBM-127225
    
    Kirill Tkhai (18):
          ploop: Simplify ploop_write_cluster_sync()
          ploop: Rename hook->pio, h->pio, ploop_cow::hook->aux_pio
          ploop: Rename force_link_inflight_bios
          ploop: Introduce separate lock for inflight pios
          ploop: Use hlist instead of rbtree
          ploop: Always link submitted pios
          ploop: Unexport ploop_inflight_bios_ref_switch()
          ploop: Refactor submit_pio()
          ploop: Introduce ploop_suspend_submitting_pios
          ploop: Refactor ploop_ctr()
          ploop: Use ploop_call_rw_iter() in submit_delta_read()
          ploop: Generalize submit_rw_mapped()
          ploop: Kill submit_delta_read()
          ploop: Rename submit_rw_mapped()
          ploop: Extract submit_rw_mapped() to separate function
          ploop: Save level before submitting pio
          ploop: Make fsync work be able to run in parallel with main work
          ploop: Introduce resubmitting partially completed pios
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- drivers/md/dm-ploop-map.c
+++ drivers/md/dm-ploop-map.c
@@ -111,6 +111,7 @@ void init_pio(struct ploop *ploop, unsigned int bi_op, struct pio *pio)
 	INIT_LIST_HEAD(&pio->endio_list);
 	/* FIXME: assign real cluster? */
 	pio->cluster = UINT_MAX;
+	pio->level = BAT_LEVEL_INVALID;
 }
 
 /* Get cluster related to pio sectors */
@@ -1022,7 +1023,7 @@ static void data_rw_complete(struct pio *pio)
 	pio_endio(pio);
 }
 
-static void submit_rw_mapped(struct ploop *ploop, struct pio *pio, u8 level)
+static void submit_rw_mapped(struct ploop *ploop, struct pio *pio)
 {
 	unsigned int rw, nr_segs;
 	struct bio_vec *bvec;
@@ -1030,7 +1031,7 @@ static void submit_rw_mapped(struct ploop *ploop, struct pio *pio, u8 level)
 	struct file *file;
 	loff_t pos;
 
-	BUG_ON(level > top_level(ploop));
+	BUG_ON(pio->level > top_level(ploop));
 
 	pio->complete = data_rw_complete;
 
@@ -1043,14 +1044,16 @@ static void submit_rw_mapped(struct ploop *ploop, struct pio *pio, u8 level)
 
 	pos = to_bytes(pio->bi_iter.bi_sector);
 
-	file = ploop->deltas[level].file;
+	file = ploop->deltas[pio->level].file;
 	ploop_call_rw_iter(file, pos, rw, &iter, pio);
 }
 
 void map_and_submit_rw(struct ploop *ploop, u32 dst_clu, struct pio *pio, u8 level)
 {
 	remap_to_cluster(ploop, pio, dst_clu);
-	submit_rw_mapped(ploop, pio, level);
+	pio->level = level;
+
+	submit_rw_mapped(ploop, pio);
 }
 
 static void initiate_delta_read(struct ploop *ploop, unsigned int level,
