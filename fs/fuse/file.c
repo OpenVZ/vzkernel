f9594fdf249a219115b8704b124d28985f9a5c6b
Alexey Kuznetsov <kuznet@acronis.com>
2021-06-20 19:42:48 +0000

    fuse: fix fiemap looping
    Well, the function returns size on success.
    
    Together with previous bug fixed in "[RHEL8] fuse: memory leakage"
    this resulted in instant death of machine with aggressive oom killing.
    It took long time to figure this out, very inconvenient situation,
    when working with remote access.
    
    In the scope of https://jira.sw.ru/browse/PSBM-129289
    
    Signed-off-by: Alexey Kuznetsov <kuznet@acronis.com>
    Feature: vStorage

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -3630,6 +3630,7 @@ static int fuse_request_fiemap(struct inode *inode, u32 cur_max,
 	int err;
 	int npages = 0;
 	int allocated = 0;
+	int out_size;
 
 	err = 0;
 	spin_lock(&fi->lock);
@@ -3707,8 +3708,10 @@ static int fuse_request_fiemap(struct inode *inode, u32 cur_max,
 	}
 
 	err = fuse_simple_request(fm, &ap.args);
-	if (err)
+	if (err < 0)
 		goto out;
+	out_size = err;
+	err = 0;
 
 	if (cur_max == 0) {
 		dest->fi_extents_mapped += ofiemap.fm_mapped_extents;
@@ -3724,6 +3727,11 @@ static int fuse_request_fiemap(struct inode *inode, u32 cur_max,
 		int i;
 
 		if (ofiemap.fm_mapped_extents > cur_max) {
+			err = -EIO;
+			goto out;
+		}
+
+		if (ofiemap.fm_mapped_extents * sizeof(struct fiemap_extent) > out_size) {
 			err = -EIO;
 			goto out;
 		}
