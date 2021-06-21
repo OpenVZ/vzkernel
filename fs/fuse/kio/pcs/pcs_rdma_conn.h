f01edb51c859284f137c7c919e9dd14c4faaa65f
Konstantin Khorenko <khorenko@virtuozzo.com>
2021-06-21 17:01:17 +0300

    fs/fuse/kio/pcs/pcs_rdma_conn.h: copyright update
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/fuse/kio/pcs/pcs_rdma_conn.h
+++ fs/fuse/kio/pcs/pcs_rdma_conn.h
@@ -1,3 +1,10 @@
+/*
+ *  fs/fuse/kio/pcs/pcs_rdma_conn.h
+ *
+ *  Copyright (c) 2018-2021 Virtuozzo International GmbH. All rights reserved.
+ *
+ */
+
 #ifndef _PCS_RMDA_CONN_H_
 #define _PCS_RMDA_CONN_H_ 1
 
