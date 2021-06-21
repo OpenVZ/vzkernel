b2348156b05012b71647e8047e1655ae332e519f
Konstantin Khorenko <khorenko@virtuozzo.com>
2021-06-21 17:01:18 +0300

    fs/fuse/kio/pcs/pcs_sock_conn.h: copyright update
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/fuse/kio/pcs/pcs_sock_conn.h
+++ fs/fuse/kio/pcs/pcs_sock_conn.h
@@ -1,3 +1,10 @@
+/*
+ *  fs/fuse/kio/pcs/pcs_sock_conn.h
+ *
+ *  Copyright (c) 2018-2021 Virtuozzo International GmbH. All rights reserved.
+ *
+ */
+
 #ifndef _PCS_SOCK_CONN_H_
 #define _PCS_SOCK_CONN_H_ 1
 
