a877622f759c91036ca4be2272d66e87474a2068
Konstantin Khorenko <khorenko@virtuozzo.com>
2021-06-21 17:01:16 +0300

    fs/fuse/kio/pcs/pcs_prot_types.h: copyright update
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/fuse/kio/pcs/pcs_prot_types.h
+++ fs/fuse/kio/pcs/pcs_prot_types.h
@@ -1,3 +1,10 @@
+/*
+ *  fs/fuse/kio/pcs/pcs_prot_types.h
+ *
+ *  Copyright (c) 2018-2021 Virtuozzo International GmbH. All rights reserved.
+ *
+ */
+
 #ifndef _PCS_PROT_TYPES_H_
 #define _PCS_PROT_TYPES_H_ 1
 
