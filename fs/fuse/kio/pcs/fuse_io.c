d82a99eebeb17bfa3b2f37c77a1e25ac35d318a8
Konstantin Khorenko <khorenko@virtuozzo.com>
2021-06-21 17:01:13 +0300

    fs/fuse/kio/pcs/fuse_io.c: copyright update
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/fuse/kio/pcs/fuse_io.c
+++ fs/fuse/kio/pcs/fuse_io.c
@@ -1,3 +1,10 @@
+/*
+ *  fs/fuse/kio/pcs/fuse_io.c
+ *
+ *  Copyright (c) 2018-2021 Virtuozzo International GmbH. All rights reserved.
+ *
+ */
+
 #include <net/sock.h>
 #include <linux/net.h>
 #include <linux/kthread.h>
