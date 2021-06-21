a31c15e51f85113c4edaf55d0cc7b98dea99b13d
Konstantin Khorenko <khorenko@virtuozzo.com>
2021-06-21 17:01:16 +0300

    fs/fuse/kio/pcs/pcs_mds_prot.h: copyright update
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/fuse/kio/pcs/pcs_mds_prot.h
+++ fs/fuse/kio/pcs/pcs_mds_prot.h
@@ -1,3 +1,10 @@
+/*
+ *  fs/fuse/kio/pcs/pcs_mds_prot.h
+ *
+ *  Copyright (c) 2018-2021 Virtuozzo International GmbH. All rights reserved.
+ *
+ */
+
 #ifndef _PCS_MDS_PROT_H_
 #define _PCS_MDS_PROT_H_ 1
 
