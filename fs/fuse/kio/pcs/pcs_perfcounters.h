c156e21f7b31bedee940d7aa371bbf9c4b9a2af8
Konstantin Khorenko <khorenko@virtuozzo.com>
2021-06-21 17:01:16 +0300

    fs/fuse/kio/pcs/pcs_perfcounters.h: copyright update
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/fuse/kio/pcs/pcs_perfcounters.h
+++ fs/fuse/kio/pcs/pcs_perfcounters.h
@@ -1,3 +1,10 @@
+/*
+ *  fs/fuse/kio/pcs/pcs_perfcounters.h
+ *
+ *  Copyright (c) 2018-2021 Virtuozzo International GmbH. All rights reserved.
+ *
+ */
+
 #ifndef _PCS_PERFCOUNTERS_H_
 #define _PCS_PERFCOUNTERS_H_ 1
 
