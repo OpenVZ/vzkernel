0a288732a9b28050062c2ce8ff7bd91eacf92a93
Konstantin Khorenko <khorenko@virtuozzo.com>
2021-06-21 17:01:18 +0300

    fs/fuse/kio/pcs/pcs_rpc_prot.h: copyright update
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- fs/fuse/kio/pcs/pcs_rpc_prot.h
+++ fs/fuse/kio/pcs/pcs_rpc_prot.h
@@ -1,3 +1,10 @@
+/*
+ *  fs/fuse/kio/pcs/pcs_rpc_prot.h
+ *
+ *  Copyright (c) 2018-2021 Virtuozzo International GmbH. All rights reserved.
+ *
+ */
+
 #ifndef _PCS_NET_PROT_H_
 #define _PCS_NET_PROT_H_ 1
 
