2ad20223291dcaa51889e10ed27f5df493d6f573
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2021-09-22 14:50:54 +0300

    kernfs/sysfs: add ioctl to get fd network namespace tag
    Sysfs mounts save current netns when mounting and show different set of
    network devices in /sys/class/net based on it. But there is currently no
    simple way to find out to that netns the sysfs mount is tagged to.
    
    Lets add an ioctl so that we can get open file descriptor to this
    namespace on nsfs. Next we would be able to readlink this fd to get
    network namespace id.
    
    Sysfs holds only "passive" reference to the net namespace, so the only
    guaranty we have is that struct net is not freed yet. So lets add a
    helper maybe_get_net_ns to get net only if it's fully alive.
    
    https://jira.sw.ru/browse/PSBM-105161
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    +++
    kernfs/sysfs: include proc_fs.h for open_related_ns() visibility
    
    mFixes: d53f348850ab ("kernfs/sysfs: add ioctl to get fd network namespace
    tag")
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    (cherry-picked from vz8 commit ddb41e4466a775d9a956721581d311bc6a832516)
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Feature: sysfs: per-CT entries visibility and permissions configuration

--- fs/kernfs/kernfs-internal.h
+++ fs/kernfs/kernfs-internal.h
@@ -165,6 +165,7 @@ struct kernfs_node *kernfs_new_node(struct kernfs_node *parent,
 extern const struct file_operations kernfs_file_fops;
 
 void kernfs_drain_open_files(struct kernfs_node *kn);
+long kernfs_ioctl(struct file *file, unsigned int ioctl, unsigned long arg);
 
 /*
  * symlink.c
--- include/linux/kernfs.h
+++ include/linux/kernfs.h
@@ -17,6 +17,7 @@
 #include <linux/uidgid.h>
 #include <linux/wait.h>
 #include <linux/rwsem.h>
+#include <linux/ioctl.h>
 
 struct file;
 struct dentry;
@@ -593,5 +594,9 @@ static inline int kernfs_rename(struct kernfs_node *kn,
 {
 	return kernfs_rename_ns(kn, new_parent, new_name, NULL);
 }
+
+#define KERNIO 0xb8
+
+#define KERNFS_GET_NS _IO(KERNIO, 0x1)
 
 #endif	/* __LINUX_KERNFS_H */
