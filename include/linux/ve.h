57851f8a608e3c14421e5869907ab3508a58cdc2
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2021-09-22 14:50:58 +0300

    ve/netns: limit number of network namespaces per container
    Network namespaces are destroyed asynchronously from net_cleanup_work
    (cleanup_net()). This work destroys all network namespaces which are
    queued in net_kill_list. It's doing this under net_mutex.
    
    This work is executed from the "kworker" kernel thead. We know that here
    is an avalanch affect. If N processes which creates network namespaces
    don't affect a time of creating new namespaces in other containers, it
    doesn't mean that N + 1 processes will not increase a creation time in
    dozen times. It can be explained too.
    
    The longer kworker is running on cpu, the longer it will be
    not scheduled in the next time and the more namespaces will be
    created for this period of time.
    
    The next script can be used to reproduce the problem:
    while :; do
    	n=`jobs | wc -l`
    	n=$((10000 - $n))
    	for i in `seq $n`; do
    		unshare -m true &
    	done
    	wait -n 1
    done
    
    https://jira.sw.ru/browse/PSBM-49690
    
    v2: Describe the real reason of the issue.
    v3: - don't limit a number of netns for ve0
        - initialize netns before returning an error, otherwise
          net_namespace structures are not freed.
    v4: don't allocate a net_namespace structure if a ct hits the limit.
    
    khorenko@: v5: s/netns_nr/netns_avail_nr/g
    The variable contains the number of available netns, not currently existing.
    
    Cc: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    
    Signed-off-by: Andrey Vagin <avagin@openvz.org>
    
    Reviewed-by: Cyrill Gorcunov <gorcunov@openvz.org>
    
    Rebase 4.14:
    Add separate read/write callbacks for cgroup files as for iptables_mask
    and features. Add helpers to change netns_avail_nr and handle yet
    unhandled error-path for net_alloc().
    
    We still need it as ms sysctl max_net_namespaces is per-user not per
    ve/userns and it sets same max value for all users, thus it is not
    flexible enough for us.
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    (cherry-picked from vz8 commit 43e6ffb41ba7dccc3d225b0d8a954a9453167e7b)
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Feature: netns: limit number of netns per-CT

--- include/linux/ve.h
+++ include/linux/ve.h
@@ -48,9 +48,14 @@ struct ve_struct {
 	__u64			features;
 
 	struct kmapset_key	sysfs_perms_key;
+
+	atomic_t		netns_avail_nr;
+	int			netns_max_nr;
 };
 
 extern int nr_ve;
+
+#define NETNS_MAX_NR_DEFAULT	256	/* number of net-namespaces per-VE */
 
 #define capable_setveid() \
 	(ve_is_super(get_exec_env()) && capable(CAP_SYS_ADMIN))
