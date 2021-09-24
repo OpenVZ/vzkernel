ef6436eff36a6b69925120d210196fb0c015e662
Vladimir Davydov <vdavydov@parallels.com>
2021-09-24 15:04:56 +0300

    oom: resurrect berserker mode
    Feature: oom: berserker mode
    
    The logic behind the OOM berserker is the same as in PCS6: if processes
    are killed by oom killer too often (< sysctl vm.oom_relaxation, 1 sec by
    default), we increase "rage" (min -10, max 20) and kill 1 << "rage"
    youngest worst processes if "rage" >= 0.
    
    https://jira.sw.ru/browse/PSBM-17930
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    
    [aryabinin: vz8 rebase]
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    +++
    oom: Restore vm.oom_relaxation sysctl
    
    'Berserker mode' is used if the OOM killer has to act too often: if
    several tasks are killed within 'oom_relaxation' interval, additional tasks
    will be killed.
    
    In VZ7, 'vm.oom_relaxation' sysctl defined that value, but it is missing
    in VZ8. Restore it, because the default value (1000 jiffies, 1 sec)
    might be too small, for example, for slow or highly loaded machines.
    
    Done in the scope of https://jira.sw.ru/browse/PSBM-131983.
    
    Signed-off-by: Evgenii Shatokhin <eshatokhin@virtuozzo.com>
    
    +++
    oom: Initialize oom_rage_lock spinlock
    
    Lockdep complained about it as follows:
    Done in the scope of https://jira.sw.ru/browse/PSBM-131983.
    
    Signed-off-by: Evgenii Shatokhin <eshatokhin@virtuozzo.com>
    
    (cherry picked from vz8 commit 300a06439b2754e3486a00b68c2753e6c27a8f16)
    Signed-off-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>
    Feature: mm/oom: berserker mode
    
    +++
    mm/oom_kill: account berserker kills to process memcg
    
    General __oom_kill_process() accounts oom kills to process memcg. Change
    berserker to do the same.
    
    https://jira.sw.ru/browse/PSBM-131983
    Signed-off-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -300,6 +300,11 @@ struct mem_cgroup {
 	/* OOM-Killer disable */
 	int		oom_kill_disable;
 
+	int		oom_rage;
+	spinlock_t	oom_rage_lock;
+	unsigned long	prev_oom_time;
+	unsigned long	oom_time;
+
 	/* memory.events and memory.events.local */
 	struct cgroup_file events_file;
 	struct cgroup_file events_local_file;
@@ -808,6 +813,7 @@ static inline struct lruvec *mem_cgroup_page_lruvec(struct page *page)
 
 struct mem_cgroup *mem_cgroup_from_task(struct task_struct *p);
 
+bool task_in_mem_cgroup(struct task_struct *task, struct mem_cgroup *memcg);
 struct mem_cgroup *get_mem_cgroup_from_mm(struct mm_struct *mm);
 
 struct lruvec *lock_page_lruvec(struct page *page);
@@ -1297,6 +1303,12 @@ static inline struct mem_cgroup *parent_mem_cgroup(struct mem_cgroup *memcg)
 
 static inline bool mm_match_cgroup(struct mm_struct *mm,
 		struct mem_cgroup *memcg)
+{
+	return true;
+}
+
+static inline bool task_in_mem_cgroup(struct task_struct *task,
+				      const struct mem_cgroup *memcg)
 {
 	return true;
 }
