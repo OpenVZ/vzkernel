574660fa2b6b5f7b51100bc52ae27b5e5c78ed10
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2021-09-24 15:02:30 +0300

    ve/net: introduce vz_security_*_check checks
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@openvz.org>
    
    +++
    ve/netlink: allow messages with family PF_BRIDGE type RTM_xxxNEIGH in CT
    
    While reproducing the problem mentioned in patch 1 I found that
    we need it to be able to configure vxlan fdb (Forwarding Database entry).
    
    https://jira.sw.ru/browse/PSBM-53629
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Acked-by: Andrei Vagin <avagin@virtuozzo.com>
    
    (cherry picked from vz7 commit da2b4bd3e7a5 ("ve/netlink: allow messages
    with family PF_BRIDGE type RTM_xxxNEIGH in CT"))
    
    VZ 8 rebase part https://jira.sw.ru/browse/PSBM-127783
    
    Signed-off-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
    
    +++
    ve: Change error code in vz_security_protocol_check to -EPROTONOSUPPORT
    
    'vz_security_protocol_check' is a part of socket creation routine.
    Socket creation can be split into separate stages:
     - family validation and family specific object creation
     - protocol validation and protocol specific object creation
    First family argument is validated. If family is ok, then the code
    can proceeds to further work with protocol agrument.
    
    As part of family validation procedure for containers
    vz_security_family_check is called. If family is not supported in
    container environment and the current context is container the
    function returns with -EAFNOSUPPORT code.
    
    As part of protocol validation procedure for containers
    vz_security_protocol_check is called. If protocol is not supported
    in container environment and the current context is container the
    function CURRENTLY returns with -EAFNOSUPPORT code, although by
    context of the current socket preparation step it should instead
    return -EPROTONOSUPPORT.
    
    https://jira.sw.ru/browse/PSBM-104225
    
    Signed-off-by: Valeriy Vdovin <valeriy.vdovin@virtuozzo.com>
    
    Acked-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ve/net: allow IPPROTO_ICMPV6 protocol inside a Container
    
    This patch allows "ping6" utility to work via ICMP socket
    without necessity to failback to RAW socket for ipv6.
    
    We do allow this for ipv4, let's allow ICMP socket for ipv6 as well.
    
    Note: by default ping6 will still use RAW socket because of default
    settings in "net.ipv4.ping_group_range", but this is another side of the
    problem.
    
    https://jira.sw.ru/browse/PSBM-104225
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    https://jira.sw.ru/browse/PSBM-133986
    
    (cherry picked from commit 6b08f48fc06695995e8388ca9de5acf91f125cdb)
    Signed-off-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>

--- include/linux/ve.h
+++ include/linux/ve.h
@@ -89,6 +89,9 @@ extern struct cgroup *cgroup_get_ve_root1(struct cgroup *cgrp);
 
 #define ve_uevent_seqnum       (get_exec_env()->_uevent_seqnum)
 
+extern int vz_security_family_check(struct net *net, int family, int type);
+extern int vz_security_protocol_check(struct net *net, int protocol);
+
 #else	/* CONFIG_VE */
 #define get_ve(ve)	((void)(ve), NULL)
 #define put_ve(ve)	do { (void)(ve); } while (0)
@@ -113,6 +116,9 @@ static inline struct cgroup *cgroup_get_ve_root1(struct cgroup *cgrp)
 	return NULL;
 }
 #define ve_uevent_seqnum uevent_seqnum
+
+static inline int vz_security_family_check(struct net *net, int family, int type) { return 0; }
+static inline int vz_security_protocol_check(struct net *net, int protocol) { return 0; }
 
 #endif	/* CONFIG_VE */
 
