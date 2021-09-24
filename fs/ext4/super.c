defcfacbb67526b9a433d1093bc3cd09233e12d1
Kirill Tkhai <ktkhai@virtuozzo.com>
2021-09-24 15:48:44 +0300

    fs: Mask appropriate filesystems FS_VIRTUALIZED
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    
    +++
    ve/fs/autofs: Allow autofs to be used inside a container
    
    It turned out that autofs is used at least for NFS/CIFS and binfmt_misc.
    
    Let's use new FS_VE_MOUNT flag to only allow autofs mounts in
    conainer-init userns, which would mimic mainstream host behaviour.
    
    https://jira.sw.ru/browse/PSBM-26968
    https://jira.sw.ru/browse/PSBM-86138
    https://jira.sw.ru/browse/PSBM-128649
    
    Rebase from VZ7, with use of FS_VE_MOUNT and with comment cleanup:
    vz7 commits:
    83c27c7bffcc1 ("ve/fs/autofs: mark autofs as virtualized")
    0045b317c97b ("ve/fs/autofs: allow to mount autofs inside non-root userns")
    
    mFixes: 7ddb6e8dd9f6 ("fs: Mask appropriate filesystems FS_VIRTUALIZED")
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    +++
    fs/ext4: removed FS_VIRTUALIZED flag from 'ext3' mode of ext4 module
    
    FS_VIRTUALIZED flag makes a registered filesystem visible in container.
    ext3 is part of ext4 module that is run with untested set of flags.
    
    Signed-off-by: Valeriy.Vdovin <valeriy.vdovin@virtuozzo.com>
    
    khorenko@: this flag allows to mount ext3 fs inside a Container
    if a Container has proper permissions for a device to be mounted.
    In vz7 we never allowed it, we don't test it, so don't see any
    advantages of enabling it in vz8. We can change this of course upon
    request.
    
    (cherry picked from vz8 commit b594c31c974e708e96826dcd2d903c6472dfea2d)
    Signed-off-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -6704,7 +6704,7 @@ static struct file_system_type ext4_fs_type = {
 	.name		= "ext4",
 	.mount		= ext4_mount,
 	.kill_sb	= kill_block_super,
-	.fs_flags	= FS_REQUIRES_DEV | FS_ALLOW_IDMAP,
+	.fs_flags	= FS_REQUIRES_DEV | FS_ALLOW_IDMAP | FS_VIRTUALIZED,
 };
 MODULE_ALIAS_FS("ext4");
 
