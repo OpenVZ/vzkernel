a265e42a3a99f486bdc09b3c0e7d0f946d0f8b8e
Konstantin Khorenko <khorenko@virtuozzo.com>
2021-09-28 15:41:56 +0300

    trace/osnoise: Fix compiler complain about using "main" local variable
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Feature: fix compilation

--- kernel/trace/trace_osnoise.c
+++ kernel/trace/trace_osnoise.c
@@ -1512,20 +1512,20 @@ static void stop_per_cpu_kthreads(void)
 static int start_kthread(unsigned int cpu)
 {
 	struct task_struct *kthread;
-	void *main = osnoise_main;
+	void *main_func = osnoise_main;
 	char comm[24];
 
 #ifdef CONFIG_TIMERLAT_TRACER
 	if (osnoise_data.timerlat_tracer) {
 		snprintf(comm, 24, "timerlat/%d", cpu);
-		main = timerlat_main;
+		main_func = timerlat_main;
 	} else {
 		snprintf(comm, 24, "osnoise/%d", cpu);
 	}
 #else
 	snprintf(comm, 24, "osnoise/%d", cpu);
 #endif
-	kthread = kthread_create_on_cpu(main, NULL, cpu, comm);
+	kthread = kthread_create_on_cpu(main_func, NULL, cpu, comm);
 
 	if (IS_ERR(kthread)) {
 		pr_err(BANNER "could not start sampling thread\n");
