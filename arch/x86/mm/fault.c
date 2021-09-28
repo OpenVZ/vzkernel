1e8132af9ad86ae94e72361cebb5345b4e9b1e4f
Kirill Tkhai <ktkhai@virtuozzo.com>
2021-09-28 19:31:09 +0300

    sysctl: Change sysctl::show_unhandled_signals default
    Extracted from "Initial patch".
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    Otherwise we provide a Container owner a possibility to fill host
    logs with spam.
    
    (cherry picked from commit 852eb933400bb238bf03e7f58f2f0f79f74ead5f)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    (cherry picked from vz8 commit 83b905f93421525678f3866af3f5d90b88c8cdb8)
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Feature: sysctl: various syscontrols virtualization and permission tweaks

--- arch/x86/mm/fault.c
+++ arch/x86/mm/fault.c
@@ -1080,7 +1080,7 @@ spurious_kernel_fault(unsigned long error_code, unsigned long address)
 }
 NOKPROBE_SYMBOL(spurious_kernel_fault);
 
-int show_unhandled_signals = 1;
+int show_unhandled_signals = 0;
 
 static inline int
 access_error(unsigned long error_code, struct vm_area_struct *vma)
