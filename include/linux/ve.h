0dfc36287633c7ac4118f0df8f88a9ca332089df
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2021-09-28 14:42:33 +0300

    ve/kthreadd: emulate kthreadd by kworker thread, attached to CT
    This is the first version of kthreadd in a container (there will some
    improvements on top).
    With this patch kthreadd won't be visible in a container, but its children
    will be.
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    
    (cherry-picked from vz8 commit 1cc3a639c3fb ("ve/kthreadd: emulate
    kthreadd by kworker thread, attached to CT"))
    
    Signed-off-by: Nikita Yushchenko <nikita.yushchenko@virtuozzo.com>
    Feature: nfs/nfsd: containerization

--- include/linux/ve.h
+++ include/linux/ve.h
@@ -64,6 +64,8 @@ struct ve_struct {
 	int			_randomize_va_space;
 
 	unsigned long		meminfo_val;
+
+	struct kthread_worker	*kthreadd_worker;
 };
 
 #define VE_MEMINFO_DEFAULT	1	/* default behaviour */
