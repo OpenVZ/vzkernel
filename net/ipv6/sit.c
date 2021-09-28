b45945b82822b112d5bf423316be3958197d7a13
Vasily Averin <vvs@virtuozzo.com>
2021-09-28 14:16:12 +0300

    ms/memcg: ipv6/sit: account and don't WARN on ip_tunnel_prl structs allocation
    Author: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    The size of the ip_tunnel_prl structs allocation is controllable from
    user-space, thus it's better to avoid spam in dmesg if allocation failed.
    Also add __GFP_ACCOUNT as this is a good candidate for per-memcg
    accounting. Allocation is temporary and limited by 4GB.
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    
    Signed-off-by: David S. Miller <davem@davemloft.net>
    (cherry picked from commit 1b51d8271973e4966078f0c1a5cb061ae9a424d2)
    https://jira.sw.ru/browse/PSBM-133990
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    Feature: mm: fix CT resources accounting

--- net/ipv6/sit.c
+++ net/ipv6/sit.c
@@ -326,7 +326,7 @@ static int ipip6_tunnel_get_prl(struct net_device *dev, struct ip_tunnel_prl __u
 	 * we try harder to allocate.
 	 */
 	kp = (cmax <= 1 || ve_capable(CAP_NET_ADMIN)) ?
-		kcalloc(cmax, sizeof(*kp), GFP_KERNEL | __GFP_NOWARN) :
+		kcalloc(cmax, sizeof(*kp), GFP_KERNEL_ACCOUNT | __GFP_NOWARN) :
 		NULL;
 
 	ca = min(t->prl_count, cmax);
@@ -337,7 +337,8 @@ static int ipip6_tunnel_get_prl(struct net_device *dev, struct ip_tunnel_prl __u
 		 * For root users, retry allocating enough memory for
 		 * the answer.
 		 */
-		kp = kcalloc(ca, sizeof(*kp), GFP_ATOMIC);
+		kp = kcalloc(ca, sizeof(*kp), GFP_ATOMIC | __GFP_ACCOUNT |
+					      __GFP_NOWARN);
 		if (!kp) {
 			ret = -ENOMEM;
 			goto out;
