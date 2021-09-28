eafebd89a0b8c435618da57a814e52b4f2ca59a5
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2021-09-28 14:42:36 +0300

    ve/umh: add per-container usermodehelper thread
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    
    +++
    ve, kthread: Remove some useless crud.
    
    Simplify the code by removing useless macroses.
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    (cherry-picked from vz8 commit 890476830003 ("ve/umh: add per-container
    usermodehelper thread"))
    
    Signed-off-by: Nikita Yushchenko <nikita.yushchenko@virtuozzo.com>
    Feature: nfs/nfsd: containerization

--- include/linux/ve.h
+++ include/linux/ve.h
@@ -14,6 +14,7 @@
 #include <linux/ve_proto.h>
 #include <linux/cgroup.h>
 #include <linux/kmapset.h>
+#include <linux/kthread.h>
 #include <linux/vzstat.h>
 
 struct nsproxy;
@@ -61,6 +62,9 @@ struct ve_struct {
 
 	struct kthread_worker	*kthreadd_worker;
 	struct task_struct	*kthreadd_task;
+
+	struct kthread_worker	umh_worker;
+	struct task_struct	*umh_task;
 };
 
 #define VE_MEMINFO_DEFAULT	1	/* default behaviour */
