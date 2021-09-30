f4d135d3dc9791d63e1d5fb03a7b4ff480168364
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2021-09-30 17:44:00 +0300

    x86: "pages zeroing on free" option introduced
    This patch add pages zeroing option to free_pages_prepare().
    It's disabled by default. To enable it one should add "zero-free-pages"
    kernel option to kernel boot parameters.
    
    Note, that kernel boot option handling and enabling of pages zeroing are
    divided. The reason for this is that static keys initialization can't be
    performed during kernel parameters parsing (can't specify, what is the exact
    reason, but kernel just doesn't boot, if static key is incremented on
    parameter callback). Because of this split, actual zeroing is enabled later
    during kernel boot, but still early enough.
    
    https://jira.sw.ru/browse/PSBM-33071
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Reviewed-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    
    +++
    mm: Fix section mismatch of zero_free_pages
    
    zero_free_pages is used not only from boot code:
    
    WARNING: vmlinux.o(.text+0x21f668): Section mismatch in reference from the function __free_pages_ok() to the variable .init.data:zero_free_pages
    The function __free_pages_ok() references
    the variable __initdata zero_free_pages.
    This is often because __free_pages_ok lacks a __initdata
    annotation or the annotation of zero_free_pages is wrong.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    (cherry-picked from vz8 commit 26c485e52037 ("x86: "pages zeroing on free"
    option introduced"))
    
    Signed-off-by: Nikita Yushchenko <nikita.yushchenko@virtuozzo.com>
    Feature: mm: memory sanitation

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -308,6 +308,24 @@ static inline void set_pcppage_migratetype(struct page *page, int migratetype)
 	page->index = migratetype;
 }
 
+static int zero_data_pages_enabled;
+struct static_key zero_free_pages = STATIC_KEY_INIT_FALSE;
+
+static int __init enable_zero_free_pages(char *__unused)
+{
+	zero_data_pages_enabled = 1;
+	return 1;
+}
+__setup("zero-free-pages", enable_zero_free_pages);
+
+static int __init setup_zero_free_pages(void)
+{
+	if (zero_data_pages_enabled)
+		static_key_slow_inc(&zero_free_pages);
+	return 0;
+}
+early_initcall(setup_zero_free_pages);
+
 #ifdef CONFIG_PM_SLEEP
 /*
  * The following functions are used by the suspend/hibernate code to temporarily
@@ -1462,6 +1480,9 @@ static __always_inline bool free_pages_prepare(struct page *page,
 				continue;
 			}
 			(page + i)->flags &= ~PAGE_FLAGS_CHECK_AT_PREP;
+
+			if (static_key_false(&zero_free_pages))
+				clear_highpage(page + i);
 		}
 	}
 	if (PageMappingFlags(page))
@@ -1472,6 +1493,9 @@ static __always_inline bool free_pages_prepare(struct page *page,
 		bad++;
 	if (bad)
 		return false;
+
+	if (static_key_false(&zero_free_pages))
+		clear_highpage(page);
 
 	page_cpupid_reset_last(page);
 	page->flags &= ~PAGE_FLAGS_CHECK_AT_PREP;
