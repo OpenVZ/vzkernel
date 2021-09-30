781a333a92ee971acc1a1dedc2fdf4a4f81665ea
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2021-09-30 17:43:57 +0300

    fence-watchdog: Add fence-watchdog driver
    We need to forbid system to work without a special userspace
    daemon for purposes of HA cluster. So add this watchdog module,
    which will fence the node, if that daemon won't update timer
    value in the file /sys/kernel/watchdog_timer.
    The module is needed for pstorage, so we need to protect network
    from the broken node, so we can put check to net_rx_action.
    
    Signed-off-by: Dmitry Guryanov <dguryanov@parallels.com>
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Rebase: ktkhai@
    
    Putting fence_wdog_jiffies64 in same cacheline with jiffies will
    be in a separate patch: "fence-watchdog: link fence_wdog_jiffies64 and
    jiffies in one cacheline"
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    (cherry-picked from vz8 commit aef6d38b398b ("fence-watchdog:
    Add fence-watchdog driver"))
    
    Updated use of timekeeping API since 32-bit timespec is no longer
    available.
    
    Applied minor formatting fixes.
    
    Added "CONFIG_FENCE_WATCHDOG=y" to
    redhat/configs/custom-overrides/generic/CONFIG_FENCE_WATCHDOG
    
        /dev/watchdog "softdog" is really close but it has several problems:
        - no shutdown action
        - no network blocking action
        - can not get timeout and timeleft
        - set timeout through ioctl only
        - set action in module load parameters only
        - our userspace(shaman) is using our interface
    
        so it is easier to port our watchdog.
    
        https://jira.sw.ru/browse/PSBM-40736
    
    Signed-off-by: Nikita Yushchenko <nikita.yushchenko@virtuozzo.com>
    Feature: kernel: fense watchdog driver

---
+++ include/linux/fence-watchdog.h
@@ -0,0 +1,15 @@
+/*
+ *  include/linux/fence-watchdog.h
+ *
+ *  Copyright (c) 2010-2015 Parallels IP Holdings GmbH
+ *  Copyright (c) 2017-2021 Virtuozzo International GmbH. All rights reserved.
+ *
+ */
+
+#ifndef _LINUX_FENCE_WATCHDOG_H_
+#define _LINUX_FENCE_WATCHDOG_H_
+
+inline int fence_wdog_check_timer(void);
+bool fence_wdog_tmo_match(void);
+
+#endif
