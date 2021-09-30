9c6b580d8356ab6d9fed10a2c9746a3c1f8ce967
Kirill Tkhai <ktkhai@virtuozzo.com>
2021-09-30 14:10:28 +0300

    cbt: introduce changed block tracking
    Combined patch including original patch and fixes:
    
    93f326594516 block/blk-cbt.c: copyright update
    20ff882b3eb0 cbt: selfdeadlock in __blk_cbt_set()
    c22ab989e4f9 cbt: bitmap corruption caused by ipi
    4bb4b6b568d9 cbt: license: put correct copyrights into file headers
    d991ef45b2bc cbt: don't leak ce_reserved64 in cbt_ito userspace
    c91a55b3c347 cbt: blk_cbt_update_size() must return if cbt->block_max not changed
    8c3db04ecfd5 cbt: blk_cbt_update_size() should not copy uninitialized data
    92039d6d0a9a cbt: fix possible race on alloc_page()
    c55fac6426e7 cbt: new api: blk_cbt_map_merge()
    d54e079764bd cbt: fix panic in blk_cbt_map_copy_once()
    c2c61c3eb41c cbt: fix cbt->block_max calculation
    737d22a7d677 cbt: add uuid arg to blk_cbt_map_copy_once()
    3040064c1c69 cbt: add blk_cbt_map_copy_once() helper
    981be8f5e23d cbt: fix page allocation
    99f0cacd3cba cbt: make __blk_cbt_set() smarter
    7324f0cc6139 cbt: introduce CBT_PAGE_MISSED
    7588bf9d56c9 cbt: factor out alloc_page
    d9511b56e79e cbt: introduce changed block tracking
    
    @ktkhai: Backport changes were made for bvec_iter dereferencing.
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    +++
    cbt: Update CBT size from check_disk_size_change()
    
    Here is customer node, where is CBT size is different
    to ploop size. Searching against kernel code shows,
    this is the only place we skip CBT size update after
    bd_inode size change.
    
    https://jira.sw.ru/browse/PSBM-123819
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    (cherry picked from vz7 commit 042072dc3899 ("cbt: Update CBT size from
    check_disk_size_change()"))
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    
    +++
    60576729f55c cbt: New interface to save current mask snapshot in cbt
    
    During the backup, we want to save current changed mask
    and to start tracking from clean mask again.
    
    Previously, the mask was saved in another driver:
    ploop used to call cbt primitives and saved it in ploop
    device structures. This looks better than saving the mask
    in userspace, because the mask remains alive even in case
    of userspace death. The only thing needed after died backup
    is to merge the saved mask back from ploop driver
    to cbt driver. Thus, all changed (from previous successful
    backup) blocks are still available, and it's possible
    to create partial backup even after segfaulted userspace.
    
    This patchset continues the practice of saving mask in kernel,
    but it makes possible to save CBT snapshot in cbt driver
    without distributing CBT structures over the kernel. Here is
    a new BLKCBTMISC ioctl, which allows to create, drop and
    merge back a snapshot. The ioctl has 3 switches:
    
    * CMI_SNP_CREATE: create a new mask snapshot and move changed
    blocks mask there (changed blocks mask becomes empty after
    that).
    
    * CMI_SNP_DROP: drops created snapshot (should be called after
    successful backup).
    
    * CMI_SNP_MERGE_BACK: moves snapshot bits into changing blocks
    mask and kills snapshot (should be called after failed backup).
    
    +++
    cbt: Change errno values for new ioctl
    cbt: Actually show errors on return
    cbt: Rename misc commands names
    cbt: Add size to CBT_SNAP_CREATE
    cbt: Rename also blk_user_cbt_snp_create
    cbt: Move cbt_flush_cache() before size calculation
    cbt: Fix off-by-one in map_required_size()
    cbt: endless loop on rollback in blk_cbt_snap_create()
    cbt: Fix off-by-one in map_required_size()
    
    https://jira.sw.ru/browse/PSBM-134130
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Feature: cbt: changed block tracking (for backup)
    
    Rebase to RHEL9 beta kernel (khorenko@):
     * bd_write_size() now takes "sectors" as a parameter instead of "size"
    
    +++
    cbt: Fix BLKCBTMISC case
    
    Add BLKCBTMISC case allowing to call blk_cbt_ioctl()
    
    https://jira.sw.ru/browse/PSBM-137054
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -358,6 +358,9 @@ struct request_queue {
 	/* Throttle data */
 	struct throtl_data *td;
 #endif
+#ifdef CONFIG_BLK_DEV_CBT
+	struct cbt_info *cbt;
+#endif
 	struct rcu_head		rcu_head;
 	wait_queue_head_t	mq_freeze_wq;
 	/*
@@ -1209,6 +1212,27 @@ static inline void blk_crypto_unregister(struct request_queue *q) { }
 
 #endif /* CONFIG_BLK_INLINE_ENCRYPTION */
 
+#if defined (CONFIG_BLK_DEV_CBT)
+extern void blk_cbt_update_size(struct block_device *bdev);
+extern void blk_cbt_release(struct request_queue *q);
+extern void blk_cbt_bio_queue(struct request_queue *q, struct bio *bio);
+extern int blk_cbt_ioctl(struct block_device *bdev, unsigned cmd, char __user *arg);
+#else /* CONFIG_BLK_DEV_CBT */
+static inline void blk_cbt_update_size(struct block_device *bdev)
+{
+}
+static inline void blk_cbt_release(struct request_queue *q)
+{
+}
+static inline void blk_cbt_bio_queue(struct request_queue *q, struct bio *bio)
+{
+}
+static inline int blk_cbt_ioctl(struct block_device *bdev, unsigned cmd,
+				char __user *arg)
+{
+	return 0;
+}
+#endif /* CONFIG_BLK_DEV_CBT */
 
 struct block_device_operations {
 	void (*submit_bio)(struct bio *bio);
@@ -1311,6 +1335,7 @@ void blkdev_put_no_open(struct block_device *bdev);
 struct block_device *bdev_alloc(struct gendisk *disk, u8 partno);
 void bdev_add(struct block_device *bdev, dev_t dev);
 struct block_device *I_BDEV(struct inode *inode);
+extern void bd_write_size(struct block_device *, sector_t sectors);
 int truncate_bdev_range(struct block_device *bdev, fmode_t mode, loff_t lstart,
 		loff_t lend);
 
