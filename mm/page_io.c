1baa0324dc22be326709ca5a7154b13223860ead
Vladimir Davydov <vdavydov@virtuozzo.com>
2021-09-30 17:44:03 +0300

    mm: memcontrol: add swapin/out stats
    This patch adds pswpin and pswpout fields to memory.stat file. These
    fields show the number of pages swapped in and out respectively for the
    given memory cgroup. We assume a page was swapped in/out even if there
    were no actual io (frontswap).
    
    https://jira.sw.ru/browse/PSBM-46104
    
    Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    Rebase to RHEL8.3 kernel-4.18.0-240.1.1.el8_3 notes:
    khorenko@: fixed mem_cgroup_swapout() to charge "nr_entries", not always
    1 page.
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    (cherry-picked from vz8 commit 09af291ddfd8 ("mm: memcontrol: add
    swapin/out stats"))
    
    Signed-off-by: Nikita Yushchenko <nikita.yushchenko@virtuozzo.com>
    
    +++
    mm/vmscan: fix /proc/vmscan output
    
    In vz7 memcg counters had its own enum and names, but in vz8 it changed
    to vmstat ones. We added "pswpin" and "pswpout" in vmstat_text[] during
    rebase which caused half of the /proc/vmstat values to shift two rows up.
    Remove duplicate "pswpin" and "pswpout" from vmstat_text[]
    
    https://jira.sw.ru/browse/PSBM-130829
    
    Signed-off-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>
    Feature: mm: statistic enhancements

--- mm/page_io.c
+++ mm/page_io.c
@@ -382,8 +382,10 @@ int swap_readpage(struct page *page, bool synchronous)
 		struct address_space *mapping = swap_file->f_mapping;
 
 		ret = mapping->a_ops->readpage(swap_file, page);
-		if (!ret)
+		if (!ret) {
+			count_memcg_page_event(page, PSWPIN);
 			count_vm_event(PSWPIN);
+		}
 		goto out;
 	}
 
@@ -395,6 +397,7 @@ int swap_readpage(struct page *page, bool synchronous)
 				unlock_page(page);
 			}
 
+			count_memcg_page_event(page, PSWPIN);
 			count_vm_event(PSWPIN);
 			goto out;
 		}
