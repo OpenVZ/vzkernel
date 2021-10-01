370602a8424897f2ca09305064ecee276a6b4b44
Andrey Ryabinin <aryabinin@virtuozzo.com>
2021-10-01 19:38:48 +0300

    ve: patch linux_version_code in vdso
    On the write to ve.os_release file patch the linux_version_code
    in the .note section of vdso.
    
    https://jira.sw.ru/browse/PSBM-121668
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    (cherry-picked from vz8 commit 9c63f77b5556 ("ve: patch linux_version_code
    in vdso"))
    
    Signed-off-by: Nikita Yushchenko <nikita.yushchenko@virtuozzo.com>
    Feature: mm: vdso virtualization

--- arch/x86/entry/vdso/vdso2c.c
+++ arch/x86/entry/vdso/vdso2c.c
@@ -101,6 +101,7 @@ struct vdso_sym required_syms[] = {
 	{"__kernel_sigreturn", true},
 	{"__kernel_rt_sigreturn", true},
 	{"int80_landing_pad", true},
+	{"linux_version_code", true},
 	{"vdso32_rt_sigreturn_landing_pad", true},
 	{"vdso32_sigreturn_landing_pad", true},
 };
