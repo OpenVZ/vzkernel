0492d55944cdd8b61f675d986c8d486201c2dc8c
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2021-10-04 20:39:08 +0300

    ovl: make uuid=off compatible with overlayfs created without it
    Patch makes mainstream "uuid=off" compatible with our reverted
    "index=nouuid" option.
    
    To make overlayfs mounts in containers be copyable by vzmlocal (which
    implies uuid change) we need to set "uuid=off" by default for all
    overlayfs mounts. So add CONFIG_OVERLAY_FS_UUID_OFF and uuid module
    param to configure defaults.
    
    To make overlayfs mounts created on older kernel without "uuid=off" also
    copyable by vzmlocal we also need to skip uuid checks. Mainstream
    version compares uuid with zero, but old overlayfs mount xattrs can
    store non-zero uuids as fhandles already.
    
    https://jira.sw.ru/browse/PSBM-123536
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    ====================
    Patchset descrition:
    
    ovl: replace "index=nouuid" with "uuid=off"
    
    Mainstream base is a bit different so we need some more ovl_fs
    propagation to functions. Also we still need some vz-specific hunks
    which were not allowed to mainstream overlay, so add them as a separate
    patch.
    
    https://jira.sw.ru/browse/PSBM-123536
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Pavel Tikhomirov (4):
      Revert "ovl: introduce new "index=nouuid" option for inodes index
        feature"
      ovl: propagate ovl_fs to ovl_decode_real_fh and ovl_encode_real_fh
      ovl: introduce new "uuid=off" option for inodes index feature
      ovl: make uuid=off compatible with overlayfs created without it
    
    Rebased to vz9:
     - dropped all patches already present in ms
    
    (cherry picked from vz8 commit 8d81d6d2563c029fdd8c26064ca0fdbc6666120b)
    Signed-off-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>
    Feature: fs/overlayfs: CRIU support

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -43,6 +43,11 @@ module_param_named(index, ovl_index_def, bool, 0644);
 MODULE_PARM_DESC(index,
 		 "Default to on or off for the inodes index feature");
 
+static bool ovl_uuid_def = !IS_ENABLED(CONFIG_OVERLAY_FS_UUID_OFF);
+module_param_named(uuid, ovl_uuid_def, bool, 0644);
+MODULE_PARM_DESC(uuid,
+		 "Default to on or off for the inodes uuid feature");
+
 static bool ovl_nfs_export_def = IS_ENABLED(CONFIG_OVERLAY_FS_NFS_EXPORT);
 module_param_named(nfs_export, ovl_nfs_export_def, bool, 0644);
 MODULE_PARM_DESC(nfs_export,
@@ -403,8 +408,8 @@ static int ovl_show_options(struct seq_file *m, struct dentry *dentry)
 		seq_printf(m, ",redirect_dir=%s", ofs->config.redirect_mode);
 	if (ofs->config.index != ovl_index_def)
 		seq_printf(m, ",index=%s", ofs->config.index ? "on" : "off");
-	if (!ofs->config.uuid)
-		seq_puts(m, ",uuid=off");
+	if (ofs->config.uuid != ovl_uuid_def)
+		seq_printf(m, ",uuid=%s", ofs->config.uuid ? "on" : "off");
 	if (ofs->config.nfs_export != ovl_nfs_export_def)
 		seq_printf(m, ",nfs_export=%s", ofs->config.nfs_export ?
 						"on" : "off");
@@ -2022,7 +2027,7 @@ static int ovl_fill_super(struct super_block *sb, void *data, int silent)
 	ofs->share_whiteout = true;
 
 	ofs->config.index = ovl_index_def;
-	ofs->config.uuid = true;
+	ofs->config.uuid = ovl_uuid_def;
 	ofs->config.nfs_export = ovl_nfs_export_def;
 	ofs->config.xino = ovl_xino_def();
 	ofs->config.metacopy = ovl_metacopy_def;
