ecf4de7a2620433f60d3415f86cce4be8e261084
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2021-10-04 21:40:59 +0300

    trusted/ve/fs/exec: Don't allow a privileged user to execute untrusted files
    If we run some binary (exploit) from CT on host, it can easily give a
    user in these CT an ability to do anything on host sending commands
    through unix socket to the exploit. Such an exploit can mimic to bash,
    ip, systemd, ping or some other "trusted" utility.
    
    I've tested with these patch that we don't call from VE0 any binaries
    from CT-fs on start, stop, enter, suspend, resume or migration. But to
    be on the safe side, so that in future we don't become affected, lets
    prohibit running any binary from ploop disks and from CT mounts if the
    caller is from VE0.
    
    Also we protect admins of our customer from unintentionally calling such
    an exploit:
    
    [root@kuchy ~]# strace -e trace=execve /vz/root/58a2c524-b486-42c8-849\
      b-c659bf165a91/bin/ls
    execve("/vz/root/58a2c524-b486-42c8-849b-c659bf165a91/bin/ls",\
      ["/vz/root/58a2c524-b486-42c8-849b"...], [/* 27 vars */]) = -1\
      EACCES (Permission denied)
    strace: exec: Permission denied
    +++ exited with 1 +++
    
    We need same check in sys_uselib as process from host can also try to
    load shared library from the file in CT's ploop, which cannot be trusted
    too.
    
    https://jira.sw.ru/browse/PSBM-98094
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Acked-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    https://jira.sw.ru/browse/PSBM-129741
    
    Based on vz7 commit 29154b5e5af9 ("ve/fs/exec: don't allow a privileged
    user to execute untrusted files")
    
    Signed-off-by: Valeriy Vdovin <valeriy.vdovin@virtuozzo.com>
    
    Reviewed-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Reviewed-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    (cherry picked from vz8 commit 6b90a0e83276d4fca203eabc732178d67315503a)
    Signed-off-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>
    Feature: security/fs: tructed exec feature

--- fs/exec.c
+++ fs/exec.c
@@ -66,6 +66,7 @@
 #include <linux/io_uring.h>
 #include <linux/syscall_user_dispatch.h>
 #include <linux/time_namespace.h>
+#include <linux/ve.h>
 
 #include <linux/uaccess.h>
 #include <asm/mmu_context.h>
@@ -137,10 +138,9 @@ SYSCALL_DEFINE1(uselib, const char __user *, library)
 		goto out;
 
 	file = do_filp_open(AT_FDCWD, tmp, &uselib_flags);
-	putname(tmp);
 	error = PTR_ERR(file);
 	if (IS_ERR(file))
-		goto out;
+		goto put;
 
 	/*
 	 * may_open() has already checked for this, so it should be
@@ -151,6 +151,12 @@ SYSCALL_DEFINE1(uselib, const char __user *, library)
 	if (WARN_ON_ONCE(!S_ISREG(file_inode(file)->i_mode) ||
 			 path_noexec(&file->f_path)))
 		goto exit;
+
+	if (!ve_check_trusted_exec(file, tmp))
+		goto exit;
+
+	putname(tmp);
+	tmp = NULL;
 
 	fsnotify_open(file);
 
@@ -172,6 +178,9 @@ SYSCALL_DEFINE1(uselib, const char __user *, library)
 	read_unlock(&binfmt_lock);
 exit:
 	fput(file);
+put:
+	if (tmp)
+		putname(tmp);
 out:
   	return error;
 }
@@ -932,6 +941,9 @@ static struct file *do_open_execat(int fd, struct filename *name, int flags)
 	err = -EACCES;
 	if (WARN_ON_ONCE(!S_ISREG(file_inode(file)->i_mode) ||
 			 path_noexec(&file->f_path)))
+		goto exit;
+
+	if (!ve_check_trusted_exec(file, name))
 		goto exit;
 
 	err = deny_write_access(file);
