98749b4c8c485b6937c921d9392be7e8f1d5ab65
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2021-10-05 15:55:49 +0300

    ve/sysfs: generic sysfs_set_def_perms() helper introduced
    Can be used to set default visibility permissions for sysfs entries.
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    
    (cherry picked from vz8 commit a394be4ae816d06c53c5085b7fbca4891f18b3d1)
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Feature: sysfs: per-CT entries visibility and permissions configuration

---
+++ include/linux/sysfs-ve.h
@@ -0,0 +1,20 @@
+/*
+ *  include/linux/sysfs-ve.h
+ *
+ *  Copyright (c) 2018-2021 Virtuozzo International GmbH. All rights reserved.
+ *
+ */
+
+#ifndef _SYSFS_VE_H_
+#define _SYSFS_VE_H_
+
+#ifdef CONFIG_VE
+int sysfs_set_def_perms(char *path, int mask);
+#else
+static inline int sysfs_set_def_perms(char *path, int mask)
+{
+	return 0;
+}
+#endif
+
+#endif
