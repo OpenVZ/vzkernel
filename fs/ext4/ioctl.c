33bce3a4add37c7c25e9a2baea9f2c00059cb326
Konstantin Khorenko <khorenko@virtuozzo.com>
2021-10-05 18:42:52 +0300

    ext4: Provide a balloon nipple for management
    When the fs is mounted with active balloon someone will have to
    inflate/blow off one. To make it possible there will be a special
    ioctl for obtaining the fd.
    
    Not very elegant solution maybe, but it's OK for PVC containers.
    
    +++
    ext4: fix file allocation check in ext4_open_balloon
    
    Function alloc_file() doesn't return NULL (unlike in 2.6.32-x).
    It returns error pointer. File structure allocation may fail before
    file->f_ep_links is initialized, which may lead to crash in
    eventpoll_release_file().
    
    https://jira.sw.ru/browse/PSBM-41222
    
    mFixes: 9cea7449aa589f325fff378e7256a3c2fc8f048d
    "ext4: Provide a balloon nipple for management"
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    
    (cherry picked from vz7 commit 100feb098ab22c6b8b25861c3b2dfaa9c5db0b03)
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ext4/balloon: Use proper O_ mode flags in balloon opening code
    
    alloc_file() expects O_* mode flags, so provide them, not
    internal FMODE_* ones.
    
    mFixes: bee340a206d7 ("ext4: Provide a balloon nipple for management")
    https://jira.sw.ru/browse/PSBM-129392
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Feature: fs/ext4: fast online shrink support

--- fs/ext4/ioctl.c
+++ fs/ext4/ioctl.c
@@ -1144,6 +1144,59 @@ static int ext4_ioctl_getlabel(struct ext4_sb_info *sbi, char __user *user_label
 	return 0;
 }
 
+static int ext4_open_balloon(struct super_block *sb, struct vfsmount *mnt)
+{
+	struct inode *balloon_ino;
+	int err, fd;
+	struct file *filp;
+	struct dentry *de;
+	struct path path;
+	fmode_t mode;
+
+	balloon_ino = EXT4_SB(sb)->s_balloon_ino;
+	err = -ENOENT;
+	if (balloon_ino == NULL)
+		goto err;
+
+	err = fd = get_unused_fd_flags(0);
+	if (err < 0)
+		goto err_fd;
+
+	__iget(balloon_ino);
+	de = d_obtain_alias(balloon_ino);
+	err = PTR_ERR(de);
+	if (IS_ERR(de))
+		goto err_de;
+
+	path.dentry = de;
+	path.mnt = mntget(mnt);
+	err = mnt_want_write(path.mnt);
+	if (err)
+		mode = O_RDONLY;
+	else
+		mode = O_RDWR;
+	filp = alloc_file(&path, mode, &ext4_file_operations);
+	if (filp->f_mode & FMODE_WRITE)
+		mnt_drop_write(path.mnt);
+	if (IS_ERR(filp)) {
+		err = PTR_ERR(filp);
+		goto err_filp;
+	}
+
+	filp->f_flags |= O_LARGEFILE;
+	fd_install(fd, filp);
+	return fd;
+
+err_filp:
+	path_put(&path);
+err_de:
+	put_unused_fd(fd);
+err_fd:
+	/* nothing */
+err:
+	return err;
+}
+
 static long __ext4_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
 	struct inode *inode = file_inode(filp);
@@ -1563,6 +1616,12 @@ resizefs_out:
 	case FS_IOC_SETFSLABEL:
 		return ext4_ioctl_setlabel(filp,
 					   (const void __user *)arg);
+
+	case EXT4_IOC_OPEN_BALLOON:
+		if (!capable(CAP_SYS_ADMIN))
+			return -EACCES;
+
+		return ext4_open_balloon(inode->i_sb, filp->f_path.mnt);
 
 	default:
 		return -ENOTTY;
