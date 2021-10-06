9e841d05e1eefb47be9b7f49d5c23495c3f43037
Angelo Ruocco <angeloruocco90@gmail.com>
2021-10-06 14:14:10 +0300

    ms/cgroup: let a symlink too be created with a cftype file
    This commit enables a cftype to have a symlink (of any name) that
    points to the file associated with the cftype.
    
    Signed-off-by: Angelo Ruocco <angeloruocco90@gmail.com>
    Signed-off-by: Paolo Valente <paolo.valente@linaro.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    
    https://jira.sw.ru/browse/PSBM-101019
    (cherry-picked from 54b7b868e826b294687c439b68ec55fe20cafe5b)
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Signed-off-by: Cyrill Gorcunov <gorcunov@virtuozzo.com>
    Feature: fix ms/cgroup

--- include/linux/cgroup-defs.h
+++ include/linux/cgroup-defs.h
@@ -116,6 +116,7 @@ enum {
 	CFTYPE_NO_PREFIX	= (1 << 3),	/* (DON'T USE FOR NEW FILES) no subsys prefix */
 	CFTYPE_WORLD_WRITABLE	= (1 << 4),	/* (DON'T USE FOR NEW FILES) S_IWUGO */
 	CFTYPE_DEBUG		= (1 << 5),	/* create when cgroup_debug */
+	CFTYPE_SYMLINKED	= (1 << 7),	/* pointed to by symlink too */
 
 	/*
 	 * Allow write to file when it's on writer's ve init cgroup ns root. We don't
@@ -563,6 +564,7 @@ struct cftype {
 	 * end of cftype array.
 	 */
 	char name[MAX_CFTYPE_NAME];
+	char link_name[MAX_CFTYPE_NAME];
 	unsigned long private;
 
 	/*
