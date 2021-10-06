cc7383d61a7e1ac9543d4ae802b546d87605d7e1
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2021-10-06 11:57:38 +0300

    ve/aio: Add a handle to checkpoint/restore AIO context
    This adds ioctl, which allows to set ring buffer tail
    and to wait till aio requests are finished.
    
    v2: Add pseudosuper check
    
    https://jira.sw.ru/browse/PSBM-42488
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Reviewed-by: Cyrill Gorcunov <gorcunov@openvz.org>
    
    khorenko@: we don't support migration of incomplete aio requests
               https://jira.sw.ru/browse/PSBM-41425
    
    so using added instruments we wait till all AIO requests are completed
    and migrate the results (AIO req contexts with status).
    
    +++
    ve/aio: Enumerate ioctl numbers right
    
    Do not use common used numbers, use custom.
    Also, make error codes different.
    
    https://jira.sw.ru/browse/PSBM-42488
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Acked-by: Cyrill Gorcunov <gorcunov@openvz.org>
    
    +++
    ve/aio: Kill ve_aio_set_tail()
    
    Since tail is restored using submitting requests to write in /dev/null,
    we do not need this interface anymore.
    
    https://jira.sw.ru/browse/PSBM-42488
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Acked-by: Cyrill Gorcunov <gorcunov@openvz.org>
    
    +++
    ve/aio: Wait for all inflight AIO reqs of a task
    
    Make it wait all task's AIO contexts instead of a single AIO request.
    This minimizes the number of syscall we do to dump aios.
    
    https://jira.sw.ru/browse/PSBM-42488
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Acked-by: Cyrill Gorcunov <gorcunov@openvz.org>
    ======================================
    
    Ported with respect to ms commits:
    34e83fc ("aio: reqs_active -> reqs_available")
    723be6e ("aio: percpu ioctx refcount")
    db446a0 ("aio: convert the ioctx list to table lookup v3")
    
    https://jira.sw.ru/browse/PSBM-123159
    
    Signed-off-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
    
    +++
    aio: ioctl(VE_AIO_IOC_WAIT_ACTIVE) in-flight reqs counting fix
    
    We have to take into account percpu part of reqs_available
    counter on struct kioctx.
    
    https://jira.sw.ru/browse/PSBM-128710
    
    Signed-off-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
    Reviewed-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    (cherry-picked from vz8 commit c04e652e2451 ("ve/aio: Add a handle
    to checkpoint/restore AIO context"))
    
    Signed-off-by: Nikita Yushchenko <nikita.yushchenko@virtuozzo.com>
    
    +++
    /proc/<pid>/aio: add dummy read method
    
    To address LTP proc01 test failure, add a dummy read method for
    vz-specific /proc/<pid>/aio.
    
    The dummy method does nothing and always returns 0.
    
    Signed-off-by: Nikita Yushchenko <nikita.yushchenko@virtuozzo.com>
    Feature: ve: CRIU support

--- include/linux/aio.h
+++ include/linux/aio.h
@@ -12,14 +12,27 @@ typedef int (kiocb_cancel_fn)(struct kiocb *);
 
 #define AIO_MAX_NR_DEFAULT	0x10000
 
+struct ve_ioc_arg
+{
+	aio_context_t	ctx_id;
+	unsigned	val;
+};
+
+#define VE_AIO_IOC_WAIT_ACTIVE	_IOW('a',  1, struct ve_ioc_arg)
+
 /* prototypes */
 #ifdef CONFIG_AIO
 extern void exit_aio(struct mm_struct *mm);
 void kiocb_set_cancel_fn(struct kiocb *req, kiocb_cancel_fn *cancel);
+#ifdef CONFIG_VE
+int ve_aio_ioctl(struct task_struct *, unsigned int, unsigned long);
+#endif
 #else
 static inline void exit_aio(struct mm_struct *mm) { }
 static inline void kiocb_set_cancel_fn(struct kiocb *req,
 				       kiocb_cancel_fn *cancel) { }
+static int ve_aio_ioctl(struct task_struct *task, unsigned int cmd,
+			unsigned long arg) { return 0; }
 #endif /* CONFIG_AIO */
 
 #endif /* __LINUX__AIO_H */
