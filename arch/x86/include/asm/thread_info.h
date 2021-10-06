a6b0c9b3c6811b138c1b8a4a27dd557d5939dc70
Andrey Ryabinin <aryabinin@virtuozzo.com>
2021-10-06 11:57:32 +0300

    x86: make ARCH_[SET|GET]_CPUID friends with /proc/vz/cpuid_override
    We are using cpuid faults to emulate cpuid in containers. This
    conflicts with arch_prctl(ARCH_SET_CPUID, 0) which allows to enable
    cpuid faulting so that cpuid instruction causes SIGSEGV.
    
    Add TIF_CPUID_OVERRIDE thread info flag which is added on all
    !ve0 tasks. And check this flag along with TIF_NOCPUID to
    decide whether we need to enable/disable cpuid faults or not.
    
    https://jira.sw.ru/browse/PSBM-121823
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    (cherry-picked from vz8 commit f7458a78593c ("x86: make ARCH_[SET|GET]_CPUID
    friends with /proc/vz/cpuid_override"))
    
    Signed-off-by: Nikita Yushchenko <nikita.yushchenko@virtuozzo.com>
    Feature: x86: cpu pools (cpuid override)

--- arch/x86/include/asm/thread_info.h
+++ arch/x86/include/asm/thread_info.h
@@ -96,6 +96,7 @@ struct thread_info {
 #define TIF_SPEC_FORCE_UPDATE	23	/* Force speculation MSR update in context switch */
 #define TIF_FORCED_TF		24	/* true if TF in eflags artificially */
 #define TIF_BLOCKSTEP		25	/* set when we want DEBUGCTLMSR_BTF */
+#define TIF_CPUID_OVERRIDE	26	/* CPUID emulation enabled */
 #define TIF_LAZY_MMU_UPDATES	27	/* task is updating the mmu lazily */
 #define TIF_ADDR32		29	/* 32-bit address space on 64 bits */
 
@@ -119,13 +120,14 @@ struct thread_info {
 #define _TIF_SPEC_FORCE_UPDATE	(1 << TIF_SPEC_FORCE_UPDATE)
 #define _TIF_FORCED_TF		(1 << TIF_FORCED_TF)
 #define _TIF_BLOCKSTEP		(1 << TIF_BLOCKSTEP)
+#define _TIF_CPUID_OVERRIDE	(1 << TIF_CPUID_OVERRIDE)
 #define _TIF_LAZY_MMU_UPDATES	(1 << TIF_LAZY_MMU_UPDATES)
 #define _TIF_ADDR32		(1 << TIF_ADDR32)
 
 /* flags to check in __switch_to() */
 #define _TIF_WORK_CTXSW_BASE					\
 	(_TIF_NOCPUID | _TIF_NOTSC | _TIF_BLOCKSTEP |		\
-	 _TIF_SSBD | _TIF_SPEC_FORCE_UPDATE | _TIF_SLD)
+	 _TIF_SSBD | _TIF_SPEC_FORCE_UPDATE | _TIF_SLD | _TIF_CPUID_OVERRIDE)
 
 /*
  * Avoid calls to __switch_to_xtra() on UP as STIBP is not evaluated.
