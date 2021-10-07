8468061375d1b64bcc18ffc976e0d3900f2d3fae
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2021-10-07 15:53:57 +0300

    proc connector: add per-ve init and fini foutines
    These routines will be called from main connecter per-ve init and fini
    routines.
    
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Reviewed-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    (cherry picked from vz8 commit 5e3858a525b434080e43d72e5b5a512c472cb6f7)
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Feature: cgroup: control group rules daemon (cgrulesengd) virtualization

--- include/linux/connector.h
+++ include/linux/connector.h
@@ -64,6 +64,10 @@ struct cn_private {
 };
 
 struct ve_struct;
+
+int cn_proc_init_ve(struct ve_struct *ve);
+void cn_proc_fini_ve(struct ve_struct *ve);
+
 int cn_add_callback_ve(struct ve_struct *ve,
 		       const struct cb_id *id, const char *name,
 		       void (*callback)(struct cn_msg *,
