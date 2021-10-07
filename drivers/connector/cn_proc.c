464f70a5a481c0558fcd057133fb2e843cd90807
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2021-10-07 15:53:49 +0300

    proc connector: use generic event helper for exec event
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Reviewed-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    (cherry picked from vz8 commit 7923b2d8cce6f7a5599d3e3c4a28fb9f66c6f5ac)
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Feature: cgroup: control group rules daemon (cgrulesengd) virtualization

--- drivers/connector/cn_proc.c
+++ drivers/connector/cn_proc.c
@@ -131,28 +131,17 @@ void proc_fork_connector(struct task_struct *task)
 	proc_event_connector(task, PROC_EVENT_FORK, 0, fill_fork_event);
 }
 
-void proc_exec_connector(struct task_struct *task)
+static bool fill_exec_event(struct proc_event *ev, struct task_struct *task,
+			    int unused)
 {
-	struct cn_msg *msg;
-	struct proc_event *ev;
-	__u8 buffer[CN_PROC_MSG_SIZE] __aligned(8);
-
-	if (atomic_read(&proc_event_num_listeners) < 1)
-		return;
-
-	msg = buffer_to_cn_msg(buffer);
-	ev = (struct proc_event *)msg->data;
-	memset(&ev->event_data, 0, sizeof(ev->event_data));
-	ev->timestamp_ns = ktime_get_ns();
-	ev->what = PROC_EVENT_EXEC;
 	ev->event_data.exec.process_pid = task->pid;
 	ev->event_data.exec.process_tgid = task->tgid;
+	return true;
+}
 
-	memcpy(&msg->id, &cn_proc_event_id, sizeof(msg->id));
-	msg->ack = 0; /* not used */
-	msg->len = sizeof(*ev);
-	msg->flags = 0; /* not used */
-	send_msg(msg);
+void proc_exec_connector(struct task_struct *task)
+{
+	proc_event_connector(task, PROC_EVENT_EXEC, 0, fill_exec_event);
 }
 
 void proc_id_connector(struct task_struct *task, int which_id)
