93e189c8367ec1f188126a5255fa61e6c02753d6
Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
2021-10-07 15:53:54 +0300

    proc connector: use generic event helper for coredump event
    Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>
    Reviewed-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    
    (cherry picked from vz8 commit f2d645abc744ded52e9b376dbfbd2dca22ffba63)
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Feature: cgroup: control group rules daemon (cgrulesengd) virtualization

--- drivers/connector/cn_proc.c
+++ drivers/connector/cn_proc.c
@@ -221,21 +221,11 @@ void proc_comm_connector(struct task_struct *task)
 	proc_event_connector(task, PROC_EVENT_COMM, 0, fill_comm_event);
 }
 
-void proc_coredump_connector(struct task_struct *task)
+static bool fill_coredump_event(struct proc_event *ev, struct task_struct *task,
+				int unused)
 {
-	struct cn_msg *msg;
-	struct proc_event *ev;
 	struct task_struct *parent;
-	__u8 buffer[CN_PROC_MSG_SIZE] __aligned(8);
 
-	if (atomic_read(&proc_event_num_listeners) < 1)
-		return;
-
-	msg = buffer_to_cn_msg(buffer);
-	ev = (struct proc_event *)msg->data;
-	memset(&ev->event_data, 0, sizeof(ev->event_data));
-	ev->timestamp_ns = ktime_get_ns();
-	ev->what = PROC_EVENT_COREDUMP;
 	ev->event_data.coredump.process_pid = task->pid;
 	ev->event_data.coredump.process_tgid = task->tgid;
 
@@ -246,12 +236,12 @@ void proc_coredump_connector(struct task_struct *task)
 		ev->event_data.coredump.parent_tgid = parent->tgid;
 	}
 	rcu_read_unlock();
+	return true;
+}
 
-	memcpy(&msg->id, &cn_proc_event_id, sizeof(msg->id));
-	msg->ack = 0; /* not used */
-	msg->len = sizeof(*ev);
-	msg->flags = 0; /* not used */
-	send_msg(msg);
+void proc_coredump_connector(struct task_struct *task)
+{
+	proc_event_connector(task, PROC_EVENT_COREDUMP, 0, fill_coredump_event);
 }
 
 void proc_exit_connector(struct task_struct *task)
