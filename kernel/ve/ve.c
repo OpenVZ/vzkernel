4bede8cb8d240d078afe49f16780da97367e833a
Valeriy Vdovin <valeriy.vdovin@virtuozzo.com>
2021-10-20 11:43:55 +0300

    ve/cgroup: Move release_agent from system_wq to per-ve workqueues
    Each VE should execute release agent notifications within it's own
    workqueue. This way we achieve a more fine-grained control over
    release_agent work flushing at VE destruction.
    
    Short history of the patch:
    
    https://jira.sw.ru/browse/PSBM-83887
    Signed-off-by: Valeriy Vdovin <valeriy.vdovin@virtuozzo.com>
    
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    (cherry picked from vz7 commit 9fbfb5b4cfb87ba7c9dd63eec5e5e27946a38d3c)
    https://jira.sw.ru/browse/PSBM-108270
    (cherry picked from vz8 commit 0eda8e4d0a9f6660a72f7584e8504a363c25a392)
    +++
    cgroup/ve: Do not run release_agent on non-running ve
    +++
    cgroup/ve: Move ve workqueue stop to ve_stop_ns()
    (cherry picked from vz8 commit 1481d130afa32eecffe28b34fd00b57e11d2666f)
    
    vz9 changes:
    - merge fixup hunks from 1481d130afa3 related to workqueue locking
    - do less code movements around cgroup->release_list and
      ve->release_list init
    - add comment to ve_rm_from_release_list about why we always have the
      right ve->release_list_lock there
    - rework cgroup1_release_agent, on vz8 release-agent patches are badly
      broken with conterintuitive reordering, e.g. cgroup_path_ve_relative
      used in this patch was defined in later patches...
    - fix missing cgroup->release_list initialization
    
    https://jira.sw.ru/browse/PSBM-134002
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Reviewed-by: Alexander Mikhalitsyn <alexander.mikhalitsyn@virtuozzo.com>
    Reviewed-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    
    +++
    ve/cgroup: Drop leftover cgroup_get_local_root() declaration
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    cgroup: close a race window on cgrp removal
    
    Cgroup removal via rmdir() can race with preparation to call release
    agent for the same cgroup (triggered e.g. by a recent exit of the last
    cgroup's task). Although quite unlikely, that can result into cgrp
    object reaching kfree() while it is still being used by
    cgroup1_release_agent().
    
    This scenario is a possible reason for use-after-free caught by KASAN:
    
        BUG: KASAN: use-after-free in cgroup_path_ns_locked+0xcd/0x110
        Read of size 8 at addr ffff888134b102b8 by task kworker/u8:0/769807
    ...
        dump_stack_lvl+0x56/0x7b
        print_address_description.constprop.8+0x21/0x150
        ? cgroup_path_ns_locked+0xcd/0x110
        ? cgroup_path_ns_locked+0xcd/0x110
        kasan_report.cold.14+0x7f/0x11b
        ? do_raw_spin_lock+0x250/0x290
        ? cgroup_path_ns_locked+0xcd/0x110
        cgroup_path_ns_locked+0xcd/0x110
        cgroup_path_ns+0x42/0x70
        cgroup1_release_agent+0x27e/0x680
    ...
    
    Close the race window by taking an extra reference to cgrp for the time
    while cgroup1_release_agent() uses it.
    
    https://jira.sw.ru/browse/PSBM-136884
    
    Signed-off-by: Nikita Yushchenko <nikita.yushchenko@virtuozzo.com>
    Feature: cgroup: per-CT cgroup release_agent

--- kernel/ve/ve.c
+++ kernel/ve/ve.c
@@ -73,6 +73,11 @@ struct ve_struct ve0 = {
 	.meminfo_val		= VE_MEMINFO_SYSTEM,
 	.vdso_64		= (struct vdso_image*)&vdso_image_64,
 	.vdso_32		= (struct vdso_image*)&vdso_image_32,
+	.release_list_lock	= __SPIN_LOCK_UNLOCKED(
+					ve0.release_list_lock),
+	.release_list		= LIST_HEAD_INIT(ve0.release_list),
+	.release_agent_work	= __WORK_INITIALIZER(ve0.release_agent_work,
+					cgroup1_release_agent),
 };
 EXPORT_SYMBOL(ve0);
 
@@ -477,6 +482,69 @@ static void ve_workqueue_stop(struct ve_struct *ve)
 	ve->wq = NULL;
 }
 
+/*
+ * ve_add_to_release_list - called from cgroup1_check_for_release to put a
+ * cgroup into a release workqueue. There are two codepaths that lead to this
+ * function. One starts from cgroup_exit() which holds css_set_lock, another
+ * one from cgroup_destroy_locked which does not hold css_set_lock. So we
+ * should not use any reschedulable
+ *
+ */
+void ve_add_to_release_list(struct cgroup *cgrp)
+{
+	struct ve_struct *ve;
+	unsigned long flags;
+	int need_schedule_work = 0;
+
+	rcu_read_lock();
+	ve = cgroup_ve_owner(cgrp);
+	if (!ve)
+		ve = &ve0;
+
+	if (!ve->is_running) {
+		rcu_read_unlock();
+		return;
+	}
+
+	spin_lock_irqsave(&ve->release_list_lock, flags);
+	if (!cgroup_is_dead(cgrp) &&
+	    list_empty(&cgrp->release_list)) {
+		list_add(&cgrp->release_list, &ve->release_list);
+		need_schedule_work = 1;
+	}
+	spin_unlock_irqrestore(&ve->release_list_lock, flags);
+
+	if (need_schedule_work)
+		queue_work(ve->wq, &ve->release_agent_work);
+
+	rcu_read_unlock();
+}
+
+/*
+ * As workqueue destroy happens before we unset CGRP_VE_ROOT and
+ * cgroup->ve_owner and also as destroy waits for all current works
+ * to finish, we can rely that if cgroup is in release_list of some ve
+ * then cgroup_ve_owner would return exactly the same ve, and we
+ * would take the right ve->release_list_lock for operating on
+ * our cgroup->release_list.
+ */
+void ve_rm_from_release_list(struct cgroup *cgrp)
+{
+	struct ve_struct *ve;
+	unsigned long flags;
+
+	rcu_read_lock();
+	ve = cgroup_ve_owner(cgrp);
+	if (!ve)
+		ve = &ve0;
+
+	spin_lock_irqsave(&ve->release_list_lock, flags);
+	if (!list_empty(&cgrp->release_list))
+		list_del_init(&cgrp->release_list);
+	spin_unlock_irqrestore(&ve->release_list_lock, flags);
+	rcu_read_unlock();
+}
+
 /* under ve->op_sem write-lock */
 static int ve_start_container(struct ve_struct *ve)
 {
@@ -719,6 +787,11 @@ static struct cgroup_subsys_state *ve_create(struct cgroup_subsys_state *parent_
 		goto err_lat;
 
 	ve->features = VE_FEATURES_DEF;
+
+	INIT_WORK(&ve->release_agent_work, cgroup1_release_agent);
+	spin_lock_init(&ve->release_list_lock);
+	INIT_LIST_HEAD(&ve->release_list);
+
 	ve->_randomize_va_space = ve0._randomize_va_space;
 
 	ve->meminfo_val = VE_MEMINFO_DEFAULT;
