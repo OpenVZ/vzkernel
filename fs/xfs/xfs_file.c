5a8608f84fb12792761302c0e4209580f64dd58e
Kirill Tkhai <ktkhai@virtuozzo.com>
2021-10-25 15:49:49 +0300

    xfs: Never show balloon in readdir results
    Note, that xfs_readdir() may be called from many
    places. To underline the case, when it's called
    from normal readdir syscalls (not from xfs service
    functionality), and to avoid to add a new argument
    to xfs_readdir(), we introduce a special value:
    XFS_FAKE_TRANS_IGNORE_BALLOON.
    
    https://jira.sw.ru/browse/PSBM-133811
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Feature: fs/xfs: fast online shrink support

--- fs/xfs/xfs_file.c
+++ fs/xfs/xfs_file.c
@@ -1230,7 +1230,7 @@ xfs_file_readdir(
 	 */
 	bufsize = (size_t)min_t(loff_t, XFS_READDIR_BUFSIZE, ip->i_disk_size);
 
-	return xfs_readdir(NULL, ip, ctx, bufsize);
+	return xfs_readdir(XFS_FAKE_TRANS_IGNORE_BALLOON, ip, ctx, bufsize);
 }
 
 STATIC loff_t
