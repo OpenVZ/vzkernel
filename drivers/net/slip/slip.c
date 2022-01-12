890f101ecc5c7c43ff98664350e25c00a7489786
Vasily Averin <vvs@virtuozzo.com>
2022-01-12 18:36:42 +0300

    net/drivers/slip: disable high-order allocations warnings in slip driver
    slip driver can trigger high-order allocations in sl_change_mtu(),
    sl_alloc_bufs() and slhc_init().
    
    https://jira.sw.ru/browse/PSBM-125232
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    
    (cherry picked from vz7 commit 42c0ed59307de123abaf795d4485ca87c83716cb)
    Signed-off-by: Vasily Averin <vvs@virtuozzo.com>
    
    Feature: mm: avoid high order allocations in all subsystems

--- drivers/net/slip/slip.c
+++ drivers/net/slip/slip.c
@@ -154,14 +154,14 @@ static int sl_alloc_bufs(struct slip *sl, int mtu)
 	 */
 	if (len < 576 * 2)
 		len = 576 * 2;
-	rbuff = kmalloc(len + 4, GFP_KERNEL);
+	rbuff = kmalloc(len + 4, GFP_KERNEL | __GFP_ORDER_NOWARN);
 	if (rbuff == NULL)
 		goto err_exit;
-	xbuff = kmalloc(len + 4, GFP_KERNEL);
+	xbuff = kmalloc(len + 4, GFP_KERNEL | __GFP_ORDER_NOWARN);
 	if (xbuff == NULL)
 		goto err_exit;
 #ifdef SL_INCLUDE_CSLIP
-	cbuff = kmalloc(len + 4, GFP_KERNEL);
+	cbuff = kmalloc(len + 4, GFP_KERNEL | __GFP_ORDER_NOWARN);
 	if (cbuff == NULL)
 		goto err_exit;
 	slcomp = slhc_init(16, 16);
@@ -236,10 +236,10 @@ static int sl_realloc_bufs(struct slip *sl, int mtu)
 	if (len < 576 * 2)
 		len = 576 * 2;
 
-	xbuff = kmalloc(len + 4, GFP_ATOMIC);
-	rbuff = kmalloc(len + 4, GFP_ATOMIC);
+	xbuff = kmalloc(len + 4, GFP_ATOMIC | __GFP_ORDER_NOWARN);
+	rbuff = kmalloc(len + 4, GFP_ATOMIC | __GFP_ORDER_NOWARN);
 #ifdef SL_INCLUDE_CSLIP
-	cbuff = kmalloc(len + 4, GFP_ATOMIC);
+	cbuff = kmalloc(len + 4, GFP_ATOMIC | __GFP_ORDER_NOWARN);
 #endif
 
 
