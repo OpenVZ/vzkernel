e2597a3a7fcaaa64664c9cf064421611054fff29
Trond Myklebust <trond.myklebust@hammerspace.com>
2022-01-25 12:38:15 +0300

    ms/NFS: Add sysfs support for per-container identifier
    In order to identify containers to the NFS client, we add a per-net
    sysfs attribute that udev can fill with the appropriate identifier.
    The identifier could be a unique hostname, but in most cases it
    will probably be a persisted uuid.
    
    Signed-off-by: Trond Myklebust <trond.myklebust@hammerspace.com>
    
    (cherry-picked from ms commit bf11fbdb20b385157b046ea7781f04d0c62554a3)
    https://jira.sw.ru/browse/PSBM-137154
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- fs/nfs/netns.h
+++ fs/nfs/netns.h
@@ -14,6 +14,8 @@ struct bl_dev_msg {
 	uint32_t major, minor;
 };
 
+struct nfs_netns_client;
+
 struct nfs_net {
 	struct cache_detail *nfs_dns_resolve;
 	struct rpc_pipe *bl_device_pipe;
@@ -28,6 +30,7 @@ struct nfs_net {
 	unsigned short nfs_callback_tcpport6;
 	int cb_users[NFS4_MAX_MINOR_VERSION + 1];
 #endif
+	struct nfs_netns_client *nfs_client;
 	spinlock_t nfs_client_lock;
 	struct timespec boot_time;
 #ifdef CONFIG_PROC_FS
