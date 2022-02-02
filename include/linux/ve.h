27a2c2e0c3f3691e196deb8b3c8d359ae5c63e12
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2022-02-02 16:19:52 +0300

    ve/nf_log_syslog: allow packet logging in ve init netns
    In mainstream logging from non-init network namespaces is prohibited
    unless sysctl /proc/sys/net/netfilter/nf_log_all_netns is enabled, let's
    enable logging for init netns of container only.
    
    Note: we don't handle similar hunk in ebt_log_packet because for ebt
    log printing is not virtualized (at least for now).
    
    Feature: ve/printk: printk virtualization
    
    https://jira.sw.ru/browse/PSBM-138110
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Rebase to RHEL9.1 kernel-5.14.0-162.6.1.el9_1 kernel notes:
     * common check function nf_log_allowed() has been duplicated with "_ve"
       suffix
    
    Rebased-by: Konstantin Khorenko <khorenko@virtuozzo.com>
    
    +++
    ve: fix compilation warning in nf_log_allowed_ve
    
    net/netfilter/nf_log_syslog.c: In function 'nf_log_allowed_ve':
    net/netfilter/nf_log_syslog.c:52:31: warning: passing argument 1 of 'is_ve_init_net' discards 'const' qualifier from pointer target type [-Wdiscarded-qualifiers]
       52 |         return is_ve_init_net(net) || sysctl_nf_log_all_netns;
          |                               ^~~
    In file included from net/netfilter/nf_log_syslog.c:19:
    ./include/linux/ve.h:222:40: note: expected 'struct net *' but argument is of type 'const struct net *'
      222 | extern bool is_ve_init_net(struct net *net);
          |                            ~~~~~~~~~~~~^~~
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

--- include/linux/ve.h
+++ include/linux/ve.h
@@ -217,7 +217,7 @@ extern int vz_security_protocol_check(struct net *net, int protocol);
 
 int ve_net_hide_sysctl(struct net *net);
 
-extern bool is_ve_init_net(struct net *net);
+extern bool is_ve_init_net(const struct net *net);
 
 #else	/* CONFIG_VE */
 #include <linux/init_task.h>
@@ -239,7 +239,7 @@ static inline struct user_namespace *ve_init_user_ns(void)
 	return &init_user_ns;
 }
 
-static inline bool is_ve_init_net(struct net *net)
+static inline bool is_ve_init_net(const struct net *net)
 {
 	return net_eq(net, &init_net);
 }
