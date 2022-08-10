OpenVZ/vzkernel

commit 568f747791c7dac15a4b1a7c81b1f245c7158f49

fs: Mask appropriate filesystems FS_VIRTUALIZED
Extracted from "Initial patch".

Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>

Signed-off-by: Stanislav Kinsburskiy <skinsbursky@virtuozzo.com>

+++
ve/fs/autofs: Allow autofs to be used inside a container

It turned out that autofs is used at least for NFS/CIFS and binfmt_misc.

Let's use new FS_VE_MOUNT flag to only allow autofs mounts in
conainer-init userns, which would mimic mainstream host behaviour.

https://jira.sw.ru/browse/PSBM-26968
https://jira.sw.ru/browse/PSBM-86138
https://jira.sw.ru/browse/PSBM-128649

Rebase from VZ7, with use of FS_VE_MOUNT and with comment cleanup:
vz7 commits:
83c27c7bffcc1 ("ve/fs/autofs: mark autofs as virtualized")
0045b317c97b ("ve/fs/autofs: allow to mount autofs inside non-root userns")

mFixes: 7ddb6e8dd9f6 ("fs: Mask appropriate filesystems FS_VIRTUALIZED")

Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>

+++
fs/ext4: removed FS_VIRTUALIZED flag from 'ext3' mode of ext4 module

FS_VIRTUALIZED flag makes a registered filesystem visible in container.
ext3 is part of ext4 module that is run with untested set of flags.

Signed-off-by: Valeriy.Vdovin <valeriy.vdovin@virtuozzo.com>

khorenko@: this flag allows to mount ext3 fs inside a Container
if a Container has proper permissions for a device to be mounted.
In vz7 we never allowed it, we don't test it, so don't see any
advantages of enabling it in vz8. We can change this of course upon
request.

(cherry picked from vz8 commit b594c31c974e708e96826dcd2d903c6472dfea2d)
Signed-off-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>
Feature: fs: whiltelist what can be mounted in a CT

fs/overlayfs/super.c
@@ -2166,6 +2166,7 @@ static struct file_system_type ovl_fs_type = {
 	.fs_flags	= FS_USERNS_MOUNT,
 	.mount		= ovl_mount,
 	.kill_sb	= kill_anon_super,
+	.fs_flags	= FS_VIRTUALIZED,
 };
 MODULE_ALIAS_FS("overlay");
 

fs/ext4/super.c
@@ -7079,7 +7079,8 @@ static struct file_system_type ext4_fs_type = {
 	.init_fs_context	= ext4_init_fs_context,
 	.parameters		= ext4_param_specs,
 	.kill_sb		= kill_block_super,
-	.fs_flags		= FS_REQUIRES_DEV | FS_ALLOW_IDMAP,
+	.fs_flags		= FS_REQUIRES_DEV | FS_ALLOW_IDMAP |
+				  FS_VIRTUALIZED,
 };
 MODULE_ALIAS_FS("ext4");
 

fs/autofs/init.c
@@ -18,6 +18,7 @@ struct file_system_type autofs_fs_type = {
 	.name		= "autofs",
 	.mount		= autofs_mount,
 	.kill_sb	= autofs_kill_sb,
+	.fs_flags	= FS_VIRTUALIZED | FS_VE_MOUNT,
 };
 MODULE_ALIAS_FS("autofs");
 MODULE_ALIAS("autofs");

fs/devpts/inode.c
@@ -519,7 +519,7 @@ static struct file_system_type devpts_fs_type = {
 	.name		= "devpts",
 	.mount		= devpts_mount,
 	.kill_sb	= devpts_kill_sb,
-	.fs_flags	= FS_USERNS_MOUNT,
+	.fs_flags	= FS_USERNS_MOUNT | FS_VIRTUALIZED,
 };
 
 /*

fs/fuse/inode.c
@@ -1671,7 +1671,7 @@ static void fuse_kill_sb_anon(struct super_block *sb)
 static struct file_system_type fuse_fs_type = {
 	.owner		= THIS_MODULE,
 	.name		= "fuse",
-	.fs_flags	= FS_HAS_SUBTYPE | FS_USERNS_MOUNT,
+	.fs_flags	= FS_HAS_SUBTYPE | FS_USERNS_MOUNT | FS_VIRTUALIZED,
 	.init_fs_context = fuse_init_fs_context,
 	.parameters	= fuse_fs_parameters,
 	.kill_sb	= fuse_kill_sb_anon,

fs/proc/root.c
@@ -282,7 +282,7 @@ static struct file_system_type proc_fs_type = {
 	.init_fs_context	= proc_init_fs_context,
 	.parameters		= proc_fs_parameters,
 	.kill_sb		= proc_kill_sb,
-	.fs_flags		= FS_USERNS_MOUNT | FS_DISALLOW_NOTIFY_PERM,
+	.fs_flags		= FS_USERNS_MOUNT | FS_DISALLOW_NOTIFY_PERM | FS_VIRTUALIZED,
 };
 
 void __init proc_root_init(void)

fs/sysfs/mount.c
@@ -93,7 +93,7 @@ static struct file_system_type sysfs_fs_type = {
 	.name			= "sysfs",
 	.init_fs_context	= sysfs_init_fs_context,
 	.kill_sb		= sysfs_kill_sb,
-	.fs_flags		= FS_USERNS_MOUNT,
+	.fs_flags		= FS_USERNS_MOUNT | FS_VIRTUALIZED,
 };
 
 int __init sysfs_init(void)

fs/xfs/xfs_super.c
@@ -1888,7 +1888,7 @@ static struct file_system_type xfs_fs_type = {
 	.init_fs_context	= xfs_init_fs_context,
 	.parameters		= xfs_fs_parameters,
 	.kill_sb		= kill_block_super,
-	.fs_flags		= FS_REQUIRES_DEV | FS_ALLOW_IDMAP,
+	.fs_flags		= FS_REQUIRES_DEV | FS_ALLOW_IDMAP | FS_VIRTUALIZED,
 };
 MODULE_ALIAS_FS("xfs");
 

commit aa9eeb04e4929cac9bd12e68748645378ffc09eb

ovl: Resurrect ability to mount overlayfs in nested userns
In vz7 mounting overlayfs in nested userns was prohibited and
we used FS_VIRTUALIZED | FS_VE_MOUNT fs_flags to allow mounting it
inside top Container userns.

In vz9 kernel mounting overlayfs in nested userns is enabled already:
ms commit 459c7c565ac3 ("ovl: unprivieged mounts")

So now we should not prohibit mounting ovl in nested user namespaces
inside a Container.

Fixes: 568f747791c7 ("fs: Mask appropriate filesystems FS_VIRTUALIZED")
Fixes: 90df7a5ce836 ("ve/fs/overlay: allow overlayfs to be used inside a
Container")

Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

fs/overlayfs/super.c
@@ -2197,10 +2197,9 @@ static struct dentry *ovl_mount(struct file_system_type *fs_type, int flags,
 static struct file_system_type ovl_fs_type = {
 	.owner		= THIS_MODULE,
 	.name		= "overlay",
-	.fs_flags	= FS_USERNS_MOUNT,
+	.fs_flags	= FS_USERNS_MOUNT | FS_VIRTUALIZED,
 	.mount		= ovl_mount,
 	.kill_sb	= kill_anon_super,
-	.fs_flags	= FS_VIRTUALIZED | FS_VE_MOUNT,
 };
 MODULE_ALIAS_FS("overlay");
 