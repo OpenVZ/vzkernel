torvalds/linux

commit 0c5661ecc5dd7ce296870a3eb7b62b1b280a5e89

ixgbe: fix crash in build_skb Rx code path
Add check for build_skb enabled ring in ixgbe_dma_sync_frag().
In that case &skb_shinfo(skb)->frags[0] may not always be set which
can lead to a crash. Instead we derive the page offset from skb->data.

Fixes: 42073d91a214
("ixgbe: Have the CPU take ownership of the buffers sooner")
CC: stable <stable@vger.kernel.org>
Reported-by: Ambarish Soman <asoman@redhat.com>
Suggested-by: Alexander Duyck <alexander.h.duyck@intel.com>
Signed-off-by: Emil Tantilov <emil.s.tantilov@intel.com>
Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
Signed-off-by: David S. Miller <davem@davemloft.net>

drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -1888,6 +1888,14 @@ static void ixgbe_dma_sync_frag(struct ixgbe_ring *rx_ring,
 				     ixgbe_rx_pg_size(rx_ring),
 				     DMA_FROM_DEVICE,
 				     IXGBE_RX_DMA_ATTR);
+	} else if (ring_uses_build_skb(rx_ring)) {
+		unsigned long offset = (unsigned long)(skb->data) & ~PAGE_MASK;
+
+		dma_sync_single_range_for_cpu(rx_ring->dev,
+					      IXGBE_CB(skb)->dma,
+					      offset,
+					      skb_headlen(skb),
+					      DMA_FROM_DEVICE);
 	} else {
 		struct skb_frag_struct *frag = &skb_shinfo(skb)->frags[0];
 

OpenVZ/vzkernel

commit f696a7992dfe55b79ad1fdb255dd312e36048584

ms/ixgbe: Fix packet corruption due to missing DMA sync
  When receiving a packet with multiple fragments, hardware may still
  touch the first fragment until the entire packet has been received. The
  driver therefore keeps the first fragment mapped for DMA until end of
  packet has been asserted, and delays its dma_sync call until then.

  The driver tries to fit multiple receive buffers on one page. When using
  3K receive buffers (e.g. using Jumbo frames and legacy-rx is turned
  off/build_skb is being used) on an architecture with 4K pages, the
  driver allocates an order 1 compound page and uses one page per receive
  buffer. To determine the correct offset for a delayed DMA sync of the
  first fragment of a multi-fragment packet, the driver then cannot just
  use PAGE_MASK on the DMA address but has to construct a mask based on
  the actual size of the backing page.

  Using PAGE_MASK in the 3K RX buffer/4K page architecture configuration
  will always sync the first page of a compound page. With the SWIOTLB
  enabled this can lead to corrupted packets (zeroed out first fragment,
  re-used garbage from another packet) and various consequences, such as
  slow/stalling data transfers and connection resets. For example, testing
  on a link with MTU exceeding 3058 bytes on a host with SWIOTLB enabled
  (e.g. "iommu=soft swiotlb=262144,force") TCP transfers quickly fizzle
  out without this patch.

  Cc: stable@vger.kernel.org
  Fixes: 0c5661ecc5dd7 ("ixgbe: fix crash in build_skb Rx code path")
  Signed-off-by: Markus Boehme <markubo@amazon.com>
  Tested-by: Tony Brelinski <tonyx.brelinski@intel.com>
  Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
  Signed-off-by: David S. Miller <davem@davemloft.net>

(mainstream commit 09cfae9f13d51700b0fecf591dcd658fc5375428)
https://jira.sw.ru/browse/PSBM-142789
Signed-off-by: Alexander Atanasov <alexander.atanasov@virtuozzo.com>

drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -1886,7 +1886,8 @@ static void ixgbe_dma_sync_frag(struct ixgbe_ring *rx_ring,
 				     DMA_FROM_DEVICE,
 				     &attrs);
 	} else if (ring_uses_build_skb(rx_ring)) {
-		unsigned long offset = (unsigned long)(skb->data) & ~PAGE_MASK;
+		unsigned long mask = (unsigned long)ixgbe_rx_pg_size(rx_ring) - 1;
+		unsigned long offset = (unsigned long)(skb->data) & mask;
 
 		dma_sync_single_range_for_cpu(rx_ring->dev,
 					      IXGBE_CB(skb)->dma,