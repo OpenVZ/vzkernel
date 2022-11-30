67ccc9c89143a43122750a3adcba2990c703bfb2
Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
2022-11-30 14:22:42 +0300

    cgroup_freezer: Print information about unfreezable process
    Add a sysctl kernel.freeze_cgroup_timeout (default value 30 * HZ).
    
    If one writes FROZEN to freezer.state file and after a timeout of
    kernel.freeze_cgroup_timeout one still reads FREEZING from freezer.state
    file (meaning that kernel does not succeed to freeze cgroup processes
    still) - let's print a warning with information about the problem, e.g.:
    
    [ 7196.621368] Freeze of /test took 0 sec, due to unfreezable process 13732:bash, stack:
    [ 7196.621396] [<ffffffffa2df9556>] retint_careful+0x14/0x32
    [ 7196.621431] [<ffffffffffffffff>] 0xffffffffffffffff
    
    The output includes:
     - path to problematic freezer cgroup
     - timeout in seconds
     - unfeezable process pid, comm and stack
    
    https://jira.sw.ru/browse/PSBM-142970
    
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    
    Feature: cgroup/freeze: enhance logging

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -100,6 +100,8 @@
 int ve_allow_module_load = 1;
 EXPORT_SYMBOL(ve_allow_module_load);
 
+int sysctl_freeze_timeout = DEFAULT_FREEZE_TIMEOUT;
+
 /* Constants used for minimum and  maximum */
 
 #ifdef CONFIG_PERF_EVENTS
@@ -2255,6 +2257,14 @@ static struct ctl_table kern_table[] = {
 		.extra2		= SYSCTL_ONE,
 	},
 #endif
+	{
+		.procname	= "freeze_cgroup_timeout",
+		.data		= &sysctl_freeze_timeout,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+	},
 	{ }
 };
 
