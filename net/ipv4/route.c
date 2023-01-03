3477975ffac978549192fcbe8c32121fc9ca8989
Menglong Dong <imagedong@tencent.com>
2023-01-03 17:39:31 +0200

    net: ipv4: add skb drop reasons to ip_error()
    Eventually, I find out the handler function for inputting route lookup
    fail: ip_error().
    
    The drop reasons we used in ip_error() are almost corresponding to
    IPSTATS_MIB_*, and following new reasons are introduced:
    
    SKB_DROP_REASON_IP_INADDRERRORS
    SKB_DROP_REASON_IP_INNOROUTES
    
    Isn't the name SKB_DROP_REASON_IP_HOSTUNREACH and
    SKB_DROP_REASON_IP_NETUNREACH more accurate? To make them corresponding
    to IPSTATS_MIB_*, we keep their name still.
    
    Signed-off-by: Menglong Dong <imagedong@tencent.com>
    Reviewed-by: Jiang Biao <benbjiang@tencent.com>
    Reviewed-by: Hao Peng <flyingpeng@tencent.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Acked-by: Nikolay Borisov <nborisov@suse.com>
    Signed-off-by: Nikolay Borisov <nikolay.borisov@virtuozzo.com>
    
    ======
    Patchset description:
    ms/net: Annotate skb free sites with reason
    
    This series backports most of the patches that add a reason to skb free sites.
    
    https://jira.sw.ru/browse/PSBM-143302
    
    Feature: net: improve verbosity of dropped packets reporting

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -952,6 +952,7 @@ static int ip_error(struct sk_buff *skb)
 	struct inet_peer *peer;
 	unsigned long now;
 	struct net *net;
+	SKB_DR(reason);
 	bool send;
 	int code;
 
@@ -971,10 +972,12 @@ static int ip_error(struct sk_buff *skb)
 	if (!IN_DEV_FORWARD(in_dev)) {
 		switch (rt->dst.error) {
 		case EHOSTUNREACH:
+			SKB_DR_SET(reason, IP_INADDRERRORS);
 			__IP_INC_STATS(net, IPSTATS_MIB_INADDRERRORS);
 			break;
 
 		case ENETUNREACH:
+			SKB_DR_SET(reason, IP_INNOROUTES);
 			__IP_INC_STATS(net, IPSTATS_MIB_INNOROUTES);
 			break;
 		}
@@ -990,6 +993,7 @@ static int ip_error(struct sk_buff *skb)
 		break;
 	case ENETUNREACH:
 		code = ICMP_NET_UNREACH;
+		SKB_DR_SET(reason, IP_INNOROUTES);
 		__IP_INC_STATS(net, IPSTATS_MIB_INNOROUTES);
 		break;
 	case EACCES:
@@ -1016,7 +1020,7 @@ static int ip_error(struct sk_buff *skb)
 	if (send)
 		icmp_send(skb, ICMP_DEST_UNREACH, code, 0);
 
-out:	kfree_skb(skb);
+out:	kfree_skb_reason(skb, reason);
 	return 0;
 }
 
