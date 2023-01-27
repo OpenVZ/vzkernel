605ddb38018b9ee154f34064e0b88c8db7555574
Nikolay Borisov <nikolay.borisov@virtuozzo.com>
2023-01-27 13:34:39 +0200

    blk-cbt: Factor out common capability check
    Instead of duplicating CAP_SYS_ADMIN check for every block-cbt ioctl
    apart from BLKCBTGET simply factor out the check in the beginning of
    blk_cbt_ioctl().
    
    Fixes: acdc18e4d1aa ("cbt: introduce changed block tracking")
    
    Signed-off-by: Nikolay Borisov <nikolay.borisov@virtuozzo.com>
    Reviewed-by: Alexander Atanasov <alexander.atanasov@virtuozzo.com>

--- block/blk-cbt.c
+++ block/blk-cbt.c
@@ -822,29 +822,24 @@ ioc_set_failed:
 
 int blk_cbt_ioctl(struct block_device *bdev, unsigned cmd, char __user *arg)
 {
-	struct blk_user_cbt_info __user *ucbt_ioc = (struct blk_user_cbt_info __user *) arg;
+	struct blk_user_cbt_info __user *ucbt_ioc;
+
+	ucbt_ioc = (struct blk_user_cbt_info __user *) arg;
+
+	if (cmd == BLKCBTGET)
+		return cbt_ioc_get(bdev, ucbt_ioc);
+
+	if (!capable(CAP_SYS_ADMIN))
+		return -EACCES;
 
 	switch(cmd) {
 	case BLKCBTSTART:
-		if (!capable(CAP_SYS_ADMIN))
-			return -EACCES;
 		return cbt_ioc_init(bdev, ucbt_ioc);
 	case BLKCBTSTOP:
-		if (!capable(CAP_SYS_ADMIN))
-			return -EACCES;
-
 		return cbt_ioc_stop(bdev);
-	case BLKCBTGET:
-		return cbt_ioc_get(bdev, ucbt_ioc);
 	case BLKCBTSET:
-		if (!capable(CAP_SYS_ADMIN))
-			return -EACCES;
-
 		return cbt_ioc_set(bdev, ucbt_ioc, 1);
 	case BLKCBTCLR:
-		if (!capable(CAP_SYS_ADMIN))
-			return -EACCES;
-
 		return cbt_ioc_set(bdev, ucbt_ioc, 0);
 	default:
 		BUG();
