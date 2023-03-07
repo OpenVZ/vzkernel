OpenVZ/vzkernel

commit 7f5e9ebe4c63a5ff93f11083d95d124f0fdd20a6

cbt: bitmap corruption caused by ipi
IPI generated by cbt_flush_cache() can interrupt blk_cbt_add() in "bad" places
and lead to bitmap corruption.

CPU A                       CPU B
blk_cbt_add()
...                         cbt_flush_cache()
   old = *ex;                submit IPI
   ex->start = start;

interrupt
  __cbt_flush_cpu_cache
   if (ex->len)   <<< found non-changed len
     __blk_cbt_set(cbt, ex->start, ex->len, 0, 1); <<< set wrong bitmask
     ex->start += ex->len; <<< incorrectly adjusts ex->start
     ex->len = 0;
return from interrupt back to blk_cbt_add()
   ex->len = len;   <<< set len and get wrong bitmask
(because ex->start was changed in __cbt_flush_cpu_cache)

Similar problem happens if IPI will be processed in middle of following block in blk_cbt_add()

        if (ex->start + ex->len == start) {
                ex->len += len;
                goto out_rcu;
        }

Patch disables interrupts in specified places.

https://jira.sw.ru/browse/PSBM-89323
Signed-off-by: Vasily Averin <vvs@virtuozzo.com>

block/blk-cbt.c
@@ -229,13 +229,16 @@ static void blk_cbt_add(struct request_queue *q, blkcnt_t start, blkcnt_t len)
 		goto out_rcu;
 	}
 	ex = this_cpu_ptr(cbt->cache);
+	local_irq_disable();
 	if (ex->start + ex->len == start) {
 		ex->len += len;
+		local_irq_enable();
 		goto out_rcu;
 	}
 	old = *ex;
 	ex->start = start;
 	ex->len = len;
+	local_irq_enable();
 
 	if (likely(old.len))
 		__blk_cbt_set(cbt, old.start, old.len, 1, 1, NULL, NULL);

commit f7ce23edaf87460d302a3c3bcfe20774c9aaae10

blk-cbt: Don't disable interrupts when working with percpu cache
When working with percpu variables the only guarantee we need is that
the process is not preempted. So let's do this by using the more
idiomatic get_cpu_ptr/put_cpu_ptr rather than the more heavyweight
local_irq_disable. No functional changes.

Fixes: 7f5e9ebe4c63 ("cbt: bitmap corruption caused by ipi")

Signed-off-by: Nikolay Borisov <nikolay.borisov@virtuozzo.com>

block/blk-cbt.c
@@ -233,17 +233,16 @@ static void blk_cbt_add(struct request_queue *q, blkcnt_t start, blkcnt_t len)
 		__blk_cbt_set(cbt, start, len, 1, 1, NULL, NULL);
 		goto out_rcu;
 	}
-	local_irq_disable();
-	ex = this_cpu_ptr(cbt->cache);
+	ex = get_cpu_ptr(cbt->cache);
 	if (ex->start + ex->len == start) {
 		ex->len += len;
-		local_irq_enable();
+		put_cpu_ptr(cbt->cache);
 		goto out_rcu;
 	}
 	old = *ex;
 	ex->start = start;
 	ex->len = len;
-	local_irq_enable();
+	put_cpu_ptr(cbt->cache);
 
 	if (likely(old.len))
 		__blk_cbt_set(cbt, old.start, old.len, 1, 1, NULL, NULL);