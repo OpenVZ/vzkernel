c9049aa373e337c759eb51869f98e4a0e2cd1757
Alexander Atanasov <alexander.atanasov@virtuozzo.com>
2023-05-26 10:57:19 +0300

    ploop: rework logging when punch hole or zero range is unsupported
    We added a fallback to zero range if punch hole is not supported
    but when zero range is not supported either the log gets flooded.
    
    Rework logging and restrict unsupported log messages to just one.
    
    Fixes: 07a8b5ae8adc ("ploop: Fallback to zero range for backing stores without
    punch hole support")
    https://jira.vzint.dev/browse/PSBM-147401
    Signed-off-by: Alexander Atanasov <alexander.atanasov@virtuozzo.com>
    
    Feature: dm-ploop: ploop target driver

--- drivers/md/dm-ploop-map.c
+++ drivers/md/dm-ploop-map.c
@@ -1050,17 +1050,19 @@ static int ploop_allocate_cluster(struct ploop *ploop, u32 *dst_clu)
 	if (pos < top->file_preallocated_area_start) {
 		/* Clu at @pos may contain dirty data */
 		off = min_t(loff_t, old_size, end);
-		if (!ploop->falloc_new_clu) {
+		if (!ploop->falloc_new_clu)
 			ret = ploop_punch_hole(file, pos, off - pos);
-			if (unlikely(ret == -EOPNOTSUPP)) {
+		else
+			ret = ploop_zero_range(file, pos, off - pos);
+		if (ret) {
+			if (ret != -EOPNOTSUPP)
+				PL_ERR("punch/zero area: %d", ret);
+			else if (ploop->falloc_new_clu)
+				PL_ERR_ONCE("zero range unsupported");
+			else if (!ploop->falloc_new_clu) {
 				ploop->falloc_new_clu = 1;
 				PL_WARN("punch hole unsupported - disabling");
 			}
-		} else
-			ret = ploop_zero_range(file, pos, off - pos);
-		if (ret) {
-			if (printk_ratelimit())
-				PL_ERR("punch/zero area: %d", ret);
 			return ret;
 		}
 	}
