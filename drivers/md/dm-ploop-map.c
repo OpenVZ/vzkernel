OpenVZ/vzkernel

commit 07a8b5ae8adc26198997fcda7f89befd8644b7bb

ploop: Fallback to zero range for backing stores without punch hole support
Using punch hole is on by default but it might not be supported by
the backing store. Gracefully fall back to use zero range in that case.

Ratelimit errors from punch hole/zero range to reduce log clutter.

https://jira.vzint.dev/browse/PSBM-146678
Signed-off-by: Alexander Atanasov <alexander.atanasov@virtuozzo.com>
Feature: dm-ploop: ploop target driver

drivers/md/dm-ploop-map.c
@@ -1050,12 +1050,17 @@ static int ploop_allocate_cluster(struct ploop *ploop, u32 *dst_clu)
 	if (pos < top->file_preallocated_area_start) {
 		/* Clu at @pos may contain dirty data */
 		off = min_t(loff_t, old_size, end);
-		if (!ploop->falloc_new_clu)
+		if (!ploop->falloc_new_clu) {
 			ret = ploop_punch_hole(file, pos, off - pos);
-		else
+			if (unlikely(ret == -EOPNOTSUPP)) {
+				ploop->falloc_new_clu = 1;
+				PL_WARN("punch hole unsupported - disabling");
+			}
+		} else
 			ret = ploop_zero_range(file, pos, off - pos);
 		if (ret) {
-			PL_ERR("punch/zero area: %d", ret);
+			if (printk_ratelimit())
+				PL_ERR("punch/zero area: %d", ret);
 			return ret;
 		}
 	}

drivers/md/dm-ploop.h
@@ -550,6 +550,7 @@ static inline const char *ploop_device_name(struct ploop *ploop)
 
 #define PL_FMT(fmt) "ploop: %s: " fmt "\n"
 #define PL_ERR(fmt, ...) pr_err(PL_FMT(fmt), ploop_device_name(ploop), ##__VA_ARGS__)
+#define PL_WARN(fmt, ...) pr_warn(PL_FMT(fmt), ploop_device_name(ploop), ##__VA_ARGS__)
 #define PL_INFO(fmt, ...) pr_info(PL_FMT(fmt), ploop_device_name(ploop), ##__VA_ARGS__)
 
 extern void ploop_md_page_insert(struct ploop *ploop, struct md_page *md);

commit fccd9ab13f47d69b7fe7615162ce530c69bce248

ploop: rework logging when punch hole or zero range is unsupported
We added a fallback to zero range if punch hole is not supported
but when zero range is not supported either the log gets flooded.

Rework logging and restrict unsupported log messages to just one.

Fixes: 07a8b5ae8adc ("ploop: Fallback to zero range for backing stores without
punch hole support")
https://jira.vzint.dev/browse/PSBM-147401
Signed-off-by: Alexander Atanasov <alexander.atanasov@virtuozzo.com>

Feature: dm-ploop: ploop target driver

drivers/md/dm-ploop-map.c
@@ -1050,17 +1050,19 @@ static int ploop_allocate_cluster(struct ploop *ploop, u32 *dst_clu)
 	if (pos < top->file_preallocated_area_start) {
 		/* Clu at @pos may contain dirty data */
 		off = min_t(loff_t, old_size, end);
-		if (!ploop->falloc_new_clu) {
+		if (!ploop->falloc_new_clu)
 			ret = ploop_punch_hole(file, pos, off - pos);
-			if (unlikely(ret == -EOPNOTSUPP)) {
+		else
+			ret = ploop_zero_range(file, pos, off - pos);
+		if (ret) {
+			if (ret != -EOPNOTSUPP)
+				PL_ERR("punch/zero area: %d", ret);
+			else if (ploop->falloc_new_clu)
+				PL_ERR_ONCE("zero range unsupported");
+			else if (!ploop->falloc_new_clu) {
 				ploop->falloc_new_clu = 1;
 				PL_WARN("punch hole unsupported - disabling");
 			}
-		} else
-			ret = ploop_zero_range(file, pos, off - pos);
-		if (ret) {
-			if (printk_ratelimit())
-				PL_ERR("punch/zero area: %d", ret);
 			return ret;
 		}
 	}

drivers/md/dm-ploop.h
@@ -550,6 +550,7 @@ static inline const char *ploop_device_name(struct ploop *ploop)
 
 #define PL_FMT(fmt) "ploop: %s: " fmt "\n"
 #define PL_ERR(fmt, ...) pr_err(PL_FMT(fmt), ploop_device_name(ploop), ##__VA_ARGS__)
+#define PL_ERR_ONCE(fmt, ...) pr_err_once(PL_FMT(fmt), ploop_device_name(ploop), ##__VA_ARGS__)
 #define PL_WARN(fmt, ...) pr_warn(PL_FMT(fmt), ploop_device_name(ploop), ##__VA_ARGS__)
 #define PL_INFO(fmt, ...) pr_info(PL_FMT(fmt), ploop_device_name(ploop), ##__VA_ARGS__)
 