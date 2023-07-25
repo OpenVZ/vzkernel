5317e0948b8a465edaae67ff98a1b974f492019f
Konstantin Khorenko <khorenko@virtuozzo.com>
2023-07-25 19:23:53 +0300

    qed: Fix scheduling in a tasklet while getting stats
    Here we've got to a situation when tasklet called usleep(), thus welcome
    to the "scheduling while atomic" BUG().
    
    BUG: scheduling while atomic: swapper/24/0/0x00000100
    
     [<ffffffffb41c6199>] schedule+0x29/0x70
     [<ffffffffb41c5512>] schedule_hrtimeout_range_clock+0xb2/0x150
     [<ffffffffb41c55c3>] schedule_hrtimeout_range+0x13/0x20
     [<ffffffffb41c3bcf>] usleep_range+0x4f/0x70
     [<ffffffffc08d3e58>] qed_ptt_acquire+0x38/0x100 [qed]
     [<ffffffffc08eac48>] _qed_get_vport_stats+0x458/0x580 [qed]
     [<ffffffffc08ead8c>] qed_get_vport_stats+0x1c/0xd0 [qed]
     [<ffffffffc08dffd3>] qed_get_protocol_stats+0x93/0x100 [qed]
                          qed_mcp_send_protocol_stats
              case MFW_DRV_MSG_GET_LAN_STATS:
              case MFW_DRV_MSG_GET_FCOE_STATS:
              case MFW_DRV_MSG_GET_ISCSI_STATS:
              case MFW_DRV_MSG_GET_RDMA_STATS:
     [<ffffffffc08e36d8>] qed_mcp_handle_events+0x2d8/0x890 [qed]
                          qed_int_assertion
                          qed_int_attentions
     [<ffffffffc08d9490>] qed_int_sp_dpc+0xa50/0xdc0 [qed]
     [<ffffffffb3aa7623>] tasklet_action+0x83/0x140
     [<ffffffffb41d9125>] __do_softirq+0x125/0x2bb
     [<ffffffffb41d560c>] call_softirq+0x1c/0x30
     [<ffffffffb3a30645>] do_softirq+0x65/0xa0
     [<ffffffffb3aa78d5>] irq_exit+0x105/0x110
     [<ffffffffb41d8996>] do_IRQ+0x56/0xf0
    
    There was a similar issue in mainstream, but for qede driver and caused
    by reading stats via sysfs.
    
    3rd version of the patch fixing that issue, which i have taken as a
    base: https://www.spinics.net/lists/netdev/msg901089.html
    
    ms final commit: 42510dffd0e2 ("qed/qede: Fix scheduling while atomic")
    
    Unfortunately last version of the patch which is accepted by mainstream
    is far away from version 3, it moves stats updates to a delayed work,
    which is the correct way,
    but in qed driver i do not see which exactly stats should i collect in a
    delayed work for each device (and devices supported by qed driver are
    quite different).
    
    Taking into account that we do not have a node access to reproduce and
    verify the changes, reworking the code to use delayed work seems
    impossible.
    
    So let's live with udelay() crutch which at least is technically clear.
    
    https://jira.vzint.dev/browse/PSBM-146761
    
    Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

--- drivers/net/ethernet/qlogic/qed/qed_l2.c
+++ drivers/net/ethernet/qlogic/qed/qed_l2.c
@@ -1887,7 +1887,8 @@ static void __qed_get_vport_stats(struct qed_hwfn *p_hwfn,
 }
 
 static void _qed_get_vport_stats(struct qed_dev *cdev,
-				 struct qed_eth_stats *stats)
+				 struct qed_eth_stats *stats,
+				 bool is_atomic)
 {
 	u8 fw_vport = 0;
 	int i;
@@ -1896,10 +1897,11 @@ static void _qed_get_vport_stats(struct qed_dev *cdev,
 
 	for_each_hwfn(cdev, i) {
 		struct qed_hwfn *p_hwfn = &cdev->hwfns[i];
-		struct qed_ptt *p_ptt = IS_PF(cdev) ? qed_ptt_acquire(p_hwfn)
-						    :  NULL;
+		struct qed_ptt *p_ptt;
 		bool b_get_port_stats;
 
+		p_ptt = IS_PF(cdev) ? qed_ptt_acquire_context(p_hwfn, is_atomic)
+				    : NULL;
 		if (IS_PF(cdev)) {
 			/* The main vport index is relative first */
 			if (qed_fw_vport(p_hwfn, 0, &fw_vport)) {
@@ -1925,6 +1927,13 @@ out:
 
 void qed_get_vport_stats(struct qed_dev *cdev, struct qed_eth_stats *stats)
 {
+	qed_get_vport_stats_context(cdev, stats, false);
+}
+
+void qed_get_vport_stats_context(struct qed_dev *cdev,
+				 struct qed_eth_stats *stats,
+				 bool is_atomic)
+{
 	u32 i;
 
 	if (!cdev) {
@@ -1932,7 +1941,7 @@ void qed_get_vport_stats(struct qed_dev *cdev, struct qed_eth_stats *stats)
 		return;
 	}
 
-	_qed_get_vport_stats(cdev, stats);
+	_qed_get_vport_stats(cdev, stats, is_atomic);
 
 	if (!cdev->reset_stats)
 		return;
@@ -1984,7 +1993,7 @@ void qed_reset_vport_stats(struct qed_dev *cdev)
 	if (!cdev->reset_stats) {
 		DP_INFO(cdev, "Reset stats not allocated\n");
 	} else {
-		_qed_get_vport_stats(cdev, cdev->reset_stats);
+		_qed_get_vport_stats(cdev, cdev->reset_stats, false);
 		cdev->reset_stats->common.link_change_count = 0;
 	}
 }
