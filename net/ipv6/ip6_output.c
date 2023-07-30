2f793edec0139c3c03671650337bc36d876fd9b0
Alexey Kuznetsov <kuznet@acronis.com>
2023-07-30 19:56:33 +0300

    net/ipv6: resurrect commit a0a9f33bdf8b ("net/ipv6: update flowi6_oif in ip6_dst_lookup_flow if not set")
    IMHO it was removed by subsequent VRF commit/backout by accident.
    IPsec to local addresses do not work since.
    
    Affects: #VSTOR-71337
    https://pmc.acronis.work/browse/VSTOR-71337
    
    Signed-off-by: Alexey Kuznetsov <kuznet@acronis.com>
    
    Feature: fix net

--- net/ipv6/ip6_output.c
+++ net/ipv6/ip6_output.c
@@ -1250,6 +1250,8 @@ struct dst_entry *ip6_dst_lookup_flow(struct net *net, const struct sock *sk, st
 		return ERR_PTR(err);
 	if (final_dst)
 		fl6->daddr = *final_dst;
+	if (!fl6->flowi6_oif)
+		fl6->flowi6_oif = dst->dev->ifindex;
 
 	return xfrm_lookup_route(net, dst, flowi6_to_flowi(fl6), sk, 0);
 }
