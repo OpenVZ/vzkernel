OpenVZ/vzkernel

commit 55eff3031c311cecdede9539bb90f832da258f99

mm: memcontrol: add swapin/out stats
This patch adds pswpin and pswpout fields to memory.stat file. These
fields show the number of pages swapped in and out respectively for the
given memory cgroup. We assume a page was swapped in/out even if there
were no actual io (frontswap).

https://jira.sw.ru/browse/PSBM-46104

Signed-off-by: Vladimir Davydov <vdavydov@virtuozzo.com>
Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>

Rebase to RHEL8.3 kernel-4.18.0-240.1.1.el8_3 notes:
khorenko@: fixed mem_cgroup_swapout() to charge "nr_entries", not always
1 page.

Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

(cherry-picked from vz8 commit 09af291ddfd8 ("mm: memcontrol: add
swapin/out stats"))

Signed-off-by: Nikita Yushchenko <nikita.yushchenko@virtuozzo.com>

+++
mm/vmscan: fix /proc/vmscan output

In vz7 memcg counters had its own enum and names, but in vz8 it changed
to vmstat ones. We added "pswpin" and "pswpout" in vmstat_text[] during
rebase which caused half of the /proc/vmstat values to shift two rows up.
Remove duplicate "pswpin" and "pswpout" from vmstat_text[]

https://jira.sw.ru/browse/PSBM-130829

Signed-off-by: Andrey Zhadchenko <andrey.zhadchenko@virtuozzo.com>
Feature: mm: statistic enhancements

mm/page_io.c
@@ -337,14 +337,17 @@ int swap_readpage(struct page *page, bool synchronous)
 		struct address_space *mapping = swap_file->f_mapping;
 
 		ret = mapping->a_ops->readpage(swap_file, page);
-		if (!ret)
+		if (!ret) {
+			count_memcg_page_event(page, PSWPIN);
 			count_vm_event(PSWPIN);
+		}
 		goto out;
 	}
 
 	if (sis->flags & SWP_SYNCHRONOUS_IO) {
 		ret = bdev_read_page(sis->bdev, swap_page_sector(page), page);
 		if (!ret) {
+			count_memcg_page_event(page, PSWPIN);
 			count_vm_event(PSWPIN);
 			goto out;
 		}

mm/memcontrol.c
@@ -4152,6 +4152,8 @@ static const unsigned int memcg1_events[] = {
 	PGPGOUT,
 	PGFAULT,
 	PGMAJFAULT,
+	PSWPIN,
+	PSWPOUT,
 };
 
 static int memcg_stat_show(struct seq_file *m, void *v)
@@ -7495,6 +7497,7 @@ void mem_cgroup_swapout(struct folio *folio, swp_entry_t entry)
 				   nr_entries);
 	VM_BUG_ON_FOLIO(oldid, folio);
 	mod_memcg_state(swap_memcg, MEMCG_SWAP, nr_entries);
+	__count_memcg_events(swap_memcg, PSWPOUT, nr_entries);
 
 	folio->memcg_data = 0;
 

commit aa1747c1238a6804407e03ab8c5ab6e062b0013a

mm/memcontrol: add swapin accounting to handle one more case
swap_readpage() misses one more count_memcg_page_event() before last
count_vm_event().

Note: both generic global swapin accounting and our per-memcg one incs
counters before calling submit_bio(), so both counters contain both
successful and failed swap reads. This is just a way how it is done.

https://jira.vzint.dev/browse/PSBM-147959

Fixes: 55eff3031c31 ("mm: memcontrol: add swapin/out stats")

Signed-off-by: Alexander Atanasov <alexander.atanasov@virtuozzo.com>
Signed-off-by: Konstantin Khorenko <khorenko@virtuozzo.com>

Feature: mm: statistic enhancements

mm/page_io.c
@@ -366,6 +366,7 @@ int swap_readpage(struct page *page, bool synchronous)
 		get_task_struct(current);
 		bio->bi_private = current;
 	}
+	count_memcg_page_event(page, PSWPIN);
 	count_vm_event(PSWPIN);
 	bio_get(bio);
 	submit_bio(bio);